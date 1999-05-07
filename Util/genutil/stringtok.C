d393ed22037338b2e439189275b4e960259aac58
straten <straten>
1999-05-07 11:11:20 +0000

    new token parsing of standard string

---
+++ Util/genutil/stringtok.C
@@ -0,0 +1,16 @@
+#include "string_utils.h"
+
+string stringtok (string* instr, const string& delimiters)
+{
+  ssize_t first = instr->find_first_not_of(delimiters);
+  ssize_t last = instr->find_first_of(delimiters, first);
+
+  if (first == instr->npos)
+    return string("");
+  if (last == instr->npos)
+    last = instr->length();
+
+  string retval = instr->substr(first, last);
+  instr->erase(0,last);
+  return retval;
+}
