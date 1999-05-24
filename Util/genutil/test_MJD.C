69f1ef2c53438c715c85645c6165a7bfd850cfef
straten <straten>
1999-05-24 10:32:21 +0000

    a quicky test of MJD class

---
+++ Util/genutil/test_MJD.C
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "MJD.h"
+
+int main ()
+{
+  struct tm* date;
+  time_t present;
+  char tempstr1 [256];
+  char tempstr2 [256];
+
+  present = time (NULL);
+
+  date = gmtime (&present);
+  if (date == NULL)  {
+    perror ("Error calling gmtime");
+    return -1;
+  }
+  strftime (tempstr1, 50, "%Y/%m/%d-%H:%M:%S", date);
+
+  MJD mjd (*date);
+
+  if (mjd.datestr (tempstr2, 50, "%Y/%m/%d-%H:%M:%S") == NULL) {
+    perror ("Error calling MJD::datestr");
+    return -1;
+  }
+  printf ("TM %s -> MJD %s -> TM %s\n", 
+	  tempstr1, mjd.printdays(5).data(), tempstr2);
+
+  if (strcmp (tempstr1, tempstr2))  {
+    fprintf (stderr, "\nERROR!! TM1: %s does not match TM2: %s\n",
+	     tempstr1, tempstr2);
+    return -1;
+  }
+
+  return 0;
+}
