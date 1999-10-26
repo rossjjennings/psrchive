a01e163e7ab0867aee28132e9d3b69c106dd7e5b
straten <straten>
1999-10-26 17:15:01 +0000

    1) moved from include/ 2) removed redundant functions that have their functionality met more    generally in soft_swin/utils/genutil/endian.c

---
+++ Base/Formats/Timer/convert_endian.h
@@ -0,0 +1,42 @@
+#include "machine_endian.h"
+
+#include "timer.h"
+#include "band.h"
+#include "mini.h"
+
+void timerChangeEndian (struct timer * hdr);
+void bandChangeEndian  (struct band *b);
+void miniChangeEndian  (struct mini * m);
+
+#ifdef MACHINE_LITTLE_ENDIAN
+#define timer_toBigEndian(p)       timerChangeEndian(p)
+#define timer_toLittleEndian(p)
+#define timer_fromBigEndian(p)     timerChangeEndian(p)
+#define timer_fromLittleEndian(p)
+
+#define mini_toBigEndian(p)        miniChangeEndian(p)
+#define mini_toLittleEndian(p)
+#define mini_fromBigEndian(p)      miniChangeEndian(p)
+#define mini_fromLittleEndian(p)
+
+#define band_toBigEndian(p)        bandChangeEndian(p)
+#define band_toLittleEndian(p)
+#define band_fromBigEndian(p)      bandChangeEndian(p)
+#define band_fromLittleEndian(p)
+#else
+#define timer_toBigEndian(p)
+#define timer_toLittleEndian(p)    timerChangeEndian(p)
+#define timer_fromBigEndian(p)
+#define timer_fromLittleEndian(p)  timerChangeEndian(p)
+
+#define mini_toBigEndian(p)
+#define mini_toLittleEndian(p)     miniChangeEndian(p)
+#define mini_fromBigEndian(p)
+#define mini_fromLittleEndian(p)   miniChangeEndian(p)
+
+#define band_toBigEndian(p)
+#define band_toLittleEndian(p)     bandChangeEndian(p)
+#define band_fromBigEndian(p)
+#define band_fromLittleEndian(p)   bandChangeEndian(p)
+#endif
+
