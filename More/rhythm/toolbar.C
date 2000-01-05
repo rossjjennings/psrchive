4bb9f5ccc37c250bad324773a128dd130cfa3e4c
straten <straten>
2000-01-05 16:26:58 +0000

    future frills

---
+++ More/rhythm/toolbar.C
@@ -0,0 +1,21 @@
+#include <qpixmap.h>
+#include <qtoolbar.h>
+#include <qtoolbutton.h>
+
+#include "rhythm.h"
+#include "pulsar.xpm"
+
+void Rhythm::toolbarConstruct ()
+{
+  QPixmap pulsar (pulsar_xpm);
+
+  QToolBar* toolbar = new QToolBar ( "Timing Tools", this );
+  toolbar -> addSeparator ();
+
+  QToolButton* rhythm = 
+    new QToolButton ( pulsar, "Rhythm", 
+		      "Display a brief message about this application", 
+		      this, SLOT( about() ), toolbar);
+  rhythm -> setMinimumSize ( rhythm->sizeHint() );
+}
+
