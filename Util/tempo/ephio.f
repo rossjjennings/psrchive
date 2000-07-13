744e7c6e0d36c13dbeb1eb7de9607734ea32535c
redwards <redwards>
2000-07-13 06:09:58 +0000

    fixed -00:xx bug

--- Util/tempo/ephio.f
+++ Util/tempo/ephio.f
@@ -774,7 +774,9 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 
       s = " "
       write(s,'(i3)') v1
-      call zeropad(s(2:3))      ! put leading 0's
+c This breaks -ve numbers!! :      call zeropad(s(2:3))      ! put leading 0's
+      if (s(2:2).eq." ") s(2:2) = "0" !Do this instead!
+      if (d.lt.0.0) s(1:1) = "-" ! fixes -00: case
       s(4:4) = ":"
       slen = slen + 1
 c     The following two fields use 'word' to write into since we need to
