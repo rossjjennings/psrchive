43795ae81af5ef7655dc8e0f8f0d385b215a1f23
straten <straten>
2000-09-05 12:26:37 +0000

    can now print date, given MJD

--- Util/genutil/getMJD.C
+++ Util/genutil/getMJD.C
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,46 +8,74 @@
 
 int main (int argc, char* argv[]) 
 {
-  MJD stuffit;
+  MJD  mjd;
   char printhere[40];
-  int verbose = 0;
+  bool verbose = false;
+  bool mjdgiven = false;
 
-  if (argc > 1) {
-    if (strcmp (argv[1], "-h") == 0) {
+  int c;
+  while ((c = getopt(argc, argv, "hm:v")) != -1) {
+    switch (c)  {
+    case 'h':
       fprintf (stderr, "getMJD [date]\n");
       fprintf (stderr, "WHERE: date is a UTC of the form yyyy-ddd-hh:mm:ss\n");
       fprintf (stderr, "       date is optional. [default: now]\n");
+      fprintf (stderr, "ALSO:  getMJD -m mjd\n");
+      fprintf (stderr, "       prints the date of given MJD\n");
       return 0;
+
+    case 'm':
+      if (verbose)
+	fprintf (stderr, "getMJD: parse MJD from '%s'\n", optarg);
+      mjd.Construct (optarg);
+      mjdgiven = true;
+      break;
+
+    case 'v':
+      verbose = true;
+      break;
     }
-    if (verbose) {
-      fprintf (stderr, "Converting '%s' to UTC and using as date", argv[1]);
-    }
+  }
+
+  if (mjdgiven) {
+    struct tm date;
+    double fracsec;
+    mjd.gregorian (&date, &fracsec);
+    fprintf (stderr, "Date for MJD given: %s\n", asctime(&date));
+    return 0;
+  }
+
+  if (optind < argc) {
+    if (verbose)
+      fprintf (stderr, "Converting '%s' to UTC and using as date",
+	       argv[optind]);
+
     utc_t utcdate;
-    if (str2utc (&utcdate, argv[1]) < 0) {
-      fprintf (stderr, "Error converting '%s' to UTC.\n", argv[1]);
+    if (str2utc (&utcdate, argv[optind]) < 0) {
+      fprintf (stderr, "Error converting '%s' to UTC.\n", argv[optind]);
       return -1;
     }
     fprintf (stderr, "Using UTC parsed: %s\n",
 	       utc2str (printhere, utcdate, "yyyy-ddd-hh:mm:ss"));
-    stuffit = MJD (utcdate);
+    mjd = MJD (utcdate);
   }
   else {
     time_t temp = time(NULL);
     struct tm date = *gmtime(&temp);
     fprintf (stderr, "Using today's date: %s\n", asctime(&date));
-    stuffit = MJD (date);
+    mjd = MJD (date);
   }
 
-  printf ("%11.5f\n", stuffit.in_days());
+  printf ("%11.5f\n", mjd.in_days());
 
   if (verbose) {
     utc_t  stuffback;
-    stuffit.UTC (&stuffback);
+    mjd.UTC (&stuffback);
     printf ("utc from MJD: %s\n", 
 	    utc2str (printhere, stuffback, "yyyy-ddd-hh:mm:ss"));
   }
 
-  // cerr << stuffit.printdays(15);
+  // cerr << mjd.printdays(15);
 
   return 0;
 }
