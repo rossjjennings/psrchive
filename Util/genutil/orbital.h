58e66b098bb0064d9738a340dfa7447e2a7833ae
straten <straten>
2001-02-12 01:56:45 +0000

    companion_mass added

--- Util/genutil/orbital.h
+++ Util/genutil/orbital.h
@@ -1,6 +1,6 @@
 /* $Source: /cvsroot/psrchive/psrchive/Util/genutil/orbital.h,v $
-   $Revision: 1.1 $
-   $Date: 1999/12/10 04:40:17 $
+   $Revision: 1.2 $
+   $Date: 2001/02/12 01:56:45 $
    $Author: straten $ */
 
 #ifndef __ORBITAL_H
@@ -12,6 +12,9 @@ extern "C" {
 
   /* Solves Kepler's equation using the Newton-Raphson method */
   double eccentric_anomoly (double mean_anomoly, double eccentricity);
+
+  /* Solves mass function for the companion mass */
+  double companion_mass (double mass_function, double sini, double m1);
 
 #ifdef __cplusplus
 	   }
