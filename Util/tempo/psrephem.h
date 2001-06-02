9b43f55733a648a847d342dbe35003f65dff3ef8
straten <straten>
2001-06-02 09:44:00 +0000

    new methods added

--- Util/tempo/psrephem.h
+++ Util/tempo/psrephem.h
@@ -1,9 +1,9 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/tempo/psrephem.h,v $
-   $Revision: 1.14 $
-   $Date: 2001/05/08 13:40:41 $
-   $Author: pulsar $ */
+   $Revision: 1.15 $
+   $Date: 2001/06/02 09:44:00 $
+   $Author: straten $ */
 
 #ifndef __PSREPHEM_H
 #define __PSREPHEM_H
@@ -14,6 +14,7 @@
 #include <string>
 
 #include "MJD.h"
+#include "Angle.h"
 
 class psrephem 
 {
@@ -43,6 +44,8 @@ class psrephem
 
   psrephem& operator = (const psrephem &);
 
+  void zero ();
+
   int create (const char* psr_name, int use_cwd);
   int create (const string& psr_name, int use_cwd)
     { return create (psr_name.c_str(), use_cwd); }
@@ -64,6 +67,20 @@ class psrephem
 
   // multiply all errors by efac
   void   efac (float efac);
+
+  // return the value
+  string get_string  (int ephind);
+  double get_double  (int ephind);
+  MJD    get_MJD     (int ephind);
+  Angle  get_Angle   (int ephind);
+  int    get_integer (int ephind);
+
+  // set value
+  void set_string  (int ephind, const string&);
+  void set_double  (int ephind, double );
+  void set_MJD     (int ephind, const MJD&);
+  void set_Angle   (int ephind, const Angle&);
+  void set_Integer (int ephind, int);
 
   double p() const;
   double p_err() const;
@@ -95,12 +112,26 @@ class psrephem
 
   // returns the orbital period in seconds
   int P (double& p, double& p_err) const;
-  // returns the orbital period derivative in seconds
-  int P_dot (double& p, double& p_err) const;
-  
+  // returns the orbital period derivative
+  int P_dot (double& pdot, double& pdot_err) const;
+    // returns the second orbital period derivative in seconds^-1
+  int P_ddot (double& pddot, double& pddot_err) const;
+
   // returns the transverse quadratic Doppler shift due to the apparent
   // acceleration along the line of sight that arises from proper motion
-  int Shklovskii (double& beta, double& beta_err) const;
+  int quadratic_Doppler (double& beta, double& beta_err) const;
+
+  int cubic_Doppler (double& gamma, double& gamma_err,
+		     double pmrv, double pmrv_err) const;
+
+  int Doppler_P_dotdot (double& P_dotdot, double& P_dotdot_err,
+			double pmrv, double pmrv_err) const;
+
+  int Doppler_F2 (double& f2, double& f2_err,
+		  double pmrv, double pmrv_err) const;
+
+  int intrinsic_P_dotdot (double& P_dotdot, double& P_dotdot_err,
+			  float braking_index=3) const;
 
   // returns the intrinsic x_dot due to gravitational wave emission
   int GR_x_dot (double& x_dot) const;
@@ -109,7 +140,13 @@ class psrephem
   int GR_Pb_dot (double& Pb_dot) const;
 
   // returns the intrinsic Pb_dot due to gravitational wave emission
-  int GR_omega_dot (double& Pb_dot) const;
+  int GR_omega_dot (double& w_dot) const;
+
+  // given omega_dot from par file, returns the total mass
+  int GR_omega_dot_mtot (double& mtot, double& mtot_err) const;
+
+  // uses the above and sin(i) and f(M) to derive m2
+  int GR_omega_dot_m2 (double& m2, double& m2_err) const;
 
   // returns the general relativistic prediction of gamma
   int GR_gamma (double& gamma) const;
@@ -139,8 +176,8 @@ class psrephem
  protected:
   void init ();
   void size_dataspace();
-  void zero ();
   void destroy ();
+  void zero_work ();
 
   int old_load (const char* filename);
   int old_unload (const char* filename) const;
