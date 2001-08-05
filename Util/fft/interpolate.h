5a96e0098e740e57d941927a25bbcaffbf59914b
straten <straten>
2001-08-05 05:37:39 +0000

    small optimization

--- Util/fft/interpolate.h
+++ Util/fft/interpolate.h
@@ -1,8 +1,8 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/fft/interpolate.h,v $
-   $Revision: 1.2 $
-   $Date: 2001/08/04 09:41:36 $
+   $Revision: 1.3 $
+   $Date: 2001/08/05 05:37:39 $
    $Author: straten $*/
 
 #ifndef __fft_interpolate_h
@@ -39,34 +39,30 @@ namespace fft {
 
       fft::frc1d (in.size(), dom2.begin(), dom1.begin());
 
-      // copy the transform from DC up
-      for (ipt=0; ipt < in.size(); ipt++)
-	dom1[ipt] = dom2[ipt];
-
-      unsigned dom1i = dom1.size()-2;
+      unsigned end = dom2.size()-2;
       // copy the conjugate of the transform, backwards, into the
       // end of the array, excluding DC
       for (ipt=2; ipt < in.size(); /* incr in loop */) {
-	dom1[dom1i]   = dom2[ipt];   // Re
+	dom2[end]   = dom2[ipt];   // Re
 	ipt ++;
-	dom1[dom1i+1] = -dom2[ipt];  // Im
+	dom2[end+1] = -dom2[ipt];  // Im
 	ipt ++;
-	dom1i -= 2;
+	end -= 2;
       }
 
       // zero pad the rest
-      unsigned stop = dom1.size()-in.size()+2;
+      unsigned stop = dom2.size()-in.size()+2;
       for (ipt=in.size(); ipt<stop; ipt++)
-	dom1[ipt] = 0;
+	dom2[ipt] = 0;
 
-      fft::bcc1d (out.size(), dom2.begin(), dom1.begin());
+      fft::bcc1d (out.size(), dom1.begin(), dom2.begin());
 
       // this factor may need to be carefully chosen, depending
       // on how the FFT routines operate
       float factor = in.size();
 
       for (ipt=0; ipt < out.size(); ipt++)
-	out[ipt][idim] = dom2[ipt*2]/factor;
+	out[ipt][idim] = dom1[ipt*2]/factor;
 
     } // end for each dimension
 
