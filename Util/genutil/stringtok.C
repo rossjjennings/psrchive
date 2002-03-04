fa6499a501e9341a5dc1df98c69ca0408b67ea32
ahotan <ahotan>
2002-03-04 05:17:27 +0000

    stringtok will now erase the incoming string if no non-delimiter chars found

--- Util/genutil/stringtok.C
+++ Util/genutil/stringtok.C
@@ -6,13 +6,14 @@ string stringtok (string* instr, const string& delimiters, bool skip_leading)
   ssize_t first = instr->find_first_not_of (delimiters);
   ssize_t start = (skip_leading) ? first : 0;
   ssize_t last = instr->find_first_of(delimiters, start);
-
-  string retval;
-  if (first == string::npos)
-    return retval;
-
   if (last == string::npos)
     last = instr->length();
+
+  string retval;
+  if (first == string::npos) {
+    instr->erase();
+    return retval;
+  }
 
   if (!skip_leading && last < first)
     return retval;
