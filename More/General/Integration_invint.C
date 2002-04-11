99f60d3294e3726b986eb79ebde377df461df331
straten <straten>
2002-04-11 14:23:11 +0000

    first draft: routine to calculate the invariant interval

---
+++ More/General/Integration_invint.C
@@ -0,0 +1,67 @@
+#include "Integration.h"
+#include "Profile.h"
+#include "Stokes.h"
+
+/////////////////////////////////////////////////////////////////////////////
+//
+/*! 
+  When this flag is true, Pulsar::Integration::invint will calculate
+  the square of the polarimetric invariant interval (equal to the deteminant
+  of the coherency matrix). 
+*/
+bool Pulsar::Integration::invint_square = false;
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// Pulsar::Integration::invint
+//
+/*! 
+  Forms the Stokes polarimetric invariant interval,
+  \f$\det{P}=I^2-Q^2-U^2-V^2\f$, for every bin of each band so that,
+  upon completion, npol == 1 and state == Poln::Invariant.
+
+  If invint_square is true, this function calculates
+  \f$\det\rho=I^2-Q^2-U^2-V^2\f$, otherwise \f$\sqrt{\det\rho}\f$ is
+  calcuated.
+
+  \pre The profile baselines must have been removed (unchecked).
+
+  \exception string thrown if Stokes 4-vector cannot be formed
+*/
+void Pulsar::Integration::invint ()
+{
+  // space to calculate the result
+  vector<float> invariant (get_nbin());
+  // Stokes 4-vector
+  Stokes stokes;
+
+  for (int iband=0; iband<get_nband(); ++iband) {
+
+    for (int ibin=0; ibin<get_nbin(); ++ibin) {
+      // get the Stokes 4-vector
+      get_Stokes (stokes, iband, ibin);
+      // calculate \det\rho
+      invariant[ibin] = stokes.invariant_Squared();
+    }
+
+    // prepare to reset Stokes I to the Invariant Interval
+    Profile* Sinv = (*this)[Poln::Si][iband];
+
+    // set the values
+    Sinv->set_amps (invariant.begin());
+    // remove the baseline
+    Sinv->offset (-Sinv->mean (Sinv->find_min_phase()));
+
+    if (invint_square)
+      Sinv->set_state (Poln::Sinv_sq);
+    else {
+      Sinv->set_state (Poln::Sinv);
+      Sinv->square_root();
+    }
+    
+  } // for each channel
+
+  resize (1, get_nband());
+  state = Poln::Invariant;
+}
