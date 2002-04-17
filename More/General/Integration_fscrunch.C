ebe74532add3a1b7c37beb9ed006cacae9bc4284
straten <straten>
2002-04-17 14:26:11 +0000

    standardized

--- More/General/Integration_fscrunch.C
+++ More/General/Integration_fscrunch.C
@@ -7,77 +7,71 @@
 // Pulsar::Integration::fscrunch
 //
 /*!
-  \param n_add number of neighbouring chans to integrate.  If n_add == 0,
+  \param nscrunch number of neighbouring chans to integrate.  If nscrunch == 0,
   then integrate all chans
   \param weighted_cfreq if true, calculate a new centre frequency for result
   \
  */
-void Pulsar::Integration::fscrunch (int n_add, bool weighted_cfreq)
+void Pulsar::Integration::fscrunch (int nscrunch, bool weighted_cfreq)
 {
   if (verbose)
     cerr << "Pulsar::Integration::fscrunch"
-      " nadd=" << n_add << " dm=" << dm << endl;
+      " nadd=" << nscrunch << " dm=" << dm << endl;
 
-  if (nchan <= 1 || n_add <= 1)  {
-    if (verbose)
-      cerr << "Pulsar::Integration::fscrunch invalid nchan or n_add" << endl;
+  if (nscrunch == 1 || nchan == 1)
+    // nothing to scrunch
     return;
-  }
 
-  if (n_add == 0)
-    n_add = nchan;
+  if (nscrunch == 0)
+    nscrunch = nchan;
 
-  if (nchan % n_add)
+  if (nchan % nscrunch)
     throw Error (InvalidRange, "Pulsar::Integration::fscrunch",
-		 "nchan=%d %% n_add=%d = %d", nchan, n_add, nchan%n_add);
+		 "nchan=%d %% nscrunch=%d = %d",
+		 nchan, nscrunch, nchan%nscrunch);
 
-  int newchan = nchan/n_add;
+  int newchan = nchan/nscrunch;
 
-  try {
-
-    for (int j=0; j < newchan; j++) {
+  for (int j=0; j < newchan; j++) try {
       
-      int stchan = j * n_add;
+    int stchan = j * nscrunch;
+    
+    if (verbose)
+      cerr << "Pulsar::Integration::fscrunch - chan " << j << endl;
+    
+    if (dm != 0) {
+      
+      double fcentre = centrefreq;
+      
+      if (weighted_cfreq)
+	fcentre = weighted_frequency (stchan, stchan+nscrunch);
+      
+      if (newchan == 1)
+	centrefreq = fcentre;
+      
+      for (int ipol=0; ipol < npol; ipol++)
+	for (int k=0; k<nscrunch; k++)
+	  profiles[ipol][stchan+k]->dedisperse (dm,fcentre,pfold);
+      
+    }
+    
+    for (int ipol=0; ipol < npol; ipol++)  {
       
       if (verbose)
-	cerr << "Pulsar::Integration::fscrunch - chan " << j << endl;
+	cerr << "Pulsar::Integration::fscrunch pol " << ipol << endl;
       
-      if (dm != 0) {
-	
-	double weight = 1.0;
-	double fcentre = centrefreq;
-
-	if (weighted_cfreq)
-	  fcentre = weighted_frequency (&weight, stchan, stchan+n_add);
-	
-	if (newchan == 1)
-	  centrefreq = fcentre;
-	
-	for (int ipol=0; ipol < npol; ipol++)
-	  for (int k=0; k<n_add; k++)
-	    profiles[ipol][stchan+k]->dedisperse (dm,fcentre,pfold);
-	
-      }
+      *(profiles[ipol][j]) = *(profiles[ipol][stchan]);
+      for (int k=1; k<nscrunch; k++)
+	*(profiles[ipol][j]) += *(profiles[ipol][stchan+k]);
       
-      for (int ipol=0; ipol < npol; ipol++)  {
-	
-	if (verbose)
-	  cerr << "Pulsar::Integration::fscrunch pol " << ipol << endl;
-	
-	*(profiles[ipol][j]) = *(profiles[ipol][stchan]);
-	for (int k=1; k<n_add; k++)
-	  *(profiles[ipol][j]) += *(profiles[ipol][stchan+k]);
-	
-      }
     }
-
-    resize (npol, newchan, nbin);
-  
   }
   catch (Error& error) {
     throw error += "Integration::fscrunch";
   }
 
+  resize (npol, newchan, nbin);
+  
   if (verbose) 
     cerr << "Pulsar::Integration::fscrunch - finished successfully" << endl;
 } 
