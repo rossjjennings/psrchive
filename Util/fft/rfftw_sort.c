c5f8f0300b6783a96137060884fab7af1902d2b6
straten <straten>
2002-04-18 01:28:13 +0000

    typographical change

--- Util/fft/rfftw_sort.c
+++ Util/fft/rfftw_sort.c
@@ -3,13 +3,19 @@ int rfftw_sort(int npts, float * src, float * dest)
   int i;
   dest[0] = src[0];
   dest[1] = 0.0;
+
   for(i=1; i<npts/2; ++i){
     dest[2*i] = src[i];
     dest[2*i+1] = src[npts-i];
   }
+
   dest[2*(npts/2)] = src[npts/2];
-  if(npts%2==0) dest[2*(npts/2)+1] = 0.0;
-  else dest[2*(npts/2)+1] = src[npts/2+1];
-  return(0);
+
+  if (npts%2==0)
+    dest[npts+1] = 0.0;
+  else
+    dest[npts] = src[npts/2+1];
+
+  return 0;
 }
 
