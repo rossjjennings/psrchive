754783ebd3a4aef5e803289034939ed8f26cf70c
straten <straten>
2003-01-13 10:37:13 +0000

    Archive::unload increases security of existing data

---
+++ Base/Classes/Archive_unload.C
@@ -0,0 +1,51 @@
+#include "Pulsar/Archive.h"
+#include "Error.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+
+/*! To protect data, especially when writing the output archive to a
+  file of the same name as the input archive, this method unloads data
+  to a temporary file before renaming the archive to the requested
+  output file name.  The temporary file will be written using a unique
+  filename in the same path as the requested output file. */
+void Pulsar::Archive::unload (const char* filename)
+{
+  if (filename)
+    unload_filename = filename;
+
+  if (verbose)
+    cerr << "Pulsar::Archive::unload (" << unload_filename << ")" << endl;
+
+  // create the temporary filename
+  string temp_filename = unload_filename + "XXXXXX";
+
+  int fd = mkstemp (const_cast<char*> (temp_filename.c_str()));
+  if (fd < 0)
+    throw Error (FailedSys, "Pulsar::Archive::unload", "failed mkstemp");
+  close (fd);
+
+  if (verbose)
+    cerr << "Pulsar::Archive::unload calling unload_file "
+      "(" << temp_filename << ")" << endl;
+
+  try {
+    unload_file (temp_filename.c_str());
+  }
+  catch (Error& error) {
+    remove (temp_filename.c_str());
+    throw error += "Pulsar::Archive::unload";
+  }
+  catch (...) {
+    remove (temp_filename.c_str());
+    throw Error (FailedCall, "Pulsar::Archive::unload", "failed unload_file "
+		 "(" + temp_filename + ")");
+  }
+
+  // rename the temporary file with the requested filename
+  int ret = rename (temp_filename.c_str(), unload_filename.c_str());
+  if (ret < 0)
+    throw Error (FailedSys, "Pulsar::Archive::unload", "failed rename");
+
+  __load_filename = unload_filename;
+}
