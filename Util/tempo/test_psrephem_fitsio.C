512582d74c6a9792a2fd40ad3e8310f1027c1450
pulsar <pulsar>
2003-01-20 15:30:30 +0000

    verbose option added

--- Util/tempo/test_psrephem_fitsio.C
+++ Util/tempo/test_psrephem_fitsio.C
@@ -7,16 +7,23 @@
 
 int main (int argc, char** argv)
 {
-  if ( argc < 2 || argv[1]==string("-h") ) {
+  int arg=1;
+  string first = argv[arg];
+
+  if ( argc < 2 || first == "-h" ) {
     cerr << "USAGE: test_fitsio filename [row]" << endl;
     return 0;
+  }
+  if ( first == "-v" ) {
+    psrephem::verbose = true;
+    arg++;
   }
 
   fitsfile* fptr = 0;
   int status = 0;
   char err[FLEN_STATUS];   // error message if status != 0
 
-  fits_open_file (&fptr, argv[1], READONLY, &status);
+  fits_open_file (&fptr, argv[arg], READONLY, &status);
   if (status != 0) {
     fits_get_errstatus (status, err);
     cerr << "test_fitsio: error fits_open_file " << err << endl;
