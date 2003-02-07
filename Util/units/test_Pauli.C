73453d589080bd5acc5a49884413c76a66d3b537
straten <straten>
2003-02-07 13:51:09 +0000

    double precision!

--- Util/units/test_Pauli.C
+++ Util/units/test_Pauli.C
@@ -1,67 +1,57 @@
 #include "MatrixTest.h"
 #include "Pauli.h"
 
+template <typename T>
+void test_polar (const Jones<T>& j)
+{
+  // pull the jones matrix apart into its Hermitian and Unitary components
+  complex<T> d;
+  Quaternion<T, Hermitian> hq;
+  Quaternion<T, Unitary> uq;
+
+  polar (d, hq, uq, j);
+
+  // reconstruct the Jones matrix
+  Jones<T> oj = d * convert(hq) * convert(uq);
+
+  // test that you get the same matrix back
+  double diff = norm(oj-j)/norm(oj);
+  if ( diff > 1e-10 ) {
+    cerr << "test_matrix polar\n"
+      " out=" << oj << " !=\n"
+      "  in=" << j << endl << " diff=" << diff << endl;
+    throw string ("test_matrix polar decomposition error");
+  }
+}
+
 // specialize the test_matrix template function defined in MatrixTest.h
-void test_matrix (const Jones<float>& j1, const Jones<float>& j2,
-		  const Quaternion<float>& q, const complex<float>& c,
+template <typename T>
+void test_matrix (const Jones<T>& j1, const Jones<T>& j2,
+		  const Quaternion<T>& q, const complex<T>& c,
 		  bool verbose)
 {
   if (verbose)
     cerr << "test_matrix Jones/Quaternion Isomorphism" << endl;
 
-  // pull the jones matrices apart into their Hermitian and Unitary components
-
-  complex<float> d1;
-  Quaternion<float, Hermitian> hq1;
-  Quaternion<float, Unitary> uq1;
-
-  polar (d1, hq1, uq1, j1);
-
-  complex<float> d2;
-  Quaternion<float, Hermitian> hq2;
-  Quaternion<float, Unitary> uq2;
-
-  polar (d2, hq2, uq2, j2);
-
-  // reconstruct the Jones matrices
-
-  Jones<float> oj1 = d1 * convert(hq1) * convert(uq1);
-
-  Jones<float> oj2 = d2 * convert(hq2) * convert(uq2);
-
-  // test that you get the same matrix back
-  double diff = norm(oj1-j1)/norm(oj1);
-  if ( diff > 1e-15 ) {
-    cerr << "test_matrix polar recomposition=" << oj1 
-	 << " != Jones=" << j1 << endl
-	 << " by " << diff;
-    throw string ("test_matrix polar decomposition error");
-  }
-
-  diff = norm(oj2-j2)/norm(oj2);
-  if ( diff > 1e-15 ) {
-    cerr << "test_matrix polar recomposition=" << oj2 
-	 << " != Jones=" << j2 << endl
-	 << " by " << diff;
-    throw string ("test_matrix polar decomposition error");
-  }
-
-  Jones<float> uj1 = convert (uq1);
-  Jones<float> uj2 = convert (uq2);
+  test_polar (j1);
+  test_polar (j2);
 
 #if 0
-  Quaternion<float> pq1 = uq1 * uq2;
-  Quaternion<float> pq2 = convert (uj1 * uj2);
+
+  Jones<T> uj1 = convert (uq1);
+  Jones<T> uj2 = convert (uq2);
+
+  Quaternion<T> pq1 = uq1 * uq2;
+  Quaternion<T> pq2 = convert (uj1 * uj2);
 
   if ( norm(pq2-pq1)/norm(pq2) > 1e-15 ) {
     cerr << "test_matrix pq2=" << pq2 
 	 << " != pq1=" << pq1 << endl;
     throw string ("test_matrix unequal Quaternion/Jones products");
   }
-#endif
 
-  Jones<float> pj1 = uj2 * uj1;
-  Jones<float> pj2 = convert (uq2 * uq1);
+  Jones<T> pj1 = uj2 * uj1;
+  Jones<T> pj2 = convert (uq2 * uq1);
 
   diff = norm(pj2-pj1)/norm(pj2);
   if ( diff > 1e-15 ) {
@@ -70,6 +60,7 @@ void test_matrix (const Jones<float>& j1, const Jones<float>& j2,
 	 << " by " << diff << endl;
     throw string ("test_matrix unequal Jones/Quaternion products");
   }
+#endif
 
 }
 
@@ -95,7 +86,7 @@ int main ()
   cerr << "hj=" << hj0 << " uj=" << uj0 << endl;
 
   Jones<float> j0 = hj0 * uj0;
-  cerr << "IN j0=" << j0 << endl;
+  cerr << " IN j0=" << j0 << endl;
 
   complex<float> d0;
   polar (d0, hq0, uq0, j0);
@@ -108,7 +99,7 @@ int main ()
   cerr << "OUT j0=" << j0 << endl;
 
 
-  MatrixTest <Jones<float>, Quaternion<float>, complex<float> > test;
+  MatrixTest <Jones<double>, Quaternion<double>, complex<double> > test;
 
   try {
     cerr << "Testing " << loops << " Pauli conversion variations" << endl;
