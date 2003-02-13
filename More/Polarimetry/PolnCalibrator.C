bc79c149426fd157cb21849af6d18dbe9ca7f669
straten <straten>
2003-02-13 16:39:11 +0000

    finished, but untested:   - differential gain and phase correction   - flux calibrated with respect to CAL flux (no longer need flux cal to work)   - written in terms of Jones matrix and congruence transformation

--- More/Polarimetry/PolnCalibrator.C
+++ More/Polarimetry/PolnCalibrator.C
@@ -1,10 +1,10 @@
 #include "Pulsar/PolnCalibrator.h"
 #include "Pulsar/Archive.h"
 #include "Pulsar/Integration.h"
+#include "Pauli.h"
 #include "Error.h"
 
-bool Pulsar::PolnCalibrator::verbose = false;
-
+bool Pulsar::PolnCalibrator::store_parameters = false;
 
 Pulsar::PolnCalibrator::PolnCalibrator (const Archive* arch)
 {
@@ -30,43 +30,58 @@ Pulsar::PolnCalibrator::PolnCalibrator (const Archive* arch)
   if (fullStokes && state != Signal::Coherence) {
     Archive* clone = arch->clone();
     clone->convert_state (Signal::Coherence);
-    archive = clone;
+    calibrator = clone;
   }
   else
-    archive = arch;
+    calibrator = arch;
 }
 
-//! Calibrate the flux in the given archive
+/*! Upon completion, the flux of the archive will be normalized with respect
+  to the flux of the calibrator, such that a FluxCalibrator simply needs
+  to scale the archive by the calibrator flux. */
 void Pulsar::PolnCalibrator::calibrate (Archive* arch)
 {
-  if (!archive)
+  if (!calibrator)
     throw Error (InvalidState, "Pulsar::PolnCalibrator::calibrate",
 		 "no PolnCal Archive");
 
   string reason;
-  if (!archive->mixable (arch, reason))
+  if (!calibrator->mixable (arch, reason))
     throw Error (InvalidParam, "Pulsar::FluxCalibrator", "Pulsar::Archive='"
-		 + archive->get_filename() + "'\ndoes not mix with '"
+		 + calibrator->get_filename() + "'\ndoes not mix with '"
 		 + arch->get_filename() + "\n" + reason);
 
-  if (gain.size() != arch->get_nchan()) {
+  if (jones.size() != arch->get_nchan())
+    create (arch->get_nchan());
 
-    // get the calibrator hi and lo levels from the PolnCal archive
-    vector<vector<Estimate<double> > > cal_hi;
-    vector<vector<Estimate<double> > > cal_lo;
-    archive->get_Integration(0)->cal_levels (cal_hi, cal_lo);
-
-    vector<vector<Estimate<double> > > hi;
-    vector<vector<Estimate<double> > > lo;
-
-    // make hi and lo the right size of cal_hi and cal_lo
-
-    calculate (hi, lo);
-
-  }
+  for (unsigned isub=0; isub < arch->get_nsubint(); isub++)
+    calibrate (arch->get_Integration(isub));
 
 }
 
+void Pulsar::PolnCalibrator::create (unsigned nchan)
+{
+  // get the calibrator hi and lo levels from the PolnCal archive
+  vector<vector<Estimate<double> > > cal_hi;
+  vector<vector<Estimate<double> > > cal_lo;
+
+  calibrator->get_Integration(0)->cal_levels (cal_hi, cal_lo);
+
+  if (calibrator->get_nchan() == nchan) {
+    calculate (cal_hi, cal_lo);
+    return;
+  }
+
+  throw Error (InvalidState, "Pulsar::FluxCalibrator::create",
+	       "Cannot currently calibrate archives with different nchan");
+
+  // make hi and lo the right size of cal_hi and cal_lo
+  vector<vector<Estimate<double> > > hi;
+  vector<vector<Estimate<double> > > lo;
+    
+  calculate (hi, lo);
+
+}
 
 void
 Pulsar::PolnCalibrator::calculate (vector<vector<Estimate<double> > >& cal_hi,
@@ -78,12 +93,22 @@ Pulsar::PolnCalibrator::calculate (vector<vector<Estimate<double> > >& cal_hi,
 
   bool full_polarization = npol == 4;
 
-  gain.resize (nchan);
+  if (store_parameters) {
+    boost_q.resize (nchan);
+    gain.resize (nchan);
 
-  if (full_polarization)
-    phase.resize (nchan);
-  else
-    phase.clear ();
+    if (full_polarization)
+      rotation_q.resize (nchan);
+  }
+
+  else {
+    boost_q.clear ();
+    gain.clear ();
+    rotation_q.clear ();
+  }
+
+  baseline.resize (nchan);
+  jones.resize (nchan);
 
   for (unsigned ichan=0; ichan<nchan; ++ichan) {
 
@@ -97,8 +122,8 @@ Pulsar::PolnCalibrator::calculate (vector<vector<Estimate<double> > >& cal_hi,
 	cerr << "Pulsar::PolnCalibrator channel " << ichan
 	     << ": invalid levels flagged" << endl;
 
-      gain[ichan] = 0;
-      phase[ichan] = 0;
+      boost_q[ichan] = 0;
+      rotation_q[ichan] = 0;
 
       continue;
     }
@@ -115,32 +140,63 @@ Pulsar::PolnCalibrator::calculate (vector<vector<Estimate<double> > >& cal_hi,
     //   cal_b = g_b^2 C
     // where C is the cal flux in each probe (assumed equal in this model)
 
-    gain[ichan] = 0.25 * log (cal_a / cal_b);
+    Estimate<double> beta = 0.25 * log (cal_a / cal_b);
+    if (store_parameters)
+      boost_q[ichan] = beta;
+
+    // gain, G = g_a g_b C
+    Estimate<double> Gain = sqrt (cal_a * cal_b);
+    if (store_parameters)
+      gain[ichan] = Gain;
+
+    // baseline intensity, normalized by cal flux, C
+    baseline[ichan] = cal_lo[0][ichan]/cal_a + cal_lo[1][ichan]/cal_b;
 
     if (verbose)
-      cerr << "Pulsar::PolnCalibrator channel " << ichan << ":"
-	   << " instrumental gain " << gain[ichan] << endl;
+      cerr << "Pulsar::PolnCalibrator channel " << ichan <<
+	"\n  boost=" << beta << 
+	"\n  gain=" << Gain << 
+	"\n  baseline=" << baseline[ichan] << endl;
 
-    if (!full_polarization)
-      continue;
+    Quaternion<double, Hermitian> b_q (cosh(beta.val), sinh(beta.val), 0, 0);
 
-    Estimate<double> cal_ab = cal_hi[2][ichan] - cal_lo[2][ichan];
-    Estimate<double> cal_ba = cal_hi[3][ichan] - cal_lo[3][ichan];
+    Quaternion<double, Unitary> r_q = Quaternion<double, Unitary>::identity();
 
-    if (cal_ba == 0 && cal_ab == 0)  {
-      if (verbose) cerr << "Pulsar::PolnCalibrator channel " << ichan
-			<< ": invalid instrumental phase flagged" << endl;
+    if (full_polarization) {
+      Estimate<double> cal_ab = cal_hi[2][ichan] - cal_lo[2][ichan];
+      Estimate<double> cal_ba = cal_hi[3][ichan] - cal_lo[3][ichan];
 
-      phase[ichan] = 0;
+      if (cal_ba == 0 && cal_ab == 0)  {
+	if (verbose) cerr << "Pulsar::PolnCalibrator channel " << ichan
+			  << ": invalid instrumental rotation" << endl;
+	if (store_parameters)
+	  rotation_q[ichan] = 0;
+      }
 
-      continue;
+      Estimate<double> phi = 0.5 * atan2 (cal_ba, cal_ab);
+
+      if (store_parameters)
+	rotation_q[ichan] = phi;
+
+      if (verbose)
+	cerr << "Pulsar::PolnCalibrator channel " << ichan << ":"
+	     << " differential phase=" << phi << endl;
+
+      r_q = Quaternion<double> (cos(phi.val), sin(phi.val), 0, 0);
     }
-    
-    phase[ichan] = 0.5 * atan2 (cal_ba, cal_ab);
 
-    if (verbose)
-      cerr << "Pulsar::PolnCalibrator channel " << ichan << ":"
-	   << " instrumental phase " << phase[ichan] << endl;
+    Jones<double> response = sqrt(Gain.val) * b_q * r_q;
+
+    jones[ichan] = inv (response);
+
   }
 }
 
+void Pulsar::PolnCalibrator::calibrate (Integration* subint)
+{
+  if (subint->get_npol() != 4)
+    throw Error (InvalidState, "Pulsar::PolnCalibrator::calibrate",
+		 "npol != 4 not implemented");
+
+  subint->transform (jones);
+}
--- More/Polarimetry/Pulsar/PolnCalibrator.h
+++ More/Polarimetry/Pulsar/PolnCalibrator.h
@@ -1,8 +1,8 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/More/Polarimetry/Pulsar/PolnCalibrator.h,v $
-   $Revision: 1.2 $
-   $Date: 2003/02/13 14:08:33 $
+   $Revision: 1.3 $
+   $Date: 2003/02/13 16:39:11 $
    $Author: straten $ */
 
 #ifndef __PolnCalibrator_H
@@ -12,18 +12,25 @@
 
 #include "Calibrator.h"
 #include "Estimate.h"
+#include "Jones.h"
 #include "Types.h"
 
 namespace Pulsar {
 
-  //! Forward declaration
-  class Archive;
+  class Integration;
 
+  //! Implements differenctial gain and phase corrections
+  /*! This class implements the calibration of instrumental differential
+    gain and phase, as previously implemented by tcedar and cal_arch.
+    One major improvement: this calibration step no longer requires a flux
+    calibrator in order to work.  The calibrated archive will have its flux
+    normalized by the calibrator flux, such that the FluxCalibrator class
+    need only multiply the archive by the calibrator flux in mJy */
   class PolnCalibrator : public Calibrator {
     
   public:
-    //! Verbosity flag
-    static bool verbose;
+    //! If true, then the human-readable parameters are kept
+    static bool store_parameters;
 
     //! Construct from an vector of PolnCal Pulsar::Archives
     PolnCalibrator (const vector<Archive*>& archives);
@@ -40,17 +47,32 @@ namespace Pulsar {
     vector<string> filenames;
 
     //! Differential gain, \f$ \beta \f$, as a function of frequency
-    vector< Estimate<double> > gain;
+    vector< Estimate<double> > boost_q;
 
     //! Differential phase, \f$ \Phi_I \f$, as a function of frequency
-    vector< Estimate<double> > phase;
+    vector< Estimate<double> > rotation_q;
+
+    //! Instrumental gain, \f$ G \f$, in instrumental flux units
+    vector< Estimate<double> > gain;
+
+    //! Intensity of off-pulse (system + sky), in CAL flux units
+    vector< Estimate<double> > baseline;
 
     //! Reference to the Pulsar::Archive from which this instance was created
-    Reference::To<const Archive> archive;
+    Reference::To<const Archive> calibrator;
 
+    //! Jones matrix frequency response of the instrument
+    vector< Jones<float> > jones;
+
+    //! Create the Jones matrix frequency response at the requested resolution
+    void create (unsigned nchan);
+
+    //! Calculate the Jones matrix frequency response from the CAL hi and lo
     void calculate (vector<vector<Estimate<double> > >& cal_hi,
 		    vector<vector<Estimate<double> > >& cal_lo);
 
+    //! Calibrate a single sub-integration
+    void calibrate (Integration* subint);
   };
 
 }
