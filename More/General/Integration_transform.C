a61a1fd09c96f4d7125f4489f6a2aa8fee9be2da
straten <straten>
2003-02-20 10:34:42 +0000

    get the new state back from the PolnProfile

--- More/General/Integration_transform.C
+++ More/General/Integration_transform.C
@@ -9,6 +9,8 @@ void Pulsar::Integration::transform (const Jones<float>& response)
     throw Error (InvalidState, "Pulsar::Integration::transform",
 		 "incomplete polarization information");
 
+  Signal::State state = get_state();
+
   for (unsigned ichan=0; ichan < get_nchan(); ichan++) {
 
     PolnProfile poln (get_basis(), get_state(), 
@@ -16,8 +18,12 @@ void Pulsar::Integration::transform (const Jones<float>& response)
 		      profiles[2][ichan], profiles[3][ichan]);
 
     poln.transform (response);
-    
+
+    if (ichan == 0)
+      state = poln.get_state();
   }
+
+  set_state (state);
 }
  
 void Pulsar::Integration::transform (const vector< Jones<float> >& response)
@@ -30,6 +36,8 @@ void Pulsar::Integration::transform (const vector< Jones<float> >& response)
     throw Error (InvalidState, "Pulsar::Integration::transform",
 		 "response size=%d != nchan=%d", response.size(), get_nchan());
 
+  Signal::State state = get_state();
+
   for (unsigned ichan=0; ichan < get_nchan(); ichan++) {
 
     PolnProfile poln (get_basis(), get_state(), 
@@ -38,6 +46,11 @@ void Pulsar::Integration::transform (const vector< Jones<float> >& response)
 
     poln.transform (response[ichan]);
     
+    if (ichan == 0)
+      state = poln.get_state();
   }
+
+  set_state (state);
 }
  
+
