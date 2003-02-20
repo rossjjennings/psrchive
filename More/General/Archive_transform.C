54c26cdc1e4b574098cb17969538ec1c2bcffef0
straten <straten>
2003-02-20 11:13:10 +0000

    adding to exception stack

--- More/General/Archive_transform.C
+++ More/General/Archive_transform.C
@@ -1,32 +1,56 @@
 #include "Archive.h"
 #include "Integration.h"
+#include "Error.h"
 
 //! Perform the transformation on each polarimetric profile
 void Pulsar::Archive::transform (const Jones<float>& jones)
 {
-  for (unsigned isub=0; isub < get_nsubint(); isub++)
-    get_Integration (isub) -> transform (jones);
+  try {
 
-  if (get_nsubint() > 0)
-    set_state (get_Integration(0)->get_state());
+    for (unsigned isub=0; isub < get_nsubint(); isub++)
+      get_Integration (isub) -> transform (jones);
+
+    if (get_nsubint() > 0)
+      set_state (get_Integration(0)->get_state());
+
+  }
+  catch (Error& error) {
+    throw error += "Pulsar::Archive::transform (Jones<float>)";
+  }
 }
 
 //! Perform frequency response on each polarimetric profile
 void Pulsar::Archive::transform (const vector<Jones<float> >& jones)
 {
-  for (unsigned isub=0; isub < get_nsubint(); isub++)
-    get_Integration (isub) -> transform (jones);
+  try {
 
-  if (get_nsubint() > 0)
-    set_state (get_Integration(0)->get_state());
+    for (unsigned isub=0; isub < get_nsubint(); isub++)
+      get_Integration (isub) -> transform (jones);
+    
+    if (get_nsubint() > 0)
+      set_state (get_Integration(0)->get_state());
+    
+  }
+  catch (Error& error) {
+    throw error += "Pulsar::Archive::transform (vector<Jones<float>>)";
+  }
 }
 
 //! Perform the time and frequency response on each polarimetric profile
 void Pulsar::Archive::transform (const vector<vector<Jones<float> > >& jones)
 {
-  for (unsigned isub=0; isub < get_nsubint(); isub++)
-    get_Integration (isub) -> transform (jones[isub]);
+  try {
 
-  if (get_nsubint() > 0)
-    set_state (get_Integration(0)->get_state());
+    for (unsigned isub=0; isub < get_nsubint(); isub++)
+      get_Integration (isub) -> transform (jones[isub]);
+    
+    if (get_nsubint() > 0)
+      set_state (get_Integration(0)->get_state());
+
+  }
+  catch (Error& error) {
+    throw error += "Pulsar::Archive::transform (vector<vector<Jones<float>>>)";
+  }
 }
+
+
