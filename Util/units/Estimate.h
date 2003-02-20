12be476122f24aee0709502c5a08bae6dd42911d
straten <straten>
2003-02-20 15:48:21 +0000

    PGPLOT option added

--- Util/units/Estimate.h
+++ Util/units/Estimate.h
@@ -1,8 +1,8 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/units/Estimate.h,v $
-   $Revision: 1.7 $
-   $Date: 2003/02/15 10:15:00 $
+   $Revision: 1.8 $
+   $Date: 2003/02/20 15:48:21 $
    $Author: straten $ */
 
 #ifndef __Estimate_h
@@ -157,53 +157,9 @@ class MeanEstimate
 
 };
 
+#ifdef PGPLOT
+#include "pgplot_Estimate.h"
+#endif
 
 #endif
 
-#if 0
-
-  T relerr() const;
-  void set_relerr(T);
-
-  bool consistent(const Estimate &d, T efac=1.0) const;
-
-  // print in 2.31(2) form if places<0
-  string print(int places=-1, char *sep=" ");
-
-  Estimate & operator+=(const Estimate&);
-  Estimate & operator-=(const Estimate&);
-  Estimate & operator*=(const Estimate&);
-  Estimate & operator/=(const Estimate&);
-
-  Estimate & operator+=(T);
-  Estimate & operator-=(T);
-  Estimate & operator*=(T);
-  Estimate & operator/=(T);
-
-  friend Estimate  operator+(const Estimate&, const Estimate&);
-  friend Estimate  operator-(const Estimate&, const Estimate&);
-  friend Estimate  operator*(const Estimate&, const Estimate&);
-  friend Estimate  operator/(const Estimate&, const Estimate&);
-
-  friend Estimate  operator+(const Estimate&, T);
-  friend Estimate  operator-(const Estimate&, T);
-  friend Estimate  operator*(const Estimate&, T);
-  friend Estimate  operator/(const Estimate&, T);
-
-  friend Estimate  operator+(T b, const Estimate& a);
-  friend Estimate  operator-(T b, const Estimate& a);
-  friend Estimate  operator*(T b, const Estimate& a);
-  friend Estimate  operator/(T b, const Estimate& a);
-
- protected:
-  void copy(const Estimate&);
-  void init() {val=err=0.0;}
-};
-
- inline Estimate operator+(T b, const Estimate& a) {return a+b;}
- inline Estimate operator-(T b, const Estimate& a) {return a-b;}
- inline Estimate operator*(T b, const Estimate& a) {return a*b;}
- inline Estimate operator/(T b, const Estimate& a) {return a/b;}
-
-}
-#endif
