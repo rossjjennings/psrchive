05b55d68f25edefc5914185070e45aec8f404214
straten <straten>
2003-05-16 05:45:34 +0000

    preparing to plot Stokes vs para

--- More/Polarimetry/ReceptionCalibratorPlotter.C
+++ More/Polarimetry/ReceptionCalibratorPlotter.C
@@ -54,3 +54,39 @@ Pulsar::ReceptionCalibratorPlotter::get_rotation (unsigned idat, unsigned irot)
 {
   return calibrator->equation[idat]->get_receiver()->get_rotationEuler(irot);
 }
+
+void Pulsar::ReceptionCalibratorPlotter::plot_constraints (unsigned ichan,
+							   unsigned istate)
+{
+  if (istate >= calibrator->get_nstate())
+    throw Error (InvalidRange,
+		 "Pulsar::ReceptionCalibratorPlotter::plot_constraints",
+		 "istate=%d >= nstate=%d", istate, calibrator->get_nstate());
+
+  if (ichan >= calibrator->get_nchan())
+    throw Error (InvalidRange,
+		 "Pulsar::ReceptionCalibratorPlotter::plot_constraints",
+		 "ichan=%d >= nchan=%d", ichan, calibrator->get_nchan());
+
+}
+
+#if 0
+  //! Polarimetric measurement, estimated error, and state identification code
+  class MeasuredState : public Estimate<Stokes<double>, double> {
+  public:
+    MeasuredState () { state_index = 0; }
+    unsigned state_index;
+  };
+ 
+  //! A set of measurements made at the same abscissa interval
+  class Measurements : public vector<MeasuredState> {
+  public:
+
+    //! Construct measurement with single abscissa dimension
+    Measurements (double x0 = 0.0);
+
+    //! The coordinates of the measurement
+    vector<double> interval;
+  };
+#endif
+
