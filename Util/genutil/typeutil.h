128722e53ea80b841eb554aede7b002f47cf79f6
straten <straten>
2003-10-08 13:20:08 +0000

    gcc version 2.95.3 fix

--- Util/genutil/typeutil.h
+++ Util/genutil/typeutil.h
@@ -1,12 +1,14 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/genutil/typeutil.h,v $
-   $Revision: 1.1 $
-   $Date: 2003/10/06 12:18:34 $
+   $Revision: 1.2 $
+   $Date: 2003/10/08 13:20:08 $
    $Author: straten $ */
 
 #ifndef __typeutil_h
 #define __typeutil_h
+
+#include <typeinfo>
 
 //! Find the instance with the specified type_info
 template<typename T>
