f34c7a913ef5015a182fbf17f46e0faf26459502
straten <straten>
2003-10-22 10:32:48 +0000

    load parameter values and variances

--- Base/Formats/PSRFITS/load_PolnCalibratorExtension.C
+++ Base/Formats/PSRFITS/load_PolnCalibratorExtension.C
@@ -2,6 +2,8 @@
 #include "Pulsar/PolnCalibratorExtension.h"
 #include "FITSError.h"
 
+#include <assert.h>
+
 void Pulsar::FITSArchive::load_PolnCalibratorExtension (fitsfile* fptr)
 {
   int status = 0;
@@ -14,9 +16,8 @@ void Pulsar::FITSArchive::load_PolnCalibratorExtension (fitsfile* fptr)
   fits_movnam_hdu (fptr, BINARY_TBL, "FEEDPAR", 0, &status);
   
   if (status == BAD_HDU_NUM) {
-    if (verbose)
-      cerr << "Pulsar::FITSArchive::load_PolnCalibratorExtension no FEEDPAR HDU" << endl;
-
+    if (verbose) cerr << "Pulsar::FITSArchive::load_PolnCalibratorExtension"
+		   " no FEEDPAR HDU" << endl;
     return;
   }
 
@@ -24,42 +25,34 @@ void Pulsar::FITSArchive::load_PolnCalibratorExtension (fitsfile* fptr)
     throw FITSError (status, "FITSArchive::load_PolnCalibratorExtension", 
 		     "fits_movnam_hdu FEEDPAR");
 
-  Reference::To<PolnCalibratorExtension> mypce = new PolnCalibratorExtension();
+  Reference::To<PolnCalibratorExtension> pce = new PolnCalibratorExtension;
 
   char* comment = 0;
-  float nullfloat = 0.0;
 
-  // Get NCH_FDPR
-
-  int nch_fdpr = 0;
-  fits_read_key (fptr, TINT, "NCH_FDPR", &nch_fdpr, comment, &status);
-  
-  if (status == 0) {
-    mypce->set_nchan(nch_fdpr);
-  }
-
-  // Get NCPAR
-  
-  int ncpar = 0;
-  fits_read_key (fptr, TINT, "NCPAR", &ncpar, comment, &status);
-  
   // Get CAL_MTHD
   
   char* cal_mthd = new char[80];
-  fits_read_key (fptr, TSTRING, "CAL_MTHD", cal_mthd, comment, &status);
-  
-  if (strcasecmp(cal_mthd, "flux") == 0)
-    mypce->set_type(Calibrator::Flux);
-  else if (strcasecmp(cal_mthd, "singleaxis") == 0)
-    mypce->set_type(Calibrator::SingleAxis);
-  else if (strcasecmp(cal_mthd, "polar") == 0)
-    mypce->set_type(Calibrator::Polar);
-  else if (strcasecmp(cal_mthd, "hamaker") == 0)
-    mypce->set_type(Calibrator::Hamaker);
-  else if (strcasecmp(cal_mthd, "britton") == 0)
-    mypce->set_type(Calibrator::Britton);
-  
+  fits_read_key (fptr, TSTRING, "CAL_MTHD", cal_mthd, comment, &status);  
+  pce->set_type( Calibrator::str2Type (cal_mthd) );
   delete[] cal_mthd;
+
+  if (verbose)
+    cerr << "FITSArchive::load_PolnCalibratorExtension Calibrator type=" 
+	 << Calibrator::Type2str (pce->get_type()) << endl;
+
+  // Get NCPAR 
+  int ncpar = 0;
+  fits_read_key (fptr, TINT, "NCPAR", &ncpar, comment, &status);
+  
+  // Get NCH_FDPR
+  int nch_fdpr = 0;
+  fits_read_key (fptr, TINT, "NCH_FDPR", &nch_fdpr, comment, &status);
+  
+  if (status == 0)
+    pce->set_nchan(nch_fdpr);
+
+  long dimension = nch_fdpr * ncpar;  
+  auto_ptr<float> data ( new float[dimension] );
 
   // Read the centre frequencies
   
@@ -67,18 +60,17 @@ void Pulsar::FITSArchive::load_PolnCalibratorExtension (fitsfile* fptr)
   int initflag = 0;
   fits_get_colnum (fptr, CASEINSEN, "DAT_FREQ", &colnum, &status);
   
-  float* dat_freq = new float[nch_fdpr];
+  float nullfloat = NAN;
   
   fits_read_col (fptr, TFLOAT, colnum, 1, 1, nch_fdpr, &nullfloat,
-		 dat_freq, &initflag, &status);
-  if (verbose)
-    cerr << "FITSArchive::load_PolnCalibratorExtension channel frequencies read" << endl;
-  
-  for (int i = 0; i < nch_fdpr; i++) {
-    //mypce->get_Transformation(i)->set_freq(dat_freq[i]);
-  }
+		 data.get(), &initflag, &status);
 
-  delete[] dat_freq;
+  if (status)
+    throw FITSError (status, "FITSArchive::load PolnCalibratorExtension", 
+		     "fits_read_col DAT_FREQ");
+
+  if (verbose) cerr << "FITSArchive::load_PolnCalibratorExtension"
+		 " channel frequencies read" << endl;
   
   // Read the data scale weights
   
@@ -86,42 +78,76 @@ void Pulsar::FITSArchive::load_PolnCalibratorExtension (fitsfile* fptr)
   initflag = 0;
   fits_get_colnum (fptr, CASEINSEN, "DAT_WTS", &colnum, &status);
   
-  float* dat_wts = new float[nch_fdpr];
-  
   fits_read_col (fptr, TFLOAT, colnum, 1, 1, nch_fdpr, &nullfloat, 
-		 dat_wts, &initflag, &status);
+		 data.get(), &initflag, &status);
+
+  if (status)
+    throw FITSError (status, "FITSArchive::load PolnCalibratorExtension", 
+		     "fits_read_col DAT_WTS");
+
   if (verbose)
     cerr << "FITSArchive::load_PolnCalibratorExtension weights read" << endl;
   
-  for (int i = 0; i < nch_fdpr; i++) {
-    //mypce->get_Transformation(i)->set_weight(dat_wts[i]);
-  }
-
-  delete[] dat_wts;
+  for (int ichan=0; ichan < nch_fdpr; ichan++)
+    if ( !finite(data.get()[ichan]) || data.get()[ichan]==0 )
+      pce->set_valid (ichan, false);
 
   // Read the data itself
   
   colnum = 0;
   initflag = 0;
   fits_get_colnum (fptr, CASEINSEN, "DATA", &colnum, &status);
-  
-  long dimension = nch_fdpr * ncpar;
-  float* data = new float[dimension];
-  
+
   fits_read_col (fptr, TFLOAT, colnum, 1, 1, dimension, &nullfloat, 
-		 data, &initflag, &status);
+		 data.get(), &initflag, &status);
+
+  if (status)
+    throw FITSError (status, "FITSArchive::load PolnCalibratorExtension", 
+		     "fits_read_col DATA");
+
   if (verbose)
     cerr << "FITSArchive::load_PolnCalibratorExtension data read" << endl;
   
   int count = 0;
   for (int i = 0; i < nch_fdpr; i++) {
-    for (int j = 0; j < ncpar; j++) {
-      // mypce->get_Transformation(i)->set_param(j,data[count]);
-      count++;
-    }
+    if (pce->get_valid(i))
+      for (int j = 0; j < ncpar; j++) {
+	pce->get_Transformation(i)->set_param(j,data.get()[count]);
+	count++;
+      }
+    else
+      count += ncpar;
   }
 
-  add_extension(mypce);
+  assert (count == dimension);
+
+  fits_get_colnum (fptr, CASEINSEN, "DATAERR", &colnum, &status);
+
+  fits_read_col (fptr, TFLOAT, colnum, 1, 1, dimension, &nullfloat, 
+		 data.get(), &initflag, &status);
+
+  if (status)
+    throw FITSError (status, "FITSArchive::load PolnCalibratorExtension", 
+		     "fits_read_col DATAERR");
+
+  if (verbose)
+    cerr << "FITSArchive::load_PolnCalibratorExtension dataerr read" << endl;
+  
+  count = 0;
+  for (int i = 0; i < nch_fdpr; i++) {
+    if (pce->get_valid(i))
+      for (int j = 0; j < ncpar; j++) {
+	float err = data.get()[count];
+	pce->get_Transformation(i)->set_variance (j, err*err);
+	count++;
+      }
+    else
+      count += ncpar;
+  }
+
+  assert (count == dimension);
+
+  add_extension (pce);
   
   if (verbose)
     cerr << "FITSArchive::load_PolnCalibratorExtension exiting" << endl;
--- Base/Formats/PSRFITS/unload_PolnCalibratorExtension.C
+++ Base/Formats/PSRFITS/unload_PolnCalibratorExtension.C
@@ -2,20 +2,22 @@
 #include "Pulsar/PolnCalibratorExtension.h"
 #include "FITSError.h"
 
+#include <assert.h>
+
 void Pulsar::FITSArchive::unload (fitsfile* fptr, 
 				  const PolnCalibratorExtension* pce)
 {
   int status = 0;
 
   if (verbose)
-    cerr << "FITSArchive::unload_pce entered" << endl;
+    cerr << "FITSArchive::unload PolnCalibratorExtension entered" << endl;
   
   // Move to the FEEDPAR Binary Table
   
   fits_movnam_hdu (fptr, BINARY_TBL, "FEEDPAR", 0, &status);
   
   if (status != 0)
-    throw FITSError (status, "FITSArchive::pce::unload", 
+    throw FITSError (status, "FITSArchive::unload PolnCalibratorExtension", 
 		     "fits_movnam_hdu FEEDPAR");
   
   // Initialise a new row
@@ -24,88 +26,123 @@ void Pulsar::FITSArchive::unload (fitsfile* fptr,
   
   fits_insert_rows (fptr, 0, 1, &status);
   if (status != 0)
-    throw FITSError (status, "FITSArchive::pce::unload", 
+    throw FITSError (status, "FITSArchive::unload PolnCalibratorExtension", 
 		     "fits_insert_rows FEEDPAR");
 
-  char* comment = 0;
-  
-  // Write NCH_FDPR
-  
   int nch_fdpr = pce->get_nchan();
+  int ncpar = 0;
+  
+  for (int i = 0; i < nch_fdpr; i++)
+    if (pce->get_valid(i))
+      ncpar = pce->get_Transformation(i)->get_nparam(); 
+
+  if (verbose) cerr << "FITSArchive::unload PolnCalibratorExtension nchan=" 
+		    << nch_fdpr <<  " nparam=" << ncpar << endl;
+
+  char* comment = 0;
+
+  char* cal_mthd = const_cast<char*>( Calibrator::Type2str( pce->get_type() ) );
+
+  // Write CAL_MTHD
+  fits_update_key (fptr, TSTRING, "CAL_MTHD", cal_mthd, comment, &status);
+
+  // Write NCH_FDPR  
   fits_update_key (fptr, TINT, "NCH_FDPR", &nch_fdpr, comment, &status);
 
   // Write NCPAR
-
-  int ncpar = 0;
-  
-  for (int i = 0; i < nch_fdpr; i++) {
-    if (pce->get_valid(i))
-      ncpar = pce->get_Transformation(i)->get_nparam(); 
-  }
-  
   fits_update_key (fptr, TINT, "NCPAR", &ncpar, comment, &status);
   
+
+  long dimension = nch_fdpr * ncpar;  
+  auto_ptr<float> data ( new float[dimension] );
+
   // Write the channel frequencies
-  
-  float* dat_freq = new float[nch_fdpr];
-  
-  for (int i = 0; i < nch_fdpr; i++) {
-    dat_freq[i] = 0;
-    //dat_freq[i] = pce->get_Transformation(i)->get_freq();
-  }
+    
+  for (int i = 0; i < nch_fdpr; i++)
+    data.get()[i] = 0;
   
   int colnum = 0;
   fits_get_colnum (fptr, CASEINSEN, "DAT_FREQ", &colnum, &status);
   fits_modify_vector_len (fptr, colnum, nch_fdpr, &status);
   fits_write_col (fptr, TFLOAT, colnum, 1, 1, nch_fdpr,
-		  dat_freq, &status);
+		  data.get(), &status);
 
-  delete[] dat_freq;
+  if (status)
+    throw FITSError (status, "FITSArchive::unload PolnCalibratorExtension", 
+		     "fits_write_col DAT_FREQ");
 
-  if (verbose)
-    cerr << "FITSArchive::unload_pce frequencies written" << endl;
+  if (verbose) cerr << "FITSArchive::unload PolnCalibratorExtension"
+		 " frequencies written" << endl;
 
   // Write the weights
   
-  float* dat_wts = new float[nch_fdpr];
-
-  for (int i = 0; i < nch_fdpr; i++) {
-    dat_wts[i] = pce->get_valid(i);
-  }
+  for (int i = 0; i < nch_fdpr; i++)
+    data.get()[i] = pce->get_valid(i);
   
   colnum = 0;
   fits_get_colnum (fptr, CASEINSEN, "DAT_WTS", &colnum, &status);
   fits_modify_vector_len (fptr, colnum, nch_fdpr, &status);
   fits_write_col (fptr, TFLOAT, colnum, 1, 1, nch_fdpr, 
-		  dat_wts, &status);
+		  data.get(), &status);
 
-  delete[] dat_wts;
+  if (status)
+    throw FITSError (status, "FITSArchive::unload PolnCalibratorExtension", 
+		     "fits_write_col DAT_WTS");
+
+  if (verbose) cerr << "FITSArchive::unload PolnCalibratorExtension"
+		 " weights written" << endl;
   
-  if (verbose)
-    cerr << "FITSArchive::unload_pce weights written" << endl;
-  
-  // Write the data itself
-  
-  long dimension = nch_fdpr * ncpar;
-  
-  float* data = new float[dimension];
-  
+  // Write the model parameters
+    
   int count = 0;
-  for (int i = 0; i < nch_fdpr; i++) {
-    for (int j = 0; j < ncpar; j++) {
-      data[count] = pce->get_Transformation(i)->get_param(j);
-      count++;
-    }
-  }
+  for (count = 0; count < dimension; count++)
+    data.get()[count] = NAN;
   
+  count = 0;
+  for (int i = 0; i < nch_fdpr; i++)
+    if (pce->get_valid(i))
+      for (int j = 0; j < ncpar; j++) {
+	data.get()[count] = pce->get_Transformation(i)->get_param(j);
+	count++;
+      }
+    else
+      count += ncpar;
+
+  assert (count == dimension);
+
   colnum = 0;
   fits_get_colnum (fptr, CASEINSEN, "DATA", &colnum, &status);
   fits_modify_vector_len (fptr, colnum, dimension, &status);
   fits_write_col (fptr, TFLOAT, colnum, 1, 1, dimension,
-		  data, &status);
+		  data.get(), &status);
 
-  delete[] data;
-  
+  if (status)
+    throw FITSError (status, "FITSArchive::unload PolnCalibratorExtension", 
+		     "fits_write_col DATA");
+
+  // Write the variance of the model parameters
+    
+  count = 0;
+  for (int i = 0; i < nch_fdpr; i++)
+    if (pce->get_valid(i))
+      for (int j = 0; j < ncpar; j++) {
+	data.get()[count] = sqrt( pce->get_Transformation(i)->get_variance(j) );
+	count++;
+      }
+    else
+      count += ncpar;
+
+  assert (count == dimension);
+
+  colnum = 0;
+  fits_get_colnum (fptr, CASEINSEN, "DATAERR", &colnum, &status);
+  fits_modify_vector_len (fptr, colnum, dimension, &status);
+  fits_write_col (fptr, TFLOAT, colnum, 1, 1, dimension,
+		  data.get(), &status);
+
+  if (status)
+    throw FITSError (status, "FITSArchive::unload PolnCalibratorExtension", 
+		     "fits_write_col DATAERR");
   if (verbose)
-    cerr << "FITSArchive::unload_pce exiting" << endl; 
+    cerr << "FITSArchive::unload PolnCalibratorExtension exiting" << endl; 
 }
