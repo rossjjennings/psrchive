0b82717ae928a52f10919932ae3c7f44450a8d5b
straten <straten>
2003-12-29 12:22:55 +0000

    do not exit after reporting plugins

--- More/Tests/test_Archive.C
+++ More/Tests/test_Archive.C
@@ -24,7 +24,7 @@ int main (int argc, char** argv)
       
     case 'a':
       Pulsar::Archive::agent_report ();
-      return 0;
+      break;
 
     case 'P':
       Pulsar::Archive::set_plugin_path (optarg);
