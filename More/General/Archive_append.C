42719ca05d7d7ed9c14ccfc0a89f90d80e1dbbce
straten <straten>
2004-03-05 08:05:43 +0000

    Archive::append now ensures that all Integrations are identically phased to the polyco  Previously, it was assumed that if the polycos are equal, then the Integrations are in phase.

--- More/General/Archive_append.C
+++ More/General/Archive_append.C
@@ -7,6 +7,13 @@
 
 bool Pulsar::Archive::append_chronological = false;
 
+/* By default, the phase of the rising edge of bin zero may have arbitrary 
+   phase as predicted by the polyco.  However, during Archive::append, all
+   Integrations are aligned such that the rising edge of bin zero corresponds
+   to pulse phase zero.  When this is already the case, this flag may be
+   set to speed things up. */
+bool Pulsar::Archive::append_phase_zero = false;
+
 bool Pulsar::Archive::append_must_match = true;
 
 /*!
@@ -64,6 +71,9 @@ void Pulsar::Archive::append (const Archive* arch)
     order_this->append(this, copy);
     return;
   }
+
+  if (verbose)
+    cerr << "Pulsar::Archive::append compare\n";
   
   string reason;
   if (append_must_match) {
@@ -90,27 +100,60 @@ void Pulsar::Archive::append (const Archive* arch)
   }
   
   unsigned old_nsubint = get_nsubint();
-  
+
+  if (verbose)
+    cerr << "Pulsar::Archive::append call IntegrationManager::append" << endl;
+ 
   IntegrationManager::append (arch);
   
   // if observation is not a pulsar, no further checks required
   if (get_type() != Signal::Pulsar) {
-    // may need to check cal phase, if available
+    if (verbose)
+      cerr << "Pulsar::Archive::append no pulsar; no polyco to correct" << endl;
     return;
   }
   
-  // if the polycos are equivalent, no corrections needed
-  if (!model && !arch->model)
+  // if neither archive has a polyco, no correction needed
+  if (!model && !arch->model) {
+    if (verbose)    
+      cerr << "Pulsar::Archive::append no polyco to correct" << endl;
     return;
-  
-  if (model && arch->model && *model == *(arch->model) )
+  }
+
+  // if the polycos are equivalent and the archives are already properly
+  // phased to the polycos, then no corrections are needed
+  if (append_phase_zero && model && arch->model && *model == *(arch->model))  {
+    if (verbose)
+      cerr << "Pulsar::Archive::append identical polycos [optimized]" << endl;
     return;
-  
+  }
+
   // if the current model does not span all Integrations, update the model
-  if (!model || !good_model (*model))
+  if (!model || !good_model (*model)) {
+    if (verbose)
+      cerr << "Pulsar::Archive::append update polyco" << endl;
     update_model (old_nsubint);
-  
+  }
+
+  if (!append_phase_zero && !model_updated) {
+
+    if (verbose)
+      cerr << "Pulsar::Archive::append phasing old integrations" << endl;
+
+    for (unsigned isub=0; isub < old_nsubint; isub++)
+      apply_model (*model, get_Integration(isub));
+
+  }
+
+  if (verbose)
+    cerr << "Pulsar::Archive::append phasing new integrations" << endl;
+
   // correct the new subints against their old model
   for (unsigned isub=old_nsubint; isub < get_nsubint(); isub++)
     apply_model (*(arch->model), get_Integration(isub));
+
+  model_updated = true;
+
+  if (verbose)
+    cerr << "Pulsar::Archive::append exit" << endl;
 }
