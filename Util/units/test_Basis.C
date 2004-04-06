1f2dac4978a5dc8af0aeb33f854e0c293c21d008
straten <straten>
2004-04-06 19:49:06 +0000

    test the Basis template class

---
+++ Util/units/test_Basis.C
@@ -0,0 +1,24 @@
+#include "Basis.h"
+#include <iostream>
+
+int main ()
+{
+  Basis<double> basis1;
+  basis1.set_basis (0.25*M_PI, 0.25*M_PI);
+
+  Basis<double> basis2;
+  basis2.set_basis (Signal::Circular);
+
+  for (unsigned i=0; i<3; i++)  {
+    double dot = basis1.get_basis_vector(i) * basis2.get_basis_vector(i);
+    if (dot != 1) {
+      cerr << "basis1 != basis2" << endl;
+      return -1;
+    }
+  }
+
+  cerr << "Signal::Circular basis has expected orientation=ellipticity=pi/4"
+       << endl;
+
+  return 0;
+}
