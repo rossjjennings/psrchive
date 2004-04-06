d2f9e2675b92ca6f7287deadb16ef11bd5d23b0c
straten <straten>
2004-04-06 16:43:54 +0000

    a greater variety of tests

--- Util/units/test_Matrix.C
+++ Util/units/test_Matrix.C
@@ -3,18 +3,24 @@
 
 int main ()
 {
-  Matrix<float, 3, 3> m1;
-  Matrix<float, 3, 3> m2;
-  Vector<float, 3> v;
+  Matrix<float, 3, 4> m1;
+  Matrix<float, 4, 5> m2;
+  Vector<float, 4> v;
 
   random_matrix (m1, 10.0);
   random_matrix (m2, 10.0);
   random_vector (v, 10.0);
 
-  cerr << "m1=" << m1 << " m2=" << m2 << " m1+m2=" << m1+m2 
-       << " m1*m2=" << m1*m2 << endl;
+  cerr << "m1=" << m1 << "\nm2=" << m2 << "\nm1*m2=" << m1*m2 << endl;
 
   cerr << "v=" << v << " m1*v=" << m1*v << endl;
+
+  Matrix<double, 3, 3> test;
+  random_matrix (test, 10.0);
+
+  Matrix<double, 3, 3> inverse = inv (test);
+
+  cerr << "M*inv(M)=" << test * inverse << endl;
 
   return 0;
 }
