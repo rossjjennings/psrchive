86bdb124da545685204f773a8e7f558ce7b0bf7d
straten <straten>
2004-04-23 13:20:37 +0000

    Jacobi solves both symmetric and Hermitian

--- Util/units/Jacobi.h
+++ Util/units/Jacobi.h
@@ -1,24 +1,100 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/units/Jacobi.h,v $
-   $Revision: 1.1 $
-   $Date: 2004/04/22 15:42:38 $
+   $Revision: 1.2 $
+   $Date: 2004/04/23 13:20:37 $
    $Author: straten $ */
 
 #ifndef __Jacobi_H
 #define __Jacobi_H
 
 #include "Matrix.h"
+#include "Quaternion.h"
 
+double real (double x) { return x; }
+double conj (double x) { return x; }
+
+template <typename T>
+T norm (T x) { return fabs(x); }
+
+
+//! Calculates the Jacobi rotation parameters for a real symmetric matrix
+/*! This template function calculates the Jacobi transformation parameters
+  that minimize roundoff error, as described in Section 11.1 of NR */
+template <typename T, typename U>
+void calculate_Jacobi (U p, U q, T pq,
+                       T& s, T& tau, U& correction)
+{
+  T h = q - p;
+  T g = 100.0 * fabs(pq);
+
+  T t;
+  
+  if (fabs(h)+g == fabs(h))
+
+    t=pq/h;
+
+  else {
+
+    // Equation 11.1.8
+    T theta=0.5*h/(pq);
+
+    // Equation 11.1.10
+    t=1.0/(fabs(theta)+sqrt(1.0+theta*theta));
+    if (theta < 0.0) t = -t;
+
+  }
+  
+  // Equation 11.1.11
+  T c=1.0/sqrt(1+t*t);
+
+  // Equation 11.1.12
+  s=t*c;
+
+  // Equation 11.1.18
+  tau=s/(1.0+c);
+
+  // last term of Equation 11.1.14 and 11.1.15
+  correction = t*pq;
+
+}
+  
+// returns the correction factor for the diagonal
+template <typename T, typename U>
+void calculate_Jacobi (U p, U q, const complex<T>& pq,
+                       complex<T>& s, complex<T>& tau, U& correction)
+{
+  T Sq = 0.5 * (p-q);
+  T Su = pq.real();
+  T Sv = -pq.imag();
+
+  Quaternion<T, Hermitian> herm (0, Sq, Su, Sv);
+  Quaternion<T, Unitary> rotation = eigen (herm);
+
+  T c = rotation.s0;
+  s = complex<T> (-rotation.s3, -rotation.s2);
+
+  // similar to Equation 11.1.18
+  tau = conj(s)/(1.0+c);
+
+  correction = 2.0 * ( c * ( s*conj(pq) ).real() + Sq * ( s*conj(s) ).real() );
+}
+
+
+//! Perform the Jacobi rotation
+/*! This template function performs the Jacobi transformation that
+  minimizes roundoff error, as described in Section 11.1 of NR */
 template <typename T, unsigned RC>
-static inline void rotate (Matrix<T,RC,RC>& x, T s, T tau,
+inline void rotate_Jacobi (Matrix<T,RC,RC>& x, T s, T tau,
 			   unsigned i, unsigned j, unsigned k, unsigned l)
 {
   T g=x[i][j];
   T h=x[k][l];
 
-  x[i][j]=g-s*(h+g*tau);
-  x[k][l]=h+s*(g-h*tau);
+  // Equation 11.1.16
+  x[i][j] -= conj(s)*(h+g*conj(tau));
+  // Equation 11.1.17
+  x[k][l] += s*(g-h*tau);
 }
 
 // returns the correction factor for the diagonal
@@ -26,52 +102,59 @@ template <typename T, typename U, unsigned RC>
 U JacobiRotation ( unsigned ip, unsigned iq, 
 		   Matrix<T,RC,RC>& a, Matrix<T,RC,RC>& v, Vector<U,RC>& d )
 {
-  U h = d[iq] - d[ip];
-  U t, theta;
-  
-  U g = 100.0 * norm(a[ip][iq]);
+  T s, tau;
+  U correction;
 
-  if (norm(h)+g == norm(h))
-    t=(a[ip][iq])/h;
-  else {
-    theta=0.5*h/(a[ip][iq]);
-    t=1.0/(norm(theta)+sqrt(1.0+theta*theta));
-    if (theta < 0.0) t = -t;
-  }
-  
-  U c=1.0/sqrt(1+t*t);
-  U s=t*c;
-  U tau=s/(1.0+c);
-  U correction = t*a[ip][iq];
+  //cerr << "calculate_Jacobi ip=" << ip << " iq=" << iq << endl;
+  calculate_Jacobi (d[ip], d[iq], a[ip][iq], s, tau, correction);
 
   d[ip] -= correction;
   d[iq] += correction;
   
-  a[ip][iq]=0.0;
-
   unsigned j;
 
-  for (j=0; j<ip; j++)
-    rotate(a,s,tau,j,ip,j,iq);
+  //cerr << "in: pp=" << a[ip][ip] << " qq=" << a[iq][iq] << endl;
 
-  for (j=ip+1; j<iq; j++)
-    rotate(a,s,tau,ip,j,j,iq);
-
-  for (j=iq+1; j<RC; j++)
-    rotate(a,s,tau,ip,j,iq,j);
-
+  // rotate the columns of the eigenvector matrix
   for (j=0; j<RC; j++)
-    rotate(v,s,tau,j,ip,j,iq);
+    rotate_Jacobi (v,s,tau,j,ip,j,iq);
+
+#if 0
+
+  for (j=0; j<ip; j++)
+    rotate_Jacobi(a,s,tau,j,ip,j,iq);
+  for (j=ip+1; j<iq; j++)
+    rotate_Jacobi(a,s,tau,ip,j,j,iq);
+  for (j=iq+1; j<RC; j++)
+    rotate_Jacobi(a,s,tau,ip,j,iq,j);
+
+  a[ip][iq]=0.0;
+
+#else
+
+  // rotate the columns of the input matrix
+  for (j=0; j<RC; j++)
+    rotate_Jacobi(a,s,tau,j,ip,j,iq);
+
+  s = conj(s);
+  tau = conj(tau);
+
+  // rotate the rows of the input matrix
+  for (j=0; j<RC; j++)
+    rotate_Jacobi(a,s,tau,ip,j,iq,j);
+
+  //cerr << "out: pp=" << a[ip][ip] << " qq=" << a[iq][iq] << endl;
+  //cerr << "d: pp=" << d[ip] << " qq=" << d[iq] << endl;
+
+  a[iq][ip]=a[ip][iq]=0.0;
+
+#endif
+  
 
   return correction;
 }
 
 
-template <typename T>
-T real (T x) { return x; }
-
-template <typename T>
-T norm (T x) { return fabs(x); }
 
 template <typename T, typename U, unsigned RC>
 void Jacobi (Matrix<T,RC,RC>& a, Matrix<T,RC,RC>& evec, Vector<U,RC>& eval)
@@ -115,7 +198,7 @@ void Jacobi (Matrix<T,RC,RC>& a, Matrix<T,RC,RC>& evec, Vector<U,RC>& eval)
 	     && norm(eval[ip])+g == norm(eval[ip])
 	     && norm(eval[iq])+g == norm(eval[iq]) )
 
-	  a[ip][iq]=0.0;
+	  a[ip][iq]=a[iq][ip]=0.0;
 
 
 	else if (norm(a[ip][iq]) > thresh) {
@@ -138,5 +221,6 @@ void Jacobi (Matrix<T,RC,RC>& a, Matrix<T,RC,RC>& evec, Vector<U,RC>& eval)
   } // for each iteration
   
 }
+
 
 #endif
--- Util/units/test_Jacobi.C
+++ Util/units/test_Jacobi.C
@@ -2,29 +2,34 @@
 #include "MatrixTest.h"
 #include "Jacobi.h"
 
+// specialize for complex matrices
 template <typename T, unsigned RC>
 void test_Jacobi (Matrix<T,RC,RC>& matrix, float tolerance)
 {
   unsigned i,j;
 
-  // make the incoming matrix symmetric
-  for (i=0; i<RC; i++)
+  // make the incoming matrix Hermitian
+  for (i=0; i<RC; i++) {
+    matrix[i][i] = real(matrix[i][i]);
     for (j=i+1; j<RC; j++)
-      matrix[i][j] = matrix[j][i];
+      matrix[i][j] = conj(matrix[j][i]);
+  }
 
   //cerr << "matrix = " << matrix << endl << endl;
 
   // make a copy
   Matrix<T, RC, RC> temp = matrix;
 
-  Matrix<T, RC, RC> eigenvectors;
-  Vector<T, RC>     eigenvalues;
+  Matrix<T, RC, RC>  eigenvectors;
+  Vector<double, RC> eigenvalues;
+
   Jacobi (temp, eigenvectors, eigenvalues);
 
   //cerr << "vec=" << eigenvectors << endl;
   //cerr << "val=" << eigenvalues << endl;
 
-  temp = transpose(eigenvectors) * matrix * eigenvectors;
+  temp = herm(eigenvectors) * matrix * eigenvectors;
+  //temp = eigenvectors * matrix * herm(eigenvectors);
 
   //cerr << "temp=" << temp << endl;
 
@@ -34,44 +39,54 @@ void test_Jacobi (Matrix<T,RC,RC>& matrix, float tolerance)
   for (i=0; i<RC; i++)
     for (j=0; j<RC; j++) {
 
-      T should_be = 0;
+      double should_be = 0;
       if (i == j)
 	should_be = eigenvalues[i];
 
-      if (fabs(temp[i][j] - should_be) > tolerance) {
+      if (norm(temp[i][j] - should_be) > tolerance) {
 	cerr << "test_Jacobi t[" << i << "][" << j << "]=" << temp[i][j]
 	     << " != " << should_be << endl;
+
+	cerr << "P = " << eigenvectors << endl;
+	cerr << "P^T A P = " << temp << endl;
+
 	throw string ("test_Jacobi error");
       }
     }
 
 }
 
-
-int main () 
+template<typename T, unsigned dim> 
+void runtest (unsigned loops, float tolerance)
 {
-  unsigned loops = 100000;
-
-  float tolerance = 1e-12;
-  cerr << "Testing " << loops << " symmetric 7x7 Jacobi solutions" << endl;
+  cerr << loops << " " << dim << "x" << dim << " Jacobi solutions" << endl;
 
   for (unsigned iloop=0; iloop<loops; iloop++) {
 
-    Matrix<double,7,7> m1;
+    Matrix<T,dim,dim> m1;
     random_matrix (m1, 10.0);
 
-    try {
-      test_Jacobi (m1, tolerance);
-    }
-    catch (string& error) {
-      cerr << "test_Jacobi error on loop "<< iloop <<"/"<< loops 
-	   << endl;
-      return -1;
-    }
+    test_Jacobi (m1, tolerance);
 
   }
+}
 
+int main () try {
 
+  unsigned loops = 100000;
+  float tolerance = 1e-12;
 
+  cerr << "Testing symmetric: ";
+  runtest<double,7> (loops, tolerance);
+
+  cerr << "Testing Hermitian: ";
+  runtest<complex<double>,5> (loops, tolerance);
+
+  cerr << "Successful tests" << endl;
   return 0;
+
+}
+catch (string& error) {
+  cerr << "test_Jacobi error "<< error << endl;
+  return -1;
 }
