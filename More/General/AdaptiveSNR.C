1bcf6c0056d861287d4d17875de0ea7cda6ee200
straten <straten>
2004-04-27 15:26:29 +0000

    AdaptiveSNR now uses any BaselineFunction

--- More/General/AdaptiveSNR.C
+++ More/General/AdaptiveSNR.C
@@ -1,135 +1,66 @@
 #include "Pulsar/AdaptiveSNR.h"
+#include "Pulsar/BaselineFunction.h"
+#include "Pulsar/PhaseWeight.h"
 #include "Pulsar/Profile.h"
 
+//! Default constructor
 Pulsar::AdaptiveSNR::AdaptiveSNR ()
 {
-  initial_baseline_window = Profile::default_duty_cycle;
-  baseline_threshold = 5.0;
-  max_iterations = 100;
 }
 
-//! Set the width of the window used to find the initial baseline
-void Pulsar::AdaptiveSNR::set_initial_baseline_window (float width)
+//! Destructor
+Pulsar::AdaptiveSNR::~AdaptiveSNR ()
 {
-  if (width < 0 || width > 1.0)
-    throw Error (InvalidParam,
-		 "Pulsar::AdaptiveSNR::set_initial_baseline_window",
-		 "invalid width = %f periods", width);
-
-  initial_baseline_window = width;
 }
+
 
 //! Set the threshold below which samples are included in the baseline
-void Pulsar::AdaptiveSNR::set_baseline_threshold (float sigma)
+void Pulsar::AdaptiveSNR::set_baseline (BaselineFunction* function)
 {
-  if (sigma < 0)
-    throw Error (InvalidParam,
-		 "Pulsar::AdaptiveSNR::set_baseline_threshold",
-		 "invalid threshold = %f sigma", sigma);
-
-  baseline_threshold = sigma;
-}
-
-
-//! Set the (maximum?) number of iterations
-void Pulsar::AdaptiveSNR::set_max_iterations (unsigned iterations)
-{
-  if (iterations == 0)
-    throw Error (InvalidParam,
-		 "Pulsar::AdaptiveSNR::set_max_iterations",
-		 "invalid iterations = %d", iterations);
-
-  max_iterations = iterations;
+  baseline = function;
 }
 
 
 //! Return the signal to noise ratio
 float Pulsar::AdaptiveSNR::get_snr (const Profile* profile)
 {
+  if (!baseline)
+    throw Error (InvalidState, "Pulsar::AdaptiveSNR::get_snr",
+		 "no BaselineFunction provided (use set_baseline)");
+
+  baseline->set_Profile( profile );
+
+  PhaseWeight weight;
+
+  baseline->get_weight (weight);
+
   // the mean, variance, and variance of the mean
   double mean, var, var_mean;
 
-  if (initial_baseline_window == 1.0)
-    profile->stats (&mean, &var, &var_mean);
-
-  else {
-    float centre = profile->find_min_phase(initial_baseline_window);
-    profile->stats (centre, &mean, &var, &var_mean, initial_baseline_window);
-  }
-
-  if (var == 0.0)
-    return 0;
-
-  vector<bool> mask (profile->get_nbin(), false);
-
-  const float* amps = profile->get_amps();
-  unsigned nbin = profile->get_nbin();
-  unsigned count = 0;
-
-  for (unsigned iter=0; iter < max_iterations; iter++) {
-
-    double cutoff = baseline_threshold * sqrt (var + var_mean);
-
-    if (Profile::verbose)
-      cerr << "Pulsar::AdaptiveSNR::get_snr iter=" << iter
-	   << " baseline mean=" << mean << " cutoff=" << cutoff << endl;
-
-    bool changed = false;
-
-    double tot = 0.0;
-    double totsq = 0.0;
-    count = 0;
-
-    for (unsigned ibin=0; ibin<nbin; ibin++) {
-
-      if ( fabs(amps[ibin]-mean) < cutoff ) {
-
-	tot += amps[ibin];
-	totsq += amps[ibin]*amps[ibin];
-	count ++;
-
-	if (!mask[ibin])
-	  changed = true;
-
-	mask[ibin] = true;
-
-      }
-      else {
-
-	if (mask[ibin])
-	  changed = true;
-
-	mask[ibin] = false;
-
-      }
-
-    }
-
-    if (count < 2)
-      return 0;
-
-    mean = tot / double(count);
-    double meansq = totsq / double(count);
-    var = (meansq - mean*mean) * double(count)/double(count-1);
-    var_mean = var / double(count);
-
-    if (!changed)
-      break;
-
-  }
+  weight.stats (profile, &mean, &var, &var_mean);
 
   double rms = sqrt (var);
+  unsigned nbin = profile->get_nbin();
   double energy = profile->sum() - mean * nbin;
 
-  if (Profile::verbose)
-    cerr << "Pulsar::AdaptiveSNR::get_snr " << count << " out of "
-	 << nbin << " bins in baseline\n"
-      "  mean=" << mean << " rms=" << rms << " energy=" << energy << " S/N=" << energy/rms << endl;
+  double total = weight.get_weight_sum();
+  double max   = weight.get_weight_max();
+
+  double on_pulse = nbin * max - total;
+  double snr = energy / sqrt(var*on_pulse);
+
+  if (Profile::verbose) {
+
+    cerr << "Pulsar::AdaptiveSNR::get_snr " << total << " out of " << nbin*max
+	 << " bins in baseline\n  mean=" << mean << " rms=" << rms 
+	 << " energy=" << energy << " S/N=" << snr << endl;
+
+  }
 
   if (energy < 0)
-    energy = 0.0;
+    return 0.0;
 
-  return energy/rms;
+  return snr;
 
 }    
 
--- More/General/Pulsar/AdaptiveSNR.h
+++ More/General/Pulsar/AdaptiveSNR.h
@@ -1,20 +1,21 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/AdaptiveSNR.h,v $
-   $Revision: 1.1 $
-   $Date: 2004/04/27 06:56:41 $
+   $Revision: 1.2 $
+   $Date: 2004/04/27 15:26:29 $
    $Author: straten $ */
 
 #ifndef __Pulsar_AdaptiveSNR_h
 #define __Pulsar_AdaptiveSNR_h
 
-#include "ReferenceAble.h"
+#include "Reference.h"
 
 namespace Pulsar {
 
   class Profile;
+  class BaselineFunction;
 
-  //! Calculates the signal-to-noise ratio in the Adaptive domain
+  //! Calculates the signal-to-noise ratio using a baseline computing function
   class AdaptiveSNR : public Reference::Able {
 
   public:
@@ -22,29 +23,19 @@ namespace Pulsar {
     //! Default constructor
     AdaptiveSNR ();
 
-    //! Set the width of the window used to find the initial baseline
-    void set_initial_baseline_window (float width);
+    //! Destructor
+    ~AdaptiveSNR ();
 
-    //! Set the threshold below which samples are included in the baseline
-    void set_baseline_threshold (float sigma);
-
-    //! Set the (maximum?) number of iterations
-    void set_max_iterations (unsigned iterations);
+    //! Set the function used to compute the baseline
+    void set_baseline (BaselineFunction* function);
 
     //! Return the signal to noise ratio
     float get_snr (const Profile* profile);
 
   protected:
 
-    //! The width of the window used to find the initial baseline
-    float initial_baseline_window;
-
-    //! The threshold below which samples are included in the baseline
-    float baseline_threshold;
-
-    //! The (maximum?) number of iterations
-    unsigned max_iterations;
-
+    //! The function used to compute the baseline
+    Reference::To<BaselineFunction> baseline;
 
   };
 
