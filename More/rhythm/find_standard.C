392747fa9ae7bd77967572247eb013a289fc065f
ahotan <ahotan>
2004-05-09 09:50:20 +0000

    A generic file for containing psrchive helper functions

---
+++ More/rhythm/find_standard.C
@@ -0,0 +1,58 @@
+#include "Pulsar/Utilities.h"
+
+//
+// Function to select a standard profile
+//
+Pulsar::Profile* Pulsar::find_standard (const Pulsar::Archive* data, 
+					string path) {
+  
+  // Store the current working directory
+  char* directory = new char[1024];
+  getcwd(directory, 1024);
+  
+  if (chdir(path.c_str()) != 0) {
+    chdir(directory);
+    cerr << "No suitable standard profile found." << endl;
+    return 0;
+  }
+  
+  vector <string> the_stds;
+  
+  char temp[128];
+  FILE* fptr = popen("ls -1 *.std", "r");
+  if (ferror(fptr)==0) {
+    while(fscanf(fptr, "%s\n", temp) == 1) {
+      the_stds.push_back(temp);
+    }
+  }
+  
+  Reference::To <Pulsar::Archive> candidate = 0;
+  
+  for (unsigned i = 0; i < the_stds.size(); i++) {
+    candidate = Pulsar::Archive::load("./" + the_stds[i]);
+    candidate -> centre();
+    if ((data -> get_source()) != (candidate -> get_source())) {
+      candidate = 0;
+      continue;
+    }
+    if ((data -> get_centre_frequency() != 
+	 candidate -> get_centre_frequency())) {
+      candidate = 0;
+      continue;
+    }
+    candidate -> tscrunch();
+    candidate -> pscrunch();
+    candidate -> fscrunch();
+    
+    chdir(directory);
+    
+    cerr << "Selected standard profile: " << the_stds[i] << endl;
+    
+    return (candidate.release() -> get_Profile(0,0,0));
+  }
+  
+  chdir(directory);
+  cerr << "No standard profile available." << endl;
+  return 0;
+  
+}
