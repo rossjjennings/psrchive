40a87ae53e29dfebfa4269fc69df40ffb08f7090
straten <straten>
2004-07-16 07:49:34 +0000

    adapted to changes in Integration class; load|unload_Integration.C added

---
+++ Base/Formats/PSRFITS/load_Integration.C
@@ -0,0 +1,310 @@
+#include "Pulsar/FITSArchive.h"
+#include "Pulsar/Integration.h"
+#include "Pulsar/Profile.h"
+
+#include "Pulsar/IntegrationOrder.h"
+#include "Pulsar/FITSSubintExtension.h"
+
+#include "FITSError.h"
+
+// //////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////
+//! A function to read a single integration from a FITS file on disk.
+/*! This function assumes that the Integration will have the global attributes
+  of the file. */
+Pulsar::Integration* 
+Pulsar::FITSArchive::load_Integration (const char* filename, unsigned isubint)
+{
+  if (!filename)
+    throw Error (InvalidParam, "FITSArchive::load_Integration",
+		 "filename unspecified");
+
+  Pulsar::Integration* integ = new_Integration();
+
+  int row = isubint + 1;
+  
+  int status = 0;  
+
+  if (verbose == 3) {
+    cerr << "FITSArchive::load_Integration number " << isubint << endl;
+  }
+  
+  double nulldouble = 0.0;
+  float nullfloat = 0.0;
+  int16 nullshort = -1;
+  
+  int initflag = 0;
+  int colnum = 0;
+  
+  // Open the file
+
+  fitsfile* sfptr = 0;
+  
+  if (verbose == 3)
+    cerr << "FITSArchive::load_Integration fits_open_file (" 
+	 << filename << ")" << endl;
+  
+  fits_open_file(&sfptr, filename, READONLY, &status);
+  
+  if (status != 0)
+    throw FITSError (status, "FITSArchive::load_Integration", 
+		     "fits_open_file(%s)", filename);
+  
+
+  // Move to the SUBINT Header Data Unit
+  
+  fits_movnam_hdu (sfptr, BINARY_TBL, "SUBINT", 0, &status);
+  if (status != 0)
+    throw FITSError (status, "FITSArchive::load_Integration", 
+		     "fits_movnam_hdu SUBINT");
+
+  init_Integration ( integ );
+
+  // Set the start time of the integration
+  
+  initflag = 0;
+  colnum = 0;
+  
+  fits_get_colnum (sfptr, CASEINSEN, "OFFS_SUB", &colnum, &status);
+  
+  double time = 0.0;
+  MJD newmjd;
+  
+  fits_read_col (sfptr, TDOUBLE, colnum, row, 1, 1, &nulldouble,
+		 &time, &initflag, &status);
+  
+  if (status != 0)
+    throw FITSError (status, "FITSArchive::load_Integration", 
+		     "fits_read_col OFFS_SUB");
+  
+  newmjd = reference_epoch + time;
+  
+  // Set the folding period
+
+  if (model) {
+
+    integ->set_folding_period (model->period(newmjd));
+
+    // Set the toa epoch, correcting for phase offset
+
+    Phase stt_phs = model->phase(reference_epoch);
+    Phase off_phs = model->phase(newmjd);
+
+    Phase dphase = off_phs - stt_phs;
+  
+    newmjd -= dphase.fracturns() * integ->get_folding_period();
+  
+    integ->set_epoch (newmjd);
+  
+    if (verbose == 3)
+      cerr << "Pulsar::FITSArchive::load_Integration set_epoch " 
+	   << newmjd << endl;
+  }
+
+  // Set the duration of the integration
+  
+  colnum = 0;
+  fits_get_colnum (sfptr, CASEINSEN, "TSUBINT", &colnum, &status);
+  
+  double duration = 0.0;
+  
+  fits_read_col (sfptr, TDOUBLE, colnum, row, 1, 1, &nulldouble,
+		 &duration, &initflag, &status);
+  
+  integ->set_duration (duration);
+  
+  if (get<Pulsar::IntegrationOrder>()) {
+    colnum = 0;
+    fits_get_colnum (sfptr, CASEINSEN, "INDEXVAL", &colnum, &status);
+    
+    double value = 0.0;
+
+    fits_read_col (sfptr, TDOUBLE, colnum, row, 1, 1, &nulldouble,
+		   &value, &initflag, &status);
+    
+    if (status != 0)
+      throw FITSError (status, "FITSArchive::load_Integration", 
+		       "fits_read_col INDEXVAL");
+    
+    get<Pulsar::IntegrationOrder>()->set_Index(row-1,value);
+  }
+  
+  // Load other useful info
+
+  load_FITSSubintExtension(sfptr,row,integ);
+
+  // Set up the data vector, only Pulsar::Archive base class is friend
+
+  resize_Integration (integ);
+
+  // Load the profile weights
+
+  if (verbose == 3)
+    cerr << "Pulsar::FITSArchive::load_Integration reading weights" 
+	 << endl;
+  
+  int counter = 1;
+  vector < float >  weights(get_nchan());
+  
+  colnum = 0;
+  fits_get_colnum (sfptr, CASEINSEN, "DAT_WTS", &colnum, &status);
+  
+  for(unsigned b = 0; b < get_nchan(); b++) {
+    fits_read_col (sfptr, TFLOAT, colnum, row, counter, 1, &nullfloat, 
+		   &weights[b], &initflag, &status);
+    counter ++;
+  }
+  
+  // Set the profile weights
+  
+  if (verbose == 3)
+    cerr << "Pulsar::FITSArchive::load_Integration setting weights" 
+	 << endl;
+  
+  for(unsigned j = 0; j < get_nchan(); j++)
+    integ->set_weight(j, weights[j]);
+  
+  // Load the channel centre frequencies
+  
+  if (verbose == 3)
+    cerr << "Pulsar::FITSArchive::load_Integration reading channel freqs" 
+	 << endl;
+  
+  counter = 1;
+  vector < float >  chan_freqs(get_nchan());
+  
+  colnum = 0;
+  fits_get_colnum (sfptr, CASEINSEN, "DAT_FREQ", &colnum, &status);
+  
+  fits_read_col (sfptr, TFLOAT, colnum, row, counter, get_nchan(), &nullfloat, 
+		 &(chan_freqs[0]), &initflag, &status);
+  
+  // Set the profile channel centre frequencies
+  
+  if (verbose == 3)
+    cerr << "Pulsar::FITSArchive::load_Integration setting channel freqs" 
+	 << endl;
+
+  bool all_ones = true;
+  for (unsigned j = 0; j < get_nchan(); j++)
+    if (chan_freqs[j] != 1)
+      all_ones = false;
+  
+  double chanbw = get_bandwidth() / get_nchan();
+  
+  if ( all_ones ) {
+    if (verbose == 3)
+      cerr << "FITSArchive::load_Integration all frequencies unity - reseting" << endl;
+    for (unsigned j = 0; j < get_nchan(); j++) {
+      integ->set_centre_frequency (j, get_centre_frequency()
+				   -0.5*(get_bandwidth()+chanbw)+j*chanbw);
+    }
+  }
+  else
+    for (unsigned j = 0; j < get_nchan(); j++) {
+      //integ->set_frequency(j, chan_freqs[j]);
+      integ->set_centre_frequency(j, chan_freqs[j]);
+    }
+  
+  // Load the profile scale factors
+  
+  if (verbose == 3)
+    cerr << "Pulsar::FITSArchive::load_Integration reading scale factors" 
+	 << endl;
+
+  vector < vector < float > > scales(get_npol(),vector<float>(get_nchan()));
+
+  colnum = 0;
+  fits_get_colnum (sfptr, CASEINSEN, "DAT_SCL", &colnum, &status);
+  
+  counter = 1;
+  for (unsigned a = 0; a < get_npol(); a++) {
+    fits_read_col (sfptr, TFLOAT, colnum, row, counter, get_nchan(), &nullfloat, 
+		   &(scales[a][0]), &initflag, &status);
+    counter += nchan;
+  }
+
+  // Load the profile offsets
+  
+  if (verbose == 3)
+    cerr << "Pulsar::FITSArchive::load_Integration reading offsets" 
+	 << endl;
+  
+  vector < vector < float > > offsets(get_npol(),vector<float>(get_nchan()));
+  
+  colnum = 0;
+  fits_get_colnum (sfptr, CASEINSEN, "DAT_OFFS", &colnum, &status);
+  
+  counter = 1;
+  for (unsigned a = 0; a < get_npol(); a++) {
+    fits_read_col (sfptr, TFLOAT, colnum, row, counter, get_nchan(), &nullfloat, 
+		   &(offsets[a][0]), &initflag, &status);
+    counter += nchan;
+  }
+
+  // Load the data
+  
+  if (verbose == 3)
+    cerr << "Pulsar::FITSArchive::load_Integration reading profiles" 
+	 << endl;
+  
+  counter = 1;
+  Profile* p = 0;
+  int16* temparray = new int16 [get_nbin()];
+  float* fltarray = new float [get_nbin()];
+  Signal::Component polmeas = Signal::None;
+  
+  colnum = 0;
+  fits_get_colnum (sfptr, CASEINSEN, "DATA", &colnum, &status);
+  
+  int typecode = 0;
+  long repeat = 0;
+  long width = 0;
+  
+  fits_get_coltype (sfptr, colnum, &typecode, &repeat, &width, &status);  
+
+  for (unsigned a = 0; a < get_npol(); a++) {
+    for (unsigned b = 0; b < get_nchan(); b++) {
+      
+      p = integ->get_Profile(a,b);
+      
+      fits_read_col (sfptr, TSHORT, colnum, row, counter, get_nbin(), 
+		     &nullshort, temparray, &initflag, &status);
+      
+      if (status != 0) {
+	throw FITSError (status, "FITSArchive::load_Integration",
+			 "Error reading subint data"
+			 " ipol=%d/%d ichan=%d/%d counter=%d",
+			 a, get_npol(), b, get_nchan(), counter);
+      }
+      
+      counter += get_nbin();
+      
+      for(unsigned j = 0; j < get_nbin(); j++) {
+	fltarray[j] = temparray[j] * scales[a][b] + offsets[a][b];
+	if (scale_cross_products) {
+	  if (integ->get_state() == Signal::Coherence) {
+	    if (a == 2 || a == 3)
+	      fltarray[j] *= 2;
+	  }
+	}
+      }
+      
+      p->set_amps(fltarray);
+      p->set_state(polmeas);
+
+    }  
+  }
+  
+  delete [] temparray; 
+  delete [] fltarray;
+  
+  if (verbose == 3)
+    cerr << "Pulsar::FITSArchive::load_Integration load complete" << endl;  
+  
+  // Finished with the file for now
+  
+  fits_close_file(sfptr,&status);
+
+  return integ;
+}
---
+++ Base/Formats/PSRFITS/unload_Integration.C
@@ -0,0 +1,338 @@
+#include "Pulsar/FITSArchive.h"
+#include "Pulsar/Integration.h"
+#include "Pulsar/Profile.h"
+
+#include "Pulsar/IntegrationOrder.h"
+#include "Pulsar/FITSSubintExtension.h"
+
+#include "FITSError.h"
+#include "genutil.h"
+
+// ////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////
+//! A function to write an integration to a row in a FITS file on disk.
+
+void Pulsar::FITSArchive::unload_integration (int row, 
+					      const Integration* integ, 
+					      fitsfile* thefptr) const
+{
+  int status = 0;
+ 
+  bool has_alt_order = false;
+
+  if (get<Pulsar::IntegrationOrder>()) {
+    has_alt_order = true;
+    if (verbose == 3)
+      cerr << "FITSArchive::unload_integration using " 
+	   << get<Pulsar::IntegrationOrder>()->get_extension_name()
+	   << endl;
+  }
+
+  // Set the subint number
+  
+  int colnum = 0;
+  fits_get_colnum (thefptr, CASEINSEN, "ISUBINT", &colnum, &status);
+  
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_get_colnum ISUBINT");
+  
+  fits_write_col (thefptr, TINT, colnum, row, 1, 1, &row, &status);
+
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_write_col ISUBINT");
+  
+  // Set the start time of the integration
+    
+  colnum = 0;
+  fits_get_colnum (thefptr, CASEINSEN, "OFFS_SUB", &colnum, &status);
+  
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_get_colnum OFFS_SUB");
+
+  double time = 0.0;
+  time = (integ->get_epoch () - reference_epoch).in_seconds();
+  
+  fits_write_col (thefptr, TDOUBLE, colnum, row, 1, 1, &time, &status);
+  
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_write_col OFFS_SUB");
+  
+  if (verbose == 3)
+    cerr << "FITSArchive::unload_integration OFFS_SUB set" << endl;
+  
+  // Set the duration of the integration
+  
+  colnum = 0;
+  fits_get_colnum (thefptr, CASEINSEN, "TSUBINT", &colnum, &status);
+  
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_get_colnum TSUBINT");
+  
+  double duration = integ->get_duration();  
+  fits_write_col (thefptr, TDOUBLE, colnum, row, 1, 1, &duration, &status);
+  
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_write_col TSUBINT");
+  
+  if (verbose == 3)
+    cerr << "FITSArchive::unload_integration TSUBINT set" << endl;
+  
+  // Write out the index values, if applicable
+  if (has_alt_order) {
+    colnum = 0;
+    fits_get_colnum (thefptr, CASEINSEN, "INDEXVAL", &colnum, &status);
+
+    if (status != 0)
+      throw FITSError (status, "FITSArchive:unload_integration",
+		       "fits_get_colnum INDEXVAL");
+    
+    double value = get<Pulsar::IntegrationOrder>()->get_Index(row-1);
+    fits_write_col (thefptr, TDOUBLE, colnum, row, 1, 1, &value, &status);
+  }
+
+  // Write other useful info
+  
+  const FITSSubintExtension* theExt = integ->get<FITSSubintExtension>();
+  if (theExt)
+    unload(thefptr,theExt,row);
+  
+  // Write the profile weights
+
+  colnum = 0;
+  fits_get_colnum (thefptr, CASEINSEN, "DAT_WTS", &colnum, &status);
+  
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_get_colnum DAT_WTS");
+
+  //fits_modify_vector_len (thefptr, colnum, nchan, &status);
+
+  vector < float >  weights(nchan);
+
+  for(unsigned j = 0; j < nchan; j++)
+    weights[j] = integ->get_weight(j);
+
+  fits_write_col (thefptr, TFLOAT, colnum, row, 1, nchan, 
+		  &(weights[0]), &status);
+
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_write_col DAT_WTS");
+  
+  // Write the channel centre frequencies
+
+  colnum = 0;
+  fits_get_colnum (thefptr, CASEINSEN, "DAT_FREQ", &colnum, &status);
+  
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_get_colnum DAT_FREQ");
+  
+  //fits_modify_vector_len (thefptr, colnum, nchan, &status);
+
+  vector < float >  chan_freqs(nchan);
+
+  for(unsigned j = 0; j < nchan; j++)
+    chan_freqs[j] = integ->get_centre_frequency(j);
+
+  fits_write_col (thefptr, TFLOAT, colnum, row, 1, nchan, 
+		  &(chan_freqs[0]), &status);
+
+  if (status != 0)
+    throw FITSError (status, "FITSArchive:unload_integration",
+		     "fits_write_col DAT_FREQ");
+
+  // Start writing profiles
+  
+  if (verbose == 3)
+    cerr << "FITSArchive::unload_integration writing profiles" << endl;
+
+  // Resize the FITS column arrays
+
+  // 20/02/2003 This is unnecessary. I do it before I call the function.
+  // This section is only included to help avoid confusion (including
+  // my own!)
+
+  /*
+    
+    colnum = 0;
+    fits_get_colnum (thefptr, CASEINSEN, "DAT_OFFS", &colnum, &status);
+    fits_modify_vector_len (thefptr, colnum, (nchan*npol), &status);
+    
+    colnum = 0;
+    fits_get_colnum (thefptr, CASEINSEN, "DAT_WTS", &colnum, &status);
+    fits_modify_vector_len (thefptr, colnum, (nchan*npol), &status);
+    
+    colnum = 0;
+    fits_get_colnum (thefptr, CASEINSEN, "DAT_SCL", &colnum, &status);
+    fits_modify_vector_len (thefptr, colnum, (nchan*npol), &status);
+    
+    colnum = 0;
+    fits_get_colnum (thefptr, CASEINSEN, "DATA", &colnum, &status);
+    fits_modify_vector_len (thefptr, colnum, (nchan*npol*nbin), &status);
+
+  */
+  
+  int counter1 = 1;
+  int counter2 = 1;
+
+  const Profile *p = 0;
+
+  int16* temparray2 = new int16 [nbin];
+
+  float min = 0.0;
+  float max = 0.0;
+  float offset = 0.0;
+  float scalefac = 0.0;  
+  
+  bool save_signed = true;
+
+  float max_short;
+
+  if (save_signed)
+    max_short = pow(2.0,15.0)-1.0;
+  else
+    max_short = pow(2.0,16.0)-1.0;
+
+  for(unsigned a = 0; a < npol; a++) {
+    for(unsigned b = 0; b < nchan; b++) {
+      
+      p = integ->get_Profile(a,b);
+      float* temparray1 = new float[nbin];
+      for(unsigned j = 0; j < get_nbin(); j++)
+	temparray1[j] = (p->get_amps())[j];
+      
+      if (scale_cross_products) {
+	if (integ->get_state() == Signal::Coherence) {
+	  if (a == 2 || a == 3)
+	    for(unsigned j = 0; j < get_nbin(); j++)
+	      temparray1[j] /= 2.0;
+	}
+      }
+      
+      if (verbose == 3) {
+	cerr << "FITSArchive::unload_integration got profile" << endl;
+	cerr << "nchan = " << b << endl;
+	cerr << "npol  = " << a << endl;
+      }
+
+      minmaxval(nbin, temparray1, &min, &max);
+
+      if (save_signed)
+	offset = 0.5 * (max + min);
+      else
+	offset = min;
+
+      if (verbose == 3)
+	cerr << "FITSArchive::unload_integration offset = "
+	     << offset
+	     << endl;
+      
+      scalefac = 1.0;
+      
+      // Test for dynamic range
+      if (fabs(min - max) < 0.000001) {
+	if (verbose == 3) {
+	  cerr << "FITSArchive::unload_integration WARNING no range in profile"
+	       << endl;
+	}
+      }
+      else
+	// Find the scale factor
+	scalefac = (max - min) / max_short;
+      
+      if (verbose == 3)
+	cerr << "FITSArchive::unload_integration scalefac = "
+	     << scalefac
+	     << endl;
+      
+      // Apply the scale factor
+      
+      for (unsigned i = 0; i < nbin; i++) {
+	temparray2[i] = int16 ((temparray1[i]-offset) / scalefac);
+      }
+
+      // Write the scale factor to file
+
+      if (verbose == 3)
+	cerr << "FITSArchive::unload_integration writing scale fac" << endl;
+
+      colnum = 0;
+      fits_get_colnum (thefptr, CASEINSEN, "DAT_SCL", &colnum, &status);
+
+      if (status != 0)
+	throw FITSError (status, "FITSArchive:unload_integration",
+			 "fits_get_colnum DAT_SCL");
+
+      fits_write_col (thefptr, TFLOAT, colnum, row, counter1, 1, 
+		      &scalefac, &status);
+      
+      if (status != 0)
+	throw FITSError (status, "FITSArchive:unload_integration",
+			 "fits_write_col DAT_SCL");
+      
+      // Write the offset to file
+
+      if (verbose == 3)
+	cerr << "FITSArchive::unload_integration writing offset" << endl;
+
+      colnum = 0;
+      fits_get_colnum (thefptr, CASEINSEN, "DAT_OFFS", &colnum, &status);
+
+      if (status != 0)
+	throw FITSError (status, "FITSArchive:unload_integration",
+			 "fits_get_colnum DAT_OFFS");
+
+      fits_write_col (thefptr, TFLOAT, colnum, row, counter1, 1, 
+		      &offset, &status);
+      
+      if (status != 0)
+	throw FITSError (status, "FITSArchive:unload_integration",
+			 "fits_write_col DAT_OFFS");
+
+      counter1 ++;
+
+      // Write the data
+
+      if (verbose == 3)
+	cerr << "FITSArchive:unload_integration writing data" << endl;
+
+      colnum = 0;
+      fits_get_colnum (thefptr, CASEINSEN, "DATA", &colnum, &status);
+
+      if (status != 0)
+	throw FITSError (status, "FITSArchive:unload_integration",
+			 "fits_get_colnum DATA");
+
+      fits_write_col (thefptr, TSHORT, colnum, row, counter2, nbin, 
+		      temparray2, &status);
+
+      if (status != 0)
+	throw FITSError (status, "FITSArchive:unload_integration",
+			 "fits_write_col DATA");
+
+      counter2 = counter2 + nbin;
+	
+      if (verbose == 3)
+	cerr << "FITSArchive:unload_integration looping" << endl;
+      
+      delete[] temparray1;
+    }	  
+  }
+  
+  delete[] temparray2;
+  
+  if (verbose == 3)
+    cerr << "FITSArchive::unload_integration finished" << endl;
+}
+
+//
+// End of unload_integration function
+// //////////////////////////////////
+// //////////////////////////////////
