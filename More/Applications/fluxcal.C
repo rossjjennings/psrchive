b709f6f0b952019bb9d17cb45493862176b16484
straten <straten>
2004-10-13 13:57:56 +0000

    implemented code to handle multiple flux calibrator sets in one run

--- More/Applications/fluxcal.C
+++ More/Applications/fluxcal.C
@@ -1,11 +1,17 @@
 #include "Pulsar/FluxCalibratorDatabase.h"
 #include "Pulsar/FluxCalibrator.h"
 #include "Pulsar/Archive.h"
+#include "string_utils.h"
 #include "dirutil.h"
 
 #include <unistd.h>
 #include <iostream>
 using namespace std;
+
+// class name of the processed calibrator archive
+static string archive_class = "PSRFITS";
+static string output_ext = "fluxcal";
+static double interval = 5.0 * 60.0;
 
 void usage ()
 {
@@ -14,24 +20,38 @@ void usage ()
     "\n"
     "fluxcal [options] filename[s]\n"
     "options:\n"
-    "  -a archive   Name of the archive class to which result will be output\n"
-    "  -d database  Name of file containing flux calibrator information\n"
+    "  -a class     Pulsar::Archive class used to represent output\n"
+    "  -d database  name of file containing flux calibrator information\n"
+    "  -e extension filename extension added to output archives\n"
+    "  -i minutes   maximum number of minutes between archives in same set\n"
     "\n"
-    "By default, flux calibrator information is read from " 
+    "By default, flux calibrator information is read from \n\t" 
        << Pulsar::FluxCalibratorDatabase::default_filename << "\n"
+    "and the maximum interval between archives in the same\n"
+    "flux calibrator set is " << interval/60 << " minutes.\n"
        << endl;
+}
+
+
+void unload (Pulsar::FluxCalibrator* fluxcal)
+{
+  Reference::To<Pulsar::Archive> archive;
+  cerr << "fluxcal: creating " << archive_class << " Archive" << endl;
+  archive = fluxcal->new_solution (archive_class);
+
+  string newname = fluxcal->get_filenames ();
+  newname = replace_extension( stringtok (&newname, ","), output_ext );
+
+  cerr << "fluxcal: unloading " << newname << endl;
+  archive -> unload (newname);
 }
 
 int main (int argc, char** argv) try {
 
-  char c;
-
-  // class name of the processed calibrator archive
-  string archive_class = "PSRFITS";
-
   Pulsar::FluxCalibratorDatabase* database = 0;
 
-  while ((c = getopt(argc, argv, "hqvVd:")) != -1) 
+  char c;
+  while ((c = getopt(argc, argv, "hqvVa:d:e:i:")) != -1) 
 
     switch (c)  {
 
@@ -50,12 +70,28 @@ int main (int argc, char** argv) try {
 
     case 'a':
       archive_class = optarg;
+      cerr << "fluxcal: will write to " << archive_class << " files" << endl;
       break;
 
     case 'd':
       database = new Pulsar::FluxCalibratorDatabase (optarg);
+      cerr << "fluxcal: database loaded from " << optarg << endl;
       break;
 
+    case 'e':
+      output_ext = optarg;
+      cerr << "fluxcal: output file extension: " << output_ext << endl;
+      break;
+
+    case 'i':
+      interval = atof(optarg) * 60.0;
+      cerr << "fluxcal: maximum interval between archives: " << interval/60
+	   << " minutes" << endl;
+      break;
+
+    default:
+      cerr << "fluxcal: invalid command line option: -" << c << endl;
+      break;
     } 
 
 
@@ -68,43 +104,54 @@ int main (int argc, char** argv) try {
     return -1;
   }
 
-
+  Reference::To<Pulsar::Archive> last;
   Reference::To<Pulsar::Archive> archive;
   Reference::To<Pulsar::FluxCalibrator> fluxcal;
 
-  // total number of archives processed
-  unsigned total = 0;
-  
-  for (unsigned ifile=0; ifile < filenames.size(); ifile++) {
+  for (unsigned ifile=0; ifile < filenames.size(); ifile++) try {
 
     cerr << "fluxcal: loading " << filenames[ifile] << endl;
     
     archive = Pulsar::Archive::load(filenames[ifile]);
     
-    cout << "fluxcal: loaded Archive" << endl;
+    if (fluxcal) {
 
-    if (!fluxcal)
+      double gap = (archive->start_time() - last->end_time()).in_seconds();    
+      if (gap > interval) {
+	cerr << "fluxcal: gap=" << gap << " greater than interval=" 
+	     << interval << endl;
+
+	unload (fluxcal);
+	fluxcal = 0;
+      }
+    
+    }
+
+    if (!fluxcal) {
+
+      cerr << "fluxcal: starting new FluxCalibrator" << endl;
       fluxcal = new Pulsar::FluxCalibrator (archive);
-    else
+      if (database)
+	fluxcal->set_database (database);
+
+    }
+    else {
+
+      cerr << "fluxcal: adding observation to FluxCalibrator" << endl;
       fluxcal->add_observation (archive);
 
-    total ++;
+    }
+
+    last = archive;
+
+  }
+  catch (Error& error) {
+    cerr << "fluxcal: error handling" << filenames[ifile] << endl
+	 << error.get_message() << endl;
   }
 
-  if (fluxcal) {
-
-    if (database)
-      fluxcal->set_database (database);
-
-    cerr << "fluxcal: creating " << archive_class << " Archive" << endl;
-    archive = fluxcal->new_solution (archive_class);
-
-    string newname = "fluxcal.ar";
-
-    cerr << "fluxcal: unloading " << newname << endl;
-    archive -> unload (newname);
-
-  }	
+  if (fluxcal)
+    unload (fluxcal);
 
   return 0;
 }
