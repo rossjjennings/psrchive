a8c4d44642e9bf21b3fbe58e78ce861e9dc41e9e
straten <straten>
2004-10-26 12:54:37 +0000

    use traits to enable any type

--- Util/fft/interpolate.h
+++ Util/fft/interpolate.h
@@ -1,30 +1,37 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/fft/interpolate.h,v $
-   $Revision: 1.5 $
-   $Date: 2003/05/07 15:50:43 $
+   $Revision: 1.6 $
+   $Date: 2004/10/26 12:54:37 $
    $Author: straten $*/
 
 #ifndef __fft_interpolate_h
 #define __fft_interpolate_h
 
-#include <string>
+#include "Traits.h"
+#include "Error.h"
 #include "fftm.h"
+
+#include <vector>
 
 namespace fft {
 
   extern bool verbose;
 
   template <class T>
-    void interpolate (vector<T>& out, const vector<T>& in) {
+  void interpolate (vector<T>& out, const vector<T>& in) {
     
-    if (in.size() > out.size())
-      throw string ("fft::interpolate out smaller than in.  just scrunch");
+    if (in.size() >= out.size())
+      throw Error (InvalidParam, "fft::interpolate",
+		   "in.size=%d >= out.size=%d.  just scrunch",
+		   in.size() >= out.size());
+    
+    vector< std::complex<float> > dom1 (out.size());
+    vector< std::complex<float> > dom2 (out.size());
+    
+    DatumTraits<T> datum_traits;
 
-    vector<float> dom1 (out.size()*2);
-    vector<float> dom2 (out.size()*2);
-
-    unsigned ndim = T::ndim;
+    unsigned ndim = datum_traits.ndim();
     
     if (verbose)
       cerr << "fft::interpolate " << ndim << " dimensions from "
@@ -36,34 +43,23 @@ namespace fft {
     for (idim=0; idim<ndim; idim++) {
 
       for (ipt=0; ipt < in.size(); ipt++)
-	dom1[ipt] = in[ipt][idim];
+	dom1[ipt] = datum_traits.element (in[ipt], idim);
 
-      fft::frc1d (in.size(), &(dom2[0]), &(dom1[0]));
-
-      unsigned end = dom2.size()-2;
-      // copy the conjugate of the transform, backwards, into the
-      // end of the array, excluding DC
-      for (ipt=2; ipt < in.size(); /* incr in loop */) {
-	dom2[end]   = dom2[ipt];   // Re
-	ipt ++;
-	dom2[end+1] = -dom2[ipt];  // Im
-	ipt ++;
-	end -= 2;
-      }
+      fft::fcc1d (in.size(), (float*)&(dom2[0]), (float*)&(dom1[0]));
 
       // zero pad the rest
-      unsigned stop = dom2.size()-in.size()+2;
-      for (ipt=in.size(); ipt<stop; ipt++)
+      for (ipt=in.size(); ipt<dom2.size(); ipt++)
 	dom2[ipt] = 0;
 
-      fft::bcc1d (out.size(), &(dom1[0]), &(dom2[0]));
+      fft::bcc1d (out.size(), (float*)&(dom1[0]), (float*)&(dom2[0]));
 
       // this factor may need to be carefully chosen, depending
       // on how the FFT routines operate
       float factor = in.size();
 
       for (ipt=0; ipt < out.size(); ipt++)
-	out[ipt][idim] = dom1[ipt*2]/factor;
+	datum_traits.element (out[ipt], idim) = 
+	  datum_traits.element_traits.cast (dom1[ipt*2]/factor);
 
     } // end for each dimension
 
