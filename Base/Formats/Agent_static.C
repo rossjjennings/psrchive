5a3278537e41a89d2dc53889829dbda2e03a3db2
straten <straten>
2004-12-03 17:28:00 +0000

    Static linking of plugins

---
+++ Base/Formats/Agent_static.C
@@ -0,0 +1,30 @@
+#ifdef HAVE_CONFIG_H
+#include<config.h>
+#endif
+
+#include "Pulsar/Archive.h"
+
+template<class Type>
+Registry::List<Pulsar::Archive::Agent>::Enter<typename Type::Agent>
+Pulsar::Archive::Advocate<Type>::entry;
+
+Registry::List<Pulsar::Archive::Agent> Pulsar::Archive::Agent::registry;
+
+#include "Pulsar/TimerArchive.h"
+#include "Pulsar/BasebandArchive.h"
+
+#ifdef HAVE_CFITSIO
+#include "Pulsar/FITSArchive.h"
+#endif
+
+void Pulsar::Archive::Agent::static_load () {
+
+  Archive::Advocate<TimerArchive>::ensure_linkage();
+  Archive::Advocate<BasebandArchive>::ensure_linkage();
+
+#ifdef HAVE_CFITSIO
+  Archive::Advocate<Pulsar::FITSArchive>::ensure_linkage();
+#endif
+
+}
+
