086e95fe50abbe697f03372ae5722636a2e7ebf4
straten <straten>
2004-12-13 12:27:12 +0000

    Eliminate template instantiation of Advocate::entry

--- Base/Formats/Agent_static.C
+++ Base/Formats/Agent_static.C
@@ -4,29 +4,32 @@
 
 #include "Pulsar/Archive.h"
 
-template<class Type>
-Registry::List<Pulsar::Archive::Agent>::Enter<typename Type::Agent>
-Pulsar::Archive::Advocate<Type>::entry;
-
 Registry::List<Pulsar::Archive::Agent> Pulsar::Archive::Agent::registry;
 
 #include "Pulsar/TimerArchive.h"
 #include "Pulsar/BasebandArchive.h"
 #include "Pulsar/EPNArchive.h"
 
+Registry::List<Pulsar::Archive::Agent>::Enter<Pulsar::TimerArchive::Agent>
+Pulsar::Archive::Advocate<Pulsar::TimerArchive>::entry;
+
+Registry::List<Pulsar::Archive::Agent>::Enter<Pulsar::BasebandArchive::Agent>
+Pulsar::Archive::Advocate<Pulsar::BasebandArchive>::entry;
+
+Registry::List<Pulsar::Archive::Agent>::Enter<Pulsar::EPNArchive::Agent>
+Pulsar::Archive::Advocate<Pulsar::EPNArchive>::entry;
+
 #ifdef HAVE_CFITSIO
+
 #include "Pulsar/FITSArchive.h"
+
+Registry::List<Pulsar::Archive::Agent>::Enter<Pulsar::FITSArchive::Agent>
+Pulsar::Archive::Advocate<Pulsar::FITSArchive>::entry;
+
 #endif
 
-void Pulsar::Archive::Agent::static_load () {
-
-  Archive::Advocate<TimerArchive>::ensure_linkage();
-  Archive::Advocate<BasebandArchive>::ensure_linkage();
-  Archive::Advocate<EPNArchive>::ensure_linkage();
-
-#ifdef HAVE_CFITSIO
-  Archive::Advocate<Pulsar::FITSArchive>::ensure_linkage();
-#endif
+void Pulsar::Archive::Agent::static_load ()
+{
 
 }
 
