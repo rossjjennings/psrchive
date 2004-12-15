94fdbfe6fb4a480bb70b232c78fd6a317968607c
straten <straten>
2004-12-15 14:36:31 +0000

    taylored to an array of Reference::To instances

--- Util/genutil/typeutil.h
+++ Util/genutil/typeutil.h
@@ -1,29 +1,27 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/genutil/typeutil.h,v $
-   $Revision: 1.2 $
-   $Date: 2003/10/08 13:20:08 $
+   $Revision: 1.3 $
+   $Date: 2004/12/15 14:36:31 $
    $Author: straten $ */
 
 #ifndef __typeutil_h
 #define __typeutil_h
 
+#include "Reference.h"
 #include <typeinfo>
 
-//! Find the instance with the specified type_info
-template<typename T>
-unsigned find (const vector<T>& exts, const type_info& info)  {
-  
-  unsigned index = 0;
-  
-  while (index < exts.size())  {
-    if ( typeid(exts[index]) == info )
+template<class T>
+unsigned find (const vector< Reference::To<T> >& array, const T* instance)
+{
+  unsigned index;
+
+  for (index=0; index<array.size(); index++)
+    if ( typeid(*instance) == typeid(*array[index].ptr()) )
       break;
-    index ++;
-  }
-  
+
   return index;
-  
 }
 
 #endif
+
