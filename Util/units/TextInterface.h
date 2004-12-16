98317428cf1db2af0cc3207230dafd8a1a914860
straten <straten>
2004-12-16 09:42:03 +0000

    Full text interface to class implemented

--- Util/units/TextInterface.h
+++ Util/units/TextInterface.h
@@ -1,21 +1,26 @@
 #ifndef __TextInterface_h
 #define __TextInterface_h
 
-#include "ReferenceAble.h"
+#include "Reference.h"
+#include "Error.h"
 #include "tostring.h"
 
 namespace TextInterface {
 
+  //! Abstract base class of attribute text interface
   template<class Class>
-  class Attribute {
+  class Attribute : public Reference::Able {
 
   public:
     
     //! Get the name of the attribute
     virtual std::string get_name () const = 0;
 
+    //! Get the description of the attribute
+    virtual std::string get_description () const = 0;
+
     //! Return true if the name argument matches
-    virtual bool matches (const std::string& name) const { return true; }
+    virtual bool matches (const std::string& name) const;
 
     //! Get the value of the attribute
     virtual std::string get_value (const Class* ptr) const = 0;
@@ -25,17 +30,23 @@ namespace TextInterface {
 
   };
 
-  template<class Class, class Type, class Set, class Get>
-  class AttributeSetGet : public Attribute<Class> {
+  template<class Class, class Type, class Get>
+  class AttributeGet : public Attribute<Class> {
 
   public:
 
     //! Constructor
-    AttributeSetGet (const std::string& _name, Set _set, Get _get)
-      { name = _name; set_method = _set; get_method = _get; }
+    AttributeGet (const std::string& _name, Get _get)
+      { name = _name; get_method = _get; }
 
     //! Get the name of the attribute
     std::string get_name () const { return name; }
+
+    //! Get the description of the attribute
+    std::string get_description () const { return description; }
+
+    //! Get the description of the attribute
+    void set_description (const std::string& d) { description = d; }
 
     //! Get the value of the attribute
     std::string get_value (const Class* ptr) const
@@ -43,18 +54,39 @@ namespace TextInterface {
 
     //! Set the value of the attribute
     void set_value (Class* ptr, const std::string& value)
-      { (ptr->*set_method) (fromstring<Type>(value)); }
+      { throw Error (InvalidState, "AttributeGet::set_value", 
+		     name + " cannot be set"); }
 
   protected:
 
     //! The name of the attribute
     std::string name;
 
-    //! The set method
-    Set set_method;
+    //! The description of the attribute
+    std::string description;
 
     //! The get method
     Get get_method;
+
+  };
+
+  template<class Class, class Type, class Get, class Set>
+  class AttributeGetSet : public AttributeGet<Class, Type, Get> {
+
+  public:
+
+    //! Constructor
+    AttributeGetSet (const std::string& _name, Get _get, Set _set)
+      : AttributeGet<Class,Type,Get> (_name, _get) { set_method = _set; }
+
+    //! Set the value of the attribute
+    void set_value (Class* ptr, const std::string& value)
+      { (ptr->*set_method) (fromstring<Type>(value)); }
+
+  protected:
+
+    //! The set method
+    Set set_method;
 
   };
 
@@ -62,12 +94,87 @@ namespace TextInterface {
   class Allocator {
 
   public:
-    template<class Set, class Get>
-    Attribute<Class>* new_Attribute (const std::string& n, Set s, Get g)
-    { return new AttributeSetGet<Class,Type,Set,Get> (n, s, g); }
+
+    template<class Get>
+      AttributeGet<Class,Type,Get>* 
+      new_attribute (const std::string& n, Get g)
+      { return new AttributeGet<Class,Type,Get> (n, g); }
+
+    template<class Get, class Set>
+    AttributeGetSet<Class,Type,Get,Set>* 
+      new_attribute (const std::string& n, Get g, Set s)
+      { return new AttributeGetSet<Class,Type,Get,Set> (n, g, s); }
 
   };
 
+  class Class : public Reference::Able {
+
+  public:
+
+    //! Get the value of the attribute
+    virtual std::string get_value (const std::string& name) const = 0;
+
+    //! Set the value of the attribute
+    virtual void set_value (const std::string& name, 
+				const std::string& value) = 0;
+
+  };
+
+
+  template<class C>
+  class ClassGetSet : public Class {
+
+  public:
+
+    template<class Type> class Generator : public Allocator<C,Type> { };
+
+    //! Get the value of the attribute
+    virtual std::string get_value (const std::string& name) const
+      { return find(name)->get_value(instance); }
+
+    //! Set the value of the attribute
+    virtual void set_value (const std::string& name, 
+				const std::string& value)
+      { find(name)->set_value(instance, value); }
+
+    //! Return a pointer to the named class attribute interface
+    Attribute<C>* find (const std::string& name) const;
+
+    //! Set the instance
+    virtual void set_instance (C* c) { instance = c; }
+
+    //! Add a new named class attribute interface
+    void add_attribute (Attribute<C>* att) { attributes.push_back (att); }
+
+  protected:
+
+    //! The named class attribute interfaces
+    std::vector< Reference::To< Attribute<C> > > attributes;
+
+    //! The instance of the class with which this interfaces
+    Reference::To<C> instance;
+
+  };
+
+}
+
+template<class C>
+TextInterface::Attribute<C>* 
+TextInterface::ClassGetSet<C>::find (const std::string& name) const
+{ 
+  for (unsigned i=0; i<attributes.size(); i++)
+    if (attributes[i]->matches(name))
+      return attributes[i];
+  throw Error (InvalidParam, "TextInterface::Class<C>::find",
+	       "no attributes named " + name);
+}
+
+#include <string.h>
+
+template<class Class>
+bool TextInterface::Attribute<Class>::matches (const std::string& name) const
+{
+  return strcasecmp(name.c_str(), get_name().c_str()) == 0;
 }
 
 
--- Util/units/test_TextInterface.C
+++ Util/units/test_TextInterface.C
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-class tester {
+class tester : public Reference::Able {
 
 public:
   tester () { value = 0; }
@@ -21,8 +21,8 @@ int main ()
   TextInterface::Allocator<tester,double> allocate;
 
   TextInterface::Attribute<tester>* interface;
-  interface = allocate.new_Attribute ("value",
-                                      &tester::set_value, &tester::get_value);
+  interface = allocate.new_attribute ("value",
+                                      &tester::get_value, &tester::set_value);
 
   interface->set_value (&test, "3.456");
 
@@ -33,7 +33,42 @@ int main ()
     return -1;
   }
 
-  return 0;
+  TextInterface::Attribute<tester>* read_only;
+  read_only = allocate.new_attribute ("value", &tester::get_value);
 
+  cerr << "AttributeGet::get_value=" << read_only->get_value(&test) << endl;
+
+  if (read_only->get_value(&test) != "3.456") {
+    cerr << "test_TextInterface ERROR!" << endl;
+    return -1;
+  }
+
+  try {
+    read_only->set_value (&test, "0.789");
+
+    cerr << "AttributeGet::set_value does not raise exception" << endl;
+    cerr << "test_TextInterface ERROR!" << endl;
+    return -1;
+    
+  }
+  catch (Error& err) {
+    cerr << "AttributeGet::set_value raises exception as expected" << endl;
+  }
+
+  TextInterface::ClassGetSet<tester> getset;
+  getset.set_instance (&test);
+
+  TextInterface::ClassGetSet<tester>::Generator<double> generator;
+  getset.add_attribute (generator.new_attribute("value", &tester::get_value));
+
+  cerr << "ClassGetSet::get_value=" << getset.get_value("value") << endl;
+
+  if (getset.get_value("value") != "3.456") {
+    cerr << "test_TextInterface ERROR!" << endl;
+    return -1;
+  }
+
+  cerr << "test_TextInterface SUCCESS!" << endl;
+  return 0;
 }
                   
