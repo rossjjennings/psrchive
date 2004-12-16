3dd60ba5c3a70760b885df7c219a873d31842afc
straten <straten>
2004-12-16 17:00:54 +0000

    ArchiveTUI is a Composite with import method

--- Base/Classes/Pulsar/ArchiveTI.h
+++ Base/Classes/Pulsar/ArchiveTI.h
@@ -1,21 +1,21 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/Base/Classes/Pulsar/Attic/ArchiveTI.h,v $
-   $Revision: 1.1 $
-   $Date: 2004/12/16 11:42:20 $
+   $Revision: 1.2 $
+   $Date: 2004/12/16 17:00:54 $
    $Author: straten $ */
 
 #ifndef __Pulsar_ArchiveTUI_h
 #define __Pulsar_ArchiveTUI_h
 
-#include "TextInterface.h"
+#include "Pulsar/ExtensionTUI.h"
 
 namespace Pulsar {
 
   class Archive;
 
   //! Smooths a pulse profile using the mean over a boxcar
-  class ArchiveTUI : public TextInterface::ClassGetSet<Archive> {
+  class ArchiveTUI : public TextInterface::CompositeGetSet<Archive> {
 
   public:
 
@@ -25,6 +25,11 @@ namespace Pulsar {
     //! Process a command
     void process (const std::string& command);
 
+    //! Import the TextInterface of an Extension class
+    template<class Ext>
+    void import (const std::string& name,
+		 TextInterface::ClassGetSet<Ext>* extension_tui);
+
   private:
 
     //! Constructor work
@@ -33,5 +38,14 @@ namespace Pulsar {
   };
 
 }
+
+template<class Ext>
+void Pulsar::ArchiveTUI::import (const std::string& name,
+				 TextInterface::ClassGetSet<Ext>* tui)
+{
+  TextInterface::CompositeGetSet<Archive>::import 
+    ( new ExtensionTUI<Archive,Ext>(name, tui) );
+}
+
 
 #endif
