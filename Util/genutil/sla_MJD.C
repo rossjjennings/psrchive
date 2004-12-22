bc04a97f4546b643de624ee2567786201d66d6f2
straten <straten>
2004-12-22 17:29:07 +0000

    revert to version 1.3

--- Util/genutil/sla_MJD.C
+++ Util/genutil/sla_MJD.C
@@ -5,20 +5,13 @@
 #include "MJD.h"
 #include <math.h>
 
-//#include "f772c.h"
-#ifndef F772C_H
-#define F772C_H
-#define F772C(x) x##_
-#endif
-
-extern "C"{
-  double F772C(sla_gmst)(double *);
-}
+#define SLA_gmst SLA_FUNC(sla_gmst,slaGmst)
+extern "C" double SLA_gmst(double *);
 
 double MJD::LST (float longitude) const
 {
   double passed_MJD = this->in_days();
-  double gmst = F772C(sla_gmst)(&passed_MJD);
+  double gmst = SLA_gmst(&passed_MJD);
   double lst = gmst/M_PI*180.0/15.0 + longitude/360.0*24.0;
   while (lst<0.0) lst+=24.0;
   while (lst>=24.0) lst-=24.0;
@@ -28,7 +21,7 @@ double MJD::LST (float longitude) const
 double MJD::LST (double longitude) const
 {
   double passed_MJD = this->in_days();
-  double gmst = F772C(sla_gmst)(&passed_MJD);
+  double gmst = SLA_gmst(&passed_MJD);
   double lst = gmst/M_PI*180.0/15.0 + longitude/360.0*24.0;
   while (lst<0.0) lst+=24.0;
   while (lst>=24.0) lst-=24.0;
