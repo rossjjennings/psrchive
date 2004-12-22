c48a736932248571e2f82a59b46aa113a43d983e
straten <straten>
2004-12-22 17:29:20 +0000

    revert to version 1.2

--- Util/genutil/sla_sky_coord.C
+++ Util/genutil/sla_sky_coord.C
@@ -2,21 +2,13 @@
 #include <config.h>
 #endif
 
-//#include "f772c.h"
-#ifndef F772C_H
-#define F772C_H
-#define F772C(x) x##_
-#endif
-
 #include "sky_coord.h"
 
-extern "C" {
-  double F772C(sla_galeq) (double *, double *, double *, double *);
-}
+#define SLA_galeq SLA_FUNC(sla_galeq,slaGaleq)
+extern "C" double SLA_galeq (double *, double *, double *, double *);
 
-extern "C"{
-  double F772C(sla_galeq) (double *, double *, double *, double *);
-}
+#define SLA_eqgal SLA_FUNC(sla_eqgal,slaEqgal)
+extern "C" double SLA_eqgal(double *, double *, double *, double *);
 
 // redwards -- function to construct from Galactic coordinates
 const sky_coord&
@@ -28,7 +20,7 @@ sky_coord::setGalactic(AnglePair &gal)
   l = gal.angle1.getradians();
   b = gal.angle2.getradians();
 
-  F772C(sla_galeq) (&l, &b, &ra, &dec);
+  SLA_galeq (&l, &b, &ra, &dec);
 
   angle1.setradians(ra);
   angle2.setradians(dec);
@@ -45,7 +37,7 @@ sky_coord::getGalactic() const
   ra = angle1.getradians();
   dec= angle2.getradians();
 
-  F772C(sla_galeq)(&ra, &dec, &l, &b);
+  SLA_eqgal (&ra, &dec, &l, &b);
 
   gal.angle1.setradians(l);
   gal.angle2.setradians(b);
