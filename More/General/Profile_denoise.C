a02024f0016617f67493b74c46578462975cda0a
straten <straten>
2005-01-26 17:25:47 +0000

    use the fft:: namespace interface; these are configured

--- More/General/Profile_denoise.C
+++ More/General/Profile_denoise.C
@@ -1,7 +1,8 @@
-#include <math.h>
 #include "Pulsar/Profile.h"
 #include "Error.h"
-#include "psrfft.h"
+#include "fftm.h"
+
+#include <math.h>
 
 Pulsar::Profile * Pulsar::Profile::denoise (int fraction) {
   Pulsar::Profile * profile;
@@ -19,7 +20,7 @@ Pulsar::Profile * Pulsar::Profile::denoise (int fraction) {
     cmplx_arr[2*i+1] = 0;
   }
 
-  fccfft1d(npts, fft_cmplx_arr, cmplx_arr);
+  fft::fcc1d(npts, fft_cmplx_arr, cmplx_arr);
   
   for(int i=1; i<npts/2; ++i){
     if (i> (float) npts/((float)fraction)) {
@@ -30,7 +31,7 @@ Pulsar::Profile * Pulsar::Profile::denoise (int fraction) {
     fft_cmplx_arr[2*npts-2*i+1] = -fft_cmplx_arr[2*i+1];
   }
 
-  bccfft1d(npts, cmplx_arr, fft_cmplx_arr);
+  fft::bcc1d(npts, cmplx_arr, fft_cmplx_arr);
   for (int i = 0; i<npts ; i++) {
     ampsarr[i] = cmplx_arr[2*i]/(float)npts;
   }
@@ -58,7 +59,7 @@ void Pulsar::Profile::denoise_inplace (int fraction) {
     cmplx_arr[2*i+1] = 0;
   }
 
-  fccfft1d(npts, fft_cmplx_arr, cmplx_arr);
+  fft::fcc1d(npts, fft_cmplx_arr, cmplx_arr);
   
   for(int i=1; i<npts/2; ++i){
     if (i> (float) npts/((float)fraction)) {
@@ -69,7 +70,7 @@ void Pulsar::Profile::denoise_inplace (int fraction) {
     fft_cmplx_arr[2*npts-2*i+1] = -fft_cmplx_arr[2*i+1];
   }
 
-  bccfft1d(npts, cmplx_arr, fft_cmplx_arr);
+  fft::bcc1d(npts, cmplx_arr, fft_cmplx_arr);
   for (int i = 0; i<npts ; i++) {
     ampsarr[i] = cmplx_arr[2*i]/(float)npts;
   }
