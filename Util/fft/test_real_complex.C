858a2d0e428fcff06723eb36b944ac77d28762a2
straten <straten>
2005-01-27 10:03:03 +0000

    test real-to-complex and complex-to-real transforms

---
+++ Util/fft/test_real_complex.C
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "fftm.h"
+#include "spectra.h"
+
+int main (int argc, char** argv)
+{
+  fprintf (stderr, "Test of real-to-complex and complex-to-real ...\n");
+
+  int idat, ndat = 16 * 1024;  // 16kpt set of random, Gaussian noise
+  float* data = new float [ndat];
+  float* fft1 = new float [ndat+2];
+  float* back = new float [ndat];
+
+  long idum = -1;
+  float dc_value = 3.0;
+
+  fprintf (stderr, "Generating %d random numbers\n", ndat);
+  gasdev (&idum);
+  for (idat=0; idat<ndat; idat++)
+    data[idat] = dc_value + gasdev (&idum);
+
+  // ensure that DC and Nyquist are properly set by frc1d
+  fft1[0] = fft1[ndat] = 0.0;   // should be set to other than zero
+  fft1[1] = fft1[ndat+1] = 3.0; // should be set to zero
+
+  fprintf (stderr, "Forward R->C FFT:%d\n", ndat);
+  fft::frc1d (ndat, fft1, data);
+
+  if (fft1[0] == 0.0) {
+    fprintf (stderr, "Re[DC] = 0\n");
+    return -1;
+  }
+  if (fft1[1] != 0.0) {
+    fprintf (stderr, "Im[DC] != 0\n");
+    return -1;
+  }
+
+  if (fft1[ndat] == 0.0) {
+    fprintf (stderr, "Re[Nyquist] = 0\n");
+    return -1;
+  }
+  if (fft1[ndat+1] != 0.0) {
+    fprintf (stderr, "Im[Nyquist] != 0\n");
+    return -1;
+  }
+
+  fprintf (stderr, "DC=%f,%f\n", fft1[0],fft1[1]);
+  fprintf (stderr, "Ny=%f,%f\n", fft1[ndat],fft1[ndat+1]);
+
+  fprintf (stderr, "Backward C->R FFT:%d\n", ndat);
+  fft::bcr1d (ndat, back, fft1);
+
+  for (idat=0; idat<ndat; idat++) {
+    back[idat] /= ndat;
+    float residual = (back[idat]-data[idat])/data[idat];
+    if (fabs(residual) > 2e-4) {
+      fprintf (stderr, "idat=%d data=%f back=%f diff=%g\n", 
+               idat, back[idat], data[idat], residual);
+      return -1;
+    }
+  }
+
+  return 0;
+}
