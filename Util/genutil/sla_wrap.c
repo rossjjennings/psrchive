2e206cd49c1b2f75fa35fb11c887ca2cf1225f10
straten <straten>
2005-01-29 08:29:18 +0000

    minor formatting

--- Util/genutil/sla_wrap.c
+++ Util/genutil/sla_wrap.c
@@ -5,7 +5,7 @@
 */
 
 #ifdef HAVE_CONFIG_H
-#include "config.h"
+#include <config.h>
 #endif
 
 /* *********************************************************************** */
@@ -14,15 +14,15 @@
 void SLA_altaz (double*, double*, double*, double*, double*, double*,
 		double*, double*, double*, double*, double*, double*);
 
-void slaAltaz ( double ha, double dec, double phi,
+void slaAltaz (double ha, double dec, double phi,
                 double *az, double *azd, double *azdd,
                 double *el, double *eld, double *eldd,
-                double *pa, double *pad, double *padd )
+                double *pa, double *pad, double *padd)
 {
   SLA_altaz (&ha, &dec, &phi,
 	     az, azd, azdd,
 	     el, eld, eldd,
-	     pa, pad, padd );
+	     pa, pad, padd);
 }
 
 
@@ -31,7 +31,7 @@ void slaAltaz ( double ha, double dec, double phi,
 #define SLA_dcs2c SLA_FUNC(sla_dcs2c,slaDcs2c)
 void SLA_dcs2c (double* ra, double* dec, double* v);
 
-void slaDcs2c ( double a, double b, double v[3] )
+void slaDcs2c (double a, double b, double v[3])
 {
   SLA_dcs2c (&a, &b, v);
 }
@@ -42,7 +42,7 @@ void slaDcs2c ( double a, double b, double v[3] )
 #define SLA_dmxv SLA_FUNC(sla_dmxv,slaDmxv)
 void SLA_dmxv (double* dm, double* va, double* vb);
 
-void slaDmxv ( double dm[3][3], double va[3], double vb[3] )
+void slaDmxv (double dm[3][3], double va[3], double vb[3])
 {
   SLA_dmxv (&(dm[0][0]), va, vb);
 }
@@ -52,7 +52,7 @@ void slaDmxv ( double dm[3][3], double va[3], double vb[3] )
 #define SLA_dsep SLA_FUNC(sla_dsep,slaDsep)
 double SLA_dsep (double *, double *, double *, double*);
 
-double slaDsep ( double a1, double b1, double a2, double b2 )
+double slaDsep (double a1, double b1, double a2, double b2)
 {
   return SLA_dsep (&a1, &b1, &a2, &b2);
 }
@@ -62,7 +62,7 @@ double slaDsep ( double a1, double b1, double a2, double b2 )
 #define SLA_dtt SLA_FUNC(sla_dtt,slaDtt)
 double SLA_dtt (double* mjd);
 
-double slaDtt ( double dju )
+double slaDtt (double dju)
 {
   return SLA_dtt (&dju);
 }
@@ -72,7 +72,7 @@ double slaDtt ( double dju )
 #define SLA_dvdv SLA_FUNC(sla_dvdv,slaDvdv)
 double SLA_dvdv (double* va, double* vb);
 
-double slaDvdv ( double va[3], double vb[3] )
+double slaDvdv (double va[3], double vb[3])
 {
   return SLA_dvdv (va, vb);
 }
@@ -82,7 +82,7 @@ double slaDvdv ( double va[3], double vb[3] )
 #define SLA_epj SLA_FUNC(sla_epj,slaEpj)
 double SLA_epj (double* mjd);
 
-double slaEpj ( double date )
+double slaEpj (double date)
 {
   return SLA_epj (&date);
 }
@@ -92,7 +92,7 @@ double slaEpj ( double date )
 #define SLA_eqgal SLA_FUNC(sla_eqgal,slaEqgal)
 double SLA_eqgal (double *, double *, double *, double *);
 
-void slaEqgal ( double dr, double dd, double *dl, double *db )
+void slaEqgal (double dr, double dd, double *dl, double *db)
 {
   SLA_eqgal (&dr, &dd, dl, db);
 }
@@ -100,12 +100,13 @@ void slaEqgal ( double dr, double dd, double *dl, double *db )
 /* *********************************************************************** */
 
 #define SLA_evp SLA_FUNC(sla_evp,slaEvp)
-void SLA_evp (double* tdb, double* ep, double* dvb, double* dpb,
-                   double* dvh, double* dph);
+void SLA_evp (double* tdb, double* ep,
+              double* dvb, double* dpb,
+              double* dvh, double* dph);
 
-void slaEvp ( double date, double deqx,
-              double dvb[3], double dpb[3],
-              double dvh[3], double dph[3] )
+void slaEvp (double date, double deqx,
+             double dvb[3], double dpb[3],
+             double dvh[3], double dph[3])
 {
   SLA_evp (&date, &deqx, dvb, dpb, dvh, dph);
 }
@@ -115,7 +116,7 @@ void slaEvp ( double date, double deqx,
 #define SLA_galeq SLA_FUNC(sla_galeq,slaGaleq)
 double SLA_galeq (double *, double *, double *, double *);
 
-void slaGaleq ( double dl, double db, double *dr, double *dd )
+void slaGaleq (double dl, double db, double *dr, double *dd)
 {
   return SLA_galeq (&dl, &db, dr, dd);
 }
@@ -125,7 +126,7 @@ void slaGaleq ( double dl, double db, double *dr, double *dd )
 #define SLA_gmst SLA_FUNC(sla_gmst,slaGmst)
 double SLA_gmst (double* mjd);
 
-double slaGmst ( double ut1 )
+double slaGmst (double ut1)
 {
   return SLA_gmst (&ut1);
 }
@@ -135,7 +136,7 @@ double slaGmst ( double ut1 )
 #define SLA_pa SLA_FUNC(sla_pa,slaPa)
 double SLA_pa (double* HA, double* DEC, double* PHI);
 
-double slaPa ( double ha, double dec, double phi )
+double slaPa (double ha, double dec, double phi)
 {
   return SLA_pa (&ha, &dec, &phi);
 }
@@ -145,7 +146,7 @@ double slaPa ( double ha, double dec, double phi )
 #define SLA_prec SLA_FUNC(sla_prec,slaPrec)
 void SLA_prec (double* ep0, double* ep1, double* rmatp);
 
-void slaPrec ( double ep0, double ep1, double rmatp[3][3] )
+void slaPrec (double ep0, double ep1, double rmatp[3][3])
 {
   SLA_prec (&ep0, &ep1, &(rmatp[0][0]));
 }
