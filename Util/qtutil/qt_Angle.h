27fd18b8059b7bbdf38417d9051a18d0e155e779
sord <sord>
2005-03-03 05:47:42 +0000

    added sethms member function this may not be required

--- Util/qtutil/qt_Angle.h
+++ Util/qtutil/qt_Angle.h
@@ -1,6 +1,6 @@
 /* $Source: /cvsroot/psrchive/psrchive/Util/qtutil/qt_Angle.h,v $
-   $Revision: 1.6 $
-   $Date: 2005/03/02 05:35:42 $
+   $Revision: 1.7 $
+   $Date: 2005/03/03 05:47:42 $
    $Author: sord $ */
 
 #ifndef __QT_Angle_H
@@ -22,8 +22,12 @@ class qt_Angle : public qt_value
 
   void displayHMS ();
   void displayDMS ();
-
+  // Adding these so we can differentiate between RA and DEC in psrParameter
+  // There may be an easier way but I can't find it
   void setWrap (double wrap) {valset.setWrapPoint(wrap);};
+  void sethms (bool _hms) {hms = _hms;};
+  //
+  //
 
  protected:
   bool   hms;
