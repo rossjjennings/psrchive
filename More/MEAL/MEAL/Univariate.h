deff6bef374c24226e3761dd8843ef26128aae0a
straten <straten>
2005-04-06 20:13:58 +0000

    add Univariate function interface through template qualified base class

--- More/MEAL/MEAL/Univariate.h
+++ More/MEAL/MEAL/Univariate.h
@@ -1,47 +1,72 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/More/MEAL/MEAL/Univariate.h,v $
-   $Revision: 1.4 $
-   $Date: 2005/03/20 08:45:44 $
+   $Revision: 1.5 $
+   $Date: 2005/04/06 20:13:58 $
    $Author: straten $ */
 
 #ifndef __MEAL_Univariate_H
 #define __MEAL_Univariate_H
 
-#include "MEAL/UnivariateBase.h"
+#include "MEAL/UnivariatePolicy.h"
+#include "Reference.h"
 
 namespace MEAL {
 
-  //! A function of one scalar variable
-  class Univariate : public UnivariateBase {
+  //! Abstract template base class of univariate Function implementations
+  template<class T>
+  class Univariate : public T {
 
   public:
 
     //! Default constructor
-    Univariate ();
-
-    //! Destructor
-    ~Univariate ();
+    Univariate ()
+    { set_univariate_policy ( new UnivariatePolicy (this) ); }
 
     //! Copy constructor
-    Univariate (const Univariate&);
+    Univariate (const Univariate& copy) : T (copy) { }
 
     //! Assignment operator
-    const Univariate& operator = (const Univariate&);
+    Univariate& operator = (const Univariate& copy);
 
     //! Set the abscissa value
-    void set_abscissa (double value);
+    void set_abscissa (double value)
+    { univariate_policy->set_abscissa(value); }
 
     //! Get the abscissa value
-    double get_abscissa () const;
+    double get_abscissa () const
+    { return univariate_policy->get_abscissa (); }
 
   protected:
 
-    //! The abscissa value
-    double abscissa;
+    void set_univariate_policy (UnivariatePolicy* policy)
+    { this->argument_policy = univariate_policy = policy; }
+
+    UnivariatePolicy* get_univariate_policy () const
+    { return univariate_policy; }
+
+    //! Copy the parameter policy of another instance
+    void copy_univariate_policy (const Univariate* other)
+    { set_univariate_policy (other->get_univariate_policy()); }
+
+  private:
+
+    //! The univariate implementation
+    Reference::To<UnivariatePolicy> univariate_policy;
 
   };
 
 }
 
+template<class T>
+MEAL::Univariate<T>& MEAL::Univariate<T>::operator = (const Univariate& copy)
+{ 
+  if (&copy != this) {
+    set_abscissa( copy.get_abscissa() ); 
+    T::operator=( copy );
+  }
+  return *this;
+}
+
 #endif
+
