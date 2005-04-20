8f42b99dfc80076aa7ae96b3c01e092b7c40eebd
straten <straten>
2005-04-20 07:43:13 +0000

    move StandardModel to Calibration namespace

---
+++ More/Polarimetry/Pulsar/StandardModel.h
@@ -0,0 +1,164 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/psrchive/psrchive/More/Polarimetry/Pulsar/StandardModel.h,v $
+   $Revision: 1.1 $
+   $Date: 2005/04/20 07:43:13 $
+   $Author: straten $ */
+
+#ifndef __Calibration_StandardModel_H
+#define __Calibration_StandardModel_H
+
+// Reception Model and its management
+#include "Calibration/ReceptionModel.h"
+
+#include "Calibration/MeanPolar.h"
+#include "Calibration/MeanSingleAxis.h"
+#include "Calibration/Instrument.h"
+#include "Calibration/Parallactic.h"
+#include "Calibration/ConvertMJD.h"
+
+#include "MEAL/Polar.h"
+#include "MEAL/Axis.h"
+
+namespace Pulsar {
+  class ReceptionCalibrator;
+}
+
+namespace Calibration {
+
+  //! Stores the various elements related to the calibration model
+  class StandardModel : public Reference::Able {
+
+    // friend class Pulsar::ReceptionCalibrator;
+
+  public:
+    
+    //! Default constructor
+    StandardModel (bool britton = true);
+
+    //! Set the transformation from the feed to the receptor basis
+    void set_feed_transformation (MEAL::Complex2* xform);
+
+    //! Set the transformation from the platform to the feed basis
+    void set_platform_transformation (MEAL::Complex2* xform);
+
+    //! Set the order of the differential phase polynomial
+    void set_differential_phase_order (unsigned order);
+
+    //! Update the relevant estimate
+    void update ();
+
+    //! Add a new signal path for the poln calibrator observations
+    void add_polncal_backend ();
+
+    //! Add a new signal path for the flux calibrator observations
+    void add_fluxcal_backend ();
+
+    //! Fix the rotation about the line of sight
+    void fix_orientation ();
+
+    //! Get the index for the signal path experienced by the flux calibrator
+    unsigned get_fluxcal_path () const { return FluxCalibrator_path; }
+
+    //! Get the index for the signal path experienced by the reference source
+    unsigned get_polncal_path () const { return ReferenceCalibrator_path; }
+
+    //! Get the index for the signal path experienced by the pulsar
+    unsigned get_pulsar_path () const { return Pulsar_path; }
+
+    //! Integrate a calibrator solution
+    void integrate_calibrator (const MEAL::Complex2* xform, bool fluxcal);
+
+    //! Get the measurement equation solver
+    Calibration::ReceptionModel* get_equation ();
+
+    //! Get the signal path experienced by the pulsar
+    const MEAL::Complex2* get_transformation () const;
+
+    //! Get the signal path experienced by the pulsar
+    MEAL::Complex2* get_transformation ();
+
+    //! The parallactic angle rotation
+    Calibration::Parallactic parallactic;
+
+    //! The time axis
+    MEAL::Axis<MJD> time;
+
+    //! Used to convert MJD to double
+    Calibration::ConvertMJD convert;
+
+    //! validity flag
+    bool valid;
+
+ protected:
+
+    //! ReceptionModel
+    Reference::To< Calibration::ReceptionModel > equation;
+
+    //! The signal path experienced by the calibrator
+    Reference::To< MEAL::ProductRule<MEAL::Complex2> > pcal_path;
+
+    //! The signal path experienced by the pulsar
+    Reference::To< MEAL::ProductRule<MEAL::Complex2> > pulsar_path;
+
+    //! The instrumental model and any additional transformations
+    Reference::To< MEAL::ProductRule<MEAL::Complex2> > instrument;
+
+    // ////////////////////////////////////////////////////////////////////
+    //
+    //! Polar decomposition of instrumental response (Hamaker)
+    Reference::To< MEAL::Polar > polar;
+
+    //! The best estimate of the polar model
+    Calibration::MeanPolar polar_estimate;
+
+    // ////////////////////////////////////////////////////////////////////
+    //
+    //! Phenomenological decomposition of instrumental response (Britton)
+    Reference::To< Calibration::Instrument > physical;
+
+    //! The best estimate of the physical model
+    Calibration::MeanSingleAxis physical_estimate;
+
+    // ////////////////////////////////////////////////////////////////////
+    //
+    //! Additional backend required for flux calibrator signal path
+    Reference::To< Calibration::SingleAxis > fluxcal_backend;
+
+    //! The best estimate of the flux calibration backend
+    Calibration::MeanSingleAxis fluxcal_backend_estimate;
+
+    //! The signal path of the FluxCalibrator source
+    unsigned FluxCalibrator_path;
+
+    //! The signal path of the ReferenceCalibrator source
+    unsigned ReferenceCalibrator_path;
+
+    //! The signal path of the Pulsar phase bin sources
+    unsigned Pulsar_path;
+
+    //! The feed transformation
+    Reference::To<MEAL::Complex2> feed_transformation;
+
+    //! The platform transformation
+    Reference::To<MEAL::Complex2> platform_transformation;
+    
+    //! The order of the polynomial for differential phase
+    unsigned differential_phase_order;
+
+  private:
+
+    //! built flag
+    bool built;
+
+    //! build method
+    void build ();
+
+    //! build called from const method
+    void const_build () const;
+
+  };
+  
+}
+
+#endif
---
+++ More/Polarimetry/StandardModel.C
@@ -0,0 +1,244 @@
+#include "Calibration/StandardModel.h"
+#include "Calibration/SingleAxis.h"
+#include "Calibration/Feed.h"
+
+#include "MEAL/Polynomial.h"
+
+#include <iostream>
+using namespace std;
+
+/*! 
+  \param britton if true, use the Britton (2000) decomposition of the receiver
+*/
+Calibration::StandardModel::StandardModel (bool britton)
+{
+  // ////////////////////////////////////////////////////////////////////
+  //
+  // initialize the model of the instrument
+  //
+
+  if (britton) {
+    if (MEAL::Function::verbose)
+      cerr << "Calibration::StandardModel Britton" << endl;
+    physical = new Calibration::Instrument;
+  }
+  else {
+    if (MEAL::Function::verbose)
+      cerr << "Calibration::StandardModel Hamaker" << endl;
+    polar = new MEAL::Polar;
+  }
+
+  differential_phase_order = 0;
+  valid = true;
+  built = false;
+}
+
+void 
+Calibration::StandardModel::set_feed_transformation (MEAL::Complex2* x)
+{
+  feed_transformation = x;
+}
+
+void
+Calibration::StandardModel::set_platform_transformation (MEAL::Complex2* x)
+{
+  platform_transformation = x;
+}
+
+void
+Calibration::StandardModel::set_differential_phase_order (unsigned order)
+{
+  differential_phase_order = order;
+}
+
+//! Get the measurement equation solver
+Calibration::ReceptionModel* Calibration::StandardModel::get_equation ()
+{
+  if (!built)
+    build ();
+
+  return equation;
+}
+
+//! Get the signal path experienced by the pulsar
+const MEAL::Complex2*
+Calibration::StandardModel::get_transformation () const
+{
+  if (!built)
+    const_build ();
+
+  if (polar)
+    return polar;
+
+  if (physical)
+    return physical;
+
+  return 0;
+}
+
+MEAL::Complex2*
+Calibration::StandardModel::get_transformation ()
+{
+  const StandardModel* thiz = this;
+  return const_cast<MEAL::Complex2*>( thiz->get_transformation() );
+}
+
+void Calibration::StandardModel::const_build () const
+{
+  const_cast<StandardModel*>(this)->build();
+}
+
+void Calibration::StandardModel::build ()
+{
+  if (built)
+    return;
+
+  instrument = new MEAL::ProductRule<MEAL::Complex2>;
+
+  if (differential_phase_order) {
+
+    MEAL::Complex2* operation;
+    operation = new MEAL::Rotation(Vector<double, 3>::basis(0));
+
+    MEAL::Polynomial* poly;
+    poly = new MEAL::Polynomial (differential_phase_order+1);
+
+    poly -> set_infit (0, false);
+    poly -> set_argument (0, &convert);
+
+    MEAL::ChainRule<MEAL::Complex2>* backend;
+    backend = new MEAL::ChainRule<MEAL::Complex2>;
+
+    backend -> set_model (operation);
+    backend -> set_constraint (0, poly);
+
+    *instrument *= backend;
+    time.signal.connect (&convert, &Calibration::ConvertMJD::set_epoch);
+
+  }
+
+  if (polar)
+    *instrument *= polar;
+
+  if (physical)
+    *instrument *= physical;
+
+  if (feed_transformation)
+    *instrument *= feed_transformation;
+
+  equation = new Calibration::ReceptionModel;
+
+  ReferenceCalibrator_path = 0;
+  FluxCalibrator_path = 0;
+
+  // ////////////////////////////////////////////////////////////////////
+  //
+  // initialize the signal path seen by the pulsar
+  //
+
+  pulsar_path = new MEAL::ProductRule<MEAL::Complex2>;
+  *pulsar_path *= instrument;
+
+  if (platform_transformation)
+    *pulsar_path *= platform_transformation;
+
+  *pulsar_path *= &parallactic;
+
+  equation->add_transformation ( pulsar_path );
+  Pulsar_path = equation->get_transformation_index ();
+
+  time.signal.connect (&parallactic, &Calibration::Parallactic::set_epoch);
+
+  built = true;
+}
+
+void Calibration::StandardModel::add_fluxcal_backend ()
+{
+  if (!built)
+    build ();
+
+  if (!physical)
+    throw Error (InvalidState,
+		 "Calibration::StandardModel::add_fluxcal_backend",
+		 "Cannot model flux calibrator with Hamaker model");
+
+  MEAL::ProductRule<MEAL::Complex2>* path = 0;
+  path = new MEAL::ProductRule<MEAL::Complex2>;
+
+  fluxcal_backend = new Calibration::SingleAxis;
+
+  *path *= fluxcal_backend;
+  *path *= physical->get_feed();
+
+  if (feed_transformation)
+    *path *= feed_transformation;
+
+  equation->add_transformation ( path );
+  FluxCalibrator_path = equation->get_transformation_index ();
+}
+
+void Calibration::StandardModel::add_polncal_backend ()
+{
+  if (!built)
+    build ();
+
+  pcal_path = new MEAL::ProductRule<MEAL::Complex2>;
+  *pcal_path *= instrument;
+
+  equation->add_transformation ( pcal_path );
+  ReferenceCalibrator_path = equation->get_transformation_index ();
+}
+
+void Calibration::StandardModel::fix_orientation ()
+{
+  if (physical)
+    // set the orientation of the first receptor
+    physical->set_infit (4, false);
+
+  if (polar)
+    // set the orientation of the last rotation
+    polar->set_infit (6, false);
+}
+
+void Calibration::StandardModel::update ()
+{
+  if (polar)
+    polar_estimate.update (polar);
+  if (physical)
+    physical_estimate.update (physical->get_backend());
+  if (fluxcal_backend)
+    fluxcal_backend_estimate.update (fluxcal_backend);
+}
+
+void 
+Calibration::StandardModel::integrate_calibrator (const MEAL::Complex2* xform,
+						  bool flux_calibrator)
+{
+  if (polar) {
+
+    const MEAL::Polar* polar_solution;
+
+    polar_solution = dynamic_cast<const MEAL::Polar*>( xform );
+
+    if (polar_solution)
+      polar_estimate.integrate( polar_solution );
+
+  }
+
+
+  if (physical) {
+
+    const Calibration::SingleAxis* sa;
+
+    sa = dynamic_cast<const Calibration::SingleAxis*>( xform );
+
+    if (sa) {
+      if (flux_calibrator)
+	fluxcal_backend_estimate.integrate( sa );
+      else
+	physical_estimate.integrate( sa );
+    }
+
+  }
+
+}
