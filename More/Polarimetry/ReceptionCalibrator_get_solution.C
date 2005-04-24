3bfaf77c9d1614ee4a1cf0b3e63586649b10901b
straten <straten>
2005-04-24 01:25:43 +0000

    additional error checking

--- More/Polarimetry/ReceptionCalibrator_get_solution.C
+++ More/Polarimetry/ReceptionCalibrator_get_solution.C
@@ -2,47 +2,46 @@
 #include "Pulsar/CalibratorStokes.h"
 
 Pulsar::CalibratorStokes*
-Pulsar::ReceptionCalibrator::get_calibrator_stokes () const
+Pulsar::ReceptionCalibrator::get_CalibratorStokes () const
 {
   if (calibrator_stokes)
     return calibrator_stokes;
 
-  if (verbose) cerr << "Pulsar::ReceptionCalibrator::get_calibrator_stokes"
+  if (verbose) cerr << "Pulsar::ReceptionCalibrator::get_CalibratorStokes"
 		 " create CalibratorStokes Extension" << endl;
 
   unsigned nchan = get_transformation_nchan();
 
   if (nchan != calibrator_estimate.source.size())
-    throw Error (InvalidState, "Pulsar::ReceptionCalibrator::get_calibrator_stokes",
+    throw Error (InvalidState,
+		 "Pulsar::ReceptionCalibrator::get_CalibratorStokes",
 		 "Calibrator Stokes nchan=%d != Transformation nchan=%d",
 		 calibrator_estimate.source.size(), nchan);
 
-  try {
-
-    Reference::To<CalibratorStokes> ext = new CalibratorStokes;
+  Reference::To<CalibratorStokes> ext = new CalibratorStokes;
     
-    ext->set_nchan (nchan);
+  ext->set_nchan (nchan);
     
-    for (unsigned ichan=0; ichan < nchan; ichan++) {
+  for (unsigned ichan=0; ichan < nchan; ichan++) try {
       
-      bool valid = get_transformation_valid(ichan);
+    bool valid = get_transformation_valid(ichan);
       
-      ext->set_valid (ichan, valid);
-      if (!valid)
-	continue;
-
-      ext->set_stokes (ichan, calibrator_estimate.source[ichan].get_stokes());
-            
-    }
-
-    const_cast<ReceptionCalibrator*>(this)->calibrator_stokes = ext;
-
-    return calibrator_stokes;
-
+    ext->set_valid (ichan, valid);
+    if (!valid)
+      continue;
+    
+    ext->set_stokes (ichan, calibrator_estimate.source[ichan].get_stokes());
+    
   }
   catch (Error& error) {
-    throw error += "Pulsar::ReceptionCalibrator::get_calibrator_stokes";
+    cerr << "Pulsar::ReceptionCalibrator::get_CalibratorStokes ichan="
+	 << ichan << " error\n" << error.get_message() << endl;
+    ext->set_valid (ichan, false);
   }
+
+  const_cast<ReceptionCalibrator*>(this)->calibrator_stokes = ext;
+  
+  return calibrator_stokes;
 
 }
 
