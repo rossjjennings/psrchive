1d7e66afcdcd6f414e71ca8d032fd0fae76e200d
straten <straten>
2005-04-29 12:58:29 +0000

    refer to pdf doco

--- More/MEAL/MEAL/Function.h
+++ More/MEAL/MEAL/Function.h
@@ -1,8 +1,8 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/More/MEAL/MEAL/Function.h,v $
-   $Revision: 1.3 $
-   $Date: 2005/04/06 20:02:45 $
+   $Revision: 1.4 $
+   $Date: 2005/04/29 12:58:29 $
    $Author: straten $ */
 
 /*! \mainpage 
@@ -15,65 +15,8 @@
   construction of elementary functions, which may be parameterized by
   any number or type of arguments.
 
-  \section model Function Components
-
-  All model components that inherit the MEAL::Function abstract
-  base class represent functions of an arbitrary number of variables.
-  A distinction is made between independent variables, or arguments,
-  \f${\bf x}=(x_0, x_1, ... , x_M)\f$, and model parameters, \f${\bf
-  a}=(a_0, a_1, ... , a_N)\f$.  Through use of the
-  MEAL::Argument and MEAL::Argument::Value abstract base
-  classes, model components may be constrained by one or more
-  independent variables of arbitrary type.  The model parameters,
-  \f${\bf a}\f$, represent double precision floating point values
-  that may need to be constrained by some fitting technique.  Function
-  classes should define an evaluation function that returns a result,
-  \f$M\f$, and the partial derivative of this result with respect to
-  each of the model parameters, \f$\partial M/\partial a_i\f$.  The
-  independent variables,\f$\bf x\f$, represent known values, such as
-  observing frequency and epoch, that may be used to further constrain
-  a model.
-
-  The MEAL::Function class does not define the type of value that
-  it represents.  This is defined by derived types, which must define
-  a type named Result and a method named evaluate:
-
-  virtual Result evaluate (std::vector<Result>* gradient = 0) const = 0;
-
-  The evaluate method returns a value of the type specified by Result
-  and, if a pointer to a vector of Result is passed as the first
-  argument, the vector will return the gradient of the return value
-  with respect to the model parameters.
-
-  The Return type and evaluate method are implemented by two main
-  classes of MEAL::Function derived components:
-
-  <UL> 
-  <LI> MEAL::Scalar - a scalar function, 
-  \f$f({\bf a}; {\bf x})\f$, such as the MEAL::Polynomial
-  <LI> MEAL::Complex2 - a complex 2x2 matrix function,
-  \f$J({\bf a}; {\bf x})\f$, such as the MEAL::Coherency matrix 
-  and the MEAL::Rotation transformation.
-  </UL>
-
-
-  \subsection calculus Partial Derivatives
-
-  A number of template classes may be used to simplify the modular
-  construction of more complicated functions.  These templates
-  implement the following basic rules of differentiation:
-
-  <UL> 
-
-  <LI> MEAL::ChainRule - an arbitrary function in which
-  one or more parameters is set equal to the ordinate of a 
-  MEAL::Scalar function
-
-  <LI> MEAL::BinaryRule - an associative binary operation, such
-  as the sum (MEAL::SumRule) or product
-  (MEAL::ProductRule).
-
-  </UL>
+  For more details, please read the
+  <a href="MEAL.pdf">Introduction to M.E.A.L.</a>
 
 */
 
