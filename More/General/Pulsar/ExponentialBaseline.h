c357fe0b942bc0ccf561d611b8af52b5104c0a24
straten <straten>
2005-09-04 18:20:04 +0000

    implement constructor

--- More/General/Pulsar/ExponentialBaseline.h
+++ More/General/Pulsar/ExponentialBaseline.h
@@ -1,8 +1,8 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/ExponentialBaseline.h,v $
-   $Revision: 1.1 $
-   $Date: 2005/08/16 23:52:06 $
+   $Revision: 1.2 $
+   $Date: 2005/09/04 18:20:04 $
    $Author: straten $ */
 
 #ifndef __Pulsar_ExponentialBaseline_h
@@ -14,6 +14,11 @@ namespace Pulsar {
 
   //! Finds a baseline that contains gaussian white noise
   class ExponentialBaseline : public IterativeBaseline {
+
+  public:
+
+    //! Default constructor
+    ExponentialBaseline ();
 
   protected:
 
