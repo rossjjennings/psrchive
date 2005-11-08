2b547d1e454b58e895f0b6c2d4a4d99859a41e94
rmanches <rmanches>
2005-11-08 05:25:52 +0000

    Changed < to > in check of mod time of duplicate entries

--- More/Polarimetry/Database.C
+++ More/Polarimetry/Database.C
@@ -610,7 +610,7 @@ void Pulsar::Database::add (Pulsar::Database::Entry& entry)
     if (entries[ie] == entry) {
       cerr << "Pulsar::Database::add keeping newest of duplicate entries:\n\t"
            << entries[ie].filename << " and\n\t" << entry.filename << endl;
-      if ( file_mod_time (get_filename(entry).c_str()) <
+      if ( file_mod_time (get_filename(entry).c_str()) >
 	   file_mod_time (get_filename(entries[ie]).c_str()) )
 	entries[ie] = entry;
       return;
