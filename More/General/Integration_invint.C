6acb7003962d185ed1e630238202a846e043cc03
straten <straten>
2005-11-16 00:43:20 +0000

    use PolnProfile::invint

--- More/General/Integration_invint.C
+++ More/General/Integration_invint.C
@@ -1,51 +1,20 @@
 #include "Pulsar/Integration.h"
+#include "Pulsar/PolnProfile.h"
 #include "Pulsar/Profile.h"
-#include "Stokes.h"
 
-/////////////////////////////////////////////////////////////////////////////
-//
-// Pulsar::Integration::invint
-//
 /*! 
-  Forms the Stokes polarimetric invariant interval,
-  \f$\det{P}=I^2-Q^2-U^2-V^2\f$, for every bin of each chan so that,
-  upon completion, npol == 1 and state == Signal::Invariant.
-
-  If invint_square is true, this function calculates
-  \f$\det\rho=I^2-Q^2-U^2-V^2\f$, otherwise \f$\sqrt{\det\rho}\f$ is
-  calcuated.
-
   \pre The profile baselines must have been removed (unchecked).
-
-  \exception string thrown if Stokes 4-vector cannot be formed
 */
 void Pulsar::Integration::invint ()
 {
-  // space to calculate the result
-  vector<float> invariant (get_nbin());
-  // Stokes 4-vector
-  Stokes<float> stokes;
+  Reference::To<PolnProfile> profile;
 
   for (unsigned ichan=0; ichan<get_nchan(); ++ichan) {
 
-    for (unsigned ibin=0; ibin<get_nbin(); ++ibin) {
-      // get the Stokes 4-vector
-      stokes = get_Stokes (ichan, ibin);
-      // calculate \det\rho
-      invariant[ibin] = det (stokes);
-    }
+    profile = new_PolnProfile (ichan);
 
-    // prepare to reset Stokes I to the Invariant Interval
-    Profile* Sinv = get_Profile (0, ichan);
+    profile -> invint ( profile->get_Profile(0) );
 
-    // set the values
-    Sinv->set_amps (invariant);
-    // remove the baseline
-    *(Sinv) -= Sinv->mean( Sinv->find_min_phase() );
-
-    Sinv->set_state (Signal::Inv);
-    Sinv->square_root();
-    
   } // for each channel
 
   resize (1);
