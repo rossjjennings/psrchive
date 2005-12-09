cf56b83f6bc76325cdbfce5cf9633911ef6b6bfa
straten <straten>
2005-12-09 05:40:08 +0000

    basic features implemented; outputs PAs to stdout

--- More/Polarimetry/DeltaRM.C
+++ More/Polarimetry/DeltaRM.C
@@ -1,7 +1,10 @@
 #include "Pulsar/DeltaRM.h"
 
 #include "Pulsar/Archive.h"
-#include "Pulsar/Profile.h"
+#include "Pulsar/Integration.h"
+#include "Pulsar/PolnProfile.h"
+
+#include "Pulsar/FrequencyIntegrate.h"
 
 #include <iostream>
 using namespace std;
@@ -22,9 +25,69 @@ void Pulsar::DeltaRM::set_data (Archive* archive)
   data = archive;
 }
 
+void get_PA (Pulsar::Integration *data, unsigned ichan,
+	     vector<double>& phases, vector< Estimate<double> >& PAs,
+	     float threshold);
+
 //! Refine the rotation measure estimate
 void Pulsar::DeltaRM::refine ()
 {
   if (!data)
     throw Error (InvalidState, "Pulsar::DeltaRM::refine", "no data");
+
+  Reference::To<Integration> clone = data->get_Integration(0)->clone();
+
+  FrequencyIntegrate fscr;
+  fscr.set_nchan (2);
+  fscr.set_range_policy (new FrequencyIntegrate::EvenlyDistributed);
+  fscr.transform (clone);
+
+  if (clone->get_nchan() != 2)
+    throw Error (InvalidState, "Pulsar::DeltaRM::refine",
+		 "nchan != 2 after FrequencyIntegrate");
+
+  Reference::To<PolnProfile> profile;
+
+  vector< Estimate<double> > posang0;
+  profile = clone->new_PolnProfile(0);
+  profile->get_PA (posang0, 3.0);
+
+  vector< Estimate<double> > posang1;
+  profile = clone->new_PolnProfile(1);
+  profile->get_PA (posang1, 3.0);
+
+  MeanRadian<double> mean_delta_PA;
+
+  for (unsigned ibin=0; ibin < profile->get_nbin(); ibin++)
+    if (posang0[ibin].get_variance() && posang1[ibin].get_variance()) {
+
+      cout << ibin << " " << posang0[ibin].get_value()
+	   << " " << posang0[ibin].get_error ()
+	   << " " << posang1[ibin].get_value ()
+	   << " " << posang1[ibin].get_error () << endl;
+
+      mean_delta_PA += M_PI/90.0 * (posang1[ibin] - posang0[ibin]);
+
+    }
+
+  double f0 = clone->get_Profile(0,0)->get_centre_frequency();
+  double f1 = clone->get_Profile(0,1)->get_centre_frequency();
+
+  // speed of light in m/s
+  double speed_of_light = 299792458;
+
+  double lambda_0 = speed_of_light / (f0 * 1e6);
+  double lambda_1 = speed_of_light / (f1 * 1e6);
+
+  cerr << "frequency_0 = " << f0 << " MHz"
+    "  ->  lambda_0 = " << lambda_0 << " m" << endl
+       << "frequency_1 = " << f1 << " MHz"
+    "  ->  lambda_1 = " << lambda_1 << " m" << endl;
+
+  Estimate<double> delta_PA = mean_delta_PA.get_Estimate() / 2.0;
+  Estimate<double> RM = delta_PA/(lambda_1*lambda_1-lambda_0*lambda_0);
+
+  cerr << "delta PA = <PA_1 - PA_0> = " << 180.0/M_PI * delta_PA << " deg.\n"
+       << "RM = " << RM << endl;
+
 }
