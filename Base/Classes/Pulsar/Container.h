11d6757e38cf99756ccd050136c74e09b20c4111
straten <straten>
2005-12-09 02:57:58 +0000

    adding structure to the class hierarchy

---
+++ Base/Classes/Pulsar/Container.h
@@ -0,0 +1,22 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/psrchive/psrchive/Base/Classes/Pulsar/Container.h,v $
+   $Revision: 1.1 $
+   $Date: 2005/12/09 02:57:58 $
+   $Author: straten $ */
+
+#ifndef __Pulsar_Container_h
+#define __Pulsar_Container_h
+
+#include "Reference.h"
+
+namespace Pulsar {
+
+  //! Template base class of algorithm implementations
+  class Container : public Reference::Able {
+
+  };
+
+}
+
+#endif
--- More/General/Pulsar/Algorithm.h
+++ More/General/Pulsar/Algorithm.h
@@ -1,17 +1,21 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/Algorithm.h,v $
-   $Revision: 1.1 $
-   $Date: 2005/12/08 11:10:04 $
+   $Revision: 1.2 $
+   $Date: 2005/12/09 02:57:58 $
    $Author: straten $ */
 
 #ifndef __Pulsar_Algorithm_h
 #define __Pulsar_Algorithm_h
 
+#include "Reference.h"
+
 namespace Pulsar {
 
   //! Template base class of algorithm implementations
-  template<class Container> class Algorithm {};
+  class Algorithm : public Reference::Able {
+
+  };
 
 }
 
