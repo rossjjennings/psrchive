036832384459727635ec2b47d971d125a13bedf8
straten <straten>
2005-12-12 16:17:38 +0000

    some test values

--- More/Polarimetry/iri2001/test_iri.c
+++ More/Polarimetry/iri2001/test_iri.c
@@ -8,14 +8,14 @@ void F77_FUNC(iri2rm,IRI2RM) (double* Glong, double* Glati,
 
 int main ()
 {
-  double Glong;
-  double Glati;
-  double year;
-  int mmdd;
-  double ut;
-  double az;
-  double el;
-  double RM;
+  double Glong = 110;
+  double Glati = -33;
+  double year = 1995;
+  int mmdd = 1213;
+  double ut = 7.34;
+  double az = 34;
+  double el = 34;
+  double RM = 0;
   
   F77_FUNC(iri2rm,IRI2RM) (&Glong, &Glati, 
 			   &year, &mmdd, &ut, 
