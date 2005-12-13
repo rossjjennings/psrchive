86d621939ed298b52f40197993c00eefc88888b7
straten <straten>
2005-12-13 07:22:10 +0000

    PhaseWeightFunction classes that incorporate a Profile

---
+++ More/General/Pulsar/ProfileWeightFunction.h
@@ -0,0 +1,43 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/ProfileWeightFunction.h,v $
+   $Revision: 1.1 $
+   $Date: 2005/12/13 07:22:10 $
+   $Author: straten $ */
+
+#ifndef __Pulsar_ProfileWeightFunction_h
+#define __Pulsar_ProfileWeightFunction_h
+
+#include "Pulsar/PhaseWeightFunction.h"
+
+namespace Pulsar {
+
+  class Profile;
+
+  //! PhaseWeight algorithms that compute profile baselines
+  /*! This pure virtual base class defines the interface through which
+      various baseline finding algorithms are accessed */
+  class ProfileWeightFunction : public PhaseWeightFunction {
+
+  public:
+
+    //! Set the Profile from which the PhaseWeight will be derived
+    virtual void set_Profile (const Profile*);
+
+    //! Returns a PhaseWeight with the Profile attribute set
+    virtual void get_weight (PhaseWeight& weight);
+
+  protected:
+
+    //! The Profile from which the PhaseWeight will be derived
+    Reference::To<const Profile> profile;
+
+    //! Derived classes implement the PhaseWeight calculation
+    virtual void calculate (PhaseWeight& weight) = 0;
+
+  };
+
+}
+
+
+#endif // !defined __Pulsar_BaselineWeightFunction_h
