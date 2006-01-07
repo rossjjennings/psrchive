75ce0ef2bfda7d70ed75c9693af4b75396a8fc6c
straten <straten>
2006-01-07 00:05:07 +0000

    added ASCIIArchive

--- Base/Formats/Agent_static.C
+++ Base/Formats/Agent_static.C
@@ -11,16 +11,18 @@ Registry::List<Pulsar::Archive::Agent>::Enter<typename Type::Agent>
 Pulsar::Archive::Advocate<Type>::entry;
 
 #include "Pulsar/TimerArchive.h"
-#include "Pulsar/BasebandArchive.h"
-#include "Pulsar/EPNArchive.h"
 
 template
 Registry::List<Pulsar::Archive::Agent>::Enter<Pulsar::TimerArchive::Agent>
 Pulsar::Archive::Advocate<Pulsar::TimerArchive>::entry;
 
+#include "Pulsar/BasebandArchive.h"
+
 template
 Registry::List<Pulsar::Archive::Agent>::Enter<Pulsar::BasebandArchive::Agent>
 Pulsar::Archive::Advocate<Pulsar::BasebandArchive>::entry;
+
+#include "Pulsar/EPNArchive.h"
 
 template
 Registry::List<Pulsar::Archive::Agent>::Enter<Pulsar::EPNArchive::Agent>
@@ -35,6 +37,12 @@ Registry::List<Pulsar::Archive::Agent>::Enter<Pulsar::FITSArchive::Agent>
 Pulsar::Archive::Advocate<Pulsar::FITSArchive>::entry;
 
 #endif
+
+#include "Pulsar/ASCIIArchive.h"
+
+template
+Registry::List<Pulsar::Archive::Agent>::Enter<Pulsar::ASCIIArchive::Agent>
+Pulsar::Archive::Advocate<Pulsar::ASCIIArchive>::entry;
 
 void Pulsar::Archive::Agent::static_load ()
 {
