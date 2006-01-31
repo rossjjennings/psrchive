7424d33f9eef2bbdd033094f3c21af87c4af9895
straten <straten>
2006-01-31 05:23:31 +0000

    test Rotation class in both linear and circular bases

--- More/MEAL/test_Rotation.C
+++ More/MEAL/test_Rotation.C
@@ -6,7 +6,7 @@ using namespace std;
 
 static char* name = "QUV";
 
-void test_Rotation (double theta, double phi, unsigned axis)
+void test_Rotation (double theta, double phi, unsigned axis, unsigned perm=0)
 {
   MEAL::Rotation rotation;
 
@@ -16,9 +16,9 @@ void test_Rotation (double theta, double phi, unsigned axis)
   Jones<double> xform = rotation.evaluate();
 
   Stokes<double> stokes (1.5);
-  unsigned i = (axis+1)%3;
-  unsigned j = (axis+2)%3;
-  unsigned k = axis;
+  unsigned i = (axis+perm+1)%3;
+  unsigned j = (axis+perm+2)%3;
+  unsigned k = (axis+perm)%3;
 
   stokes[i+1] = cos(theta);
   stokes[j+1] = sin(theta);
@@ -31,23 +31,40 @@ void test_Rotation (double theta, double phi, unsigned axis)
   if (stokes[0] != 1.5)
     cerr << "I=" << stokes[0] << " != " << 1.5 << endl;
 
-  if (stokes[i+1] != cos(result))
+  if (fabs (stokes[i+1] - cos(result)) > 1e-12) {
     cerr << name[i] << "=" << stokes[i+1] << " != " << cos(result) << endl;
+    exit(-1);
+  }
 
-  if (stokes[j+1] != sin(result))
+  if (fabs (stokes[j+1] - sin(result)) > 1e-12) {
     cerr << name[j] << "=" << stokes[j+1] << " != " << sin(result) << endl;
+      exit(-1);
+  }
 
-  if (stokes[k+1] != 0.2)
+  if (fabs (stokes[k+1] - 0.2) > 1e-12) {
     cerr << name[k] << "=" << stokes[k+1] << " != " << 0.2 << endl;
+    exit(-1);
+  }
 
 }
 
 int main ()
 {
+  cerr << "Testing in linear basis" << endl;
   for (unsigned i=0; i < 3; i++) {
     cerr << "Testing rotations about " << name[i] << " axis" << endl;
     test_Rotation (1.43/2, -2.15/2, i);
   }
+
+  cerr << "Testing in circular basis" << endl;
+  Pauli::basis.set_basis (Basis<double>::Circular);
+
+  for (unsigned i=0; i < 3; i++) {
+    cerr << "Testing rotations about " << name[(i+2)%3] << " axis" << endl;
+    test_Rotation (-.73/2, 3.1/2, i, 2);
+  }
+
+  cerr << "All tests passed" << endl;
   return 0;
 }
 
