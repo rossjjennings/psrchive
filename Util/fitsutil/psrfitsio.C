83357047a5366904cf68134373617775d057233e
straten <straten>
2006-02-27 16:39:49 +0000

    new template methods simplify fitsio

---
+++ Util/fitsutil/psrfitsio.C
@@ -0,0 +1,34 @@
+#include "psrfitsio.h"
+
+using namespace std;
+
+//! Specialization for string
+void psrfits_update_key (fitsfile* fptr, const char* name, const string& txt)
+{
+  // no comment
+  char* comment = 0;
+  // status
+  int status = 0;
+  
+  fits_update_key (fptr, TSTRING, const_cast<char*>(name), 
+		   const_cast<char*>(txt.c_str()),
+		   comment, &status);
+  
+  if (status)
+    throw FITSError (status, "psrfits_update_key", name);
+}
+
+void psrfits_read_key_work (fitsfile* fptr, const char* name, string* data,
+			    int* status)
+{
+  // no comment
+  char* comment = 0;
+
+  char temp [FLEN_VALUE];
+
+  fits_read_key (fptr, TSTRING, const_cast<char*>(name), temp, 
+		 comment, status);
+
+  if (*status == 0)
+    *data = temp;
+}
---
+++ Util/fitsutil/psrfitsio.h
@@ -0,0 +1,105 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/psrchive/psrchive/Util/fitsutil/psrfitsio.h,v $
+   $Revision: 1.1 $
+   $Date: 2006/02/27 16:39:49 $
+   $Author: straten $ */
+
+#ifndef __psrfitsio_h
+#define __psrfitsio_h
+
+#include "FITSError.h"
+#include <fitsio.h>
+#include <string>
+
+//! Empty template class requires specialization 
+template<typename T> struct FITS_traits { };
+  
+//! Template specialization for double
+template<> struct FITS_traits<double> {
+  static inline int datatype() { return TDOUBLE; }
+};
+
+//! Template specialization for float
+template<> struct FITS_traits<float> {
+  static inline int datatype() { return TFLOAT; }
+};
+
+//! Template specialization for int
+template<> struct FITS_traits<int> {
+  static inline int datatype() { return TINT; }
+};
+
+//! Template specialization for long
+template<> struct FITS_traits<long> {
+  static inline int datatype() { return TLONG; }
+};
+
+//! Calls fits_update_key; throws a FITSError exception if status != 0
+template<typename T>
+void psrfits_update_key (fitsfile* fptr, const char* name, T data)
+{
+  // no comment
+  char* comment = 0;
+  // status
+  int status = 0;
+  
+  fits_update_key (fptr, FITS_traits<T>::datatype(),
+		   const_cast<char*>(name), &data,
+		   comment, &status);
+  
+  if (status)
+    throw FITSError (status, "psrfits_update_key", name);
+}
+
+//! Specialization for string
+void psrfits_update_key (fitsfile* fptr, const char* name,
+			 const std::string& data);
+
+//! Worker function does not handle status
+template<typename T>
+void psrfits_read_key_work (fitsfile* fptr, const char* name, T* data, 
+			    int* status)
+{
+  // no comment
+  char* comment = 0;
+  
+  fits_read_key (fptr, FITS_traits<T>::datatype(), 
+		 const_cast<char*>(name), data,
+		 comment, status);
+}
+
+//! Specialization for string
+void psrfits_read_key_work (fitsfile* fptr, const char* name, std::string*,
+			    int* status);
+
+//! Calls fits_read_key; throws a FITSError exception if status != 0
+template<typename T>
+void psrfits_read_key (fitsfile* fptr, const char* name, T* data)
+{
+  // status
+  int status = 0;
+  psrfits_read_key_work (fptr, name, data, &status);
+  if (status)
+    throw FITSError (status, "psrfits_read_key", name);
+}
+
+//! Calls fits_read_key; sets data to default if status != 0
+template<typename T>
+void psrfits_read_key (fitsfile* fptr, const char* name, T* data,
+		       T dfault, bool verbose = false)
+{
+  // status
+  int status = 0;
+  psrfits_read_key_work (fptr, name, data, &status);
+  if (status) {
+    if (verbose) {
+      FITSError error (status, "psrfits_read_key", name);
+      std::cerr << error.get_message() << std::endl;
+      std::cerr << "psrfits_read_key: using default="<< dfault <<std::endl;
+    }
+    *data = dfault;
+  }
+}
+
+#endif
