c705958a114941553c01ba5566105fc9314fd22f
straten <straten>
2006-03-02 16:57:41 +0000

    computes invariant interval with proper error propagation and bias estimation

---
+++ More/MEAL/Invariant.C
@@ -0,0 +1,62 @@
+#include "MEAL/Invariant.h"
+#include "MEAL/ScalarParameter.h"
+#include "MEAL/ScalarMath.h"
+
+void MEAL::Invariant::init ()
+{
+  for (unsigned ipol=0; ipol<input.size(); ipol++)
+    input[ipol] = *(new MEAL::ScalarParameter);
+
+  result = input.invariant();
+}
+
+MEAL::Invariant::Invariant ()
+{
+  init ();
+}
+
+MEAL::Invariant::Invariant (const Invariant&)
+{
+  init ();
+}
+
+MEAL::Invariant::~Invariant ()
+{
+
+}
+
+MEAL::Invariant&
+MEAL::Invariant::operator = (const Invariant&)
+{
+  return *this;
+}
+
+void MEAL::Invariant::set_Stokes (const Stokes<Estimate<float> >& stokes)
+{
+  Stokes<Estimate<double> > temp = stokes;
+  set_Stokes (temp);
+}
+
+void MEAL::Invariant::set_Stokes (const Stokes<Estimate<double> >& stokes)
+{
+  bias = stokes[0].get_variance();
+
+  for (unsigned ipol=0; ipol<stokes.size(); ipol++) {
+    input[ipol].get_expression()->set_Estimate( 0, stokes[ipol] );
+    if (ipol)
+      bias -= stokes[ipol].get_variance();
+  }
+}
+
+//! Get the invariant interval
+Estimate<double> MEAL::Invariant::get_invariant () const
+{
+  return result.get_Estimate();
+}
+
+//! Get the estimated bias due to measurement error
+double MEAL::Invariant::get_bias () const
+{
+  return bias;
+}
+
---
+++ More/MEAL/MEAL/Invariant.h
@@ -0,0 +1,71 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/psrchive/psrchive/More/MEAL/MEAL/Invariant.h,v $
+   $Revision: 1.1 $
+   $Date: 2006/03/02 16:57:41 $
+   $Author: straten $ */
+
+#ifndef __MEAL_Invariant_h
+#define __MEAL_Invariant_h
+
+#include "Stokes.h"
+#include "Estimate.h"
+#include "ScalarMath.h"
+
+namespace MEAL {
+
+  class Scalar;
+
+  //! Computes the invariant interval
+  /*! This class properly handles the propagation of errors */
+
+  class Invariant : Reference::Able {
+
+  public:
+
+    //! Default constructor
+    Invariant ();
+
+    //! Copy constructor
+    Invariant (const Invariant&);
+
+    //! Destructor
+    ~Invariant ();
+
+    //! Assignment operator
+    Invariant& operator = (const Invariant&);
+
+    //! Set the Stokes parameters
+    void set_Stokes (const Stokes< Estimate<float> >&);
+
+    //! Set the Stokes parameters
+    void set_Stokes (const Stokes< Estimate<double> >&);
+
+    //! Get the invariant interval
+    Estimate<double> get_invariant () const;
+
+    //! Get the estimated bias due to measurement error
+    double get_bias () const;
+
+  protected:
+
+    //! The input Stokes parameters
+    Stokes<ScalarMath> input;
+
+    //! The invariant interval, I^2 - Q^2 - U^2 - V^2
+    ScalarMath result;
+
+    //! The bias due to measurement error
+    double bias;
+
+  private:
+
+    //! Does the work for the constructors
+    void init ();
+
+  };
+
+}
+
+#endif
+
