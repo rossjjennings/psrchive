f0dc8e458a07464a6e50ccbe88a8aa7137077fce
straten <straten>
2006-03-08 03:50:40 +0000

    add polarization to interface

--- More/Plotting/FluxPlotTI.C
+++ More/Plotting/FluxPlotTI.C
@@ -6,8 +6,6 @@ Pulsar::FluxPlotterTI::FluxPlotterTI (FluxPlotter* instance)
   if (instance)
     set_instance (instance);
 
-  import( ProfilePlotterTI() );
-
   add( &FluxPlotter::get_subint,
        &FluxPlotter::set_subint,
        "subint", "Sub-integration to plot" );
@@ -15,6 +13,12 @@ Pulsar::FluxPlotterTI::FluxPlotterTI (FluxPlotter* instance)
   add( &FluxPlotter::get_chan,
        &FluxPlotter::set_chan,
        "chan", "Frequency channel to plot" );
+
+  add( &FluxPlotter::get_pol,
+       &FluxPlotter::set_pol,
+       "pol", "Polarization to plot" );
+
+  import( ProfilePlotterTI() );
 
   add( &FluxPlotter::get_plot_error_box,
        &FluxPlotter::set_plot_error_box,
