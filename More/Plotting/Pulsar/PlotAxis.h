b0098b6a76c283221a122982678089bd2a100a90
straten <straten>
2006-03-08 22:33:46 +0000

    get_range resets min and max using normalized min and max; alternate axis option

--- More/Plotting/Pulsar/PlotAxis.h
+++ More/Plotting/Pulsar/PlotAxis.h
@@ -1,8 +1,8 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/More/Plotting/Pulsar/PlotAxis.h,v $
-   $Revision: 1.1 $
-   $Date: 2006/03/07 23:13:23 $
+   $Revision: 1.2 $
+   $Date: 2006/03/08 22:33:46 $
    $Author: straten $ */
 
 #ifndef __Pulsar_PlotAxis_h
@@ -20,10 +20,18 @@ namespace Pulsar {
     //! Default constructor
     PlotAxis ();
 
+    //! Rescale min and max according to current attribute settings
+    void get_range (float& min, float& max) const;
+
     //! Set the label to be drawn on the axis
     void set_label (const std::string& _label) { label = _label; }
     //! Get the label to be drawn on the axis
     std::string get_label () const { return label; }
+
+    //! Set if the frame opposite the axis may have an alternate scale
+    void set_alternate (bool f) { alternate = f; }
+    //! Get the world-normalized minimum value on the axis
+    bool get_alternate () const { return alternate; }
 
     //! Set the world-normalized minimum value on the axis
     void set_min_norm (float f) { min_norm = f; }
@@ -45,10 +53,17 @@ namespace Pulsar {
     //! Get the label to be drawn on the axis
     std::string get_pgbox_opt () const { return pgbox_opt; }
 
+    //! Add to the options to be passed to pgbox for this axis
+    void add_pgbox_opt (char opt);
+
+    //! Remove from the options to be passed to pgbox for this axis
+    void rem_pgbox_opt (char opt);
+
   protected:
 
     std::string label;
     std::string pgbox_opt;
+    bool alternate;
     float min_norm;
     float max_norm;
     float buf_norm;
