b18f1989e361e635640b7ee7d155280e8002772a
straten <straten>
2006-03-09 23:02:07 +0000

    getting ready for multiple panels

---
+++ More/Plotting/Pulsar/MultiFrame.h
@@ -0,0 +1,26 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/psrchive/psrchive/More/Plotting/Pulsar/MultiFrame.h,v $
+   $Revision: 1.1 $
+   $Date: 2006/03/09 23:02:07 $
+   $Author: straten $ */
+
+#ifndef __Pulsar_MultiFrame_h
+#define __Pulsar_MultiFrame_h
+
+#include "Pulsar/PlotFrameSize.h"
+
+namespace Pulsar {
+
+  //! Manages multiple plot frames
+  class MultiFrame : Reference::Able {
+
+  public:
+
+    vector of frames
+
+  };
+
+}
+
+#endif
---
+++ More/Plotting/Pulsar/PlotFrameSize.h
@@ -0,0 +1,33 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/psrchive/psrchive/More/Plotting/Pulsar/Attic/PlotFrameSize.h,v $
+   $Revision: 1.1 $
+   $Date: 2006/03/09 23:02:07 $
+   $Author: straten $ */
+
+#ifndef __Pulsar_PlotFrameSize_h
+#define __Pulsar_PlotFrameSize_h
+
+#include "Pulsar/PlotFrame.h"
+
+namespace Pulsar {
+
+  //! PlotFrame with size
+  class PlotFrameSize : public PlotFrame {
+
+  public:
+
+    //! Default constructor
+    PlotFrameSize ();
+
+    //! Destructor
+    ~PlotFrameSize ();
+
+
+  protected:
+
+  };
+
+}
+
+#endif
