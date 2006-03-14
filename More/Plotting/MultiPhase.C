bdf91055cfcac2418f87672757e8f95d09d5a898
straten <straten>
2006-03-14 16:08:05 +0000

    intelligence moved to MultiPlot

--- More/Plotting/MultiPhase.C
+++ More/Plotting/MultiPhase.C
@@ -1,61 +1,24 @@
 #include "Pulsar/MultiPhase.h"
-#include "Pulsar/MultiFrameTI.h"
-
-#include <cpgplot.h>
 
 Pulsar::MultiPhase::MultiPhase ()
 {
+  get_frame()->set_x_zoom( new PhaseScale );
   frames.set_shared_x_zoom (get_frame()->get_x_zoom());
 }
 
-TextInterface::Class* Pulsar::MultiPhase::get_frame_interface ()
+//! Manage a plot
+void Pulsar::MultiPhase::manage (const std::string& name, Plot* plot)
 {
-  return new MultiFrameTI (&frames);
+  plot->get_frame()->set_x_zoom( frames.get_shared_x_zoom() );
+  MultiPlot::manage (name, plot);
 }
 
-void Pulsar::MultiPhase::plot (const Archive* data)
+//! Get the scale
+Pulsar::PhaseScale* Pulsar::MultiPhase::get_scale ()
 {
-  prepare (data);
-
-  float x0, x1, y0, y1;
-  cpgqvp (0, &x0, &x1, &y0, &y1);
-
-  std::map< std::string, Reference::To<PhasePlot> >::iterator ptr;
-  for (ptr = plotters.begin(); ptr != plotters.end(); ptr++) {
-
-    PhasePlot* plot = ptr->second;
-    PlotFrame* frame = plot->get_frame();
-
-    // set the viewport
-    frame->focus();
-
-    // plot
-    plot->set_scale (get_scale());
-    plot->set_origin_norm (get_origin_norm());
-    plot->plot(data);
-
-    // restore the viewport
-    cpgsvp (x0,x1, y0,y1);
-
-  }
-}
-
-//! Manage a plotter
-void Pulsar::MultiPhase::manage (const std::string& name,
-				   PhasePlot* plot)
-{
-  plot->set_frame( frames.manage (name, plot->get_frame()) );
-  plot->get_frame()->set_x_zoom( get_frame()->get_x_zoom() );
-
-  plotters[name] = plot;
-}
-
-//! Set the viewport of the named plotter
-void Pulsar::MultiPhase::set_viewport (const std::string& name,
-					 float x0, float x1,
-					 float y0, float y1)
-{
-  PlotFrameSize* frame = frames.get_frame(name);
-  frame->set_x_range( std::pair<float,float> (x0,x1) );
-  frame->set_y_range( std::pair<float,float> (y0,y1) );
+  PhaseScale* scale = dynamic_cast<PhaseScale*>( frames.get_shared_x_zoom() );
+  if (!scale)
+    throw Error (InvalidState, "Pulsar::MultiPhase::get_scale",
+		 "x scale is not a PhaseScale");
+  return scale;
 }
