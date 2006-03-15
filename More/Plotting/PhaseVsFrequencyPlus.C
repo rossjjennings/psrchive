376892dc5f4bff61ed6e79a34c896a547c0966fd
straten <straten>
2006-03-15 16:34:50 +0000

    properly transposes the psd plot

--- More/Plotting/PhaseVsFrequencyPlus.C
+++ More/Plotting/PhaseVsFrequencyPlus.C
@@ -9,41 +9,57 @@ Pulsar::PhaseVsFrequencyPlus::PhaseVsFrequencyPlus ()
   ///////////////////////////////////////////////////////////////////////
 
   manage ("freq", &freq);
-  set_viewport ( "freq", 0,.7, .3,.1);
+  set_viewport ( "freq", 0,.7, 0,.7);
 
   freq.get_frame()->set_x_scale (get_frame()->get_x_scale());
   freq.get_frame()->set_y_scale (get_frame()->get_y_scale());
 
-  // remove the x enumeration
-  freq.get_frame()->get_x_axis()->rem_pgbox_opt('N');
   // remove the alternative labeling
   freq.get_frame()->get_y_axis()->add_pgbox_opt ('C');
   freq.get_frame()->get_y_axis()->set_alternate (false);
+  // remove the above frame labels
+  freq.get_frame()->get_label_above()->set_all ("");
 
   ///////////////////////////////////////////////////////////////////////
 
   manage ("flux", &flux);
-  set_viewport ( "flux", 0,.7, 0,.3);
+  set_viewport ( "flux", 0,.7, .7,1);
 
   flux.get_frame()->set_x_scale (get_frame()->get_x_scale());
 
+  // remove the x enumeration
+  flux.get_frame()->get_x_axis()->rem_pgbox_opt('N');
+  // remove the x label
+  flux.get_frame()->get_x_axis()->set_label("");
+  // remove the y enumeration
+  flux.get_frame()->get_y_axis()->rem_pgbox_opt('N');
+  // remove the y label
+  flux.get_frame()->get_y_axis()->set_label("");
   // remove the above frame labels
   flux.get_frame()->get_label_above()->set_all ("");
+  // put the filename in the upper left
+  flux.get_frame()->get_label_above()->set_left ("=file");
 
   ///////////////////////////////////////////////////////////////////////
 
   manage ("psd", &psd);
-  set_viewport ( "psd", 1,.7, .3,1);
+  set_viewport ( "psd", .7,1, 0,.7);
 
-  // shorten the y label
-  psd.get_frame()->get_y_axis()->set_label("Power");
-  // remove the x label
-  psd.get_frame()->get_x_axis()->set_label("");
   // remove the x enumeration
   psd.get_frame()->get_x_axis()->rem_pgbox_opt('N');
+  // remove the x label
+  psd.get_frame()->get_x_axis()->set_label("");
+  // remove the y enumeration
+  //psd.get_frame()->get_y_axis()->rem_pgbox_opt('N');
+  // shorten the y label
+  psd.get_frame()->get_y_axis()->set_label("Power");
   // remove the below frame labels
   psd.get_frame()->get_label_below()->set_all ("");
+  // remove the above frame labels
+  psd.get_frame()->get_label_above()->set_all ("");
 
+  // transpose the frame
+  psd.get_frame()->set_transpose ();
 }
 
 //! Get the text interface to the configuration attributes
@@ -54,9 +70,18 @@ TextInterface::Class* Pulsar::PhaseVsFrequencyPlus::get_interface ()
 
 void Pulsar::PhaseVsFrequencyPlus::prepare (const Archive*)
 {
-  freq.set_subint (isubint);
-  flux.set_subint (isubint);
-  psd.set_subint (isubint);
+  prepare (&freq);
+  prepare (&flux);
+  prepare (&psd);
 }
 
-
+//! Get the scale
+Pulsar::PhaseScale* Pulsar::PhaseVsFrequencyPlus::get_scale ()
+{
+  PhaseScale* scale = 0;
+  scale = dynamic_cast<PhaseScale*>( get_frame()->get_x_scale() );
+  if (!scale)
+    throw Error (InvalidState, "Pulsar::MultiPhase::get_scale",
+		 "x scale is not a PhaseScale");
+  return scale;
+}
