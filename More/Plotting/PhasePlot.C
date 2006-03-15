e0ebffc79a9727af8a17dae8d2f5b4c2f80e3181
straten <straten>
2006-03-15 11:38:35 +0000

    SimplePlot now does most of the work

--- More/Plotting/PhasePlot.C
+++ More/Plotting/PhasePlot.C
@@ -1,20 +1,8 @@
 #include "Pulsar/PhasePlotTI.h"
-#include "Pulsar/PlotFrame.h"
-
-#include "Pulsar/Archive.h"
-
-#include <cpgplot.h>
 
 Pulsar::PhasePlot::PhasePlot ()
 {
   get_frame()->set_x_scale( new PhaseScale );
-
-  yrange_set = false;
-  y_min = y_max = 0;
-}
-
-Pulsar::PhasePlot::~PhasePlot ()
-{
 }
 
 TextInterface::Class* Pulsar::PhasePlot::get_interface ()
@@ -22,66 +10,21 @@ TextInterface::Class* Pulsar::PhasePlot::get_interface ()
   return new PhasePlotTI (this);
 }
 
-void Pulsar::PhasePlot::set_yrange (float min, float max)
-{
-  y_min = min;
-  y_max = max;
-  yrange_set = true;
-}
-
-
 //! Get the default label for the x axis
-string Pulsar::PhasePlot::get_xlabel (const Archive*)
+std::string Pulsar::PhasePlot::get_xlabel (const Archive*)
 {
   return get_scale()->get_label();
 }
 
 //! Get the default label for the y axis
-string Pulsar::PhasePlot::get_ylabel (const Archive*)
+std::string Pulsar::PhasePlot::get_ylabel (const Archive*)
 {
   return "";
 }
 
-/*!
-  Plots the profile in the currently open pgplot device, using the current
-  viewport.  The profile may be rotated, scaled, and scaleed.
-*/
-void Pulsar::PhasePlot::plot (const Archive* data)
+void Pulsar::PhasePlot::set_yrange (float min, float max)
 {
-  yrange_set = false;
-
-  prepare (data);
-
-  if (!yrange_set)
-    throw Error (InvalidState, "Pulsar::PhasePlot::plot",
-		 "range in y values not set after call to prepare");
-
-  float x_min = 0.0;
-  float x_max = 1.0;
-
-  get_frame()->get_y_scale()->get_range (y_min, y_max);
-  get_frame()->get_x_scale()->get_range (x_min, x_max);
-
-  cpgswin (x_min, x_max, y_min, y_max);
-
-  phases.resize (data->get_nbin());
-  for (unsigned ibin = 0; ibin < phases.size(); ibin++)
-    phases[ibin] = (float(ibin) + 0.5) / phases.size();
-
-  draw (data);
-
-  cpgsls (1);
-  cpgsci (1);
-
-  get_scale()->get_range_units (data, x_min, x_max);
-
-  cpgswin (x_min, x_max, y_min, y_max);
-
-  get_frame()->draw_axes();
-
-  get_frame()->label_axes( get_xlabel (data), get_ylabel (data) );
-
-  get_frame()->decorate(data);
+  get_frame()->get_y_scale()->set_minmax (min, max);
 }
 
 //! Get the scale
--- More/Plotting/Pulsar/PhasePlot.h
+++ More/Plotting/Pulsar/PhasePlot.h
@@ -1,21 +1,21 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/psrchive/psrchive/More/Plotting/Pulsar/PhasePlot.h,v $
-   $Revision: 1.14 $
-   $Date: 2006/03/14 22:09:27 $
+   $Revision: 1.15 $
+   $Date: 2006/03/15 11:38:35 $
    $Author: straten $ */
 
 #ifndef __Pulsar_PhasePlot_h
 #define __Pulsar_PhasePlot_h
 
-#include "Pulsar/Plot.h"
+#include "Pulsar/SimplePlot.h"
 #include "Pulsar/PhaseScale.h"
 
 #include <vector>
 
 namespace Pulsar {
 
-  //! Plots with pulse phase along the x-axis
+  //! Simple plots with pulse phase along the x-axis
   /*! Children of the PhasePlot always:
     <UL>
     <LI> plot in the currently open viewport
@@ -29,56 +29,27 @@ namespace Pulsar {
     <LI> change the line style within the plot frame
     </UL>
   */
-  class PhasePlot : public Plot {
+  class PhasePlot : public SimplePlot {
 
   public:
 
     //! Default constructor
     PhasePlot ();
 
-    //! Destructor
-    virtual ~PhasePlot ();
-
-    //! Plot in the current viewport
-    virtual void plot (const Archive*);
-
     //! Get the text interface to the configuration attributes
     TextInterface::Class* get_interface ();
 
     //! Get the default label for the x axis
-    virtual std::string get_xlabel (const Archive*);
+    std::string get_xlabel (const Archive*);
 
     //! Get the default label for the y axis
-    virtual std::string get_ylabel (const Archive*);
+    std::string get_ylabel (const Archive*);
 
-    //! Derived classes must prepare to draw
-    /*! Derived classes must at least call set_yrange */
-    virtual void prepare (const Archive*) = 0;
-
-    //! Derived classes must draw in the current viewport
-    virtual void draw (const Archive*) = 0;
+    //! Set the y-range
+    void set_yrange (float min, float max);
 
     //! Get the scale
     PhaseScale* get_scale ();
-
-  protected:
-
-    //! Phases can be used
-    std::vector<float> phases;
-
-    //! Derived classes must set the range in y values during prepare
-    void set_yrange (float min, float max);
-
-  private:
-
-    //! Set true when yrange is called
-    bool yrange_set;
-
-    //! The minimum value of y
-    float y_min;
-
-    //! The maximum value of y
-    float y_max;
 
   };
 
