c4d92cf8235ccc0570f748f302d465a15885a8e2
straten <straten>
2006-03-15 16:35:00 +0000

    imports the PhaseScale and PowerSpectra interfaces

--- More/Plotting/PhaseVsFrequencyPlusTI.C
+++ More/Plotting/PhaseVsFrequencyPlusTI.C
@@ -1,9 +1,11 @@
 #include "Pulsar/PhaseVsFrequencyPlusTI.h"
 #include "Pulsar/PhaseVsFrequencyTI.h"
-//#include "Pulsar/SpectrumPlotTI.h"
+#include "Pulsar/PowerSpectraTI.h"
 #include "Pulsar/ProfilePlotTI.h"
+#include "Pulsar/PhaseScaleTI.h"
 
-Pulsar::PhaseVsFrequencyPlusTI::PhaseVsFrequencyPlusTI (PhaseVsFrequencyPlus* instance)
+Pulsar::PhaseVsFrequencyPlusTI::PhaseVsFrequencyPlusTI
+(PhaseVsFrequencyPlus* instance)
 {
   if (instance)
     set_instance (instance);
@@ -12,10 +14,16 @@ Pulsar::PhaseVsFrequencyPlusTI::PhaseVsFrequencyPlusTI (PhaseVsFrequencyPlus* in
        &PhaseVsFrequencyPlus::set_subint,
        "subint", "Sub-integration to plot" );
 
+  add( &PhaseVsFrequencyPlus::get_pol,
+       &PhaseVsFrequencyPlus::set_pol,
+       "pol", "Polarization to plot" );
+
+  import( "x", PhaseScaleTI(), &PhaseVsFrequencyPlus::get_scale );
+
   // when this is set, import will filter out attributes with identical names
   import_filter = true;
 
   import ( "freq", PhaseVsFrequencyTI(), &PhaseVsFrequencyPlus::get_freq );
-  import ( "flux", ProfilePlotTI(), &PhaseVsFrequencyPlus::get_flux );
-  // import ( "psd", SpectrumPlotTI(), &PhaseVsFrequencyPlus::get_psd );
+  import ( "flux", ProfilePlotTI(),      &PhaseVsFrequencyPlus::get_flux );
+  import ( "psd",  PowerSpectraTI(),     &PhaseVsFrequencyPlus::get_psd );
 }
