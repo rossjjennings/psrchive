4001f47c2eea6f04c5522b4bb23f1153a1e4a947
straten <straten>
2006-03-23 21:18:55 +0000

    base class of Integration weighting functions

---
+++ More/General/Pulsar/IntegrationWeight.h
@@ -0,0 +1,29 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2005 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/IntegrationWeight.h,v $
+   $Revision: 1.1 $
+   $Date: 2006/03/23 21:18:55 $
+   $Author: straten $ */
+
+#ifndef _Pulsar_IntegrationWeight_H
+#define _Pulsar_IntegrationWeight_H
+
+#include "Pulsar/Weight.h"
+
+namespace Pulsar {
+  
+  //! Algorithms that set the weights of sub-integrations
+  class IntegrationWeight : public Weight {
+
+  };
+  
+}
+
+#endif
+
