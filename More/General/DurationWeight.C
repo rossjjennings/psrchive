65686e33e99373f6da325adf4a395924de07be2e
straten <straten>
2006-03-23 21:18:50 +0000

    Weight each Profile by the duration of the Integration

---
+++ More/General/DurationWeight.C
@@ -0,0 +1,32 @@
+#include "Pulsar/DurationWeight.h"
+#include "Pulsar/Integration.h"
+
+//! Default constructor
+Pulsar::DurationWeight::DurationWeight ()
+{
+  weight_absolute = false;
+}
+
+//! Set integration weights
+void Pulsar::DurationWeight::weight (Integration* integration)
+{
+  double duration = integration->get_duration();
+  unsigned nchan = integration->get_nchan();
+
+  for (unsigned ichan=0; ichan < nchan; ichan++) {
+
+    float chan_weight = integration->get_weight (ichan);
+
+    // always ignore zapped channels
+    if (!chan_weight)
+      continue;
+
+    if (weight_absolute)
+      chan_weight = duration;
+    else
+      chan_weight *= duration;
+
+    integration->set_weight (ichan, chan_weight);
+
+  }
+}
---
+++ More/General/Pulsar/DurationWeight.h
@@ -0,0 +1,49 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2005 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/DurationWeight.h,v $
+   $Revision: 1.1 $
+   $Date: 2006/03/23 21:18:50 $
+   $Author: straten $ */
+
+#ifndef _Pulsar_DurationWeight_H
+#define _Pulsar_DurationWeight_H
+
+#include "Pulsar/IntegrationWeight.h"
+
+namespace Pulsar {
+  
+  //! Sets the weight of each Integration according to its duration
+  class DurationWeight : public IntegrationWeight {
+
+  public:
+
+    //! Default constructor
+    DurationWeight ();
+
+    //! Set weight equal to duration or scale weight by duration
+    /*! 
+      When true: the weight is set equal to the duration (weight = duration)
+      When false: the weight is scaled by the duration (weight *= duration)
+    */
+    void set_weight_absolute (bool f) { weight_absolute = f; }
+    bool get_weight_absolute () const { return weight_absolute; }
+
+  protected:
+
+    //! Set integration weights
+    void weight (Integration* integration);
+
+    bool weight_absolute;
+
+  };
+  
+}
+
+#endif
+
