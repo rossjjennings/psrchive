7389bfb8033c68b998a07e73db7bb66b70ee7674
straten <straten>
2006-03-23 21:18:46 +0000

    inherit Weight

--- More/General/Pulsar/ChannelWeight.h
+++ More/General/Pulsar/ChannelWeight.h
@@ -7,32 +7,21 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/ChannelWeight.h,v $
-   $Revision: 1.3 $
-   $Date: 2006/03/17 13:34:50 $
+   $Revision: 1.4 $
+   $Date: 2006/03/23 21:18:46 $
    $Author: straten $ */
 
 #ifndef _Pulsar_ChannelWeight_H
 #define _Pulsar_ChannelWeight_H
 
-#include "Pulsar/Algorithm.h"
+#include "Pulsar/Weight.h"
 
 namespace Pulsar {
   
-  class Archive;
-  class Integration;
-
   //! Algorithms that set the weights of frequency channels
-  /*! This pure virtual base class defines the interface to various
+  /*! This base class defines the interface to various
       channel weighting algorithms, such as RFI excision. */
-  class ChannelWeight : public Algorithm {
-    
-  public:
-
-    //! Set the weight of each frequency channel in each sub-integration
-    virtual void weight (Archive*);
-
-    //! Set the weight of each frequency channel in the sub-integration
-    virtual void weight (Integration*) = 0;
+  class ChannelWeight : public Weight {
 
   };
   
