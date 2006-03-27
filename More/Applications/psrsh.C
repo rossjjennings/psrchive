4fb6bfa967a6fc6c375885129ee5106411ab6590
straten <straten>
2006-03-27 17:59:24 +0000

    script mode

--- More/Applications/psrsh.C
+++ More/Applications/psrsh.C
@@ -6,6 +6,7 @@
  ***************************************************************************/
 
 #include "Pulsar/Interpreter.h"
+#include "Pulsar/Archive.h"
 
 using namespace std;
 
@@ -13,8 +14,41 @@ int main (int argc, char** argv)
 {
   Pulsar::Interpreter processor (argc, argv);
 
-  while (!processor.quit)
-    cout << processor.parse( processor.readline() );
+  if (argc == 1) {
 
+    // no arguments: interactive mode
+    processor.initialize_readline ("psrsh");
+    processor.set_reply( true );
+    
+    while (!processor.quit)
+      cout << processor.parse( processor.readline() );
+
+  }
+  else if (argc == 2) {
+
+    // one argument: undefined mode
+    cerr << "psrsh: specify script followed by archive filename[s]" << endl;
+    return -1;
+
+  }
+  else {
+
+    // two or more arguments: script mode
+
+    // the first argument is the script to execute
+    string script = argv[1];
+
+    // all subsequent arguments are the archive filenames
+    for (int i=2; i<argc; i++) try {
+      processor.set( Pulsar::Archive::load (argv[i]) );
+      processor.script( script );
+    }
+    catch (Error& error) {
+      cerr << "psrsh: error while processing " << argv[i] << ":\n"
+	   << error.get_message() << "\n"
+	"script = " << script << endl;
+    }
+
+  }
   return 0;
 }
