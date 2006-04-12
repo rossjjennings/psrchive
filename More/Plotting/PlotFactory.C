1ce3c071ed0cf4e0b1f29fcceffc58368c0b4ee6
straten <straten>
2006-04-12 19:29:30 +0000

    shortcut keys and fluctation power spectra added

--- More/Plotting/PlotFactory.C
+++ More/Plotting/PlotFactory.C
@@ -5,6 +5,7 @@
  *
  ***************************************************************************/
 #include "Pulsar/PlotFactory.h"
+
 #include "Pulsar/ProfilePlot.h"
 #include "Pulsar/StokesPlot.h"
 #include "Pulsar/PhaseVsFrequency.h"
@@ -15,52 +16,62 @@
 #include "Pulsar/StokesSpherical.h"
 #include "Pulsar/SpectrumPlot.h"
 #include "Pulsar/PhaseVsFrequencyPlus.h"
+#include "Pulsar/StokesFluctPlot.h"
 #include "Pulsar/Poincare.h"
 
 #include "Error.h"
+#include "pad.h"
 
 // Add plots here
 Pulsar::PlotFactory::PlotFactory ()
 {
   add ( new PlotFactory::Advocate<ProfilePlot>
-	("flux", "Single plot of flux") );
+	('p', "flux", "Single plot of flux") );
 
   add ( new PlotFactory::Advocate<StokesPlot>
-	("stokes", "Stokes parameters") );
-
-  add ( new PlotFactory::Advocate<PhaseVsFrequency>
-	("freq", "Phase vs. frequency image of flux") );
-
-  add ( new PlotFactory::Advocate<PhaseVsFrequencyPlus>
-	("freq+", "freq + integrated profile and spectrum") );
-
-  add ( new PlotFactory::Advocate<PhaseVsTime>
-	("time", "Phase vs. time image of flux") );
-
-  add ( new PlotFactory::Advocate<PosAngPlot>
-	("pa", "Position angle") );
-
-  add ( new PlotFactory::Advocate<EllAngPlot>
-	("ell", "Ellipticity angle") );
+	('S', "stokes", "Stokes parameters") );
 
   add ( new PlotFactory::Advocate<StokesCylindrical>
-	("Scyl", "Stokes; vector in cylindrical") );
+	('c', "Scyl",   "Stokes; vector in cylindrical") );
 
   add ( new PlotFactory::Advocate<StokesSpherical>
-	("Ssph", "Stokes; vector in spherical") );
+	('s', "Ssph",   "Stokes; vector in spherical") );
+
+  add ( new PlotFactory::Advocate<StokesFluctPlot>
+	('f', "Sfluct", "Stokes; fluctuation power spectra") );
+
+  add ( new PlotFactory::Advocate<PhaseVsFrequency>
+	('F', "freq", "Phase vs. frequency image of flux") );
+
+  add ( new PlotFactory::Advocate<PhaseVsFrequencyPlus>
+	('+', "freq+", "freq + integrated profile and spectrum") );
+
+  add ( new PlotFactory::Advocate<PhaseVsTime>
+	('T', "time", "Phase vs. time image of flux") );
+
+  add ( new PlotFactory::Advocate<PosAngPlot>
+	('o', "pa", "Orientation (Position) angle") );
+
+  add ( new PlotFactory::Advocate<EllAngPlot>
+	('e', "ell", "Ellipticity angle") );
 
   add ( new PlotFactory::Advocate<Poincare>
-	("p3d", "Stokes vector in Poincare space") );
+	('P', "p3d", "Stokes vector in Poincare space") );
 
   add ( new PlotFactory::Advocate<SpectrumPlot>
-	("psd", "Total power spectrum") );
+	('b', "psd", "Total power spectrum (bandpass)") );
 }
 
 //! Return a new instance of the named plotter
 Pulsar::Plot* Pulsar::PlotFactory::construct (std::string name)
 {
+  char shortcut = 0;
+  if (name.length() == 1)
+    shortcut = name[0];
+
   for (unsigned i=0; i < agents.size(); i++)
-    if (name == agents[i]->get_name())
+    if ( (shortcut && shortcut == agents[i]->get_shortcut()) ||
+	 (name == agents[i]->get_name()) )
       return agents[i]->construct();
 
   throw Error (InvalidParam, "Pulsar::PlotFactory::construct",
@@ -77,4 +88,22 @@ std::string Pulsar::PlotFactory::get_name (unsigned i)
 std::string Pulsar::PlotFactory::get_description (unsigned i)
 {
   return agents[i]->get_description();
+}
+
+std::string Pulsar::PlotFactory::help()
+{
+  unsigned ia, maxlen = 0;
+  for (ia=0; ia < agents.size(); ia++)
+    if (get_name(ia).length() > maxlen)
+      maxlen = get_name(ia).length();
+
+  maxlen += 2;
+
+  std::string result;
+
+  for (ia=0; ia < agents.size(); ia++)
+    result += pad(maxlen, get_name(ia)) + 
+      "[" + agents[ia]->get_shortcut() + "]  " + get_description(ia) + "\n";
+  
+  return result;
 }
