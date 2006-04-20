9d16abdf303557a02c03a60e5ccc0a657e7a18d1
straten <straten>
2006-04-20 22:13:34 +0000

    no change

--- More/Polarimetry/Pulsar/PolarCalibrator.h
+++ More/Polarimetry/Pulsar/PolarCalibrator.h
@@ -1,14 +1,14 @@
 //-*-C++-*-
 /***************************************************************************
  *
- *   Copyright (C) 2003 by Aidan Hotan
+ *   Copyright (C) 2003 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/More/Polarimetry/Pulsar/PolarCalibrator.h,v $
-   $Revision: 1.17 $
-   $Date: 2006/03/17 13:34:51 $
+   $Revision: 1.18 $
+   $Date: 2006/04/20 22:13:34 $
    $Author: straten $ */
 
 #ifndef __PolarCalibrator_H
