e5c6fe5d82aaf7367c441b18e9526565e2e07df3
hknight <hknight>
2006-07-19 02:06:51 +0000

    file_exists now more verbose on failutre

--- Util/genutil/dirutil.c
+++ Util/genutil/dirutil.c
@@ -38,6 +38,8 @@ int file_exists (const char* filename)
 {
   struct stat statistics;
   if (stat (filename, &statistics) < 0) {
+    fprintf (stderr, "file_exists() error stat (%s)", filename);
+    perror ("");
     return 0;
   }
   return 1;
