0740a5e8d33283e394349292fe141de5e3b25b34
straten <straten>
2006-09-09 16:30:41 +0000

    test new DeltaPA computation

--- More/Polarimetry/DeltaRM.C
+++ More/Polarimetry/DeltaRM.C
@@ -5,6 +5,7 @@
  *
  ***************************************************************************/
 #include "Pulsar/DeltaRM.h"
+#include "Pulsar/DeltaPA.h"
 
 #include "Pulsar/Archive.h"
 #include "Pulsar/Integration.h"
@@ -59,19 +60,20 @@ void Pulsar::DeltaRM::refine ()
     throw Error (InvalidState, "Pulsar::DeltaRM::refine",
 		 "nchan != 2 after FrequencyIntegrate");
 
-  Reference::To<PolnProfile> profile;
+  Reference::To<PolnProfile> profile0;
+  Reference::To<PolnProfile> profile1;
 
   vector< Estimate<double> > posang0;
-  profile = clone->new_PolnProfile(0);
-  profile->get_orientation (posang0, 3.0);
+  profile0 = clone->new_PolnProfile(0);
+  profile0->get_orientation (posang0, 3.0);
 
   vector< Estimate<double> > posang1;
-  profile = clone->new_PolnProfile(1);
-  profile->get_orientation (posang1, 3.0);
+  profile1 = clone->new_PolnProfile(1);
+  profile1->get_orientation (posang1, 3.0);
 
   MeanRadian<double> mean_delta_PA;
 
-  for (unsigned ibin=0; ibin < profile->get_nbin(); ibin++)
+  for (unsigned ibin=0; ibin < profile0->get_nbin(); ibin++)
     if (posang0[ibin].get_variance() && posang1[ibin].get_variance()) {
 
       cout << ibin << " " << posang0[ibin].get_value()
@@ -80,7 +82,6 @@ void Pulsar::DeltaRM::refine ()
 	   << " " << posang1[ibin].get_error () << endl;
 
       mean_delta_PA += M_PI/90.0 * (posang1[ibin] - posang0[ibin]);
-
     }
 
   double f0 = clone->get_Profile(0,0)->get_centre_frequency();
@@ -101,6 +102,10 @@ void Pulsar::DeltaRM::refine ()
   Estimate<double> delta_RM = delta_PA/(lambda_1*lambda_1-lambda_0*lambda_0);
 
   rotation_measure = delta_RM + data->get_rotation_measure();
+  
+  Pulsar::DeltaPA dpa;
+  Estimate<double> other = dpa.get (profile0, profile1);
+  cerr << "DeltaPA::get = " << other * 180.0/M_PI<< " deg." << endl;
 
   cerr << "delta PA = <PA_1 - PA_0> = " << 180.0/M_PI * delta_PA << " deg.\n"
        << "delta RM = " << delta_RM << endl
