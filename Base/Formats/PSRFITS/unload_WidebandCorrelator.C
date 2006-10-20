d343323a59d1b37d09abce42758c23b4e9f31d2a
straten <straten>
2006-10-20 02:02:50 +0000

    Backed base class attributes now unloaded in FITSArchive::unload_file method

--- Base/Formats/PSRFITS/unload_WidebandCorrelator.C
+++ Base/Formats/PSRFITS/unload_WidebandCorrelator.C
@@ -12,11 +12,7 @@ void Pulsar::FITSArchive::unload (fitsfile* f,
 				  const WidebandCorrelator* ext)
 try {
 
-  psrfits_update_key (f, "BACKEND",  ext->get_name());
   psrfits_update_key (f, "BECONFIG", ext->configfile);
-  psrfits_update_key (f, "BE_PHASE", (int)ext->get_argument());
-  psrfits_update_key (f, "BE_DCC",   (int)ext->get_downconversion_corrected());
-  // psrfits_update_key (f, "BE_HAND",  (int)ext->get_hand());
   psrfits_update_key (f, "NRCVR",    ext->nrcvr);
   psrfits_update_key (f, "TCYCLE",   ext->tcycle);
 
