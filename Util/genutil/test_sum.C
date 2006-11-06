f2d4e0a17e6b6a0633629030be48c596c2aa5d5a
straten <straten>
2006-11-06 13:25:15 +0000

    test new sum template

---
+++ Util/genutil/test_sum.C
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 1999 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "templates.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main ()
+{
+  vector<float> x (5);
+
+  x[0] = 1.2;
+  x[1] = 3.4;
+  x[2] = 5.6;
+  x[3] = 7.8;
+  x[4] = 9.0;
+
+  double dsum = sum<double>(x.begin(),x.end());
+  float fsum = sum(x);
+
+  double expect = 27.0;
+
+  if ((fsum != dsum) || fabs(dsum - expect) > 1e-6) {
+    cerr << "sum template error" << endl;
+    cerr << "fsum=" << fsum << " dsum=" << dsum << " expect=" << expect
+         << " diff=" << dsum-expect << endl;
+    return -1;
+  }
+
+  cerr << "sum template passes all tests" << endl;
+  return 0;
+}
