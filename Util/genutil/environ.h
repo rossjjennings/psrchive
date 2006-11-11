8b7a2a585d3d6d2775f5946f12f0a3585928ca11
straten <straten>
2006-11-11 19:04:52 +0000

    use the standard C integer types; removed float64|128 and all endian flags

--- Util/genutil/environ.h
+++ Util/genutil/environ.h
@@ -5,218 +5,40 @@
  *
  ***************************************************************************/
 /* $Source: /cvsroot/psrchive/psrchive/Util/genutil/Attic/environ.h,v $
-   $Revision: 1.15 $
-   $Date: 2006/11/02 22:36:13 $
+   $Revision: 1.16 $
+   $Date: 2006/11/11 19:04:52 $
    $Author: straten $ */
 
 /*
- * When sending packets back and forth between different architectures,
- * one hopes that they all agree on packet size and the size of each field
- * within the packet.  Using the types typedef'd in this header should help
- * keep things straight.
- * If you are worried about endian, see endian.h.
- *
- * Programmed by Willem van Straten using ideas hacked from YorkU CS dept.
- * and Russell Edwards' tips on environment -D defines
+ * Use the standard C integer types
  */
 
 #ifndef __ENVIRON_H
 #define __ENVIRON_H
 
-/* this will hopefully cover all the different ways that compilers like
-   to define unix */
-#ifdef __unix__
-#ifndef unix
-#define unix
-#endif
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
 #endif
 
-#ifdef __unix
-#ifndef unix
-#define unix
-#endif
-#endif
+#include <inttypes.h>
 
-/* ********************************************** UNIX */
-#ifdef unix 
-typedef short          int16;
-typedef unsigned short uint16;
+typedef int8_t    int8;
+typedef uint8_t   uint8;
 
-typedef float          float32;
-typedef double         float64;
+typedef int16_t   int16;
+typedef uint16_t  uint16;
 
-/* ********************************************** on DEC alphas */
-#ifdef __alpha
-typedef int            int32;
-typedef unsigned int   uint32;
+typedef int32_t   int32;
+typedef uint32_t  uint32;
 
-typedef long           int64;
-typedef unsigned long  uint64;
+typedef int64_t   int64;
+typedef uint64_t  uint64;
 
-typedef struct {double f1; double f2;} float128;
+/* for parsing */
+#define I32  PRIi32
+#define UI32 PRIu32
 
-#define ORDER_LITTLE_ENDIAN 1
-#define ORDER_BIG_ENDIAN    0
-
-/* for C++ compiler */
-#define NAMESPACE_WORKS 1
-
-/* for parsing a long */
-#define I64 "%ld"
-#define UI64 "%lu"
-#define I32 "%d"
-
-#endif
-
-/* ********************************************** on Silcon Graphics */
-#ifdef sgi
-typedef int            int32;
-typedef unsigned int   uint32;
-
-typedef long           int64;
-typedef unsigned long  uint64;
-
-typedef struct {double f1; double f2;} float128;
-#define ORDER_LITTLE_ENDIAN 1
-#endif
-
-/* ********************************************** on Sun Microsystems */
-#ifdef sun
-typedef long           int32;
-typedef unsigned long  uint32;
-
-typedef long long      int64;
-typedef unsigned long long uint64;
-
-typedef long double    float128;
-#define ORDER_BIG_ENDIAN 1
-
-/* for parsing a long */
-#define I64 "%lld"
-#define UI64 "%llu"
-#define I32 "%ld"
-
-#endif
-
-#endif  /* ****** end UNIX */
-
-/* ********************************************** LINUX */
-#ifdef linux
-
-typedef int      int32;
-typedef unsigned uint32;
-
-typedef long long int64;
-typedef unsigned long long uint64;
-typedef struct {double f1; double f2;} float128;
-
-/* for parsing a long */
-#define I64 "%Ld"
-#define UI64 "%Lu"
-#define I32 "%ld"
-
-#define ORDER_LITTLE_ENDIAN 1
-#define ORDER_BIG_ENDIAN    0
-
-#endif
-
-/* ********************************************** FreeBSD */
-#ifdef __FreeBSD__
-typedef int int32;
-typedef unsigned uint32;
-typedef long long int64;
-typedef unsigned long long uint64;
-typedef struct {double f1;double f2;} float128;
-
-/* for parsing a long */
-#define I64 "%Ld"
-#define UI64 "%Lu"
-#define I32 "%ld"
-
-#define ORDER_LITTLE_ENDIAN 1
-#define ORDER_BIG_ENDIAN    0
-
-#endif
-
-
-
-/* ********************************************** Darwin */
-#if defined(__APPLE__) && defined(__POWERPC__) 
-typedef short          int16;
-typedef unsigned short uint16;
-
-typedef float          float32;
-typedef double         float64;
-typedef long           int32;
-typedef unsigned long  uint32;
-
-typedef long long int64;
-typedef unsigned long long uint64;
-typedef struct {double f1; double f2;} float128;
-
-/* for parsing a long long */
-#define I64 "%lld"
-#define UI64 "%llu"
-#define I32 "%ld"
-
-#define ORDER_LITTLE_ENDIAN 0
-#define ORDER_BIG_ENDIAN    1
-
-#endif
-
-/* By default, struct elements should be aligned on double memory addresses */
-#define MALIGN_DOUBLE 1
-
-#if defined(linux) || (defined (sun) && defined(__i386))
-#undef MALIGN_DOUBLE
-#endif
-
-/* For future reference */
-
-#ifdef WINDOWS
-typedef short int16;
-typedef long  int32;
-typedef unsigned short uint16;
-typedef unsigned long  uint32;
-typedef float real;
-
-#define REORDER_BYTES
-#endif
-
-#ifdef MSDOS
-typedef short int16;
-typedef long  int32;
-typedef unsigned short uint16;
-typedef unsigned long  uint32;
-typedef float real;
-
-/*
- * replace malloc with farmalloc((unsigned int))
- * to get around 64k limit
- *
- */
-#include <stdlib.h>
-#include <malloc.h> /* for Borland C++ 4.0 */
-
-#if 0
-#define malloc(x) farmalloc((unsigned long)(x))
-#define calloc(x,y) farcalloc((unsigned long)(x),(y))
-#define free(x) farfree(x)
-#endif /* 0 */
-
-void far *dbg_malloc( unsigned long );
-void far *dbg_calloc( unsigned long, unsigned long );
-void far *dbg_realloc( void far *, unsigned long );
-void dbg_free( void far * );
-
-#define malloc(x) dbg_malloc(x)
-#define calloc(x,y) dbg_calloc(x,y)
-#define free(x) dbg_free(x)
-
-#ifndef REORDER_BYTES
-#define REORDER_BYTES
-#endif /* REORDER_BYTES */
-#endif /* MSDOS */
+#define I64  PRIi64
+#define UI64 PRIu64
 
 #endif /* ENVIRON_H */
-
