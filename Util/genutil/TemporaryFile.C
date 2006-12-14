f5de42b6281566ceaddca7972a3e5f83d145321a
straten <straten>
2006-12-14 22:50:29 +0000

    creates a temporary file with a unique name; removes file when program exits

---
+++ Util/genutil/TemporaryFile.C
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2006 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "TemporaryFile.h"
+#include "Error.h"
+
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+using namespace std;
+
+//! Set true when the signal handler has been installed
+bool TemporaryFile::signal_handler_installed = false;
+
+void TemporaryFile::install_signal_handler ()
+{
+  struct sigaction sa;
+
+  sa.sa_handler = TemporaryFile::signal_handler;
+  sa.sa_flags = SA_RESTART;
+
+  // "man 7 signal" to see which signals are being handled
+  for (unsigned i=1; i<16; i++)
+    sigaction (i, &sa, NULL);
+}
+
+void remove_instance (TemporaryFile* file)
+{
+  file->remove ();
+}
+
+//! The signal handler ensures that all temporary files are removed
+void TemporaryFile::signal_handler (int)
+{
+  for_each (instances.begin(), instances.end(), remove_instance);
+}
+
+//! The current temporary file instances
+std::set<TemporaryFile*> TemporaryFile::instances;
+
+
+//! Construct with regular expression
+TemporaryFile::TemporaryFile (const string& basename)
+{
+  // open the temporary file
+  filename = basename + ".XXXXXXXX";
+
+  fd = mkstemp (const_cast<char*> (filename.c_str()));
+  if (fd < 0)
+    throw Error (FailedSys, "TemporaryFile",
+		 "failed mkstemp(" + filename + ")");
+
+  if (signal_handler_installed)
+    install_signal_handler ();
+
+  removed = false;
+
+  instances.insert (this);
+}
+
+//! Destructor
+TemporaryFile::~TemporaryFile ()
+{
+  remove ();
+  instances.erase (this);
+}
+
+void TemporaryFile::close ()
+{
+  if (fd < 0)
+    return;
+
+  if (::close (fd) < 0)
+    throw Error (FailedSys, "TemporaryFile",
+		 "failed close (%d)", fd);
+
+  fd = -1;
+}
+
+void TemporaryFile::remove ()
+{
+  if (removed)
+    return;
+
+  close ();
+
+  if (unlink (filename.c_str()) < 0)
+    throw Error (FailedSys, "TemporaryFile",
+		 "failed unlink(" + filename + ")");
+
+  removed = true;
+}
+
+
---
+++ Util/genutil/TemporaryFile.h
@@ -0,0 +1,72 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2006 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/psrchive/psrchive/Util/genutil/TemporaryFile.h,v $
+   $Revision: 1.1 $
+   $Date: 2006/12/14 22:50:29 $
+   $Author: straten $ */
+
+#ifndef __TemporaryFile_h
+#define __TemporaryFile_h
+
+#include <string>
+#include <set>
+
+//! Opens a temporary file with a unique name and deletes it when finished
+class TemporaryFile {
+
+ public:
+
+  //! Construct a temporary file with the specified basename
+  TemporaryFile (const std::string& basename);
+
+  //! Close and remove the temporary file
+  ~TemporaryFile ();
+
+  //! Close the temporary file
+  void close ();
+
+  //! Remove the temporary file
+  void remove ();
+
+  //! Get the file descriptor of the temporary file
+  int get_fd () const { return fd; }
+
+  //! Get the name of the temporary file
+  std::string get_filename () const { return filename; }
+
+  //! Set to true if the temporary file need not be removed
+  void set_removed (bool flag = true) { removed = flag; }
+
+ private:
+
+  //! The name of the temporary file
+  std::string filename;
+
+  //! The file descriptor of the temporary file
+  int fd;
+
+  //! Set true when the temporary file has been removed
+  bool removed;
+
+  //! Install the signal handler
+  static void install_signal_handler ();
+
+  //! Set true when the signal handler has been installed
+  static bool signal_handler_installed;
+
+  //! The signal handler ensures that all temporary files are removed
+  static void signal_handler (int);
+
+  //! The current temporary file instances
+  static std::set<TemporaryFile*> instances;
+
+};
+
+#endif
+
