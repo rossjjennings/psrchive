7b8ee20b328f8e88b4d1a6ed544f9defe9e1b94a
straten <straten>
2007-02-12 17:40:58 +0000

    correct the estimated variance, given that points > threshold are not included

--- More/General/GaussianBaseline.C
+++ More/General/GaussianBaseline.C
@@ -4,15 +4,38 @@
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
-using namespace std;
+
 #include "Pulsar/GaussianBaseline.h"
 #include "Pulsar/PhaseWeight.h"
 #include "Pulsar/Profile.h"
+#include "normal.h"
 
 #include <iostream>
 using namespace std;
 
 // #define _DEBUG 1
+
+Pulsar::GaussianBaseline::GaussianBaseline ()
+{
+  // The threshold attribute is already set by the IterativeBaseline
+  // constructor.  This call sets the moment_correction attribute
+  // accordingly.
+
+  set_threshold (threshold);
+}
+
+//! Set the threshold below which samples are included in the baseline
+void Pulsar::GaussianBaseline::set_threshold (float sigma)
+{
+  IterativeBaseline::set_threshold (sigma);
+
+  moment_correction = 0.5 / normal_moment2 (threshold);
+#ifndef _DEBUG
+  if (Profile::verbose)
+#endif
+    cerr << "Pulsar::GaussianBaseline::set_threshold correction = "
+	 << moment_correction << endl;
+}
 
 void Pulsar::GaussianBaseline::get_bounds (PhaseWeight& weight, 
 					   float& lower, float& upper)
@@ -20,12 +43,25 @@ void Pulsar::GaussianBaseline::get_bounds (PhaseWeight& weight,
   double mean, var, var_mean;
 
   weight.stats (profile, &mean, &var, &var_mean);
-  double cutoff = threshold * sqrt (var + var_mean);
+
+  double effective_variance = var + var_mean;
+
+  if (!get_initial_bounds())
+    effective_variance *= moment_correction;
 
 #ifndef _DEBUG
   if (Profile::verbose)
 #endif
-    cerr << "Pulsar::GaussianBaseline::get_bounds "
+    cerr << "Pulsar::GaussianBaseline::get_bounds"
+	 << " baseline var=" << var << " var_mean=" << var_mean
+	 << " effective=" << effective_variance << endl;
+
+  double cutoff = threshold * sqrt (effective_variance);
+
+#ifndef _DEBUG
+  if (Profile::verbose)
+#endif
+    cerr << "Pulsar::GaussianBaseline::get_bounds"
 	 << " baseline mean=" << mean << " cutoff=" << cutoff
 	 << " = " << threshold << " sigma" << endl;
 
--- More/General/Pulsar/GaussianBaseline.h
+++ More/General/Pulsar/GaussianBaseline.h
@@ -7,8 +7,8 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/GaussianBaseline.h,v $
-   $Revision: 1.3 $
-   $Date: 2006/10/06 21:13:53 $
+   $Revision: 1.4 $
+   $Date: 2007/02/12 17:40:58 $
    $Author: straten $ */
 
 #ifndef __Pulsar_GaussianBaseline_h
@@ -21,9 +21,20 @@ namespace Pulsar {
   //! Finds a baseline that contains gaussian white noise
   class GaussianBaseline : public IterativeBaseline {
 
+  public:
+
+    //! Default constructor
+    GaussianBaseline ();
+
+    //! Set the threshold below which samples are included in the baseline
+    void set_threshold (float sigma);
+
   protected:
 
     void get_bounds (PhaseWeight& weight, float& lower, float& upper);
+
+    //! Adjustment when computing variance of samples below threshold
+    float moment_correction;
 
   };
 
