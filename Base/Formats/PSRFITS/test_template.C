9c0134ca743a2437e92a47bf1b5c225ce1611f08
straten <straten>
2007-04-20 06:30:12 +0000

    added verbosity option

--- Base/Formats/PSRFITS/test_template.C
+++ Base/Formats/PSRFITS/test_template.C
@@ -14,11 +14,16 @@
 
 using namespace std;
 
-void test_template (const char* template_file, bool populate = true, 
+void test_template (const char* tplate, bool populate = true, 
 		    bool verbose = true);
-void parse_template (const char* template_file, bool verbose = true);
+void parse_template (const char* tplate, bool verbose = true);
+
+static bool verbose = false;
 
 int main (int argc, char** argv) try {
+
+  if (argc > 1 && string(argv[1]) == "-v")
+    verbose = true;
 
   unsigned nloops = 100;
 
@@ -31,11 +36,11 @@ int main (int argc, char** argv) try {
   filename += "psrheader.fits";
 
   cerr << "Parsing " << filename << endl;
-  parse_template (filename.c_str());
+  parse_template (filename.c_str(), verbose);
 
   cerr << "Creating " << nloops << " files from template" << endl;
   for (unsigned iloop=0; iloop<nloops; iloop++)
-    test_template (filename.c_str(), true, false);
+    test_template (filename.c_str(), true, verbose);
 
   cerr << "Test passed" << endl;
   return 0;
@@ -56,12 +61,12 @@ void check_length (const char* str, const char* name)
 		 name, strlen(str), FLEN_CARD, str);
 }
 
-void parse_template (const char* template_file, bool verbose)
+void parse_template (const char* tplate, bool verbose)
 {
 
-  FILE* fptr = fopen (template_file, "r");
+  FILE* fptr = fopen (tplate, "r");
   if (!fptr)
-    throw Error (FailedSys, "parse_template", "fopen(%s)", template_file);
+    throw Error (FailedSys, "parse_template", "fopen(%s)", tplate);
 
   char templt [FLEN_CARD];
   char card   [FLEN_CARD];
@@ -124,13 +129,12 @@ void parse_template (const char* template_file, bool verbose)
 }
 
 
-void test_template (const char* template_file, bool populate, bool verbose)
+void test_template (const char* tplate, bool populate, bool verbose)
 {
   char* filename = "!test_template.psrfits";
 
   if (verbose)
-    cerr << "test_template call fits_create_template (" << filename << ", " 
-	 << template_file << ")" << endl;
+    cerr << "test_template call fits_create_file (" << filename << ")" << endl;
 
   fitsfile* fptr = 0;
   int status = 0;
@@ -139,38 +143,64 @@ void test_template (const char* template_file, bool populate, bool verbose)
   if (status)
     throw FITSError (status, "test_template", "fits_create_file");
 
-  fits_execute_template (fptr, const_cast<char*>(template_file), &status);
+  if (verbose)
+    cerr << "test_template call fits_execute_template (" << tplate << ")" 
+	 << endl;
+
+  fits_execute_template (fptr, const_cast<char*>(tplate), &status);
   if (status)
     throw FITSError (status, "test_template",
-		     "fits_execute_template (%s)", template_file);
+		     "fits_execute_template (%s)", tplate);
 
   if (populate)
   {
+    if (verbose)
+      cerr << "test_template populating the file" << endl;
+
     fits_movabs_hdu (fptr, 1, 0, &status);
     if (status)
       throw FITSError (status, "test_template",
 		       "fits_moveabs_hdu");
+
+    if (verbose)
+      cerr << "test_template move to SUBINT HDU" << endl;
+
     // populate some tables
     fits_movnam_hdu (fptr, BINARY_TBL, "SUBINT", 0, &status);
     if (status != 0)
       throw FITSError (status, "test_template", "fits_movnam_hdu SUBINT");
+
     int npol = 2, nbin=3, nchan=4;
     int16 data[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
     int colnum = 0;
+
+    if (verbose)
+      cerr << "test_template get DATA column" << endl;
+
     fits_get_colnum (fptr, CASEINSEN, "DATA", &colnum, &status);
     if (status != 0)
       throw FITSError (status, "test_template", "fits_get_colnum DATA");
+
+    if (verbose)
+      cerr << "test_template modify vector length in column " << colnum << endl;
+
     fits_modify_vector_len (fptr, colnum, nchan*npol*nbin, &status);
     if (status != 0)
       throw FITSError (status, "test_template", "fits_modify_vector_len DATA");
+
+    if (verbose)
+      cerr << "test_template write column" << endl;
+
     fits_write_col (fptr, TSHORT, colnum, 1, 1, npol*nbin*nchan, 
 		    data, &status);
     if (status != 0)
       throw FITSError (status, "test_template", "fits_write_col DATA");
+
     fits_write_col (fptr, TSHORT, colnum, 2, 1, npol*nbin*nchan, 
 		    data, &status);
     if (status != 0)
       throw FITSError (status, "test_template", "fits_write_col DATA");
+
     fits_write_col (fptr, TSHORT, colnum, 3, 1, npol*nbin*nchan, 
 		    data, &status); // 3 subints
     if (status != 0)
