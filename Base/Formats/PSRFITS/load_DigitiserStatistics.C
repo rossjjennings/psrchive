7fb440f714b464f25bed0742679fd1f2941901ef
straten <straten>
2007-05-02 21:12:56 +0000

    bug fix: header version independent switch between NLEV and NPAR

--- Base/Formats/PSRFITS/load_DigitiserStatistics.C
+++ Base/Formats/PSRFITS/load_DigitiserStatistics.C
@@ -6,22 +6,19 @@
  ***************************************************************************/
 #include "Pulsar/FITSArchive.h"
 #include "Pulsar/DigitiserStatistics.h"
-#include "Pulsar/FITSHdrExtension.h"
-#include "FITSError.h"
+#include "psrfitsio.h"
 
 using namespace std;
 
-void load (fitsfile* fptr, Pulsar::DigitiserStatistics::row* drow,
-	   Pulsar::FITSHdrExtension* hdr_ext)
+void load (fitsfile* fptr, Pulsar::DigitiserStatistics::row* drow)
 {
-  int status = 0;
   int row = drow->index;
 
   if (row <= 0)
     throw Error (InvalidParam, "load (Pulsar::DigitiserStatistics::row*)",
 		 "invalid row number=%d", row);
   
-  if (Pulsar::Archive::verbose == 3) {
+  if (Pulsar::Archive::verbose > 2) {
     cerr << "FITSArchive::load_digistat_row entered" << endl;
     cerr << "loading row #" << row << endl;
   }
@@ -33,59 +30,38 @@ void load (fitsfile* fptr, Pulsar::DigitiserStatistics::row* drow,
   float nullfloat = 0.0;
   char* comment = 0;
   
-  char* temp = new char[128];
-  
-  fits_read_key (fptr, TSTRING, "DIG_MODE", temp, comment, &status);
-  if (Pulsar::Archive::verbose == 3)
-    cerr << "Read DIG_MODE = " << temp << endl;
-  drow->dig_mode = temp;
+  psrfits_read_key (fptr, "DIG_MODE", &(drow->dig_mode));
+
+  if (Pulsar::Archive::verbose > 2)
+    cerr << "Read DIG_MODE = " << drow->dig_mode << endl;
 
   // Get NDIGR
   
-  fits_read_key (fptr, TINT, "NDIGR", 
-		 &(drow->ndigr), comment, &status);
+  psrfits_read_key (fptr, "NDIGR", &(drow->ndigr));
   
   
   // Get NPAR (called NLEV prior to version 2.3)
-  char nparkeyname[8];
-  strcpy(nparkeyname,
-	 (hdr_ext->major_version < 2 ||
-	  (hdr_ext->major_version == 2 && hdr_ext->minor_version < 3)
-	  ? "NLEV" : "NPAR"));
-  fits_read_key (fptr, TINT, nparkeyname, 
- 		 &(drow->nlev), comment, &status);
-  if (status != 0)
-    throw FITSError (status, "FITSArchive::load_digistat", 
-		     nparkeyname);
-  
+
+  psrfits_read_key (fptr, "NLEV", &(drow->nlev), 0,
+		    Pulsar::Archive::verbose > 2);
+
+  if (!(drow->nlev))
+    psrfits_read_key (fptr, "NPAR", &(drow->nlev));
+
   // Get NCYCSUB
   
-  fits_read_key (fptr, TINT, "NCYCSUB", 
-		 &(drow->ncycsub), comment, &status);
+  psrfits_read_key (fptr, "NCYCSUB", &(drow->ncycsub));
   
   // Get DIGLEV
 
-  fits_read_key (fptr, TSTRING, "DIGLEV", temp, comment, &status);
-  if (Pulsar::Archive::verbose == 3)
-    cerr << "Read DIGLEV = " << temp << endl;
-  drow->diglev = temp;
+  psrfits_read_key (fptr, "DIGLEV", &(drow->diglev));
   
   // Read the data itself
   
-  colnum = 0;
-  initflag = 0;
-  fits_get_colnum (fptr, CASEINSEN, "DATA", &colnum, &status);
-  
-  int dimension = drow->nlev * drow->ndigr * drow->ncycsub;
-  
-  drow->data.resize(dimension);
-  
-  fits_read_col (fptr, TFLOAT, colnum, row, 1, dimension, &nullfloat, 
-		 &(drow->data[0]), &initflag, &status);
+  drow->data.resize( drow->nlev * drow->ndigr * drow->ncycsub );  
+  psrfits_read_col (fptr, "DATA", drow->data, row, float(0.0));
 
-  delete[] temp;
-
-  if (Pulsar::Archive::verbose == 3)
+  if (Pulsar::Archive::verbose > 2)
     cerr << "FITSArchive::load_digistat_row exiting" << endl;
 }
 
@@ -93,7 +69,7 @@ void Pulsar::FITSArchive::load_DigitiserStatistics (fitsfile* fptr)
 {
   int status = 0;
   
-  if (verbose == 3)
+  if (verbose > 2)
     cerr << "FITSArchive::load_digistat entered" << endl;
 
   // Move to the DIG_STAT HDU
@@ -101,7 +77,7 @@ void Pulsar::FITSArchive::load_DigitiserStatistics (fitsfile* fptr)
   fits_movnam_hdu (fptr, BINARY_TBL, "DIG_STAT", 0, &status);
 
   if (status == BAD_HDU_NUM) {
-    if (verbose == 3)
+    if (verbose > 2)
       cerr << "Pulsar::FITSArchive::load_digistat no DIG_STAT HDU" << endl;
 
     return;
@@ -123,15 +99,14 @@ void Pulsar::FITSArchive::load_DigitiserStatistics (fitsfile* fptr)
 
   (dstats->rows).resize(numrows);
   
-  FITSHdrExtension* hdr_ext = getadd<FITSHdrExtension>();
   for (int i = 0; i < numrows; i++) {
     dstats->rows[i] = DigitiserStatistics::row();
     dstats->rows[i].index = i+1;
-    ::load( fptr, &(dstats->rows[i]), hdr_ext );
+    ::load( fptr, &(dstats->rows[i]) );
   }
   
   add_extension (dstats);
 
-  if (verbose == 3)
+  if (verbose > 2)
     cerr << "FITSArchive::load_digistat exiting" << endl;
 }
