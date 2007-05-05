964ea42fb40615ce8dc4bcfbd65c1cf24f762842
straten <straten>
2007-05-05 23:25:14 +0000

    implements iphase

--- Util/tempo2/T2Predictor.C
+++ Util/tempo2/T2Predictor.C
@@ -8,6 +8,12 @@
 #include "T2Predictor.h"
 #include "Error.h"
 
+#include "tempo2pred_int.h"
+#include "inverse_phase.h"
+
+#include <vector>
+using namespace std;
+
 Tempo2::Predictor::Predictor ()
 {
   T2Predictor_Init (&predictor);
@@ -70,6 +76,18 @@ long double from_MJD (const MJD& t)
     (long double) (t.get_fracsec()) / secs_in_day;
 }
 
+//! convert a long double to an MJD
+MJD to_MJD (long double t)
+{
+  long double ld = floorl(t);
+  t -= ld;
+  long double lsec = ld * 86400.0L;
+  long double ls = floorl(lsec);
+  lsec -= ls;
+
+  return MJD (double(ld), double(ls), double(lsec));
+}
+
 //! convert a long double to Phase
 Phase to_Phase (long double p)
 {
@@ -86,10 +104,87 @@ Phase Tempo2::Predictor::phase (const MJD& t) const
 					 observing_frequency) );
 }
 
+
+class cheby_interface {
+
+public:
+
+  static double precision;
+
+  void construct (ChebyModel*, long double obs_freq);
+
+  MJD get_reftime() const { return t0; }
+  Phase get_refphase() const { return p0; }
+  long double get_reffrequency() const { return f0; }
+
+  Phase phase (const MJD&) const;
+  long double frequency (const MJD&) const;
+
+  MJD iphase (const Phase&) const;
+
+protected:
+
+  ChebyModel* model;
+  long double obs_freq;
+
+  MJD t0;
+  Phase p0;
+  long double f0;
+  
+};
+
+double cheby_interface::precision = 1e-15;
+
+void cheby_interface::construct (ChebyModel* _model, long double _obs_freq)
+{
+  model = _model;
+  obs_freq = _obs_freq;
+
+  long double tmid = 0.5 * (model->mjd_start + model->mjd_end);
+  t0 = to_MJD( tmid );
+  p0 = to_Phase( ChebyModel_GetPhase (model, tmid, obs_freq) );
+  f0 = ChebyModel_GetFrequency (model, tmid, obs_freq);
+}
+
+Phase cheby_interface::phase (const MJD& t) const
+{
+ return to_Phase( ChebyModel_GetPhase (model, from_MJD (t), obs_freq) );
+}
+
+MJD cheby_interface::iphase (const Phase& phase) const
+{
+  return Pulsar::inverse_phase (*this, phase);
+}
+
+long double cheby_interface::frequency (const MJD& t) const
+{
+ return ChebyModel_GetFrequency (model, from_MJD (t), obs_freq);
+}
+
 //! Return the epoch, given the phase
 MJD Tempo2::Predictor::iphase (const Phase& phase) const
 {
-  throw Error (InvalidState, "Tempo2::Predictor::iphase", "not implemented");
+  vector<cheby_interface> chebys ( predictor.modelset.cheby.nsegments );
+
+  float min_dist = 0;
+  int imin = -1;
+  unsigned icheby = 0;
+
+  for (icheby=0; icheby<chebys.size(); icheby ++)  {
+
+    chebys[icheby].construct( predictor.modelset.cheby.segments + icheby,
+			      observing_frequency );
+
+    float dist = fabs ( (chebys[icheby].get_refphase() - phase).in_turns() );
+    if (icheby == 0 || dist < min_dist) {
+      imin = icheby;
+      min_dist = dist;
+    }
+  }
+
+  MJD result;
+  if (imin > 0)
+    result = chebys[imin].iphase (phase);
 }
 
 //! Return the spin frequency, given the epoch
