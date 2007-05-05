67a32bd023fb06d758368d7092197045c2b627a5
straten <straten>
2007-05-05 13:16:20 +0000

    first attempt at interfacing fitsfile* with FILE*

--- Base/Formats/PSRFITS/load_T2Predictor.C
+++ Base/Formats/PSRFITS/load_T2Predictor.C
@@ -11,6 +11,19 @@
 
 using namespace std;
 
+typedef struct 
+{
+  fitsfile* fptr;
+  int colnum;
+  int nrow;
+  int row;
+
+  char* buffer;
+  int offset;
+} fitstream;
+
+int fitstream_readrow (void *, char *, int);
+
 void Pulsar::FITSArchive::load_T2Predictor (fitsfile* fptr)
 {
   if (verbose == 3)
@@ -22,8 +35,6 @@ void Pulsar::FITSArchive::load_T2Predictor (fitsfile* fptr)
   
   if (status != 0)
     return;
-
-  Reference::To<Tempo2::Predictor> history = new Tempo2::Predictor;
 
   long numrows = 0;
   fits_get_num_rows (fptr, &numrows, &status);
@@ -49,15 +60,78 @@ void Pulsar::FITSArchive::load_T2Predictor (fitsfile* fptr)
   char* nul = " ";
   char* temp = &(text[0]);
 
-  int anynul = 0;
-  for (int row=1; row <= numrows; row++) {
-    fits_read_col (fptr, TSTRING, colnum, row, 1, 1, nul, 
-                   &temp, &anynul, &status);
-    cerr << temp << endl;
-  }
+  fitstream input;
+  input.fptr = fptr;
+  input.colnum = colnum;
+  input.nrow = numrows;
+  input.row = 1;
+  input.buffer = &(text[0]);
+  input.offset = 0;
+
+  FILE* stream = fropen (&input, fitstream_readrow);
+  if (!stream)
+    throw Error (SysCall, "FITSArchive::load T2Predictor", "fropen");
+
+  Reference::To<Tempo2::Predictor> predictor = new Tempo2::Predictor;
+
+  predictor->load (stream);
 
   if (verbose == 3)
     cerr << "FITSArchive::load_T2Predictor exiting" << endl;
 }
 
+int fitstream_readrow (void* cookie, char* text, int toread)
+{
+  cerr << "toread=" << toread << endl;
 
+  fitstream* c = (fitstream*) cookie;
+
+  int haveread = 0;
+
+  while (haveread < toread) {
+
+    int left_to_read = toread - haveread;
+
+    if (c->offset == 0) {
+
+      if (c->row > c->nrow)
+	break;
+
+      int anynul = 0;
+      int status = 0;
+      fits_read_col (c->fptr, TSTRING, c->colnum, c->row, 1, 1, 0, 
+		     &(c->buffer), &anynul, &status);
+      if (status != 0)
+	throw FITSError (status, "fitstream_readrow", 
+			 "fits_read_col");
+
+      cerr << "loaded\n" << c->buffer << endl;
+
+      c->row ++;
+    }
+
+    int length = strlen (c->buffer);
+    int available = length - c->offset;
+
+    if (available > left_to_read)
+      available = left_to_read;
+      
+    memcpy (text + haveread, c->buffer + c->offset, available);
+
+    c->offset += available;
+    haveread += available;
+
+    left_to_read -= available;
+
+    if (c->offset == length) {
+
+      if (left_to_read) {
+	text[haveread] = '\n';
+	haveread ++;
+      }
+
+      c->offset = 0;
+    }
+  }
+  return haveread;
+}
