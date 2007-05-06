dd9cbf4ccd69a6dda28af8ff7270925a6d80d457
straten <straten>
2007-05-06 23:04:37 +0000

    inverse_phase.C defines global variables

---
+++ Util/tempo/inverse_phase.C
@@ -0,0 +1,11 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "inverse_phase.h"
+
+unsigned Pulsar::inverse_phase_calls = 0;
+unsigned Pulsar::inverse_phase_iterations = 0;
