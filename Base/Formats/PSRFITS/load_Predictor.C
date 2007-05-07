9ed369b2a1e6c5640c00234777d1ba993941f352
straten <straten>
2007-05-07 04:31:26 +0000

    bug fix: set the model attribute when loading T2Predictor

--- Base/Formats/PSRFITS/load_Predictor.C
+++ Base/Formats/PSRFITS/load_Predictor.C
@@ -15,19 +15,21 @@
 using namespace std;
 
 // load a tempo polyco, defined in load_polyco.C
-Pulsar::Predictor* load_polyco (fitsfile* fptr, FITSPolyco*, bool verbose);
+Pulsar::Predictor* load_polyco (fitsfile* fptr, double* phase, bool verbose);
 
 // load a tempo2 predictor, defined in load_T2Predictor.C
 Pulsar::Predictor* load_T2Predictor (fitsfile* fptr, bool verbose);
 
 void Pulsar::FITSArchive::load_Predictor (fitsfile* fptr)
 {
-  hdr_model = model = load_polyco (fptr, &extra_polyco, verbose > 2);
+  model = load_polyco (fptr, &predicted_phase, verbose > 2);
 
 #ifdef HAVE_TEMPO2
   if (!model)
     // Load the Tempo2 Predictor, if any
-    load_T2Predictor (fptr, verbose > 2);
+    model = load_T2Predictor (fptr, verbose > 2);
 #endif
+
+  hdr_model = model;
 }
 
