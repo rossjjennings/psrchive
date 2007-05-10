6556bc72827ea42ceb206b995a931a1206c32784
straten <straten>
2007-05-10 06:00:52 +0000

    bug fix: elapsed time calculation was completely wrong

--- More/Plotting/PhaseVsTime.C
+++ More/Plotting/PhaseVsTime.C
@@ -31,29 +31,28 @@ void Pulsar::PhaseVsTime::prepare (const Archive* data)
     return;
   }
 
-  double mjd0 = data->start_time().in_days();
-  double mjd1 = data->end_time().in_days();
+  double range = (data->end_time() - data->start_time()).in_days();
 
-  float to_hours = 1.0 / 24.0;
-  float to_minutes = to_hours / 60.0;
-  float to_seconds = to_minutes / 60.0;
+  const float mjd_hours = 1.0 / 24.0;
+  const float mjd_minutes = mjd_hours / 60.0;
+  const float mjd_seconds = mjd_minutes / 60.0;
 
-  time_string = "Time ";
-  if (mjd1 - mjd0 > 1.0) {
+  time_string = "Approximate Elapsed Time ";
+  if (range > 1.0) {
     time_string += "(days)";
-    set_yrange (mjd0, mjd1);
+    set_yrange (0, range);
   }
-  else if (mjd1 - mjd0 > to_hours) {
+  else if (range > mjd_hours) {
     time_string += "(hours)";
-    set_yrange (mjd0*to_hours, mjd1*to_hours);
+    set_yrange (0, range/mjd_hours);
   }
-  else if (mjd1 - mjd0 > to_minutes) {
+  else if (range > mjd_minutes) {
     time_string += "(minutes)";
-    set_yrange (mjd0*to_minutes, mjd1*to_minutes);
+    set_yrange (0, range/mjd_minutes);
   }
   else {
     time_string += "(seconds)";
-    set_yrange (mjd0*to_seconds, mjd1*to_seconds);
+    set_yrange (0, range/mjd_seconds);
   }
 }
 
