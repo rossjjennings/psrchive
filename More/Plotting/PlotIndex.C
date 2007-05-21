5b63fb0ec28ebe08ef34b6bcdda69d6047030032
straten <straten>
2007-05-21 08:06:58 +0000

    Bugs Request ID 1722374

--- More/Plotting/PlotIndex.C
+++ More/Plotting/PlotIndex.C
@@ -9,6 +9,8 @@
 #include "Pulsar/IntegrationExpert.h"
 #include "Pulsar/PolnProfile.h"
 
+using namespace std;
+
 Pulsar::PlotIndex::PlotIndex (unsigned value, bool flag)
 {
   index = value;
@@ -22,33 +24,55 @@ Pulsar::get_Profile (const Archive* data,
   if (!data)
     throw Error (InvalidParam, "Pulsar::get_Profile", "no Archive");
 
-  Reference::To<const Integration> integration;
-  integration = get_Integration( data, subint );
+  try {
 
-  Reference::To<Integration> integration_clone;
+    if (Archive::verbose > 2) cerr << "Pulsar::get_Profile "
+      "(" << subint << "," << pol << "," << chan << ")" << endl;
 
-  if (pol.get_integrate()) {
-    if (!integration_clone)
-      integration_clone = integration->clone();
-    integration_clone->expert()->pscrunch();
-    integration = integration_clone;
+    Reference::To<const Integration> integration;
+    integration = get_Integration( data, subint );
+
+    Reference::To<Integration> integration_clone;
+    
+    if (pol.get_integrate()) {
+
+      if (Archive::verbose > 2)
+	cerr << "Pulsar::get_Profile pol=I pscrunch" << endl;
+
+      if (!integration_clone)
+	integration_clone = integration->clone();
+
+      integration_clone->expert()->pscrunch();
+      integration = integration_clone;
+
+    }
+    
+    if (chan.get_integrate()) {
+
+      if (Archive::verbose > 2)
+	cerr << "Pulsar::get_Profile chan=I fscrunch" << endl;
+
+      if (!integration_clone)
+	integration_clone = integration->clone();
+
+      integration_clone->expert()->fscrunch();
+      integration = integration_clone;
+
+    }
+
+    Reference::To<const Profile> profile;
+    profile = integration -> get_Profile (pol.get_value(), chan.get_value());
+    
+    // ensure that no one destroys the profile when they go out of scope
+    integration.release();
+    integration_clone.release();
+
+    return profile.release();
+
   }
-
-  if (chan.get_integrate()) {
-    if (!integration_clone)
-      integration_clone = integration->clone();
-    integration_clone->expert()->fscrunch();
-    integration = integration_clone;
+  catch (Error& error) {
+    throw error += "Pulsar::get_Profile (PlotIndex)";
   }
-
-  Reference::To<const Profile> profile;
-  profile = integration -> get_Profile (pol.get_value(), chan.get_value());
-
-  // ensure that no one destroys the profile when they go out of scope
-  integration = 0;
-  integration_clone = 0;
-
-  return profile.release();
 }
 
 const Pulsar::Integration* 
@@ -57,56 +81,83 @@ Pulsar::get_Integration (const Archive* data, PlotIndex subint)
   if (!data)
     throw Error (InvalidParam, "Pulsar::get_Integration", "no Archive");
 
-  Reference::To<const Archive> archive = data;
+  try {
 
-  if (subint.get_integrate()) {
-    Reference::To<Archive> archive_clone = archive->clone();
-    archive_clone->tscrunch();
-    archive = archive_clone;
+    if (Archive::verbose > 2) cerr << "Pulsar::get_Integration "
+      "(" << subint << ")" << endl;
+
+    Reference::To<const Archive> archive = data;
+
+    if (subint.get_integrate()) {
+
+      if (Archive::verbose > 2)
+	cerr << "Pulsar::get_Integration chan=I tscrunch" << endl;
+
+      Reference::To<Archive> archive_clone = archive->clone();
+      archive_clone->tscrunch();
+      archive = archive_clone;
+
+    }
+
+    Reference::To<const Integration> integration;
+    integration = archive->get_Integration(subint.get_value());
+    
+    // ensure that no one destroys the profile when they go out of scope
+    archive.release();
+
+    return integration.release();
   }
-
-  Reference::To<const Integration> integration;
-  integration = archive->get_Integration(subint.get_value());
-
-  // ensure that no one destroys the profile when they go out of scope
-  archive = 0;
-
-  return integration.release();
+  catch (Error& error) {
+    throw error += "Pulsar::get_Integration (PlotIndex)";
+  }
 }
 
 //! Return a newly constructed PolnProfile with state == Stokes
 const Pulsar::PolnProfile* 
 Pulsar::get_Stokes (const Archive* data, PlotIndex subint, PlotIndex chan)
 {
-  Reference::To<const Integration> integration;
-  integration = get_Integration( data, subint );
+  if (!data)
+    throw Error (InvalidParam, "Pulsar::get_Stokes (PlotIndex)", "no Archive");
 
-  Reference::To<Integration> integration_clone;
+  try {
 
-  if (chan.get_integrate()) {
-    integration_clone = integration->clone();
-    integration_clone->expert()->fscrunch();
-    integration = integration_clone;
+    if (Archive::verbose > 2) cerr << "Pulsar::get_Stokes "
+      "(" << subint << "," << chan << ")" << endl;
+
+    Reference::To<const Integration> integration;
+    integration = get_Integration( data, subint );
+
+    Reference::To<Integration> integration_clone;
+    
+    if (chan.get_integrate()) {
+      integration_clone = integration->clone();
+      integration_clone->expert()->fscrunch();
+      integration = integration_clone;
+    }
+
+    Reference::To<const PolnProfile> profile;
+    profile = integration->new_PolnProfile(chan.get_value());
+
+    Reference::To<PolnProfile> profile_clone;
+    if (integration_clone)
+      profile_clone = integration_clone->new_PolnProfile(chan.get_value());
+
+    if (profile->get_state() != Signal::Stokes) {
+      if (!profile_clone)
+	profile_clone = profile->clone();
+      profile_clone->convert_state(Signal::Stokes);
+      profile = profile_clone;
+    }
+    
+    // ensure that profile_clone doesn't destroy as it goes out of scope
+    profile_clone.release();
+    
+    return profile.release();
+
   }
-
-  Reference::To<const PolnProfile> profile;
-  profile = integration->new_PolnProfile(chan.get_value());
-
-  Reference::To<PolnProfile> profile_clone;
-  if (integration_clone)
-    profile_clone = integration_clone->new_PolnProfile(chan.get_value());
-
-  if (profile->get_state() != Signal::Stokes) {
-    if (!profile_clone)
-      profile_clone = profile->clone();
-    profile_clone->convert_state(Signal::Stokes);
-    profile = profile_clone;
+  catch (Error& error) {
+    throw error += "Pulsar::get_Stokes (PlotIndex)";
   }
-
-  // ensure that profile_clone doesn't destroy instance as it goes out of scope
-  profile_clone = 0;
-
-  return profile.release();
 }
 
 std::ostream& Pulsar::operator << (std::ostream& os, const PlotIndex& i)
