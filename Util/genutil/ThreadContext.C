33ae601fd90ffcb23abedc5d952a9b3c861376bb
straten <straten>
2007-05-27 00:25:53 +0000

    define empty methods and throw exception in constructor if HAVE_PTHREAD == 0

--- Util/genutil/ThreadContext.C
+++ Util/genutil/ThreadContext.C
@@ -11,8 +11,14 @@
 
 ThreadContext::ThreadContext ()
 {
+#if HAVE_PTHREAD
   pthread_cond_init (&cond, NULL);
   pthread_mutex_init (&mutex, NULL);
+#else
+  // if pthreads aren't available, throw an exception
+  throw Error (InvalidState, "ThreadContext ctor",
+	       "pthread support is not available");
+#endif
 }
 
 ThreadContext::~ThreadContext ()
@@ -21,35 +27,43 @@ ThreadContext::~ThreadContext ()
 
 void ThreadContext::lock ()
 {
+#if HAVE_PTHREAD
   errno = pthread_mutex_lock(&mutex);
   if (errno != 0)
     throw Error (FailedSys, "Calibration::ReceptionModel::solve",
 		 "pthread_mutex_lock");
+#endif
 }
 
 void ThreadContext::unlock ()
 {
+#if HAVE_PTHREAD
   errno = pthread_mutex_unlock(&mutex);
   if (errno != 0)
     throw Error (FailedSys, "Calibration::ReceptionModel::solve",
 		 "pthread_mutex_unlock");
+#endif
 }
 
 void ThreadContext::wait ()
 {
+#if HAVE_PTHREAD
   errno = pthread_cond_wait(&cond, &mutex);
   if (errno != 0) {
     pthread_mutex_unlock(&mutex);
     throw Error (FailedSys, "Calibration::ReceptionModel::solve",
 		 "pthread_cond_wait");
   }
+#endif
 }
 
 void ThreadContext::signal ()
 {
+#if HAVE_PTHREAD
   errno = pthread_cond_signal (&cond);
   if (errno != 0)
     throw Error (FailedSys, "Calibration::ReceptionModel::solve",
 		 "pthread_cond_signal");
+#endif
 }
 
