15437be070700e58f2f014ac3448afc53a48ce68
straten <straten>
2007-05-27 08:52:48 +0000

    implement the 2D transforms

--- Util/fft/MKL_DFTI_Transform.C
+++ Util/fft/MKL_DFTI_Transform.C
@@ -4,11 +4,6 @@
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#if HAVE_MKL_DFTI
 
 #include "MKL_DFTI_Transform.h"
 #include "Error.h"
@@ -21,13 +16,7 @@ using namespace std;
 
    *********************************************************************** */
 
-FTransform::MKL_DFTI::Plan::~Plan()
-{
-  if (plan)
-    DftiFreeDescriptor(&plan);
-}
-
-FTransform::MKL_DFTI::Plan::Plan (size_t n, type t)
+FTransform::MKL_DFTI::Plan::Plan (size_t n_fft, type t)
 {
 #ifdef _DEBUG
   cerr << "FTransform::MKL_DFTI::Plan nfft=" << n
@@ -35,6 +24,8 @@ FTransform::MKL_DFTI::Plan::Plan (size_t n, type t)
 #endif
 
   int status;
+
+  long n = n_fft;
 
   if (t & real)
     status = DftiCreateDescriptor( &plan, DFTI_SINGLE, DFTI_REAL, 1, n );
@@ -70,9 +61,15 @@ FTransform::MKL_DFTI::Plan::Plan (size_t n, type t)
     throw Error (InvalidState, "FTransform::MKL_DFTI::Plan ctor",
 		 "fail DftiCommitDescriptor");
 
-  this->nfft = n;
+  this->nfft = n_fft;
   this->call = t;
   this->optimized = optimize;
+}
+
+FTransform::MKL_DFTI::Plan::~Plan()
+{
+  if (plan)
+    DftiFreeDescriptor(&plan);
 }
 
 void FTransform::MKL_DFTI::Plan::frc1d (size_t nfft, float* dest, 
@@ -99,8 +96,6 @@ void FTransform::MKL_DFTI::Plan::bcr1d (size_t nfft, float* dest,
   DftiComputeBackward( plan, (void*) src, dest );
 }
 
-#if 0
-
 /* ***********************************************************************
 
    Two-dimensional interface
@@ -110,43 +105,67 @@ void FTransform::MKL_DFTI::Plan::bcr1d (size_t nfft, float* dest,
 
 FTransform::MKL_DFTI::Plan2::Plan2 (size_t n_x, size_t n_y, type t)
 {
-  fftw_direction direction_flags;
-  if( t & forward )
-    direction_flags = MKL_DFTI_FORWARD;
+  long n[2];
+  n[0] = n_x;
+  n[1] = n_y;
+
+  int status;
+
+  if (t & real)
+    status = DftiCreateDescriptor( &plan, DFTI_SINGLE, DFTI_REAL, 1, n );
   else
-    direction_flags = MKL_DFTI_BACKWARD;
+    status = DftiCreateDescriptor( &plan, DFTI_SINGLE, DFTI_COMPLEX, 1, n );
 
-  int flags = 0;
-  if (optimized)
-    flags |= MKL_DFTI_MEASURE;
-  else
-    flags |= MKL_DFTI_ESTIMATE;
+  if (! DftiErrorClass(status, DFTI_NO_ERROR))
+    throw Error (InvalidState, "FTransform::MKL_DFTI::Plan2 ctor",
+		 "fail DftiCreateDescriptor");
 
-  plan = fftw2d_create_plan (n_x, n_y, direction_flags, flags);
-  nx = n_x;
-  ny = n_y;
-  call = t;
+#if 0
 
+  // this code is in the double precision version in
+  // interfaces/fftw3xc/wrappers
+  if (t & real) {
+    status = DftiSetValue
+      ( plan, DFTI_CONJUGATE_EVEN_STORAGE, DFTI_COMPLEX_COMPLEX );
+
+    if (! DftiErrorClass(status, DFTI_NO_ERROR))
+      throw Error (InvalidState, "FTransform::MKL_DFTI::Plan2 ctor",
+		   "fail DftiSetValue DFTI_CONJUGATE_EVEN_STORAGE");
+  }
+
+#endif
+
+  status = DftiSetValue( plan, DFTI_PLACEMENT, DFTI_NOT_INPLACE );
+  if (! DftiErrorClass(status, DFTI_NO_ERROR))
+    throw Error (InvalidState, "FTransform::MKL_DFTI::Plan2 ctor",
+		 "fail DftiSetValue DFTI_PLACEMENT");
+
+  status = DftiCommitDescriptor( plan );
+  if (! DftiErrorClass(status, DFTI_NO_ERROR))
+    throw Error (InvalidState, "FTransform::MKL_DFTI::Plan2 ctor",
+		 "fail DftiCommitDescriptor");
+
+  this->nx = n_x;
+  this->ny = n_y;
+  this->call = t;
 }
 
 FTransform::MKL_DFTI::Plan2::~Plan2 ()
 {
-  fftwnd_destroy_plan ((fftwnd_plan)plan);
+  if (plan)
+    DftiFreeDescriptor(&plan);
 }
 
 void FTransform::MKL_DFTI::Plan2::fcc2d (size_t nx, size_t ny,
 					 float* dest, const float* src)
 {
-  fftwnd_one ((fftwnd_plan)plan, (fftw_complex*)src, (fftw_complex*)dest);
+  DftiComputeForward( plan, (void*) src, dest );
 }
 
 
 void FTransform::MKL_DFTI::Plan2::bcc2d (size_t nx, size_t ny,
 					 float* dest, const float* src)
 {
-  fftwnd_one ((fftwnd_plan)plan, (fftw_complex*)src, (fftw_complex*)dest);
+  DftiComputeBackward( plan, (void*) src, dest );
 }
 
-#endif
-
-#endif
