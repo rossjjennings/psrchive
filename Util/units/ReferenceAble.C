55369df9312d78100c5f05c94c49923d108986db
straten <straten>
2007-05-28 19:41:04 +0000

    cleaning up the code in preparation for optimization testing

--- Util/units/ReferenceAble.C
+++ Util/units/ReferenceAble.C
@@ -12,28 +12,22 @@
 using namespace std;
 
 #if _DEBUG
-
 #include <iostream>
-
-vector<Reference::Able*> Reference::Able::null_ables(){
-  vector<Able*> nullie;
-  return nullie;
-}
-
-int Reference::Able::instantiation_count = 0;
-int Reference::Able::full_count = 0;
-vector<Reference::Able*> Reference::Able::ables = Reference::Able::null_ables();
 #endif
+
+// static ensures read-only access via get_instance_count
+static uint64_t instance_count = 0;
+
+uint64_t Reference::Able::get_instance_count ()
+{
+  return instance_count;
+}
 
 //////////////////////////////////////////////////////////////////////////
 //
 Reference::Able::Able ()
 {
-#if _DEBUG
-  instantiation_count++;
-  full_count++;
-  ables.push_back(this);
-#endif
+  instance_count++;
   __reference_count = 0;
 }
 
@@ -41,36 +35,28 @@ Reference::Able::Able ()
 //
 Reference::Able::Able (const Able&)
 {
-#if _DEBUG
-  instantiation_count++;
-  full_count++;
-  ables.push_back(this);
-#endif
+  instance_count++;
   __reference_count = 0;
+}
+
+template<class C>
+void clean (C& c)
+{
+  typename C::iterator it = c.begin();
+  while (it != c.end()) {
+    *(*it) = 0;
+    it ++;
+  }
+  c.resize(0);
 }
 
 //////////////////////////////////////////////////////////////////////////
 //
 Reference::Able::~Able ()
 { 
-#if _DEBUG
-  instantiation_count--;
-  for( unsigned iable=0; iable<ables.size(); iable++){
-    if( ables[iable]==this ){
-      ables.erase(ables.begin()+iable);
-      break;
-    }
-  }
+  instance_count--;
 
-  cerr << "Reference::Able::~Able with " << __reference_list.size()
-       << " references" <<endl;
-#endif
-
-  vector<Able**>::iterator it = __reference_list.begin();
-  while (it != __reference_list.end()) {
-    *(*it) = 0;
-    it ++;
-  }
+  clean (__reference_list);
 
 #ifdef _DEBUG
   cerr << "Reference::Able::~Able exit" <<endl;
@@ -105,6 +91,16 @@ void Reference::Able::__add_reference (bool active, Able** ref_address) const
 
 }
 
+template<class C, class T>
+void erase (C& c, T t)
+{
+  for (typename C::iterator it = c.begin(); it != c.end(); it++)
+    if (*it == t) {
+      c.erase(it);
+      break;
+    }
+}
+
 //////////////////////////////////////////////////////////////////////////
 //
 /*! Declared const in order to enable Reference::To<const Klass> */
@@ -120,18 +116,12 @@ void Reference::Able::__remove_reference (bool active, Able** ref_address,
   // function is declared const, but __reference_list must be modified
   Able* thiz = const_cast<Able*> (this);
 
-  vector<Able**>::iterator it = thiz->__reference_list.begin();
-  while (it != thiz->__reference_list.end())  {
-    if (*it == ref_address) {
-      thiz->__reference_list.erase(it);
-      break;
-    }
-    else
-      it ++;
-  }
+  erase (thiz->__reference_list, ref_address);
 
-  if (active)
+  if (active)  {
+    // assert (__reference_count > 0);
     thiz->__reference_count --;
+  }
 
 #ifdef _DEBUG
   cerr << "Reference::Able::__remove_reference count="
@@ -139,11 +129,14 @@ void Reference::Able::__remove_reference (bool active, Able** ref_address,
 #endif
 
   // delete when reference count reaches zero and instance is on heap
-  if (active && auto_delete && __is_on_heap() && __reference_count == 0) {
+  if ( active && auto_delete && __reference_count == 0 && __is_on_heap() ) {
 
 #ifdef _DEBUG
     cerr << "Reference::Able::__remove_reference delete this=" << this << endl;
 #endif
+    thiz->__heap_state = 0x02;
+
+    clean(thiz->__reference_list);
 
     delete this;
 
