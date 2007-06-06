1948fbf71e6fbef76c60d6402e624090719f1031
straten <straten>
2007-06-06 01:05:51 +0000

    there isn't really an 'info' option to the set= command

--- More/Plotting/PlotAttributesTI.C
+++ More/Plotting/PlotAttributesTI.C
@@ -25,5 +25,5 @@ Pulsar::PlotAttributes::Interface::Interface (PlotAttributes* instance)
 
   add( &PlotAttributes::get_applied,
        &PlotAttributes::apply_set,
-       "set", "Apply an option set (pub,blank,info)" );
+       "set", "Apply an option set ('pub' or 'blank')" );
 }
