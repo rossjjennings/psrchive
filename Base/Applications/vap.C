7f23b79d889cfab74b336d97e7cae04784cd3000
nopeer <nopeer>
2007-06-20 02:49:39 +0000

    added most of the old vap functionality back in

--- Base/Applications/vap.C
+++ Base/Applications/vap.C
@@ -5,125 +5,346 @@
  *
  ***************************************************************************/
 
-#include "Pulsar/Archive.h"
-#include "TextInterface.h"
+#include <Pulsar/Archive.h>
+#include <Pulsar/FITSHdrExtension.h>
+#include <Pulsar/ObsExtension.h>
+#include <Pulsar/ITRFExtension.h>
+#include <Pulsar/Backend.h>
+#include <Pulsar/Receiver.h>
+#include <Pulsar/ArchiveTI.h>
+#include <Pulsar/WidebandCorrelator.h>
+#include <Pulsar/FITSHdrExtensionTI.h>
+#include <Pulsar/ObsExtensionTI.h>
+#include <Pulsar/ITRFExtensionTI.h>
+#include <Pulsar/BackendTI.h>
+#include <Pulsar/ReceiverTI.h>
+#include <Pulsar/WidebandCorrelatorTI.h>
+#include <Pulsar/FluxCalibratorExtension.h>
+#include <Pulsar/ProcHistory.h>
+#include <Pulsar/Parameters.h>
+#include <Pulsar/Predictor.h>
+#include <TextInterface.h>
 
-#include "dirutil.h"
-#include "strutil.h"
+#include <dirutil.h>
+#include <strutil.h>
+#include "remap.h"
+#include "tostring.h"
+#include <Angle.h>
+
+
+#include "TextFinder.h"
 
 #include <unistd.h>
 
+#include <sstream>
 
-/////////////////////////////////////////////////////////////////////////////
 
-namespace Pulsar {
-
-  //! Provides a text interface for read-only access to Archive attributes
-  class VapTI : public TextInterface::To<Archive> {
-
-  public:
-
-    //! Constructor
-    VapTI ();
-
-  };
-
-  VapTI::VapTI ()
-  {
-    add( &Archive::get_filename, "file",  "Name of the file" );
-    add( &Archive::get_source,   "name",  "Name of the source" );
-    add( &Archive::get_nbin,     "nbin",  "Number of pulse phase bins" );
-    add( &Archive::get_nchan,    "nchan", "Number of frequency channels" );
-    add( &Archive::get_npol,     "npol",  "Number of polarizations" );
-    add( &Archive::get_nsubint,  "nsub",  "Number of sub-integrations" );
-    add( &Archive::start_time,   "stime", "Observation start time (MJD)" );
-    add( &Archive::end_time,     "etime", "Observation end time (MJD)" );
-
-    add( &Archive::integration_length,   "leng",  "Total integrated time (s)");
-    add( &Archive::get_telescope_code,   "site",  "Telescope tempo code" );
-    add( &Archive::get_centre_frequency, "freq",  "Centre frequency (MHz)" );
-    add( &Archive::get_bandwidth,        "bw",    "Bandwidth (MHz)" );
-    add( &Archive::get_coordinates,      "coord", "Sky coordinates (RA,DEC)" );
-
-    add( &Archive::get_dispersion_measure, 
-	 "dm", "Dispersion measure (pc/cm^3)" );
-    
-    add( &Archive::get_rotation_measure,
-	 "rm", "Rotation measure (rad/m^2)" );
-  
-    add( &Archive::get_state,    "state", "Data state identifier" );
-    add( &Archive::get_scale,    "scale", "Units of amplitude flux scale" );
-    add( &Archive::get_type,     "type",  "Observation type code" );
-
-    add( &Archive::get_dedispersed, 
-	 "dmc", "Dispersion corrected (boolean)");
-    
-    add( &Archive::get_faraday_corrected,
-	 "rmc", "Faraday Rotation corrected (boolean)" );
-    
-    add( &Archive::get_poln_calibrated,
-	 "polc", "Polarization calibrated (boolean)" );
-
-  };
-
-}
-
-/////////////////////////////////////////////////////////////////////////////
 
 
 using namespace std;
+using namespace Pulsar;
 
-void usage ()
+
+
+
+bool ephemmode = false;
+bool polycmode = false;
+bool verbose = false;
+bool show_extensions = false;
+bool hide_headers = false;
+vector< string > commands;
+vector< vector< string > > results;
+vector< string > current_row;
+
+
+
+
+class VapTextFinder : public ArchiveTextFinder
 {
-  cout <<
-    "A program for getting Pulsar::Archive attributes\n"
-    "Usage:\n"
-    "     vap -c PAR [-c PAR ...] [-E] [-p] filenames\n"
-    "Where:\n"
-    "\n"
-    "PAR is a string containing one or more parameter names, separated by\n"
-    "    commas.\n"
-    "    If any whitespace is required, then the string containing it must\n"
-    "    be enclosed in quotation marks. Multiple parameters may also be\n"
-    "    specified by using multiple -c options.\n"
-    "\n"
-    "    vap -c name,freq\n"
-    "\n"
-    "    will print the source name and centre frequency.\n"
-    "    Note that parameter names are case insensitive.\n"
-    "    For a full list of parameter names, type \"vap -H\"\n"
-    "\n"
-    "-E  is used to print the most recent ephemeris in an archive\n"
-    "\n"
-    "-p  is used to print the set of polynomial coefficients\n"
-       << endl;
+public:
+  VapTextFinder() : ArchiveTextFinder()
+  {
+    AddGrp( "COMMON PARAMETERS" );
+    AddMap( "name", "ArchiveTI-name" );
+    AddMap( "nbin", "ArchiveTI-nbin" );
+    AddMap( "nchan", "ArchiveTI-nchan" );
+    AddMap( "npol", "ArchiveTI-npol" );
+    AddMap( "nsub", "ArchiveTI-nsub" );
+    AddMap( "stime", "ArchiveTI-stime" );
+    //AddMap( "etime", "ArchiveTI-etime" );
+    AddMap( "length", "ArchiveTI-length" );
+    AddMap( "rm", "ArchiveTI-rm" );
+    AddMap( "state", "ArchiveTI-state" );
+    AddMap( "scale", "ArchiveTI-scale" );
+    AddMap( "type", "ArchiveTI-type" );
+    AddMap( "dmc", "ArchiveTI-dmc" );
+    AddMap( "rmc", "ArchiveTI-rmc" );
+    AddMap( "polc", "ArchiveTI-polc" );
+    AddMap( "freq", "ArchiveTI-freq" );
+    AddMap( "bw", "ArchiveTI-bw" );
+    AddMap( "intmjd", "ArchiveTI-intmjd" );
+    AddMap( "fracmjd", "ArchiveTI-fracmjd" );
+    AddMap( "para", "ArchiveTI-para" );
+    AddMap( "tsub", "ArchiveTI-tsub" );
+
+    AddGrp( "OBSERVER PARAMETERS" );
+    AddMap( "observer", "ObsExtensionTI-observer" );
+    AddMap( "projid", "ObsExtensionTI-projid" );
+
+    AddGrp( "RECEIVER PARAMETERS" );
+    AddMap( "ta", "ReceiverTI-ta" );
+    AddMap( "fac", "ReceiverTI-fac" );
+    AddMap( "basis", "ReceiverTI-basis" );
+    AddMap( "hand", "ReceiverTI-hand" );
+    AddMap( "rph", "ReceiverTI-rph" );
+    AddMap( "oa", "ReceiverTI-oa" );
+    AddMap( "recv_ra", "ReceiverTI-ra" );
+    AddMap( "xo", "ReceiverTI-xo" );
+    AddMap( "yo", "ReceiverTI-yo" );
+    AddMap( "co", "ReceiverTI-co" );
+
+    AddGrp( "TELESCOPE PARAMETERS" );
+    AddMap( "ant_x", "ITRFExtensionTI-ant_x" );
+    AddMap( "ant_y", "ITRFExtensionTI-ant_y" );
+    AddMap( "ant_z", "ITRFExtensionTI-ant_z" );
+    AddMap( "telescop", "ObsExtensionTI-telescop" );
+    AddMap( "site", "ArchiveTI-site" );
+
+    AddGrp( "BACKEND PARAMETERS" );
+    AddMap( "backend", "BackendTI-name" );
+    AddMap( "be_dcc", "BackendTI-dcc" );
+    AddMap( "be_phase", "BackendTI-phase" );
+    AddMap( "beconfig", "WidebandCorrelatorTI-beconfig" );
+    AddMap( "tcycle", "WidebandCorrelatorTI-tcycle" );
+
+    AddGrp( "PSRFITS PARAMETERS" );
+    AddMap( "obs_mode", "FITSHdrExtensionTI-obs_mode" );
+    AddMap( "hdrver", "FITSHdrExtensionTI-hdrver" );
+    AddMap( "stt_date", "FITSHdrExtensionTI-stt_date" );
+    AddMap( "stt_time", "FITSHdrExtensionTI-stt_time" );
+    AddMap( "coord_md", "FITSHdrExtensionTI-coord_md" );
+    AddMap( "equinox", "FITSHdrExtensionTI-equinox" );
+    AddMap( "trk_mode", "FITSHdrExtensionTI-trk_mode" );
+    AddMap( "bpa", "FITSHdrExtensionTI-bpa" );
+    AddMap( "bmaj", "FITSHdrExtensionTI-bmaj" );
+    AddMap( "bmin", "FITSHdrExtensionTI-bmin" );
+    AddMap( "stt_imjd", "FITSHdrExtensionTI-stt_imjd" );
+    AddMap( "stt_smjd", "FITSHdrExtensionTI-stt_smjd" );
+    AddMap( "stt_offs", "FITSHdrExtensionTI-stt_offs" );
+    AddMap( "ra", "FITSHdrExtensionTI-ra" );
+    AddMap( "dec", "FITSHdrExtensionTI-dec" );
+    AddMap( "stt_crd1", "FITSHdrExtensionTI-stt_crd1" );
+    AddMap( "stt_crd2", "FITSHdrExtensionTI-stt_crd2" );
+    AddMap( "stp_crd1", "FITSHdrExtensionTI-stp_crd1" );
+    AddMap( "stp_crd2", "FITSHdrExtensionTI-stp_crd2" );
+
+    AddGrp( "FLUXCAL" );
+    AddMap( "nchan_fluxcal", "FluxCalibratorExtensionTI-nchan" );
+    AddMap( "nrcvr_fluxcal", "FluxCalibratorExtensionTI-nrcvr" );
+    AddMap( "epoch_fluxcal", "FluxCalibratorExtensionTI-epoch" );
+
+    AddGrp( "HISTORY" );
+    AddMap( "nbin_prd", "ProcHistoryTI-nbin_prd" );
+    AddMap( "tbin", "ProcHistoryTI-tbin" );
+    AddMap( "chbw", "ProcHistoryTI-chan_bw" );
+
+    AddGrp( "BANDPASS" );
+    AddMap( "npol_bp", "PassbandTI-npol" );
+    AddMap( "nch_bp", "PassbandTI-nchan" );
+
+    AddGrp( "FEED PARAMETERS" );
+    AddMap( "npar_feed", "PolnCalibratorExtensionTI-npar" );
+    AddMap( "nchan_feed", "PolnCalibratorExtensionTI-nchan" );
+    AddMap( "MJD_feed", "PolnCalibratorExtensionTI-epoch" );
+
+    AddGrp( "DIGITISER STATISTICS PARAMETERS" );
+    AddMap( "ndigstat", "DigitiserStatisticsTI-ndigr" );
+    AddMap( "npar_digstat", "DigitiserStatisticsTI-npar" );
+    AddMap( "ncycsub", "DigitiserStatisticsTI-ncycsub" );
+    AddMap( "levmode_digstat", "DigitiserStatisticsTI-diglev" );
+
+    AddGrp( "DIGITISER COUNTS PARAMETERS" );
+    AddMap( "dig_mode", "DigitiserCountsTI-dig_mode" );
+    AddMap( "nlev_digcnts", "DigitiserCountsTI-nlev" );
+    AddMap( "npthist", "DigitiserCountsTI-npthist" );
+    AddMap( "levmode_digcnts", "DigitiserCountsTI-diglev" );
+
+    AddGrp( "SUBINT PARAMETERS" );
+    AddMap( "subint_type", "FITSSUBHdrExtensionTI-int_type" );
+    AddMap( "subint_unit", "FITSSUBHdrExtensionTI-int_unit" );
+    AddMap( "tsamp", "FITSSUBHdrExtensionTI-tsamp" );
+    AddMap( "nbin_subint", "FITSSUBHdrExtensionTI-nbin" );
+    AddMap( "nbits", "FITSSUBHdrExtensionTI-nbits" );
+    AddMap( "nch_file", "FITSSUBHdrExtensionTI-nch_file" );
+    AddMap( "nch_strt", "FITSSUBHdrExtensionTI-nch_strt" );
+    AddMap( "npol_subint", "FITSSUBHdrExtensionTI-npol" );
+    AddMap( "nsblk", "FITSSUBHdrExtensionTI-nsblk" );
+  }
+};
+
+
+// NOTE - who would have thought is_numeric would be so complicated, need to find a better one, this comes nowhere
+//        near what we require for vap.
+
+bool is_numeric( string src )
+{
+  for( int i =0; i < src.size(); i ++ )
+  {
+    char next_char = src[i];
+    if( next_char != '.' &&
+        next_char != 'e' &&
+        next_char != '-' &&
+        next_char != '+' &&
+        (next_char < char('0') || next_char > char('9') ) )
+      return false;
+  }
+
+  return true;
 }
 
 
-using namespace Pulsar;
+// NOTE - These methods are just for formatting the output, In future I will create a class derived from ostream
+//        that overrides the << operators and automatically places all the data into a table, then outputs it
+//        correctly formatted when you call flush.
 
-int main (int argc, char** argv) try {  
+void FixColumnWidths( vector< vector< string > > &all_rows, vector< int > &col_widths )
+{
+  if( !results.size() )
+    return;
+  // start off setting the column widths to the size of the headings
 
-  // print in degrees
-  Angle::default_type = Angle::Degrees;
+  vector< string >::iterator hit;
+  for( hit = (*all_rows.begin()).begin(); hit != (*all_rows.begin()).end(); hit ++ )
+  {
+    string heading = (*hit).substr( 0, (*hit).find_last_of('=') );
+    col_widths.push_back( heading.size() + 1);
+  }
 
-  // suppress warnings by default
-  Pulsar::Archive::set_verbosity (0);
-  bool verbose = false;
+  // check every cell, if the col width is to small for that cell, set it to the
+  // cell width.
 
-  bool polycmode = false;
-  bool ephemmode = false;
+  vector< vector< string > >::iterator row_it;
+  for( row_it = all_rows.begin(); row_it != all_rows.end(); row_it ++ )
+  {
+    int col = 0;
+    vector< string >::iterator col_it;
+    for( col_it = (*row_it).begin(); col_it != (*row_it).end(); col_it ++ )
+    {
+      string data = (*col_it).substr((*col_it).find_last_of('=')+1);
+      if( data.size() + 1 > col_widths[col] )
+        col_widths[col] = data.size() + 1;
+      col ++;
+    }
+  }
+}
 
-  vector <string> commands;
 
-  Pulsar::VapTI tui;
+void OutputTable( vector< vector< string > > &all_rows, vector< int > &col_widths )
+{
+  if( !results.size() )
+    return;
 
-  // so that a space precedes each parameter processed
-  tui.set_indentation (" ");
+  int col = 0;
+  if( !hide_headers )
+  {
+    vector< string >::iterator hit;
+    for( hit = (*all_rows.begin()).begin(); hit != (*all_rows.begin()).end(); hit ++ )
+    {
+      string heading = (*hit).substr( 0, (*hit).find_last_of('=') );
+      cout << pad( col_widths[col], heading ) << ' ';
+      col ++;
+    }
+    cout << endl;
+  }
 
+  vector< vector< string > >::iterator row_it;
+  for( row_it = all_rows.begin(); row_it != all_rows.end(); row_it ++ )
+  {
+    col = 0;
+    vector< string >::iterator col_it;
+    for( col_it = (*row_it).begin(); col_it != (*row_it).end(); col_it ++ )
+    {
+      string data = (*col_it).substr((*col_it).find_last_of('=')+1);
+      if( is_numeric( data ) )
+      {
+        for( int i = 0; i < col_widths[col] - data.size(); i ++ )
+          cout << ' ';
+        cout << data;
+      }
+      else
+        cout << pad( col_widths[col], data );
+      cout << ' ';
+      col ++;
+    }
+    cout << endl;
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+/**
+ * PrintBasicHlp - message output when we get -h on command line.
+ **/
+
+void PrintBasicHlp( void )
+{
+  cout <<
+  "A program for getting Pulsar::Archive attributes\n"
+  "Usage:\n"
+  "     vap -c PAR [-c PAR ...] [-E] [-p] filenames\n"
+  "Where:\n"
+  "\n"
+  "PAR is a string containing one or more parameter names, separated by\n"
+  "    commas.\n"
+  "    If any whitespace is required, then the string containing it must\n"
+  "    be enclosed in quotation marks. Multiple parameters may also be\n"
+  "    specified by using multiple -c options.\n"
+  "\n"
+  "    vap -c name,freq\n"
+  "\n"
+  "    will print the source name and centre frequency.\n"
+  "    Note that parameter names are case insensitive.\n"
+  "    For a full list of parameter names, type \"vap -H\"\n"
+  "\n"
+  "-n  suppress outputting headers when doing -c\n"
+  "\n"
+  "-E  is used to print the most recent ephemeris in an archive\n"
+  "\n"
+  "-p  is used to print the set of polynomial coefficients\n"
+  "\n"
+  "-s show the extensions present in an archive\n"
+  << endl;
+}
+
+
+/**
+ * PrintExtdHelp - Show all the parameteres available via the text finder.
+ **/
+
+void PrintExtdHlp( void )
+{
+  VapTextFinder tf;
+  tf.DisplayHelp();
+}
+
+
+
+/**
+ * Process the command line options, return the index into argv of the first non option.
+ **/
+
+void ProcArgs( int argc, char *argv[] )
+{
   int gotc;
-  while ((gotc = getopt (argc, argv, "c:EphHvV")) != -1)
-    switch (gotc) {
+  while ((gotc = getopt (argc, argv, "nc:sEphHvVtT")) != -1)
+    switch (gotc)
+    {
 
     case 'E':
       ephemmode = true;
@@ -134,28 +355,16 @@ int main (int argc, char** argv) try {
       break;
 
     case 'c':
-      separate (optarg, commands, ",");
+      separate (optarg, commands, " ,");
       break;
 
     case 'h':
-      usage ();
-      return 0;
+      PrintBasicHlp();
+      break;
 
-    case 'H': {
-
-      cout << 
-	  "-----------------------------------------------------\n"
-	  "Attribute Name   Description\n"
-	  "-----------------------------------------------------"
-	     << endl;
-  
-      for (unsigned i=0; i<tui.get_nattribute(); i++)
-	cout << pad(16,tui.get_name(i)) 
-             << " " << tui.get_description(i) << endl;
-  
-      return 0;
-
-    }
+    case 'H':
+      PrintExtdHlp();
+      break;
 
     case 'v':
       verbose = true;
@@ -168,52 +377,141 @@ int main (int argc, char** argv) try {
       Pulsar::Archive::set_verbosity(3);
       break;
 
+    case 'n':
+      hide_headers = true;
+      break;
+
+    case 's':
+      show_extensions = true;
+      break;
+
     default:
       cerr << "Unknown command line option" << endl;
-      return -1;
-    }
+      return;
+    };
+}
+
+
+
+/**
+ * ProcessArchive - load an archive and process all the command line parameters using the text finder.
+ **/
+
+void ProcessArchive( string filename )
+{
+  Reference::To< Archive > archive = Archive::load( filename );
+
+  if( !archive )
+    return;
+
+  current_row.push_back( string("File=") + filename );
+
+  VapTextFinder tf;
+  tf.SetArchive( archive );
+
+  vector< string >::iterator it;
+  for( it = commands.begin(); it != commands.end(); it ++ )
+  {
+    current_row.push_back( tf.FetchValue( (*it) ) );
+  }
+
+  results.push_back( current_row );
+  current_row.clear();
+}
+
+
+
+
+/**
+ * ExtractPolyco - Get the polyco data from the archive, this will be moved elsewhere
+ **/
+
+void ExtractPolyco( string filename )
+{
+  Reference::To< Archive > archive = Archive::load( filename );
+
+  if( !archive )
+    return;
+
+  cout << endl << filename << endl;
+
+  archive->get_model()->unload( stdout );
+}
+
+
+
+/**
+ * ExtractEphemeris - Get the ephemeris, this will be moved elsewhere.
+ **/
+
+void ExtractEphemeris( string filename )
+{
+  Reference::To< Archive > archive = Archive::load( filename );
+
+  if( !archive )
+    return;
+
+  cout << endl << filename << endl;
+
+  archive->get_ephemeris()->unload( stdout );
+}
+
+
+
+/**
+ * ShowExtensions - a simple debugging routine for showing the extensions available on an archive.
+ **/
+
+void ShowExtensions( string filename )
+{
+  Reference::To< Archive > archive = Archive::load( filename );
+
+  cout << "Extensions for " << filename << endl;
+
+  unsigned int num_extensions = archive->get_nextension();
+  for( unsigned int i = 0; i < num_extensions; i ++ )
+  {
+    Reference::To< Archive::Extension > next_ext = archive->get_extension(i);
+    cout << next_ext->get_extension_name() << endl;
+  }
+}
+
+
+
+/**
+ * main - 
+ **/
+
+int main( int argc, char *argv[] )
+{
+  tostring_precision = 4;
+  Angle::default_type = Angle::Degrees;
   
-  vector<string> filenames;
+  ProcArgs( argc, argv );
+
+  vector< string > filenames;
   for (int ai=optind; ai<argc; ai++)
     dirglob (&filenames, argv[ai]);
 
-  string filenm;
-  string cmdout;
 
-  for (unsigned ifile = 0; ifile < filenames.size(); ifile++) try {
+  if( filenames.size() )
+  {
+    if( polycmode )
+      for_each( filenames.begin(), filenames.end(), ExtractPolyco );
+    else if( ephemmode )
+      for_each( filenames.begin(), filenames.end(), ExtractEphemeris );
+    else if( show_extensions )
+      for_each( filenames.begin(), filenames.end(), ShowExtensions );
+    else
+      for_each( filenames.begin(), filenames.end(), ProcessArchive );
 
-    Reference::To<Pulsar::Archive> archive;
-    archive = Pulsar::Archive::load(filenames[ifile]);
+    vector< int > column_widths;
 
-    filenm = archive->get_filename();
-
-    cout << filenm.substr(filenm.find_last_of('/')+1) << "\t";
-
-    tui.set_instance (archive);
-
-    for (unsigned j = 0; j < commands.size(); j++)  {
-      cmdout = tui.process (commands[j]);
-      cout << pad(8,cmdout.substr(cmdout.find_last_of('=')+1)) << " ";
-    }
-
-    cout << endl;
-
-    if (ephemmode)
-      cout << "\n" << archive->get_ephemeris() << endl;
-    
-    if (polycmode)
-      cout << archive->get_model() << endl;
-
-  } // for each archive
-
-  catch (Error& error) {
-    cout << " " << error.get_message() << endl;
+    FixColumnWidths( results, column_widths );
+    OutputTable( results, column_widths );
   }
 
   return 0;
+}
 
-}
-catch (Error& error) {
-  cerr << "vap: " << error << endl;
-  return -1;
-}
+
