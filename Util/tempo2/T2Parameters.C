6e395d1a5e335a29808fe267e5a97d9b2a8777c1
straten <straten>
2007-06-20 15:57:11 +0000

    encapsulates the pulsar struct of libtempo2

---
+++ Util/tempo2/T2Parameters.C
@@ -0,0 +1,93 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "T2Parameters.h"
+
+using namespace std;
+
+//! Default constructor
+Tempo2::Parameters::Parameters ()
+{
+  if (verbose)
+    cerr << "Tempo2::Parameters constructor" << endl;
+
+  int noWarnings = 1;
+  int fullSetup = 0;
+
+  initialiseOne (&psr, noWarnings, fullSetup);
+
+  if (verbose)
+    cerr << "Tempo2::Parameters initialiseOne completed" << endl;
+
+  if (setupParameterFileDefaults (&psr) < 0)
+    throw Error (InvalidState, "Tempo2::Parameters ctor", TEMPO2_ERROR);
+
+  if (verbose)
+    cerr << "Tempo2::Parameters setupParameterFileDefaults completed" << endl;
+}
+
+//! Destructor
+Tempo2::Parameters::~Parameters ()
+{
+}
+
+//! Copy constructor
+Tempo2::Parameters::Parameters (const Parameters&)
+{
+  throw Error (InvalidState, "Tempo2::Parameters",
+	       "copy constructor not implemented");
+}
+
+Pulsar::Parameters* Tempo2::Parameters::clone () const
+{
+  return new Parameters (*this);
+}
+
+//! Return true if *this == *that
+bool Tempo2::Parameters::equals (const Pulsar::Parameters* that)
+{
+  const Parameters* like = dynamic_cast<const Parameters*>( that );
+  throw Error (InvalidState, "Tempo2::Parameters::equals", "not implemented");
+}
+
+//! Load from an open stream
+void Tempo2::Parameters::load (FILE* fptr)
+{
+  cerr << "Tempo2::Parameters::load (FILE*)" << endl;
+  readSimpleParfile (fptr, &psr);
+  cerr << "Tempo2::Parameters::load readSimpleParfile completed" << endl;
+}
+
+//! Unload to an open stream
+void Tempo2::Parameters::unload (FILE*) const
+{
+  throw Error (InvalidState, "Tempo2::Parameters::unload", "not implemented");
+}
+
+//! Return the name of the source
+std::string Tempo2::Parameters::get_name () const
+{
+  return "Tempo2::Paramters::get_name unknown";
+}
+
+//! Return the coordinates of the source
+sky_coord Tempo2::Parameters::get_coordinates () const
+{
+  return sky_coord();
+}
+
+//! Return the dispersion measure
+double Tempo2::Parameters::get_dispersion_measure () const
+{
+  return 0;
+}
+  
+//! Return the rotation measure
+double Tempo2::Parameters::get_rotation_measure () const
+{
+  return 0;
+}
---
+++ Util/tempo2/T2Parameters.h
@@ -0,0 +1,76 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/psrchive/psrchive/Util/tempo2/T2Parameters.h,v $
+   $Revision: 1.1 $
+   $Date: 2007/06/20 15:57:11 $
+   $Author: straten $ */
+
+#ifndef __Tempo2Parameters_h
+#define __Tempo2Parameters_h
+
+#include "Pulsar/Parameters.h"
+
+#include "tempo2.h"
+
+namespace Tempo2 {
+
+  class Generator;
+
+  //! Storage of pulsar parameters used to create a Predictor
+  /*! This pure virtual base class defines the interface to pulsar
+    parameters */
+  class Parameters : public Pulsar::Parameters {
+
+  public:
+
+    //! Default constructor
+    Parameters ();
+
+    //! Destructor
+    ~Parameters ();
+
+    //! Copy constructor
+    Parameters (const Parameters&);
+
+    //! Return a new, copy constructed instance of self
+    Pulsar::Parameters* clone () const;
+
+    //! Return true if *this == *that
+    bool equals (const Pulsar::Parameters* that);
+
+    //! Load from an open stream
+    void load (FILE*);
+
+    //! Unload to an open stream
+    void unload (FILE*) const;
+
+    //! Return the name of the source
+    std::string get_name () const;
+    
+    //! Return the coordinates of the source
+    sky_coord get_coordinates () const;
+    
+    //! Return the dispersion measure
+    double get_dispersion_measure () const;
+    
+    //! Return the rotation measure
+    double get_rotation_measure () const;
+    
+  protected:
+
+    friend class Generator;
+
+    // the tempo2 pulsar structure defined in tempo2.h
+    pulsar psr;
+
+  };
+
+}
+
+#endif
