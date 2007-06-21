1303e393188df95311f72930e092ce0a5af6822d
straten <straten>
2007-06-21 17:32:28 +0000

    added Greenbank and AAT

--- Util/genutil/Telescope.h
+++ Util/genutil/Telescope.h
@@ -7,12 +7,14 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/genutil/Attic/Telescope.h,v $
-   $Revision: 1.6 $
-   $Date: 2006/10/06 21:13:54 $
+   $Revision: 1.7 $
+   $Date: 2007/06/21 17:32:28 $
    $Author: straten $ */
 
 #ifndef __Telescope_h
 #define __Telescope_h
+
+#include <string>
 
 //! Tempo telescope codes
 namespace Telescope {
@@ -22,11 +24,12 @@ namespace Telescope {
   const char Tidbinbilla = '6';
   const char Arecibo = '3';
   const char Hobart = '4';
-  const char GreenBank = '1';
+  const char Greenbank = '1';
   const char WSRT = 'i';
+  const char AAT = '5';
 
   //! Convert a telescope name to a code
-  char code (const char* telescope_name);
+  char code (const std::string& telescope_name);
 
   //! Convert a telecope code to a name
   const char* name (char code);
