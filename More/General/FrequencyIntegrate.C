1a3fc483877c7a402e6a3147361c3a38a9c437d1
straten <straten>
2007-06-21 17:32:15 +0000

    use Integrate<Container> template base class interfaces

--- More/General/FrequencyIntegrate.C
+++ More/General/FrequencyIntegrate.C
@@ -4,27 +4,29 @@
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
-using namespace std;
 #include "Pulsar/FrequencyIntegrate.h"
-#include "Pulsar/Integration.h"
+#include "Pulsar/IntegrationExpert.h"
 #include "Pulsar/Profile.h"
 
 #include "ModifyRestore.h"
 #include "Error.h"
+
+using namespace std;
 
 void Pulsar::FrequencyIntegrate::transform (Integration* integration)
 {
   unsigned subint_nchan = integration->get_nchan();
   unsigned subint_npol  = integration->get_npol();
 
-  if (nscrunch == 1 || subint_nchan <= 1 || nchan >= subint_nchan) {
+  range_policy->initialize (this, integration);
+
+  unsigned output_nchan = range_policy->get_nrange();
+
+  if (subint_nchan <= 1 || output_nchan >= subint_nchan) {
     if (Integration::verbose) 
       cerr << "Pulsar::FrequencyIntegrate::transform nothing to do" << endl;
    return;
   }
-
-  range_policy->initialize (this, integration);
-  unsigned output_nchan = range_policy->get_nrange();
 
   double dm = integration->get_dispersion_measure();
   bool must_dedisperse = dm != 0 && !integration->get_dedispersed();
@@ -34,9 +36,9 @@ void Pulsar::FrequencyIntegrate::transform (Integration* integration)
     rm != 0 && !integration->get_faraday_corrected();
 
   if (Integration::verbose)
-    cerr << "Pulsar::FrequencyIntegrate::transform"
-      " nscrunch=" << nscrunch << " nchan=" << nchan 
-	 << " dm=" << dm << " rm=" << rm << endl;
+    cerr << "Pulsar::FrequencyIntegrate::transform nchan"
+      " input=" << subint_nchan << " output=" << output_nchan <<
+      " dm=" << dm << " rm=" << rm << endl;
 
   unsigned start = 0;
   unsigned stop = 0;
@@ -54,10 +56,10 @@ void Pulsar::FrequencyIntegrate::transform (Integration* integration)
 	   << " freq=" << reference_frequency << endl;
 
     if (must_dedisperse)
-      integration->dedisperse (start, stop, dm, reference_frequency);
+      integration->expert()->dedisperse (start, stop, dm, reference_frequency);
 
     if (must_defaraday)
-      integration->defaraday (start, stop, rm, reference_frequency);
+      integration->expert()->defaraday (start, stop, rm, reference_frequency);
     
     for (unsigned ipol=0; ipol < subint_npol; ipol++)  {
       
@@ -86,193 +88,10 @@ void Pulsar::FrequencyIntegrate::transform (Integration* integration)
   if (Integration::verbose)
     cerr << "Pulsar::FrequencyIntegrate::transform resize" << endl;
 
-  integration->resize (0, output_nchan, 0);
+  integration->expert()->resize (0, output_nchan, 0);
 
   if (Integration::verbose) 
     cerr << "Pulsar::FrequencyIntegrate::transform finish" << endl;
 
 } 
-
-//! Default constructor
-Pulsar::FrequencyIntegrate::FrequencyIntegrate ()
-{
-  nchan = 0;
-  nscrunch = 0;
-  range_policy = new EvenlySpaced;
-}
-
-//! Set the number of output frequency channels
-void Pulsar::FrequencyIntegrate::set_nchan (unsigned _nchan)
-{
-  nchan = _nchan;
-  nscrunch = 0;
-}
-
-//! Get the number of output frequency channels
-unsigned Pulsar::FrequencyIntegrate::get_nchan () const
-{
-  return nchan;
-}
-
-//! Set the number of channels to integrate
-void Pulsar::FrequencyIntegrate::set_nscrunch (unsigned _nscrunch)
-{
-  nscrunch = _nscrunch;
-  nchan = 0;
-}
-
-//! Get the number of channels to integrate
-unsigned Pulsar::FrequencyIntegrate::get_nscrunch () const
-{
-  return nscrunch;
-}
-
-void Pulsar::FrequencyIntegrate::set_range_policy (RangePolicy* policy)
-{
-  range_policy = policy;
-}
-
-
-void Pulsar::FrequencyIntegrate::divide (unsigned div_nchan,
-					 unsigned& nrange,
-					 unsigned& spacing) const
-{
-  nrange = 1;
-  spacing = div_nchan;
-
-  if (nchan) {
-
-    if (Integration::verbose)
-      cerr << "Pulsar::FrequencyIntegrate::divide nchan=" << nchan << endl;
-
-    // the number of output frequency channel ranges was specified
-    nrange = nchan;
-    // calculate the spacing
-    spacing = div_nchan / nrange;
-    if (div_nchan % nrange)
-      spacing ++;
-
-  }
-  else if (nscrunch) {
-
-    if (Integration::verbose)
-      cerr << "Pulsar::FrequencyIntegrate::divide nscrunch=" << nscrunch
-	   << endl; 
-
-    // the spacing of frequency channels (scrunch) was specified
-    spacing = nscrunch;
-    // calculate the number of output frequency channel ranges
-    nrange = div_nchan / spacing;
-    if (div_nchan % spacing)
-      nrange ++;
-
-  }
-
-  if (Integration::verbose)
-    cerr << "Pulsar::FrequencyIntegrate::divide into " << nrange 
-	 << " ranges with " << spacing << " channels per range" << endl;
-
-}
-
-
-using namespace Pulsar;
-
-/* ***********************************************************************
-
-   Evenly Spaced Frequency Channels Policy
-
-   ************************************************************************ */
-
-void FrequencyIntegrate::EvenlySpaced::initialize (FrequencyIntegrate* freq,
-						   Integration* integration)
-{
-  freq->divide (integration->get_nchan(), nrange, spacing);
-  subint_nchan = integration->get_nchan();
-}
-
-void FrequencyIntegrate::EvenlySpaced::get_range (unsigned irange, 
-						  unsigned& start, 
-						  unsigned& stop)
-{
-  if (irange >= nrange)
-    throw Error (InvalidParam, 
-		 "Pulsar::FrequencyIntegrate::EvenlySpaced::get_range",
-		 "irange=%u >= nrange=%u", irange, nrange);
-
-  start = irange * spacing;
-  stop = start + spacing;
-  
-  if (stop > subint_nchan)
-    stop = subint_nchan;
-
-  if (Integration::verbose)
-    cerr << "Pulsar::FrequencyIntegrate::EvenlySpaced::get_range\n "
-      " irange=" << irange << " spacing=" << spacing <<
-      " subint_nchan=" << subint_nchan <<
-      " start=" << start << " stop=" << stop << endl;
-
-}
-
-/* ***********************************************************************
-
-   Evenly Distributed Frequency Channels Policy
-
-   ************************************************************************ */
-
-void 
-FrequencyIntegrate::EvenlyDistributed::initialize (FrequencyIntegrate* freq,
-						   Integration* integration)
-{
-  unsigned subint_nchan = integration->get_nchan();
-  unsigned good_nchan = 0;
-
-  // count the number of good channels
-  for (unsigned ichan=0; ichan < subint_nchan; ichan++)
-    if (integration->get_weight(ichan) != 0)
-      good_nchan ++;
-
-#ifdef _DEBUG
-  cerr << "EvenlyDistributed::initialize good_nchan=" << good_nchan << endl;
-#endif
-
-  // divide them up
-  unsigned nrange = 0;
-  unsigned spacing = 0;
-  freq->divide (good_nchan, nrange, spacing);
-  stop_indeces.resize (nrange);
-
-  // and count off 'spacing' good channels per range
-  unsigned curchan = 0;
-  for (unsigned irange=0; irange < nrange; irange++) {
-
-    for (good_nchan=0; good_nchan<spacing && curchan<subint_nchan; curchan++)
-      if (integration->get_weight(curchan) != 0)
-	good_nchan ++;
-
-#ifdef _DEBUG
-    cerr << "  stop[" << irange << "] = " << curchan << endl;
-#endif
-
-    stop_indeces[irange] = curchan;
-
-  }
-
-}
-
-void FrequencyIntegrate::EvenlyDistributed::get_range (unsigned irange, 
-						       unsigned& start, 
-						       unsigned& stop)
-{
-  if (irange >= stop_indeces.size())
-    throw Error (InvalidParam, 
-		 "Pulsar::FrequencyIntegrate::EvenlyDistributed::get_range",
-		 "irange=%u >= nrange=%u", irange, stop_indeces.size());
-
-  if (irange > 0)
-    start = stop_indeces[irange-1];
-  else
-    start = 0;
-
-  stop = stop_indeces[irange];
-}
 
--- More/General/Pulsar/FrequencyIntegrate.h
+++ More/General/Pulsar/FrequencyIntegrate.h
@@ -7,16 +7,17 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/FrequencyIntegrate.h,v $
-   $Revision: 1.5 $
-   $Date: 2006/10/06 21:13:53 $
+   $Revision: 1.6 $
+   $Date: 2007/06/21 17:32:15 $
    $Author: straten $ */
 
 #ifndef __Pulsar_FrequencyIntegrate_h
 #define __Pulsar_FrequencyIntegrate_h
 
-#include "Pulsar/Transformation.h"
-#include "Reference.h"
-#include <vector>
+#include "Pulsar/Integrate.h"
+#include "Pulsar/EvenlySpaced.h"
+#include "Pulsar/EvenlyWeighted.h"
+#include "Pulsar/Integration.h"
 
 namespace Pulsar {
 
@@ -38,124 +39,36 @@ namespace Pulsar {
     frequencies will be corrected.
     </UL>
   */
-  class FrequencyIntegrate : public Transformation<Integration> {
+  class FrequencyIntegrate : public Integrate<Integration> {
 
   public:
 
-    //! Default constructor
-    FrequencyIntegrate ();
-
-    //! Set the number of output frequency channels
-    void set_nchan (unsigned nchan);
-
-    //! Get the number of output frequency channels
-    unsigned get_nchan () const;
-
-    //! Set the number of channels to integrate
-    void set_nscrunch (unsigned nscrunch);
-
-    //! Get the number of channels to integrate
-    unsigned get_nscrunch () const;
-
     //! The frequency integration operation
     void transform (Integration*);
-
-    //! Policy for dividing frequency channels into ranges
-    class RangePolicy;
 
     //! Policy for producing evenly spaced frequency channel ranges
     class EvenlySpaced;
 
     //! Policy for producing evenly distributed frequency channel ranges
-    class EvenlyDistributed;
-
-    //! Set the frequency channel range division policy
-    void set_range_policy (RangePolicy*);
-
-    //! Compute a roughly even division of nchan into ranges
-    void divide (unsigned nchan, unsigned& nrange, unsigned& spacing) const;
-
-  protected:
-
-    //! The number of output frequency channels
-    unsigned nchan;
-
-    //! The number of frequency channels to integrate
-    unsigned nscrunch;
-
-    //! The frequency channel range selection policy
-    Reference::To<RangePolicy> range_policy;
+    class EvenlyWeighted;
 
   };
 
-
-  //! Policies for dividing frequency channels into ranges
-  /*! This pure virtual base class defines the interface to the various
-    frequency channel range selection policies. */
-  class FrequencyIntegrate::RangePolicy : public Reference::Able {
-
-  public:
-
-    //! Initialize ranges for the specified parameters
-    virtual void initialize (FrequencyIntegrate*, Integration*) = 0;
-    
-    //! Return the number of output frequency channel ranges
-    virtual unsigned get_nrange () = 0;
-    
-    //! Return the frequency channels indeces for the specified range
-    virtual void get_range (unsigned irange, 
-			    unsigned& ichan_start,
-			    unsigned& ichan_stop) = 0;
-
+  class FrequencyIntegrate::EvenlySpaced :
+    public Integrate<Integration>::EvenlySpaced
+  {
+    unsigned get_size (const Integration* sub)
+    { return sub->get_nchan(); }
   };
 
-  //! Policy for producing evenly spaced frequency channel ranges
-  /*! Evenly spaced frequency channel ranges have a constant number
-    of input frequency channels per output frequency channel. */
-  class FrequencyIntegrate::EvenlySpaced : public RangePolicy {
+  class FrequencyIntegrate::EvenlyWeighted : 
+    public Integrate<Integration>::EvenlyWeighted
+  {
+    unsigned get_size (const Integration* sub)
+    { return sub->get_nchan(); }
 
-  public:
-
-    //! Initialize ranges for the specified parameters
-    void initialize (FrequencyIntegrate* freqint, Integration* integration);
-
-    //! Return the number of output frequency channel ranges
-    unsigned get_nrange () { return nrange; }
-
-     //! Return the frequency channels indeces for the specified range
-   void get_range (unsigned irange, unsigned& start, unsigned& stop);
-
-  protected:
-
-    //! The number of frequency channels in the input Integration
-    unsigned subint_nchan;
-    //! The number of frequency ranges (channels) in the output Integration
-    unsigned nrange;
-    //! The number of input channels in each output range
-    unsigned spacing;
-
-  };
-
-
-  //! Policy for producing evenly distributed frequency channel ranges
-  /*! Evenly distributed frequency channel ranges have the same number of
-    valid input frequency channels integrated into each output frequency
-    channel. */
-  class FrequencyIntegrate::EvenlyDistributed : public RangePolicy {
-
-  public:
-
-    void initialize (FrequencyIntegrate* freqint, Integration* integration);
-
-    unsigned get_nrange () { return stop_indeces.size(); }
-
-    void get_range (unsigned irange, unsigned& start, unsigned& stop);
-
-  protected:
-
-    //! The input frequency channel index at the end of each range
-    std::vector<unsigned> stop_indeces;
-
+    double get_weight (const Integration* sub, unsigned ichan)
+    { return sub->get_weight (ichan); }
   };
 
 }
