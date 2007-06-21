08d7bbdebeb9a9fba4a3ee5c91692fe28bcc79ae
straten <straten>
2007-06-21 17:32:30 +0000

    Util/tempo/Generator.h moved to Util/tempo/Pulsar/Generator.h

--- Util/tempo/Pulsar/Generator.h
+++ Util/tempo/Pulsar/Generator.h
@@ -6,19 +6,21 @@
  *
  ***************************************************************************/
 
-/* $Source: /cvsroot/psrchive/psrchive/Util/tempo/Attic/Generator.h,v $
+/* $Source: /cvsroot/psrchive/psrchive/Util/tempo/Pulsar/Generator.h,v $
    $Revision: 1.1 $
-   $Date: 2007/05/08 06:55:21 $
+   $Date: 2007/06/21 17:32:30 $
    $Author: straten $ */
 
 #ifndef __PulsarGenerator_h
 #define __PulsarGenerator_h
 
 #include "Reference.h"
+#include "MJD.h"
 
 namespace Pulsar {
 
   class Predictor;
+  class Parameters;
 
   //! Generator of functions that predict pulse phase
   /*! This pure virtual base class defines the interface to pulse
@@ -27,8 +29,20 @@ namespace Pulsar {
 
   public:
 
+    //! Set the parameters used to generate the predictor
+    virtual void set_parameters (const Parameters*) = 0;
+
+    //! Set the range of epochs over which to generate
+    virtual void set_time_span (const MJD& start, const MJD& finish) = 0;
+
+    //! Set the range of frequencies over which to generate
+    virtual void set_frequency_span (long double low, long double high) = 0;
+
+    //! Set the site at which the signal is observed
+    virtual void set_site (const std::string&) = 0;
+
     //! Return a new Predictor instance
-    virtual Pulsar::Predictor* generate () const = 0;
+    virtual Predictor* generate () const = 0;
 
   };
 
