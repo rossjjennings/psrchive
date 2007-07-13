c738649858daea6d7911230f8e8db78d6d624099
straten <straten>
2007-07-13 06:28:41 +0000

    internal documentation modifications

--- More/General/Archive_remove_baseline.C
+++ More/General/Archive_remove_baseline.C
@@ -4,9 +4,11 @@
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
-using namespace std;
+
 #include "Pulsar/Archive.h"
 #include "Pulsar/Integration.h"
+
+using namespace std;
 
 /*!
   If phase is not specified, this method calls
@@ -15,13 +17,14 @@ using namespace std;
   reaches a minimum.  This phase is then used to remove the baseline from
   each of the Integrations.
   */
-void Pulsar::Archive::remove_baseline (float phase, float dc) try {
+
+void Pulsar::Archive::remove_baseline (float phase, float duty_cycle) try {
   
   if (phase < 0.0)
-    phase = find_min_phase (dc);
+    phase = find_min_phase (duty_cycle);
   
   for (unsigned isub=0; isub < get_nsubint(); isub++)
-    get_Integration(isub) -> remove_baseline (phase, dc);
+    get_Integration(isub) -> remove_baseline (phase, duty_cycle);
   
 }
 catch (Error& error) {
