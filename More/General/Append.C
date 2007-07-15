284c14741416f0d93037c5d70b279f2022e86bee
straten <straten>
2007-07-15 23:37:19 +0000

    copyright year corrected

--- More/General/Append.C
+++ More/General/Append.C
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- *   Copyright (C) 2002 by Willem van Straten
+ *   Copyright (C) 2007 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
