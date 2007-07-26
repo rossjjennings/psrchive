71a65015bbd697bc5ec4ff5b416fd45a321c353f
straten <straten>
2007-07-26 00:13:04 +0000

    compute weighted mean real and imaginary components of cross correlation

--- More/Polarimetry/DeltaPA.C
+++ More/Polarimetry/DeltaPA.C
@@ -13,6 +13,97 @@ using namespace std;
 
 // #define _DEBUG 1
 
+template <typename T, typename U = T>
+class MeanArc
+{
+
+public:
+
+  //! Default constructor
+  MeanArc () { covar = 0; }
+
+  //! Copy constructor
+  MeanArc (const MeanArc& mean)
+  { operator = (mean); }
+
+  //! Assignment operator
+  const MeanArc& operator= (const MeanArc& mean)
+  { cosine = mean.cosine; sine = mean.sine; covar = mean.covar; return *this; }
+
+  //! Addition operator
+  const MeanArc& operator+= (const MeanArc& d)
+  { cosine += d.cosine; sine += d.sine; covar += d.covar; return *this; }
+
+  //! Add data
+  void add (const Estimate<T,U>& x0, const Estimate<T,U>& y0, 
+            const Estimate<T,U>& x1, const Estimate<T,U>& y1)
+  {
+    Estimate<T,U> cosarc = x0*x1 + y0*y1; cosine += cosarc;
+    Estimate<T,U> sinarc = x0*y1 - y0*x1; sine += sinarc;
+
+    T norm = 1.0 / (cosarc.get_variance() * sinarc.get_variance());
+
+    // del cosarc del x0 * del sinarc del x0 * var(x0)
+    covar += x1.get_value() * y1.get_value() * x0.get_variance() * norm;
+ 
+    // del cosarc del y0 * del sinarc del y0 * var(y0)
+    covar -= y1.get_value() * x1.get_value() * y0.get_variance() * norm;
+ 
+    // del cosarc del x1 * del sinarc del x1 * var(x1)
+    covar -= x0.get_value() * y0.get_value() * x1.get_variance() * norm;
+ 
+    // del cosarc del y1 * del sinarc del y1 * var(y1)
+    covar += y0.get_value() * x0.get_value() * y1.get_variance() * norm;
+
+#ifdef _DEBUG
+    cerr << "CC=" << 1.0/cosine.inv_var << " SS=" << 1.0/sine.inv_var
+	 << " CS=" << covar << " norm=" << norm << endl;
+#endif
+
+  }
+
+  //!
+  Estimate<T,U> get_Estimate () const
+  {
+    if (sine.norm_val==0 && cosine.norm_val==0) return Estimate<T,U>(0,0);
+
+    Estimate<T,U> sbar = sine.get_Estimate();
+    Estimate<T,U> cbar = cosine.get_Estimate();
+
+    Estimate<T,U> arc = atan2 (sbar, cbar);
+
+    T covar_sbarcbar = covar * sbar.get_variance() * cbar.get_variance();
+
+    T one = (sbar.val*sbar.val + cbar.val*cbar.val);
+
+    T delarc_delsbar = cbar.get_value() / one;
+    T delarc_delcbar = -sbar.get_value() / one; 
+
+    arc.var += 2.0 * covar_sbarcbar * delarc_delsbar * delarc_delcbar;
+
+    return arc;
+  }
+
+  Estimate<T,U> get_sin () const
+  { return sine.get_Estimate(); }
+
+  Estimate<T,U> get_cos () const
+  { return cosine.get_Estimate(); }
+
+protected:
+
+  //! The average cosine
+  MeanEstimate<T,U> cosine;
+
+  //! The average sine
+  MeanEstimate<T,U> sine;
+
+  //! The covariance
+  T covar;
+
+};
+
+
 Estimate<double> 
 Pulsar::DeltaPA::get (const PolnProfile* p0, const PolnProfile* p1) const
 {
@@ -25,7 +116,18 @@ Pulsar::DeltaPA::get (const PolnProfile* p0, const PolnProfile* p1) const
   Profile linear1;
   p1->get_linear (&linear1);
 
-  float min_phase = (linear0.find_min_phase() + linear1.find_min_phase()) / 2;
+  float min_phase0 = linear0.find_min_phase();
+  float min_phase1 = linear1.find_min_phase();
+  MeanRadian<double> mean;
+  mean += Estimate<double> (min_phase0 * 2.0 * M_PI, 1);
+  mean += Estimate<double> (min_phase1 * 2.0 * M_PI, 1);
+
+#ifdef _DEBUG
+  cerr << "MC=" << mean.get_cos() << endl;
+  cerr << "MS=" << mean.get_sin() << endl;
+#endif
+
+  float min_phase = mean.get_Estimate().get_value() / (2.0*M_PI);
 
   double var_q0 = p0->get_variance (1, min_phase);
   double var_u0 = p0->get_variance (2, min_phase);
@@ -46,6 +148,10 @@ Pulsar::DeltaPA::get (const PolnProfile* p0, const PolnProfile* p1) const
   double cos_delta_PA = 0.0;
   double sin_delta_PA = 0.0;
 
+  MeanArc<double> arc;
+
+  FILE* fptr = fopen ("delta_pa.txt", "w");
+  
   for (unsigned ibin=0; ibin<nbin; ibin++)
     if (linear0.get_amps()[ibin] > cutoff0 &&
 	linear1.get_amps()[ibin] > cutoff1) {
@@ -53,7 +159,24 @@ Pulsar::DeltaPA::get (const PolnProfile* p0, const PolnProfile* p1) const
       cos_delta_PA += q0[ibin]*q1[ibin] + u0[ibin]*u1[ibin];
       sin_delta_PA += q0[ibin]*u1[ibin] - q1[ibin]*u0[ibin];
 
+      MeanArc<double> arc1;
+      arc1.add( Estimate<double> (q0[ibin], var_q0),
+	        Estimate<double> (u0[ibin], var_u0),
+	        Estimate<double> (q1[ibin], var_q1),
+	        Estimate<double> (u1[ibin], var_u1) );
+
+      Estimate<double> delta_pa = arc1.get_Estimate();
+
+      if (fptr)
+        fprintf (fptr, "%u  %lf  %lf\n", ibin,
+			delta_pa.get_value(), delta_pa.get_error());
+
+      arc += arc1; 
+
     }
+
+  if (fptr)
+    fclose (fptr);
 
   double one = cos_delta_PA*cos_delta_PA + sin_delta_PA*sin_delta_PA;
   cos_delta_PA /= one;
@@ -87,7 +210,9 @@ Pulsar::DeltaPA::get (const PolnProfile* p0, const PolnProfile* p1) const
 
   Estimate<double> radians( atan2(sin_delta_PA,cos_delta_PA), var_delta_PA );
 
+  cerr << "radians=" << radians << " arc=" << arc.get_Estimate() << endl;
+
   // return the answer in P.A. = 1/2 radians
-  return 0.5 * radians;
+  return 0.5 * arc.get_Estimate();
 
 }
