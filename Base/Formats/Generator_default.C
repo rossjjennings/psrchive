2be06b9f3951dde25d392b8c9db4ce0bd86c98bc
straten <straten>
2007-08-19 19:55:18 +0000

    return a predictor of the specified type

--- Base/Formats/Generator_default.C
+++ Base/Formats/Generator_default.C
@@ -9,18 +9,36 @@
 #include "config.h"
 #endif
 
-#include "Predict.h"
+#include "Pulsar/Predictor.h"
+#include "Pulsar/Config.h"
 
 #ifdef HAVE_TEMPO2
 #include "T2Generator.h"
 #endif
+#include "Predict.h"
 
 using namespace std;
 
+/*!
+  policy == "input"   -> new predictors will have same type as input
+  policy == "default" -> new predictors will be of the default type
+*/
+string Pulsar::Predictor::policy
+= Pulsar::Config::get<string>("Predictor::policy", "input");
+
+string Pulsar::Predictor::default_type
+= Pulsar::Config::get<string>("Predictor::default", "polyco");
+
 Pulsar::Generator* Pulsar::Generator::get_default ()
 {
+  if (Predictor::verbose)
+    cerr << "Pulsar::Generator::get_default type="
+	 << Predictor::default_type << endl;
+
 #ifdef HAVE_TEMPO2
-  return new Tempo2::Generator;
+  if (Predictor::default_type == "tempo2")
+    return new Tempo2::Generator;
 #endif
+
   return new Tempo::Predict;
 }
