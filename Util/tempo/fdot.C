6a9948cf5fbe4ac49009d2e7ecaf13b6c730371d
straten <straten>
2007-08-22 05:48:25 +0000

    computes the first derivative of the pulsar spin frequency at a given epoch

---
+++ Util/tempo/fdot.C
@@ -0,0 +1,84 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* A simple program to report the frequency derivative at a given epoch */
+
+#include "Predict.h"
+#include "psrephem.h"
+
+#include <unistd.h>
+
+using namespace std;
+
+void usage ();
+
+int main (int argc, char** argv) try
+{
+  char c;
+  char* input_ephemeris = 0;
+  char* input_epoch = 0;
+
+  while ((c = getopt(argc, argv, "hp:t:")) != -1) {
+
+    switch (c) {
+
+    case 'h':
+      usage ();
+      return 0;
+
+    case 'p':
+      input_ephemeris = optarg;
+      break;
+
+    case 't':
+      input_epoch = optarg;
+      break;
+
+    default:
+      cerr << "fdot: unrecognized parameter '" << c << "'" << endl;
+      break;
+
+    }
+
+  }
+
+  if (!input_epoch) {
+    cerr << "fdot: please specify the epoch with -t MJD" << endl;
+    return -1;
+  }
+
+  if (!input_ephemeris) {
+    cerr << "fdot: please specify the ephemeris with -p filename" << endl;
+    return -1;
+  }
+
+  psrephem ephemeris (input_ephemeris);
+  MJD epoch (epoch);
+
+  Tempo::Predict predict;
+
+  predict.set_parameters (ephemeris);
+
+  polyco model = predict.get_polyco (epoch, epoch);
+
+  cout << "fdot: " << model.chirp(epoch)
+       << "  acceleration: " << model.accel(epoch) << endl;
+
+  return 0;
+}
+catch (Error& error) {
+  cerr << error << endl;
+  return -1;
+}
+
+void usage ()
+{
+  cout <<
+    "fdot - print the first derivative of the pulse frequency \n"
+    "USAGE: fdot [-n pulsar | -e ephemeris] -t epoch \n"
+       << endl;
+}
