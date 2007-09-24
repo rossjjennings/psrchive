522226cd17f55f78898accb3544a1b493d345b38
straten <straten>
2007-09-24 08:04:04 +0000

    finds peaks made of consecutive amplitudes above a threshold

---
+++ More/General/PeakConsecutive.C
@@ -0,0 +1,230 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "Pulsar/PeakConsecutive.h"
+
+#include "Pulsar/BaselineEstimator.h"
+#include "Pulsar/PhaseWeight.h"
+
+#include "Pulsar/Accumulate.h"
+#include "Pulsar/Profile.h"
+
+#include <iostream>
+
+using namespace std;
+
+// defined in Profile.C
+void nbinify (int& istart, int& iend, int nbin);
+
+//! Default constructor
+Pulsar::PeakConsecutive::PeakConsecutive ()
+{
+  threshold = 3.0;  // 3 sigma
+  consecutive = 3;  // pretty unlikely by chance
+
+  bin_start = bin_end = 0;
+  range_specified = false;
+
+  bin_rise = bin_fall = 0;
+
+  built = false;
+}
+
+void Pulsar::PeakConsecutive::set_Profile (const Profile* p)
+{
+  profile = p;
+  built = false;
+}
+
+//! Set the duty cycle
+void Pulsar::PeakConsecutive::set_threshold (float _threshold)
+{
+  if (threshold != _threshold)
+    built = false;
+  threshold = _threshold;
+}
+
+float Pulsar::PeakConsecutive::get_threshold () const
+{
+  return threshold;
+}
+
+//! Set the consecutive
+void Pulsar::PeakConsecutive::set_consecutive (unsigned c)
+{
+  if (consecutive != c)
+    built = false;
+  consecutive = c;
+}
+
+//! Get the consecutive
+unsigned Pulsar::PeakConsecutive::get_consecutive () const
+{
+  return consecutive;
+}
+
+//! Set the BaselineEstimator used to find the off-pulse phase bins
+void Pulsar::PeakConsecutive::set_baseline_estimator (BaselineEstimator* b)
+{
+  baseline_estimator = b;
+  built = false;
+}
+
+//! Get the BaselineEstimator used to find the off-pulse phase bins
+const Pulsar::BaselineEstimator* 
+Pulsar::PeakConsecutive::get_baseline_estimator () const
+{
+  return baseline_estimator;
+}
+
+Pulsar::BaselineEstimator*
+Pulsar::PeakConsecutive::get_baseline_estimator ()
+{
+  return baseline_estimator;
+}
+
+//! Set the start and end bins of the search
+void Pulsar::PeakConsecutive::set_range (int start, int end)
+{
+  bin_start = start;
+  bin_end = end;
+  range_specified = true;
+  built = false;
+}
+
+template<typename C>
+void cyclic_shift (vector<C>& array)
+{
+  C temp = array[0];
+  for (unsigned i=1; i<array.size(); i++)
+    array[i-1]=array[i];
+  array[array.size()-1] = temp;
+}
+
+void Pulsar::PeakConsecutive::build ()
+{
+  if (!profile)
+    throw Error (InvalidState, "Pulsar::PeakConsecutive::build",
+		 "Profile not set");
+
+  Reference::To<PhaseWeight> baseline;
+
+  if (baseline_estimator) {
+    baseline = new PhaseWeight;
+    baseline_estimator->set_Profile (profile);
+    baseline_estimator->get_weight (*baseline);
+  }
+  else
+    baseline = profile->baseline();
+
+  Estimate<double> mean = baseline->get_mean ();
+  Estimate<double> var = baseline->get_variance ();
+  Estimate<double> rms = sqrt(var);
+
+  float cutoff = mean.get_value() + rms.get_value() * threshold;
+
+  if (Profile::verbose)
+    cerr << "Pulsar::PeakConsecutive::build baseline"
+      " mean=" << mean << " rms=" << rms << " cut=" << cutoff << endl;
+
+  unsigned nbin = profile->get_nbin();
+  const float* amps = profile->get_amps();
+
+  int start = 0;
+  int stop = nbin+consecutive-1;
+
+  if (range_specified) {
+    start = bin_start;
+    stop  = bin_end;
+    nbinify (start, stop, nbin);
+  }
+
+  const int on_pulse = 1;
+  const int undefined = 0;
+  const int off_pulse = -1;
+
+  int current = undefined;
+  int started = undefined;
+
+  unsigned consecutive_on = 0;
+
+  vector<unsigned> on_transitions;
+  vector<unsigned> off_transitions;
+
+  for (int ibin=start; ibin < stop; ibin++) {
+
+    if (amps[ibin%nbin] > cutoff)
+      consecutive_on ++;
+    else
+      consecutive_on = 0;
+
+    if (consecutive_on == consecutive) {
+      if (current == off_pulse)
+	on_transitions.push_back (ibin+1-consecutive);
+      current = on_pulse;
+    }
+
+    if (consecutive_on == 0) {
+      if (current == on_pulse)
+	off_transitions.push_back (ibin-1);
+      current = off_pulse;
+    }
+
+    if (started == undefined && current != undefined)
+      started = current;
+
+  }
+
+  if (on_transitions.size() != off_transitions.size())
+    throw Error (InvalidState, "Pulsar::PeakConsecutive::build",
+		 "transitions on=%u != off=%u",
+		 on_transitions.size(),
+		 off_transitions.size());
+
+  if (Profile::verbose)
+    cerr << "Pulsar::PeakConsecutive::build transitions="
+	 << on_transitions.size() << endl;
+
+  // if the pulse was on in the first phase bin, then shift and correct
+  if (started == on_pulse) {
+    cyclic_shift (on_transitions);
+    off_transitions[0] += nbin;
+  }
+
+  // choose the grouping of peaks that minimizes their collective width
+
+  unsigned min_width = nbin;
+  unsigned offset = on_transitions.size() - 1;
+  unsigned number = on_transitions.size();
+
+  for (unsigned i=0; i < number; i++) {
+
+    unsigned last = (i+offset) % number;
+    unsigned width = off_transitions[last] - on_transitions[i];
+    if (width < min_width) {
+      min_width = width;
+      bin_rise = on_transitions[i];
+      bin_fall = off_transitions[last];
+    }
+
+  }
+
+}
+
+
+/*!
+  \retval rise bin at which the cumulative power last remains above threshold
+  \retval fall bin at which the cumulative power first falls below threshold
+*/
+void Pulsar::PeakConsecutive::get_indeces (int& rise, int& fall) const
+{
+  if (!built)
+    const_cast<PeakConsecutive*>(this)->build ();
+
+  rise = bin_rise;
+  fall = bin_fall;
+}
---
+++ More/General/Pulsar/PeakConsecutive.h
@@ -0,0 +1,98 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/PeakConsecutive.h,v $
+   $Revision: 1.1 $
+   $Date: 2007/09/24 08:04:04 $
+   $Author: straten $ */
+
+#ifndef __Pulsar_PeakConsecutive_h
+#define __Pulsar_PeakConsecutive_h
+
+#include "Pulsar/RiseFall.h"
+
+namespace Pulsar {
+
+  class BaselineEstimator;
+
+  //! Finds pulse defined by number of consecutive points above threshold
+  /*!
+    Finds the edges at which the power remains above a threshold
+    for a while.    
+  */
+
+  class PeakConsecutive : public RiseFall {
+
+  public:
+
+    //! Default constructor
+    PeakConsecutive ();
+
+    //! Set the profile from which the rise and fall will be computed
+    void set_Profile (const Profile*);
+
+    //! Set the threshold
+    void set_threshold (float threshold);
+
+    //! Get the threshold
+    float get_threshold () const;
+
+    //! Set the consecutive
+    void set_consecutive (unsigned consecutive);
+
+    //! Get the consecutive
+    unsigned get_consecutive () const;
+
+    //! Set the BaselineEstimator used to find the off-pulse phase bins
+    void set_baseline_estimator (BaselineEstimator*);
+
+    //! Get the BaselineEstimator used to find the off-pulse phase bins
+    const BaselineEstimator* get_baseline_estimator () const;
+    BaselineEstimator* get_baseline_estimator ();
+
+    //! Set the start and end bins of the search
+    void set_range (int bin_start, int bin_end);
+
+    //! Get the pulse phase bin indeces of the rise and fall
+    void get_indeces (int& rise, int& fall) const;
+
+  protected:
+
+    //! Compute the rise and fall of the pulse
+    void build ();
+
+    //! Flag set when the rise and fall are computed
+    bool built;
+
+    //! The threshold
+    float threshold;
+
+    //! The number of bins that must remain above threshold
+    unsigned consecutive;
+
+    //! The first bin in the selected range
+    int bin_start;
+
+    //! The last bin in the selected range
+    int bin_end;
+
+    //! Set true when range is specified
+    bool range_specified;
+
+    int bin_rise;
+    int bin_fall;
+
+    //! The BaselineEstimator used to find the off-pulse phase bins
+    Reference::To<BaselineEstimator> baseline_estimator;
+
+  };
+
+}
+
+
+#endif // !defined __Pulsar_PeakConsecutive_h
