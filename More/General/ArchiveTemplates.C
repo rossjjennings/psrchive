ea319b1cfd169006202c2e464419ab6704b36a5d
straten <straten>
2007-10-12 02:45:24 +0000

    foreach templates added for Combination transformations

--- More/General/ArchiveTemplates.C
+++ More/General/ArchiveTemplates.C
@@ -12,17 +12,35 @@
 #include "Pulsar/Profile.h"
 
 void Pulsar::foreach (Archive* archive,
-		       Reference::To< Transformation<Profile> > xform)
+		      Reference::To< Transformation<Profile> > xform)
 {
   for (unsigned i=0; i < archive->get_nsubint(); i++)
     foreach( archive->get_Integration(i), xform );
 }
 
 void Pulsar::foreach (Integration* subint,
-		       Reference::To< Transformation<Profile> > xform)
+		      Reference::To< Transformation<Profile> > xform)
 {
   for (unsigned ipol = 0; ipol < subint->get_npol(); ipol++)
     for (unsigned ichan = 0; ichan < subint->get_nchan(); ichan++)
       xform->transform( subint->get_Profile (ipol, ichan) );
 }
+
+void Pulsar::foreach (Archive* archive, const Archive* operand,
+		      Reference::To< Combination<Profile> > xform)
+{
+  for (unsigned i=0; i < archive->get_nsubint(); i++)
+    foreach( archive->get_Integration(i), operand->get_Integration(i), xform );
+}
+
+void Pulsar::foreach (Integration* subint, const Integration* operand,
+		      Reference::To< Combination<Profile> > xform)
+{
+  for (unsigned ipol = 0; ipol < subint->get_npol(); ipol++)
+    for (unsigned ichan = 0; ichan < subint->get_nchan(); ichan++) {
+      xform->set_operand( operand->get_Profile (ipol, ichan) );
+      xform->transform( subint->get_Profile (ipol, ichan) );
+    }
+}
+
 
--- More/General/Pulsar/ArchiveTemplates.h
+++ More/General/Pulsar/ArchiveTemplates.h
@@ -7,14 +7,15 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/ArchiveTemplates.h,v $
-   $Revision: 1.1 $
-   $Date: 2007/09/24 08:52:29 $
+   $Revision: 1.2 $
+   $Date: 2007/10/12 02:45:24 $
    $Author: straten $ */
 
 #ifndef __Pulsar_ArchiveTemplates_h
 #define __Pulsar_ArchiveTemplates_h
 
 #include "Pulsar/Transformation.h"
+#include "Pulsar/Combination.h"
 
 namespace Pulsar {
 
@@ -25,6 +26,12 @@ namespace Pulsar {
   void foreach (Archive*, Reference::To< Transformation<Profile> >);
 
   void foreach (Integration*, Reference::To< Transformation<Profile> >);
+
+  void foreach (Archive*, const Archive*, 
+		Reference::To< Combination<Profile> >);
+
+  void foreach (Integration*, const Integration*,
+		Reference::To< Combination<Profile> >);
 
 }
 
