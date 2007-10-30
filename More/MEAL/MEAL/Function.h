aef8935ef5157d7067e2627798a8ab78f0c2908a
straten <straten>
2007-10-30 02:30:59 +0000

    provide access to set ParameterPolicy

--- More/MEAL/MEAL/Function.h
+++ More/MEAL/MEAL/Function.h
@@ -7,8 +7,8 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/More/MEAL/MEAL/Function.h,v $
-   $Revision: 1.9 $
-   $Date: 2007/10/02 05:42:36 $
+   $Revision: 1.10 $
+   $Date: 2007/10/30 02:30:59 $
    $Author: straten $ */
 
 /*! \mainpage 
@@ -181,6 +181,9 @@ namespace MEAL {
 
     //! Copy the parameter policy of another instance
     void copy_parameter_policy (const Function*);
+
+    //! Set the parameter policy
+    void set_parameter_policy (ParameterPolicy* policy);
 
   private:
 
