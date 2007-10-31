d568600e8d0c08ad870118b47f626b84527a2f3c
straten <straten>
2007-10-31 05:59:35 +0000

    functor interface added

--- More/General/Pulsar/Transformation.h
+++ More/General/Pulsar/Transformation.h
@@ -7,8 +7,8 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/More/General/Pulsar/Transformation.h,v $
-   $Revision: 1.5 $
-   $Date: 2006/10/06 21:13:53 $
+   $Revision: 1.6 $
+   $Date: 2007/10/31 05:59:35 $
    $Author: straten $ */
 
 #ifndef __Pulsar_Transformation_h
@@ -22,7 +22,13 @@ namespace Pulsar {
   template<class Container> class Transformation : public Algorithm
   {
   public:
+
+    //! Defined by derived classes
     virtual void transform (Container*) = 0;
+
+    //! Functor interface
+    void operator () (Container* container) { transform (container); }
+
   };
 
 }
