1a6abaeef81e4044a02bf30248e601d07f10b02f
straten <straten>
2007-11-20 03:59:17 +0000

    gcc version 4.1+ fix: declare function before using it

--- Util/tempo/inverse_phase.h
+++ Util/tempo/inverse_phase.h
@@ -7,8 +7,8 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/tempo/inverse_phase.h,v $
-   $Revision: 1.7 $
-   $Date: 2007/11/14 03:08:26 $
+   $Revision: 1.8 $
+   $Date: 2007/11/20 03:59:17 $
    $Author: straten $ */
 
 #ifndef __P_inverse_phase_h
@@ -21,7 +21,12 @@
 
 namespace Pulsar {
 
-  //! implements an inverse phase function using the Newton-Raphson method
+  //! Number of times inverse_phase is called
+  extern unsigned inverse_phase_calls;
+  //! Total number of iterations
+  extern unsigned inverse_phase_iterations;
+
+  //! Compute the inverse phase function using the Newton-Raphson method
 
   /*! This template function uses the Newton-Raphson method to solve:
 
@@ -29,41 +34,17 @@ namespace Pulsar {
 
   for TIME, given PHASE.
 
-  This function requires P to implement the following interface:
+  This function requires class P to implement the following interface:
 
   class P {
   public:
     static double precision;
 
-    MJD get_reftime() const;
-    Phase get_refphase() const;
-    long double get_reffrequency() const;
     Phase phase (const MJD&) const;
     long double frequency (const MJD&) const;
   };
 
   */
-
-  //! Number of times inverse_phase is called
-  extern unsigned inverse_phase_calls;
-  //! Total number of iterations
-  extern unsigned inverse_phase_iterations;
-
-  template<typename P>
-  MJD inverse_phase (const P& predictor, const Phase& p)
-  {
-    MJD guess = predictor.get_reftime()
-      + (p - predictor.get_refphase()) / predictor.get_reffrequency();
-
-    if (Pulsar::Predictor::verbose)
-      std::cerr << "inverse_phase: reftime=" << predictor.get_reftime() 
-		<< " refphase=" << predictor.get_refphase() 
-		<< " reffreq=" << predictor.get_reffrequency() 
-		<< " first guess = " << guess << std::endl;
-
-    return inverse_phase (predictor, p, guess);
-  }
-
   template<typename P>
   MJD inverse_phase (const P& predictor, const Phase& p, MJD guess)
   {
@@ -103,6 +84,46 @@ namespace Pulsar {
     
     return guess;
   }
+
+  //! Computes the inverse phase function using the Newton-Raphson method
+
+  /*! This template function uses the Newton-Raphson method to solve:
+
+  PHASE = p (TIME)
+
+  for TIME, given PHASE.
+
+  This function requires P to implement the following interface:
+
+  class P {
+  public:
+    static double precision;
+
+    MJD get_reftime() const;
+    Phase get_refphase() const;
+    long double get_reffrequency() const;
+    Phase phase (const MJD&) const;
+    long double frequency (const MJD&) const;
+  };
+
+  */
+
+  template<typename P>
+  MJD inverse_phase (const P& predictor, const Phase& p)
+  {
+    MJD guess = predictor.get_reftime()
+      + (p - predictor.get_refphase()) / predictor.get_reffrequency();
+
+    if (Pulsar::Predictor::verbose)
+      std::cerr << "inverse_phase: reftime=" << predictor.get_reftime() 
+		<< " refphase=" << predictor.get_refphase() 
+		<< " reffreq=" << predictor.get_reffrequency() 
+		<< " first guess = " << guess << std::endl;
+
+    return inverse_phase (predictor, p, guess);
+  }
+
+
  
 };
 
