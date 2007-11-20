3ce0050b1dfd04934a5a90c70295d26f2d8d7087
straten <straten>
2007-11-20 04:55:20 +0000

    check for null pointers

--- Util/genutil/typeutil.h
+++ Util/genutil/typeutil.h
@@ -7,8 +7,8 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/genutil/typeutil.h,v $
-   $Revision: 1.6 $
-   $Date: 2006/10/06 21:13:54 $
+   $Revision: 1.7 $
+   $Date: 2007/11/20 04:55:20 $
    $Author: straten $ */
 
 #ifndef __typeutil_h
@@ -17,16 +17,43 @@
 #include "Reference.h"
 #include <typeinfo>
 
+using namespace std;
+
 template<class T>
-unsigned find (const std::vector< Reference::To<T> >& array, const T* instance)
+unsigned find (const vector< Reference::To<T> >& array, const T* instance)
+try 
 {
   unsigned index;
 
+  if (!instance)
+    throw Error (InvalidParam, "typeinfo find", "null pointer to instance");
+
+#ifdef _DEBUG
+  cerr << "typeinfo find array size = " << array.size() 
+       << " ptr=" << instance 
+       << " name=" << instance->get_extension_name() << endl;
+#endif
+
   for (index=0; index<array.size(); index++)
-    if ( typeid(*instance) == typeid(*array[index].ptr()) )
+  {
+#ifdef _DEBUG
+    cerr << "typeinfo find test index=" << index 
+         << " ptr=" << array[index].ptr();
+    cerr << " name=" << array[index]->get_extension_name () << endl;
+#endif
+    if ( typeid(*instance) == typeid(*array[index].get()) )
       break;
+  }
+
+#ifdef _DEBUG
+  cerr << "typinfo find return index = " << index << endl;
+#endif
 
   return index;
+}
+catch (Error& error)
+{
+  throw error += "typeutil find";
 }
 
 #endif
