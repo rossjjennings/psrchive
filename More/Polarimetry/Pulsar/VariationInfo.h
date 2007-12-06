9e1c3bf5dd39b5b10601b5fabd3a069136de712c
straten <straten>
2007-12-06 18:19:34 +0000

    base class of plot interfaces to time variation functions

---
+++ More/Polarimetry/Pulsar/VariationInfo.h
@@ -0,0 +1,58 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/psrchive/psrchive/More/Polarimetry/Pulsar/VariationInfo.h,v $
+   $Revision: 1.1 $
+   $Date: 2007/12/06 18:19:34 $
+   $Author: straten $ */
+
+#ifndef __Pulsar_VariationInfo_H
+#define __Pulsar_VariationInfo_H
+
+#include "Pulsar/ReceptionCalibrator.h"
+#include "MEAL/Scalar.h"
+
+namespace Pulsar {
+
+  //! Communicates Step parameters to plotting routines
+  class VariationInfo : public Calibrator::Info {
+
+  public:
+
+    //! Things that vary in a ReceptionCalibrator
+    enum Which {
+      //! Absolute gain
+      Gain,
+      //! Differential gain
+      Boost,
+      //! Differential phase
+      Rotation
+    };
+
+    //! Construct with which function to plot
+    VariationInfo (const ReceptionCalibrator* calibrator, Which which);
+    
+    //! Return the name of the specified class
+    std::string get_name (unsigned iclass) const;
+    
+    //! Return the Scalar variation function for the specified channel
+    const MEAL::Scalar* get_Scalar (unsigned ichan);
+
+  protected:
+
+    //! The ReceptionCalibrator containing the step functions
+    Reference::To<const ReceptionCalibrator> calibrator;
+
+    //! Which function to plot
+    Which which;
+
+  };
+
+}
+
+#endif
---
+++ More/Polarimetry/VariationInfo.C
@@ -0,0 +1,61 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+#include "Pulsar/VariationInfo.h"
+
+#include <assert.h>
+
+using namespace std;
+
+//! Constructor
+Pulsar::VariationInfo::VariationInfo (const ReceptionCalibrator* cal, Which w)
+{
+  if (Calibrator::verbose)
+    cerr << "Pulsar::VariationInfo::VariationInfo" << endl;
+
+  calibrator = cal;
+  which = w;
+}
+
+const MEAL::Scalar* Pulsar::VariationInfo::get_Scalar (unsigned ichan)
+try
+{
+  const Calibration::StandardModel* model = calibrator->get_model( ichan );
+
+  switch (which)
+  {
+  case Gain:
+    return model->get_gain();
+  case Boost:
+    return model->get_diff_gain();
+  case Rotation:
+    return model->get_diff_phase();
+  default:
+    return 0;
+  }
+}
+catch (Error& error)
+{
+  return 0;
+}
+
+  
+//! Return the name of the specified class
+string Pulsar::VariationInfo::get_name (unsigned iclass) const
+{
+  switch (which)
+  {
+  case Gain:
+    return "\\fiG\\fr";
+  case Boost:
+    return "\\gb";
+  case Rotation:
+    return "\\gf";
+  default:
+    return "";
+  }
+}
+
