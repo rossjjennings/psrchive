1e2e5899c3304e80ba91db98f3ca94573b76da15
straten <straten>
2008-01-11 03:43:39 +0000

    1869002: change to PolnProfileStats implementation

--- More/Polarimetry/StandardData.C
+++ More/Polarimetry/StandardData.C
@@ -65,7 +65,7 @@ Calibration::StandardData::get_stokes (unsigned ibin)
     normalize->set_other (total_other);
     normalize->normalize (result);
 
-    result *= sqrt( stats->get_on_pulse_nbin() );
+    result *= sqrt( stats->get_stats()->get_on_pulse_nbin() );
   }
 
   return result;
