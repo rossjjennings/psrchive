8781dbed486c805fd45a1fb3cab30bcd35a54a77
demorest <demorest>
2008-01-28 21:01:09 +0000

    Filled in more GBT and Arecibo receivers.

--- Base/Extensions/GBT.C
+++ Base/Extensions/GBT.C
@@ -11,6 +11,14 @@
 
 using namespace std;
 
+// Note: All polarization bases here refer to the native feeds of
+// the receiver.  If a hybrid was in use during observing, that
+// will have to be explicitly accounted for somehow outside these 
+// routines.
+//
+// Also, this is not a complete list of all GBT receivers, just
+// the ones that are commonly used for pulsar observing.
+
 void Pulsar::GBT::Rcvr_350 (Receiver* r)
 {
   r->set_name ("Rcvr_350");
@@ -46,5 +54,11 @@ void Pulsar::GBT::Rcvr2_3 (Receiver* r)
 //! Initialize the Receiver Extension with GBT best guess
 void Pulsar::GBT::guess (Receiver* r, Archive* a)
 {
-
+    // Guess based on center freq.  could be tricky since
+    // some receivers have overlapping bands.
+    double f = a->get_centre_frequency() / 1000.0; // Freq in GHz
+    if ((f>0.290) && (f<0.395)) { GBT::Rcvr_350(r); }
+    else if ((f>0.680) && (f<0.920)) { GBT::Rcvr_800(r); }
+    else if ((f>1.15) && (f<1.73)) { GBT::Rcvr1_2(r); }
+    else if ((f>1.73) && (f<2.60)) { GBT::Rcvr2_3(r); }
 }
