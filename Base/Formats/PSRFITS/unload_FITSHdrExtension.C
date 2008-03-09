5ffa442238ad01de7082d8b071b0b60ee3bf6b6b
nopeer <nopeer>
2008-03-09 23:40:11 +0000

    unload OBSFREQ

--- Base/Formats/PSRFITS/unload_FITSHdrExtension.C
+++ Base/Formats/PSRFITS/unload_FITSHdrExtension.C
@@ -4,13 +4,17 @@
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
+
+
+
 #include "Pulsar/FITSArchive.h"
 #include "Pulsar/FITSHdrExtension.h"
-#include "psrfitsio.h"
+#include <psrfitsio.h>
+#include <tostring.h>
 #include <FITSUTC.h>
-#include <ctime>
+// #include <ctime>
 
-using namespace std;
+
 
 void Pulsar::FITSArchive::unload (fitsfile* fptr, const FITSHdrExtension* ext)
 {
@@ -21,11 +25,12 @@ void Pulsar::FITSArchive::unload (fitsfile* fptr, const FITSHdrExtension* ext)
   time_t now = time( NULL );
   FITSUTC now_utc( now );
   psrfits_update_key( fptr, "DATE", tostring<FITSUTC>( now_utc ) );
-  
+
   psrfits_update_key (fptr, "TRK_MODE", ext->trk_mode);
   psrfits_update_key (fptr, "DATE-OBS", ext->stt_date + "T" + ext->stt_time);
   psrfits_update_key (fptr, "STT_LST", ext->stt_lst);
   psrfits_update_key (fptr, "STT_TIME", ext->stt_time);
   psrfits_update_key (fptr, "STT_DATE", ext->stt_date);
+  psrfits_update_key (fptr, "OBSFREQ", ext->obsfreq );
 }
 
