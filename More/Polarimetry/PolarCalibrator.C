edfe9dcfedd44fa25a9af45d9351a615c975f04a
straten <straten>
2008-04-30 17:12:42 +0000

    Polar::solve moved here - not really accurate any longer

--- More/Polarimetry/PolarCalibrator.C
+++ More/Polarimetry/PolarCalibrator.C
@@ -1,10 +1,12 @@
 /***************************************************************************
  *
- *   Copyright (C) 2003 by Aidan Hotan
+ *   Copyright (C) 2003-2008 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
+
 #include "Pulsar/PolarCalibrator.h"
+#include "MEAL/Rotation.h"
 #include "Pauli.h"
 
 using namespace std;
@@ -140,7 +142,7 @@ string Pulsar::PolarCalibrator::Info::get_name (unsigned iclass) const
   case 1:
     return "sinh\\(2128)\\.m\\b\\u \\(0832)";
   case 2:
-    return "\\gf\\dk\\u (rad.)";
+    return "sin\\gf\\.n\\b\\u\\(0832)";
   default:
     return "";
   }
@@ -160,6 +162,57 @@ unsigned Pulsar::PolarCalibrator::Info::get_nparam (unsigned iclass) const
   }
 }
 
+template<typename T, typename U>
+const Estimate<T,U> sinc (const Estimate<T,U>& u)
+{
+  T val = 1.0;
+  T dval_du = 0.0;
+
+  if (u.val != 0.0)
+  {
+    val = ::sin(u.val) / u.val; 
+    dval_du = (::cos(u.val) - val) / u.val;
+  }
+
+  return Estimate<T,U> (val, dval_du*dval_du*u.var);
+}
+
+//! Return the estimate of the specified parameter
+Estimate<float> 
+Pulsar::PolarCalibrator::Info::get_param (unsigned ichan, unsigned iclass,
+					  unsigned iparam) const
+{
+  if( ! calibrator->get_transformation_valid (ichan) )
+    return 0;
+
+  if (iclass < 2)
+    return PolnCalibrator::Info::get_param (ichan, iclass, iparam);
+
+  const MEAL::Polar* polar
+    = dynamic_cast<const MEAL::Polar*> 
+    ( calibrator->get_transformation (ichan) );
+
+  if (!polar)
+    throw Error (InvalidState, "Pulsar::PolarCalibrator::Info::get_param",
+		 "transformation[%d] is not an Polar", ichan);
+
+  const MEAL::Rotation* rotation = polar->get_rotation_transformation();
+
+  vector< Jones<double> > grad;
+  Jones<double> R = rotation->evaluate (&grad);
+
+  Quaternion<double, Unitary> q = real( unitary(R) );
+  double val = q[iparam + 1];
+  double var = 0.0;
+  for (unsigned i=0; i<3; i++)
+  {
+    Quaternion<double, Unitary> dq_dri = real( unitary(grad[i]) );
+    double dval_dri = dq_dri[iparam + 1];
+    var += dval_dri * dval_dri * rotation->get_variance(i);
+  }
+
+  return Estimate<float> (val, var);
+}
 
 Pulsar::PolarCalibrator::Info*
 Pulsar::PolarCalibrator::get_Info () const
