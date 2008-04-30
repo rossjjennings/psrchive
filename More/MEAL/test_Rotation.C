aa3be1e4f4e60d43488a29ac8769eb1189c25654
straten <straten>
2008-04-30 01:08:18 +0000

    loop over phi and theta; verify by comparison with expected rotation

--- More/MEAL/test_Rotation.C
+++ More/MEAL/test_Rotation.C
@@ -12,7 +12,26 @@ using namespace std;
 
 static char* name = "QUV";
 
-void test_Rotation (double theta, double phi, unsigned axis, unsigned perm=0)
+static complex<double> Ci (0, 1);
+
+static Jones<double> identity (1, 0,
+                               0, 1);
+
+static Jones<double> sigma[3];
+
+void init ()
+{
+  sigma[0] = Jones<double> (1, 0,
+                            0, -1);
+
+  sigma[1] = Jones<double> (0, 1,
+                            1, 0);
+
+  sigma[2] = Jones<double> (0, -Ci,
+                            Ci, 0);
+}
+
+void test_Rotation (double theta, double phi, unsigned axis, unsigned perm)
 {
   MEAL::Rotation1 rotation (Vector<3,float>::basis(axis));
   rotation.set_param (0, phi);
@@ -24,6 +43,16 @@ void test_Rotation (double theta, double phi, unsigned axis, unsigned perm=0)
   unsigned j = (axis+perm+2)%3;
   unsigned k = (axis+perm)%3;
 
+  Jones<double> expect = cos(phi) * identity + sin(phi) * Ci * sigma[axis];
+
+  if ( norm (xform - expect) != 0 )
+  {
+    cerr << "rotation[" << axis << "," << phi << "]=" << xform << endl;
+    cerr << "expected=" << expect << endl;
+    cerr << "norm=" << norm (xform - expect) << endl;
+    exit (-1);
+  }
+
   stokes[i+1] = cos(theta);
   stokes[j+1] = sin(theta);
   stokes[k+1] = 0.2;
@@ -32,17 +61,22 @@ void test_Rotation (double theta, double phi, unsigned axis, unsigned perm=0)
 
   double result = theta - 2.0*phi;
 
-  if (stokes[0] != 1.5)
+  if (fabs (stokes[0] - 1.5) > 1e-12)
+  {
     cerr << "I=" << stokes[0] << " != " << 1.5 << endl;
+    exit(-1);
+  }
 
-  if (fabs (stokes[i+1] - cos(result)) > 1e-12) {
+  if (fabs (stokes[i+1] - cos(result)) > 1e-12)
+  {
     cerr << name[i] << "=" << stokes[i+1] << " != " << cos(result) << endl;
     exit(-1);
   }
 
-  if (fabs (stokes[j+1] - sin(result)) > 1e-12) {
+  if (fabs (stokes[j+1] - sin(result)) > 1e-12)
+  {
     cerr << name[j] << "=" << stokes[j+1] << " != " << sin(result) << endl;
-      exit(-1);
+    exit(-1);
   }
 
   if (fabs (stokes[k+1] - 0.2) > 1e-12) {
@@ -51,21 +85,34 @@ void test_Rotation (double theta, double phi, unsigned axis, unsigned perm=0)
   }
 
 }
+ 
+void test_loop (unsigned axis, unsigned perm)
+{
+  double increment = M_PI/87.0;
+
+  for (double theta = -2*M_PI; theta < 2*M_PI+increment; theta += increment)
+    for (double phi = -M_PI; phi < M_PI+increment; phi += increment)
+      test_Rotation (theta, phi, axis, perm);
+}
 
 int main ()
 {
+  init ();
+
   cerr << "Testing in linear basis" << endl;
-  for (unsigned i=0; i < 3; i++) {
-    cerr << "Testing rotations about " << name[i] << " axis" << endl;
-    test_Rotation (1.43/2, -2.15/2, i);
+  for (unsigned axis=0; axis < 3; axis++)
+  {
+    cerr << "Testing rotations about " << name[axis] << " axis" << endl;
+    test_loop (axis, 0);
   }
 
   cerr << "Testing in circular basis" << endl;
   Pauli::basis.set_basis (Signal::Circular);
 
-  for (unsigned i=0; i < 3; i++) {
-    cerr << "Testing rotations about " << name[(i+2)%3] << " axis" << endl;
-    test_Rotation (-.73/2, 3.1/2, i, 2);
+  for (unsigned axis=0; axis < 3; axis++)
+  {
+    cerr << "Testing rotations about " << name[(axis+2)%3] << " axis" << endl;
+    test_loop (axis, 2);
   }
 
   cerr << "All tests passed" << endl;
