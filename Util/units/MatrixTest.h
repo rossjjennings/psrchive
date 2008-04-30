ba0e8406a61b42aa4c45741c55b02c5bd2e262d5
straten <straten>
2008-04-30 01:10:01 +0000

    compiler warning cleared

--- Util/units/MatrixTest.h
+++ Util/units/MatrixTest.h
@@ -16,6 +16,8 @@ class MatrixTest {
   
   MatrixTest (unsigned int seed=13)
     { verbose = false; if (seed) srand(seed); scale=1e3; tolerance = 1e-12; }
+
+  virtual ~MatrixTest () {}
   
   virtual void runtest (unsigned nloops);
   
