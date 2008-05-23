ddfbe2b85e7b8c21ff8d87e0b1112f1cf3d7e922
straten <straten>
2008-05-23 12:37:43 +0000

    1873445: aliases added

--- Util/tempo/Observatory.C
+++ Util/tempo/Observatory.C
@@ -7,7 +7,11 @@
  ***************************************************************************/
 
 #include "Observatory.h"
+#include "tempo++.h"
+
 #include <math.h>
+
+using namespace std;
 
 Tempo::Observatory::Observatory ()
 {
@@ -54,7 +58,7 @@ Tempo::ObservatoryITRF::ObservatoryITRF (double _x, double _y, double _z)
 }
 
 
-void Tempo::ObservatoryITRF::get_xyz (double& _x, double& _y, double& _z)
+void Tempo::ObservatoryITRF::get_xyz (double& _x, double& _y, double& _z) const
 {
   _x = x; _y = y; _z = z;
 }
@@ -62,7 +66,7 @@ void Tempo::ObservatoryITRF::get_xyz (double& _x, double& _y, double& _z)
 //! Get the latitude and longitude in radians
 void Tempo::ObservatoryITRF::get_latlonel (double& lat,
 					   double& lon,
-					   double& el)
+					   double& el) const
 {
   double radius = x*x + y*y + z*z;
   lat = asin (z/radius);
@@ -82,7 +86,7 @@ Tempo::ObservatoryWGS84::ObservatoryWGS84 (double _lat,
 };
 
 //! Get the geocentric XYZ coordinates in metres
-void Tempo::ObservatoryWGS84::get_xyz (double& x, double& y, double& z)
+void Tempo::ObservatoryWGS84::get_xyz (double& x, double& y, double& z) const
 {
   throw Error (InvalidState, "Tempo::ObservatoryWGS84::get_xyz",
 	       "not implemented");
@@ -91,8 +95,122 @@ void Tempo::ObservatoryWGS84::get_xyz (double& x, double& y, double& z)
 //! Get the latitude and longitude in radians
 void Tempo::ObservatoryWGS84::get_latlonel (double& _lat,
 					    double& _lon,
-					    double& _el)
+					    double& _el) const
 {
   _lat = lat; _lon = lon; _el = el;
 };
+
+static string itoa_code (const string& telescope_name);
+
+const Tempo::Observatory*
+Tempo::observatory (const string& telescope_name)
+{
+  for (unsigned i=0; i < antennae.size(); i++)
+    if (strcasestr( antennae[i]->get_name().c_str(), telescope_name.c_str() ))
+      return antennae[i];
+
+  string itoa = itoa_code (telescope_name);
+
+  for (unsigned i=0; i < antennae.size(); i++)
+    if (antennae[i]->get_itoa_code() == itoa)
+      return antennae[i];
+
+  return 0;
+}
+
+class aliases
+{
+public:
+  aliases (const string& code, const string& alias)
+  {
+    itoa_code = code;
+    aka.push_back (alias);
+  }
+
+  bool match (const string& name)
+  {
+    for (unsigned i=0; i<aka.size(); i++)
+      if (strcasecmp (aka[i].c_str(), name.c_str()) == 0)
+	return true;
+    return false;
+  }
+
+  string itoa_code;
+  vector<string> aka;
+};
+
+static int init = 0;
+
+static vector<aliases> itoa_aliases;
+
+static int default_aliases ();
+
+string itoa_code (const string& telescope_name)
+{
+  for (unsigned i=0; i<itoa_aliases.size(); i++)
+    if (itoa_aliases[i].match( telescope_name ))
+      return itoa_aliases[i].itoa_code;
+
+  return string();
+}
+
+void add_alias (const string& itoa_code, const string& alias)
+{
+  if (!init)
+    default_aliases ();
+
+  for (unsigned i=0; i<itoa_aliases.size(); i++)
+    if (itoa_aliases[i].itoa_code == itoa_code)
+    {
+      itoa_aliases[i].aka.push_back (alias);
+      return;
+    }
+
+  itoa_aliases.push_back ( aliases( itoa_code, alias ) );
+}
+
+int default_aliases ()
+{
+  add_alias ("GB", "gbt");
+  add_alias ("GB", "green bank");
+  add_alias ("GB", "greenbank");
+
+  add_alias ("NA", "atca");
+  add_alias ("NA", "narrabri");
+
+  add_alias ("AO", "arecibo");
+
+  add_alias ("HO", "hobart");
+
+  add_alias ("NS", "urumqi");
+
+  add_alias ("TD", "tid");
+  add_alias ("TD", "tidbinbilla");
+  add_alias ("TD", "DSS43");
+  add_alias ("TD", "DSS 43");
+
+  add_alias ("PK", "pks");
+  add_alias ("PK", "parkes");
+
+  add_alias ("JB", "jodrell");
+  add_alias ("JB", "jodrell bank");
+
+  add_alias ("VL", "vla");
+
+  add_alias ("BO", "northern cross");
+
+  add_alias ("MO", "most");
+
+  add_alias ("NC", "nancay");
+
+  add_alias ("EF", "effelsberg");
+
+  add_alias ("WT", "wsrt");
+  add_alias ("WT", "westerbork");
+
+  add_alias ("GM", "gmrt");
+
+  return 1;
+}
+
 
