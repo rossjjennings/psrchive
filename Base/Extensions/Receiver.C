3f101eb9a2c32eb029f86dbaf5b8d31a65d4d0c9
straten <straten>
2008-06-06 05:48:07 +0000

    1985022: oa, xo, yo, co removed; fac->fdc; vac->prc

--- Base/Extensions/Receiver.C
+++ Base/Extensions/Receiver.C
@@ -261,38 +261,22 @@ public:
     add( &Receiver::get_field_orientation,
 	 &Receiver::set_field_orientation,
 	 "sa", "Symmetry angle of receptor basis" );
-    
+
     add( &Receiver::get_reference_source_phase,
 	 &Receiver::set_reference_source_phase,
 	 "rph", "Reference source phase" );
-    
-    add( &Receiver::get_orientation,
-	 &Receiver::set_orientation,
-	 "oa", "Orientation of receptor basis" );
-    
-    add( &Receiver::get_X_offset,
-	 &Receiver::set_X_offset,
-	 "xo", "Offset of feed X-axis wrt platform zero" );
-    
-    add( &Receiver::get_Y_offset,
-	 &Receiver::set_Y_offset,
-	 "yo", "Offset of feed Y-axis wrt nominal value" );
-    
-    add( &Receiver::get_calibrator_offset,
-	 &Receiver::set_calibrator_offset,
-	 "co", "Offset of calibrator wrt nominal value" );
-    
+
+    add( &Receiver::get_basis_corrected,
+	 &Receiver::set_basis_corrected,
+	 "fdc", "Receptor basis corrected" );
+
+    add( &Receiver::get_projection_corrected,
+	 &Receiver::set_projection_corrected,
+	 "prc", "Receptor projection corrected" );
+
     add( &Receiver::get_tracking_angle,
 	 &Receiver::set_tracking_angle,
 	 "ta", "Tracking angle of feed" );
-    
-    add( &Receiver::get_basis_corrected,
-	 &Receiver::set_basis_corrected,
-	 "fac", "Feed angle corrected" );
-    
-    add( &Receiver::get_projection_corrected,
-	 &Receiver::set_projection_corrected,
-	 "vac", "Vertical (parallactic) angle corrected" );
   }
   
   virtual std::string get_interface_name() { return "ReceiverTI"; }
