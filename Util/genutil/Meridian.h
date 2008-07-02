b2c440721a3ef00f18d046417656477d1a4229c2
straten <straten>
2008-07-02 11:23:18 +0000

    meridian mounted antenna derived from Directional base class

---
+++ Util/genutil/Meridian.h
@@ -0,0 +1,45 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2008 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/psrchive/psrchive/Util/genutil/Meridian.h,v $
+   $Revision: 1.1 $
+   $Date: 2008/07/02 11:23:18 $
+   $Author: straten $ */
+
+#ifndef __Meridian_H
+#define __Meridian_H
+
+#include "Directional.h"
+
+//! Meridian mounted antenna, with first rotation in meridianal plane
+/*! Also known as x-y mount */
+
+class Meridian : public Directional
+{  
+public:
+  
+  //! Get the x angle in radians
+  double get_x () const;
+
+  //! Get the y angle in radians
+  double get_y () const;
+
+protected:
+
+  //! Get the receptor basis in the reference frame of the observatory
+  Matrix<3,3,double> get_basis (const Vector<3,double>& from_source) const;
+
+  //! The telescope x in radians
+  mutable double x;
+
+  //! The telescope y in radians
+  mutable double y;
+
+};
+
+#endif
