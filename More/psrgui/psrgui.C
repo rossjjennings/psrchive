a3e666624681ae255e45f5cb3373fceb96d36a13
straten <straten>
2008-07-13 12:12:47 +0000

    with instant feedback, PulsarGUI cannot load file until it is shown

--- More/psrgui/psrgui.C
+++ More/psrgui/psrgui.C
@@ -2,18 +2,20 @@
 
 #include "PulsarGUI.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
   QApplication app(argc, argv);
 
-  QString filename;
-  if (app.argc() > 1) {
-    filename = app.argv()[1];
-  }
-
-  Pulsar::PulsarGUI* pg = new Pulsar::PulsarGUI(&app, filename);
+  Pulsar::PulsarGUI* pg = new Pulsar::PulsarGUI(&app);
   app.setMainWidget(pg);
   pg -> show();
-  
+
+  if (app.argc() > 1)
+  {
+    QString filename = app.argv()[1];
+    pg->readFile(filename);
+  }
+
   return app.exec();
 
 }
