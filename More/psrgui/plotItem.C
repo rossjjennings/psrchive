651bbeee857100da1064c110a034422b45c3bc93
straten <straten>
2008-07-16 02:28:01 +0000

    perhaps will use QToolTip instead of QWhatsThis

--- More/psrgui/plotItem.C
+++ More/psrgui/plotItem.C
@@ -1,9 +1,10 @@
 #include "plotItem.h"
+#include "qtooltip.h"
 
 Pulsar::plotItem::plotItem(QWidget* parent, Pulsar::Plot* figPtr, 
 			   const QString& name, const QString& desc)
   : QRadioButton(name, parent)
 {
-  QWhatsThis::add(this, desc);
+  // QToolTip::add(this, desc);
   figStyle = figPtr;
 }
