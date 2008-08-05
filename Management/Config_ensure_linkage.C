a977082745da8946a443f5396840fab6004190ef
straten <straten>
2008-08-05 13:59:47 +0000

    ensure that configuration options are linked into the executable

---
+++ Management/Config_ensure_linkage.C
@@ -0,0 +1,22 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2008 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "Pulsar/Config.h"
+
+#include "Pulsar/Interpreter.h"
+#include "Pulsar/FrequencyAppend.h"
+
+void Pulsar::Config::ensure_linkage ()
+{
+  // the standard shell uses a lot of different options
+  Reference::To<Interpreter> temp = standard_shell();
+
+  if (FrequencyAppend::weight_strategy)
+    // do nothing
+    ;
+}
+
