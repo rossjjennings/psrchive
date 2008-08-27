c13c35eda842696cce7547ec92143896cd73f1e1
straten <straten>
2008-08-27 01:08:52 +0000

    width method moved to width.C (used by rmfit)

--- More/Applications/pdv.C
+++ More/Applications/pdv.C
@@ -7,9 +7,9 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/More/Applications/pdv.C,v $
-   $Revision: 1.33 $
-   $Date: 2008/07/25 04:48:07 $
-   $Author: jonathan_khoo $ */
+   $Revision: 1.34 $
+   $Date: 2008/08/27 01:08:52 $
+   $Author: straten $ */
 
 
 #ifdef HAVE_CONFIG_H
@@ -27,7 +27,9 @@ using Pulsar::FITSArchive;
 #include "Pulsar/Profile.h"
 #include "Pulsar/Interpreter.h"
 #include "Pulsar/PolnProfile.h"
+
 #include "Pulsar/ProfileStats.h"
+#include "Pulsar/FourierSNR.h"
 
 #include <Pulsar/ProcHistory.h>
 #include <Pulsar/DigitiserStatistics.h>
@@ -416,105 +418,16 @@ Estimate<float> flux2 (const Profile* profile)
   return stats.get_total() / profile->get_nbin();
 }
 
-float width (const Profile* profile, float& error, float pc, float dc)
+Estimate<float> flux3 (const Profile* profile)
 {
+  Pulsar::FourierSNR stats;
 
-  float min_phs = profile->find_min_phase(dc);
-  int   min_bin = int(min_phs * float(profile->get_nbin()));
-
-  double min_mean = 0.0;
-  double min_var  = 0.0;
-  double var_var  = 0.0;
-
-  int start = int(float(profile->get_nbin()) * (min_phs - dc/2.0));
-  int end   = int(float(profile->get_nbin()) * (min_phs + dc/2.0));
-
-  profile->stats(&min_mean, &min_var, &var_var, start, end);
-
-  float stdev = sqrt(min_var);
-
-  float level = profile->max() * pc/100.0 - stdev;
-
-  std::vector<float> results;
-
-  for (unsigned tries = 0; tries < 3; tries++)
-  {
-
-    if (level < 5.0*min_var)
-    {
-      return 0.0;
-    }
-    float c = 0.0;
-    float m = 0.0;
-    float lo_edge = 0.0;
-    float hi_edge = 0.0;
-
-    int bc = min_bin;
-
-    while (1)
-    {
-      if (profile->get_amps()[bc] > level)
-      {
-        // Linerly interpolate between both sides of the edge
-        int x1 = bc;
-        int x2 = bc+1;
-
-        float y1 = profile->get_amps()[x1];
-        float y2 = profile->get_amps()[x2];
-
-        m = (y2 - y1) / (float(x2) - float(x1));
-        c = y1 - m*x1;
-
-        hi_edge = (level - c)/m;
-
-        break;
-      }
-      bc--;
-      if (bc < 0)
-        bc += profile->get_nbin();
-      if (bc == min_bin)
-        break;
-    }
-
-    bc = min_bin;
-
-    while (1)
-    {
-      if (profile->get_amps()[bc] > level)
-      {
-        // Linerly interpolate between both sides of the edge
-        int x1 = bc;
-        int x2 = bc-1;
-
-        float y1 = profile->get_amps()[x1];
-        float y2 = profile->get_amps()[x2];
-
-        m = (y2 - y1) / (float(x2) - float(x1));
-        c = y1 - m*x1;
-
-        lo_edge = (level-c)/m;
-
-        break;
-      }
-      bc++;
-      if (bc > int(profile->get_nbin()-1))
-        bc -= profile->get_nbin();
-      if (bc == min_bin)
-        break;
-    }
-
-    float bin_dist = hi_edge - lo_edge;
-    if(bin_dist < 0)bin_dist += float(profile->get_nbin());
-
-    results.push_back(bin_dist / float(profile->get_nbin()));
-    level += stdev;
-
-  }
-
-  error = fabs(results.front()-results.back())/2.0;
-  return results[1];
+  stats.set_profile( profile );
+  return sqrt(stats.get_total());
 }
 
+// defined in width.C
+float width (const Profile* profile, float& error, float pc, float dc);
 
 void CalParameters( Reference::To< Archive > archive )
 {
@@ -586,9 +499,6 @@ void Flux( Reference::To< Archive > archive )
   if (archive->get_npol() == 4)
     archive->convert_state (Signal::Stokes);
 
-  //   if( !keep_baseline )
-  //     archive->remove_baseline ();
-
   cout << "File\t\t\tSub\tChan\tPol\tMJD\t\tFreq\tFlux\tUnit\t10\% Width\t50\% Width"
   << endl;
 
@@ -658,7 +568,7 @@ void Flux2( Reference::To< Archive > archive )
   {
     for (int c = fchan; c <= lchan; c++)
     {
-      cout << archive->get_filename() << "\t";
+      cout << archive->get_filename() << " \t";
       cout << s << "\t" << c << "\t";
       cout.setf(ios::showpoint);
 
@@ -666,9 +576,10 @@ void Flux2( Reference::To< Archive > archive )
       cout << flux.get_value() << " " << flux.get_error();
 
       if (archive->get_scale() == Signal::Jansky)
-        cout << "\t" << "mJy";
+	cout << "\t" << "mJy";
 
-      cout << endl;
+      Estimate<float> fourier = flux3( archive->get_Profile(s,0,c) );
+      cout << " " << fourier.get_value() << " " << fourier.get_error() << endl;
     }
   }
 }
@@ -1038,7 +949,8 @@ try
 
   preprocessor.script( jobs );
 
-  if( archive->get_state() != Signal::Stokes && (show_pol_frac || show_lin_frac || show_circ_frac || show_pa ) )
+  if( archive->get_state() != Signal::Stokes && 
+      (show_pol_frac || show_lin_frac || show_circ_frac || show_pa ) )
     archive->convert_state(Signal::Stokes);
 
 
