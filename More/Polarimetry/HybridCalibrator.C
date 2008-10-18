eaa12715cde295a805a3631130820758dc5e1202
straten <straten>
2008-10-18 06:14:31 +0000

    2170654: integrate precalibrator/calibrator_stokes to lower observation nchan

--- More/Polarimetry/HybridCalibrator.C
+++ More/Polarimetry/HybridCalibrator.C
@@ -4,6 +4,7 @@
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
+
 #include "Pulsar/HybridCalibrator.h"
 #include "Pulsar/ReferenceCalibrator.h"
 #include "Pulsar/CalibratorStokes.h"
@@ -13,6 +14,8 @@
 
 #include "Pulsar/SingleAxisSolver.h"
 #include "Pulsar/SingleAxis.h"
+
+#include "MEAL/Complex2Value.h"
 #include "Pauli.h"
 
 using namespace std;
@@ -79,6 +82,7 @@ void Pulsar::HybridCalibrator::set_precalibrator (PolnCalibrator* _calibrator)
   filenames[0] = precalibrator->get_filenames();
 }
 
+
 unsigned Pulsar::HybridCalibrator::get_maximum_nchan ()
 {
   if (precalibrator)
@@ -94,15 +98,103 @@ unsigned Pulsar::HybridCalibrator::get_maximum_nchan ()
 	       "none of the required data have been added");
 }
 
+//! Return the Stokes parameters of the ichan'th target channel
+/*! \param target_nchan the number of frequency channels of the target
+  If target_nchan is less than the number of frequency channels in the 
+  CalibratorStokes instance, then neighbouring frequency channels will
+  be integrated.
+*/
+Stokes< Estimate<double> > 
+get_stokes (const Pulsar::CalibratorStokes* stokes,
+	    const unsigned ichan, unsigned target_nchan)
+{
+  const unsigned have_nchan = stokes->get_nchan();
+  const unsigned factor = have_nchan / target_nchan;
+
+  if (!factor)
+    throw Error (InvalidParam, "get_stokes",
+		 "CalibratorStokes nchan=%u < required=%u",
+		 have_nchan, target_nchan);
+
+  Stokes< MeanEstimate<double> > result;
+  unsigned stokes_ichan = ichan * factor;
+  bool valid = false;
+
+  for (unsigned i=0; i<factor; i++)
+  {
+    if (!stokes->get_valid (stokes_ichan+i))
+    {
+      if (Pulsar::Calibrator::verbose > 2)
+	cerr << "get_stokes(CalibratorStokes) ichan=" << stokes_ichan+i
+	     << " flagged invalid" << endl;
+      continue;
+    }
+
+    result += stokes->get_stokes (stokes_ichan+i);
+    valid = true;
+  }
+
+  if (!valid)
+    throw Error (InvalidParam, "get_stokes(CalibratorStokes)",
+		 "no valid data for ichan=%d", ichan);
+
+  // MeanEstimate sets Stokes I to zero because the estimates have no error
+  Stokes< Estimate<double> > answer = result;
+  answer[0] = 1.0;
+
+  return answer;
+}
+
+Jones< Estimate<double> > 
+get_response (const Pulsar::PolnCalibrator* response,
+	      const unsigned ichan, unsigned target_nchan)
+{
+  const unsigned have_nchan = response->get_nchan();
+  const unsigned factor = have_nchan / target_nchan;
+
+  if (!factor)
+    throw Error (InvalidParam, "get_response",
+		 "PolnCalibrator nchan=%u < required=%u",
+		 have_nchan, target_nchan);
+
+  Jones< MeanEstimate<double> > result;
+  unsigned response_ichan = ichan * factor;
+  bool valid = false;
+
+  for (unsigned i=0; i<factor; i++)
+  {
+    if (!response->get_transformation_valid (response_ichan+i))
+    {
+      if (Pulsar::Calibrator::verbose > 2)
+	cerr << "get_response(PolnCalibrator) ichan=" << response_ichan+i
+	     << " flagged invalid" << endl;
+      continue;
+    }
+
+    Jones< Estimate<double> > value;
+    response->get_transformation (response_ichan+i) -> evaluate (value);
+    result += value;
+    valid = true;
+  }
+
+  if (!valid)
+    throw Error (InvalidParam, "get_response(PolnCalibrator)",
+		 "no valid data for ichan=%d", ichan);
+
+  return result;
+}
+
+template<typename T>
+Jones<T> val (const Jones< Estimate<T> >& J)
+{
+  return Jones<T> (complex<T>(J(0,0).real().val, J(0,0).imag().val),
+		   complex<T>(J(0,1).real().val, J(0,1).imag().val),
+		   complex<T>(J(1,0).real().val, J(1,0).imag().val),
+		   complex<T>(J(1,1).real().val, J(1,1).imag().val));
+}
+
 void Pulsar::HybridCalibrator::calculate_transformation ()
 {
-#if 0 
-  if (!precalibrator)
-    throw Error (InvalidState,
-		 "Pulsar::HybridCalibrator::calculate_transformation",
-		 "no precalibrator PolnCalibrator");
-#endif
-
   if (!reference_input)
     cerr << "Pulsar::HybridCalibrator::calculate_transformation\n"
             "  WARNING: no reference source input Stokes parameters" << endl;
@@ -112,34 +204,44 @@ void Pulsar::HybridCalibrator::calculate_transformation ()
 		 "Pulsar::HybridCalibrator::calculate_transformation",
 		 "no reference observation ReferenceCalibrator");
 
-  unsigned nchan;
-  if (precalibrator)
-    nchan = precalibrator->get_nchan();
-  else
-    nchan = reference_observation->get_nchan();
+  const unsigned target_nchan 
+    = std::min (observation_nchan, get_maximum_nchan());
 
-  if (reference_input && reference_input->get_nchan() != nchan)
-    throw Error (InvalidState,
-		 "Pulsar::HybridCalibrator::calculate_transformation",
-		 "reference input CalibratorStokes nchan=%d != %d",
-		 reference_input->get_nchan(), nchan);
-  
   if (verbose > 2)
     cerr << "Pulsar::HybridCalibrator::calculate_transformation"
-      " nchan=" << nchan << endl;
+      " target_nchan=" << target_nchan << endl;
 
   // the calibrator hi and lo levels from the PolnCal archive
   vector<vector<Estimate<double> > > cal_hi;
   vector<vector<Estimate<double> > > cal_lo;
 
-  reference_observation->get_levels (nchan, cal_hi, cal_lo);
+  reference_observation->get_levels (target_nchan, cal_hi, cal_lo);
 
-  unsigned npol = cal_hi.size();
+  const unsigned npol = cal_hi.size();
 
   // coherency products in a single channel
   vector<Estimate<double> > cal (npol);
 
-  transformation.resize (nchan);
+  // get the Receiver correction, if any
+  const Receiver* receiver = 0;
+
+  // Try using precalibrator's version first
+  if (precalibrator && precalibrator->has_Receiver())
+    receiver = precalibrator->get_Receiver();
+
+  // Next try the reference obs version
+  else if (reference_observation->has_Receiver())
+    receiver = reference_observation->get_Receiver();
+
+  Jones<double> basis_correction (1.0);
+
+  if (receiver)
+  {
+    BasisCorrection correction;
+    basis_correction = correction (receiver);
+  }
+
+  transformation.resize (target_nchan);
 
   // the correction to supplement the PolnCalibrator
   Reference::To<Calibration::SingleAxis> correction;
@@ -147,29 +249,12 @@ void Pulsar::HybridCalibrator::calculate_transformation ()
   // the SingleAxis interpretation of reference signal
   Calibration::SingleAxis pre_single_axis;
 
-  for (unsigned ichan=0; ichan<nchan; ++ichan) try
+
+  for (unsigned ichan=0; ichan<target_nchan; ++ichan) try
   {
     if (verbose > 2)
       cerr << "Pulsar::HybridCalibrator::calculate_transformation"
 	" ichan=" << ichan << endl;
-
-    if (reference_input && !reference_input->get_valid (ichan))
-    {
-      if (verbose > 2)
-	cerr << "Pulsar::HybridCalibrator::calculate_transformation"
-	  " reference input flagged invalid" << endl;
-      transformation[ichan] = 0;
-      continue;
-    }
-
-    if (precalibrator && !precalibrator->get_transformation_valid (ichan))
-    {
-      if (verbose > 2)
-	cerr << "Pulsar::HybridCalibrator::calculate_transformation"
-	  " precalibrator flagged invalid" << endl;
-      transformation[ichan] = 0;
-      continue;
-    }
 
     // get the coherency vector of the measured reference source
     for (unsigned ipol=0; ipol<npol; ++ipol)
@@ -189,31 +274,24 @@ void Pulsar::HybridCalibrator::calculate_transformation ()
 
     // get the Stokes parameters of the reference source input
     if (reference_input)
-      cal_stokes = reference_input->get_stokes(ichan);
+    {
+      cal_stokes = get_stokes (reference_input, ichan, target_nchan);
+      if (verbose > 2)
+	cerr << "HybridCalibrator cal_stokes=" << cal_stokes << endl;
+    }
 
     // get the precalibrator transformation
     Jones< Estimate<double> > response (1.0);
     if (precalibrator)
-      precalibrator->get_transformation (ichan)->evaluate (response);
-
-    // get the Receiver correction, if any
     {
-      const Receiver* receiver;
-      BasisCorrection basis_correction;
-
-      // Try using precalibrator's version first
-      if (precalibrator && precalibrator->has_Receiver())
-        receiver = precalibrator->get_Receiver();
-
-      // Next try the reference obs version
-      else if (reference_observation->has_Receiver())
-        receiver = reference_observation->get_Receiver();
-
-      response *= basis_correction (receiver);
+      response = ::get_response (precalibrator, ichan, target_nchan);
+      if (verbose > 2)
+	cerr << "HybridCalibrator response=" << response << endl;
     }
 
     // pass the reference Stokes parameters through the instrument
-    Stokes< Estimate<double> > input_stokes = transform (cal_stokes, response);
+    Stokes< Estimate<double> > input_stokes;
+    input_stokes = transform (cal_stokes, response * basis_correction);
 
     // solve for the SingleAxis model that relates input and output states
     correction = new Calibration::SingleAxis;
@@ -230,9 +308,9 @@ void Pulsar::HybridCalibrator::calculate_transformation ()
     MEAL::ProductRule<MEAL::Complex2>* result;
     result = new MEAL::ProductRule<MEAL::Complex2>;
 
-    *result *= correction;
+    result->add_model( correction );
     if (precalibrator) 
-      *result *= precalibrator->get_transformation (ichan);
+      result->add_model( new MEAL::Complex2Value (val(response)) );
 
     transformation[ichan] = result;
 
