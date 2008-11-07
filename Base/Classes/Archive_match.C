9cc630058d7d4d400ce4aea81c0370c5caab31bc
straten <straten>
2008-11-07 03:15:10 +0000

    set unset matching policies to default in get_policy methods

--- Base/Classes/Archive_match.C
+++ Base/Classes/Archive_match.C
@@ -17,7 +17,7 @@ void setup (F& policy, M method)
     policy = Pulsar::Archive::Match::factory (method);
 }
 
-bool apply (Pulsar::Archive::Match* policy, 
+bool apply (const Pulsar::Archive::Match* policy, 
             const Pulsar::Archive* a1, const Pulsar::Archive* a2,
             string& reason)
 {
@@ -28,34 +28,31 @@ bool apply (Pulsar::Archive::Match* policy,
 
 bool Pulsar::Archive::mixable (const Archive* archive, string& reason) const
 {
-  setup (mixable_policy, &Archive::Match::set_check_mixable);
-  return apply (mixable_policy, this, archive, reason);
+  return apply (get_mixable(), this, archive, reason);
 }
 
 bool Pulsar::Archive::standard_match (const Archive* archive,
 				      string& reason) const
 {
-  setup (standard_match_policy, &Archive::Match::set_check_standard);
-  return apply (standard_match_policy, this, archive, reason);
+  return apply (get_standard_match(), this, archive, reason);
 }
 
 bool Pulsar::Archive::processing_match (const Archive* archive,
 					string& reason) const
 {
-  setup (processing_match_policy, &Archive::Match::set_check_processing);
-  return apply (processing_match_policy, this, archive, reason);
+  return apply (get_processing_match(), this, archive, reason);
 }
 
 
 bool Pulsar::Archive::calibrator_match (const Archive* archive,
 					string& reason) const
 {
-  setup (calibrator_match_policy, &Archive::Match::set_check_calibrator);
-  return apply (calibrator_match_policy, this, archive, reason);
+  return apply (get_calibrator_match(), this, archive, reason);
 }
 
 const Pulsar::Archive::Match* Pulsar::Archive::get_standard_match () const
 {
+  setup (standard_match_policy, &Archive::Match::set_check_standard);
   return standard_match_policy;
 }
 
@@ -67,6 +64,7 @@ void Pulsar::Archive::set_standard_match (Match* policy)
 //! Policy determines if a calibrator matches an observation
 const Pulsar::Archive::Match* Pulsar::Archive::get_calibrator_match () const
 {
+  setup (calibrator_match_policy, &Archive::Match::set_check_calibrator);
   return calibrator_match_policy;
 }
 
@@ -78,6 +76,7 @@ void Pulsar::Archive::set_calibrator_match (Match* policy)
 //! Policy determines if data were processed identically
 const Pulsar::Archive::Match* Pulsar::Archive::get_processing_match () const
 {
+  setup (processing_match_policy, &Archive::Match::set_check_processing);
   return processing_match_policy;
 }
 
@@ -89,6 +88,7 @@ void Pulsar::Archive::set_processing_match (Match* policy)
 //! Policy determines if data can be combined/integrated
 const Pulsar::Archive::Match* Pulsar::Archive::get_mixable () const
 {
+  setup (mixable_policy, &Archive::Match::set_check_mixable);
   return mixable_policy;
 }
 
