30fc9b65ea2c13b5b9729730f9dd70a202e17454
straten <straten>
2009-02-05 02:10:07 +0000

    write access to epoch added; name changed to mjd

--- Base/Extensions/FluxCalibratorExtensionTI.C
+++ Base/Extensions/FluxCalibratorExtensionTI.C
@@ -1,34 +1,29 @@
-//-*-C++-*-
 /***************************************************************************
  *
- *   Copyright (C) 2007 by David Smith
+ *   Copyright (C) 2009 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
 
-/* $Source: /cvsroot/psrchive/psrchive/Base/Extensions/FluxCalibratorExtensionTI.C,v $
-   $Revision: 1.4 $
-   $Date: 2008/03/04 00:44:53 $
-   $Author: nopeer $ */
-
-
-
 #include "Pulsar/FluxCalibratorExtension.h"
 
-
-
-using Pulsar::FluxCalibratorExtension;
-
-
-
-FluxCalibratorExtension::Interface::Interface( FluxCalibratorExtension *s_instance )
+Pulsar::FluxCalibratorExtension::Interface::Interface
+( FluxCalibratorExtension *s_instance )
 {
-  if( s_instance )
-    set_instance ( s_instance );
+  if (s_instance)
+    set_instance (s_instance);
 
-  add( &FluxCalibratorExtension::get_nchan, "nchan", "Nr of frequency channels (I)" );
-  add( &FluxCalibratorExtension::get_nreceptor, "nrcvr", "Number of receiver channels (I)" );
-  add( &FluxCalibratorExtension::get_epoch, "epoch", "[MJD] Epoch of calibration obs" );
+  // read-only: requires resize
+  add( &FluxCalibratorExtension::get_nchan,
+       "nchan", "Number of frequency channels" );
+
+  // read-only: requires resize
+  add( &FluxCalibratorExtension::get_nreceptor,
+       "nrcvr", "Number of receiver channels" );
+
+  add( &FluxCalibratorExtension::get_epoch,
+       &FluxCalibratorExtension::set_epoch,
+       "mjd", "Epoch of calibration observation" );
 }
 
 
--- Base/Extensions/PolnCalibratorExtensionTI.C
+++ Base/Extensions/PolnCalibratorExtensionTI.C
@@ -1,28 +1,29 @@
 /***************************************************************************
  *
- *   Copyright (C) 2006 by Willem van Straten
+ *   Copyright (C) 2009 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
 
-
-
 #include "Pulsar/PolnCalibratorExtension.h"
 
-
-
-using Pulsar::PolnCalibratorExtension;
-
-
-
-PolnCalibratorExtension::Interface::Interface( PolnCalibratorExtension *s_instance )
+Pulsar::PolnCalibratorExtension::Interface::Interface
+( PolnCalibratorExtension *s_instance )
 {
-  if( s_instance )
-    set_instance( s_instance );
+  if (s_instance)
+    set_instance (s_instance);
 
-  add( &PolnCalibratorExtension::get_epoch, "epoch", "[MJD] Epoch of calibration obs" );
-  add( &PolnCalibratorExtension::get_ncpar, "npar", "Number of coupling parameters" );
-  add( &PolnCalibratorExtension::get_nchan, "nchan", "Nr of channels in Feed coupling data" );
+  // read-only: requires resize
+  add( &PolnCalibratorExtension::get_nchan,
+       "nchan", "Number of frequency channels" );
+
+  // read-only: requires resize
+  add( &PolnCalibratorExtension::get_ncpar,
+       "npar", "Number of receiver parameters" );
+
+  add( &PolnCalibratorExtension::get_epoch,
+       &PolnCalibratorExtension::set_epoch,
+       "mjd", "Epoch of calibration observation" );
 }
 
 
