40813d87034b059c028328872f637dc82095288e
straten <straten>
2009-02-15 23:34:10 +0000

    digitizer spacing and input differential phase options added

--- More/Polarimetry/simpol.C
+++ More/Polarimetry/simpol.C
@@ -33,18 +33,19 @@ void usage ()
     "options: \n"
     " \n"
     " -b nbit     digitize using nbit bits \n"
+    " -d space    spacing between digitizer levels \n"
     " -n Msamp    simulate Msamp mega samples \n"
     " -s i,q,u,v  specify the Stokes parameters of the input signal \n"
-
+    " -p rad      apply differential phase (in radians) to input \n"
        << endl;
 }
 
-void transform (Jones<float>& jones, float* ex, float* ey)
+void transform (Jones<double>& jones, double* ex, double* ey)
 {
-  complex<float> x (ex[0], ex[1]);
-  complex<float> y (ey[0], ey[1]);
+  complex<double> x (ex[0], ex[1]);
+  complex<double> y (ey[0], ey[1]);
 
-  complex<float> t;
+  complex<double> t;
 
   t = jones(0,0) * x + jones(0,1) * y;
   ex[0] = t.real();
@@ -55,14 +56,11 @@ void transform (Jones<float>& jones, float* ex, float* ey)
   ey[1] = t.imag();
 }
 
-bool unity_variance = false;
-
-void digitize (float& volts, float scale, float max, float rescale)
+void digitize (double& volts, double scale, double max, double rescale)
 {
-  float value = (0.5 + std::min(max, floor(fabs(volts) * scale)));
+  double value = (0.5 + std::min(max, floor(fabs(volts) * scale)));
 
-  if (unity_variance)
-    value *= rescale;
+  value *= rescale;
 
   if (volts < 0)
     volts = -value;
@@ -73,16 +71,19 @@ void digitize (float& volts, float scale, float max, float rescale)
 int main (int argc, char** argv)
 {
   uint64 ndat = 1024 * 1024;       // 1Mpt set of random samples
+
   unsigned nbit = 0;               // number of bits per sample in digitizer
+  double digitizer_spacing = 0;    // spacing between digitizer levels
 
   bool maintain_thresholds = true; // do not reset sampling thresholds
   bool polarized = false;
 
-  Stokes<float> stokes (1,0,0,0);
+  Stokes<double> stokes (1,0,0,0);
+  double phase = 0;
 
   bool verbose = false;
   int c;
-  while ((c = getopt(argc, argv, "hb:n:s:")) != -1) {
+  while ((c = getopt(argc, argv, "hb:d:n:p:s:")) != -1) {
     switch (c)  {
 
     case 'h':
@@ -93,14 +94,19 @@ int main (int argc, char** argv)
       nbit = atoi (optarg);
       break;
 
+    case 'd':
+      digitizer_spacing = atof (optarg);
+      break;
+
     case 's': {
-      float i,q,u,v;
-      if (sscanf (optarg, "%f,%f,%f,%f", &i,&q,&u,&v) != 4) {
+      double i,q,u,v;
+      if (sscanf (optarg, "%lf,%lf,%lf,%lf", &i,&q,&u,&v) != 4)
+      {
 	cerr << "Error parsing " << optarg << " as Stokes 4-vector" << endl;
 	return -1;
       }
       polarized = true;
-      stokes = Stokes<float> (i,q,u,v);
+      stokes = Stokes<double> (i,q,u,v);
 
       if (stokes.abs_vect() > i) {
 	cerr << "Invalid Stokes parameters (p>I) " << stokes << endl;
@@ -114,16 +120,33 @@ int main (int argc, char** argv)
       ndat *= atoi (optarg);
       break;
 
+    case 'p':
+      phase = atof (optarg);
+      break;
+
     case 'v':
       verbose = true;
       break;
     }
   }
 
+  if (phase)
+  {
+    double x = stokes[2];
+    double y = stokes[3];
+    double cp = cos(phase);
+    double sp = sin(phase);
+
+    stokes[2] = cp*x - sp*y;
+    stokes[3] = sp*x + cp*y;
+
+    cerr << "Phase rotated stokes: " << stokes << endl;
+  }
+
   cerr << "Simulating " << ndat << " samples ..." << endl;
 
-  Quaternion<float,Hermitian> root = sqrt (natural(stokes));
-  Jones<float> polarizer = convert (root);
+  Quaternion<double,Hermitian> root = sqrt (natural(stokes));
+  Jones<double> polarizer = convert (root);
 
   double input_rms = 0.5;
 
@@ -139,7 +162,7 @@ int main (int argc, char** argv)
   double scale0 = 0.0;
   double scale1 = 0.0;
   double rescale = 0.0;
-  float saturation = 0.0;
+  double saturation = 0.0;
 
   if (nbit)
   {
@@ -154,7 +177,8 @@ int main (int argc, char** argv)
 				  0.05445,
 				  0.02957 };
 
-    float the_spacing = spacing[nbit];
+    if (digitizer_spacing == 0.0)
+      digitizer_spacing = spacing[nbit];
 
     saturation = pow(2.0,double(nbit-1)) - 1.0;
     cerr << "ditigiter saturates at " << saturation << endl;
@@ -166,12 +190,12 @@ int main (int argc, char** argv)
     unsigned imax = (saturation+1);
     for (unsigned i=0; i<imax; i++)
     {
-      double output = 0.5 + float (i);
+      double output = 0.5 + double (i);
       double interval = 0.0;
 
       if (i+1 < imax)
       {
-	double threshold = double(i+1) * the_spacing;
+	double threshold = double(i+1) * digitizer_spacing;
 	double cumulative = normal.cumulative_distribution (threshold) - 0.5;
 	interval = cumulative - cumulative_probability;
 	cumulative_probability = cumulative;
@@ -196,15 +220,15 @@ int main (int argc, char** argv)
     // want the rms in each of the voltages to be around unity
     rescale = 0.5/sqrt(variance);
 
-    scale0 = 1.0/(the_spacing*expected_rms0);
+    scale0 = 1.0/(digitizer_spacing*expected_rms0);
     cerr << "x scale=" << scale0 << endl;
 
-    scale1 = 1.0/(the_spacing*expected_rms1);
+    scale1 = 1.0/(digitizer_spacing*expected_rms1);
     cerr << "y scale=" << scale1 << endl;
   }
 
-  float e_x [2];
-  float e_y [2];
+  double e_x [2];
+  double e_y [2];
 
   BoxMuller gasdev (-1);
 
@@ -229,10 +253,10 @@ int main (int argc, char** argv)
       digitize (e_y[1], scale1, saturation, rescale);
     }
 
-    float ex_r = e_x[0];
-    float ex_i = e_x[1];
-    float ey_r = e_y[0];
-    float ey_i = e_y[1];
+    double ex_r = e_x[0];
+    double ex_i = e_x[1];
+    double ey_r = e_y[0];
+    double ey_i = e_y[1];
  
     double var_x = ex_r * ex_r + ex_i * ex_i;
     double var_y = ey_r * ey_r + ey_i * ey_i;
@@ -254,7 +278,10 @@ int main (int argc, char** argv)
 
   cerr << "mean=" << tot << endl;
 
-  cerr << "covar=" << totsq << endl;
+  if (phase)
+    cerr << "phase=" << atan2(tot[3],tot[2]) << endl;
+
+  cerr << "covar=\n" << totsq << endl;
 
   return 0;
 }
