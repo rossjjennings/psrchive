53dae38d0351615a9342ff50650f41be8a511085
straten <straten>
2009-02-20 04:52:03 +0000

    Integration::bscrunch_to_nbin implemented

--- More/General/Archive_bscrunch.C
+++ More/General/Archive_bscrunch.C
@@ -4,25 +4,24 @@
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
-using namespace std;
 
 #include "Pulsar/Archive.h"
 #include "Pulsar/Integration.h"
+
+using namespace std;
 
 /*!
   Useful wrapper for Archive::bscrunch
 */
 void Pulsar::Archive::bscrunch_to_nbin (unsigned new_nbin)
 {
-  if (new_nbin <= 0)
-    throw Error (InvalidParam, "Pulsar::Archive::bscrunch_to_nbin",
-                 "Invalid nbin request");
-  else if (get_nbin() < new_nbin)
-    throw Error (InvalidParam, "Pulsar::Archive::bscrunch_to_nbin",
-                 "Archive has too few bins (%d) to bscrunch to %d bins",
-                 get_nbin(), new_nbin);
-  else
-    bscrunch(get_nbin() / new_nbin);
+  if (get_nsubint() == 0)
+    return;
+
+  for (unsigned isub=0; isub < get_nsubint(); isub++)
+    get_Integration(isub) -> bscrunch_to_nbin (new_nbin);
+
+  set_nbin (get_Integration(0)->get_nbin());
 }
 
 /*!
