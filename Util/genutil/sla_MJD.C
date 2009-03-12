a082153146557835e0b39efe71a655e5238d7875
straten <straten>
2009-03-12 09:17:53 +0000

    verbosity mods

--- Util/genutil/sla_MJD.C
+++ Util/genutil/sla_MJD.C
@@ -9,11 +9,26 @@
 #include <math.h>
 #include <slalib.h>
 
+// #define _DEBUG
+
+#ifdef _DEBUG
+#include <iostream>
+using namespace std;
+#define VERBOSE(x) cerr << x << endl;
+#else
+#define VERBOSE(x)
+#endif
+
 double MJD::LST (double longitude) const
 {
   double passed_MJD = this->in_days();
+
+  VERBOSE("MJD::LST mjd=" << passed_MJD);
   double gmst = slaGmst(passed_MJD);
+  VERBOSE("MJD::LST gmst=" << gmst);
   double lst = gmst/M_PI*180.0/15.0 + longitude/360.0*24.0;
+  VERBOSE("MJD::LST lst=" << lst);
+
   while (lst<0.0) lst+=24.0;
   while (lst>=24.0) lst-=24.0;
   return lst;
