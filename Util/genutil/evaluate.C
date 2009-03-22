a1c473c0023fd82ab068e776e0e7c19dc79b31c1
straten <straten>
2009-03-22 22:48:07 +0000

    vector statistics interface added: min, max, mean, median, sum, rms, ...

--- Util/genutil/evaluate.C
+++ Util/genutil/evaluate.C
@@ -1,6 +1,15 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2007-2009 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "templates.h"
 #include "Error.h"
 #include "pad.h"
 #include "tostring.h"
+#include "stringtok.h"
 
 // evaluateExpression from Parsifal Software
 #include "evaldefs.h"
@@ -8,68 +17,170 @@
 
 #include <string>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-string evaluate (const string& text, char cstart, char cend)
+#ifdef _DEBUG
+#define DEBUG(x) cerr << x << endl;
+#else
+#define DEBUG(x)
+#endif
+
+string evaluate1 (const string& eval, unsigned precision)
+{
+  string result = "__result";
+  string expression = result + " = (" + eval + ")";
+
+  // Call parser function from Parsifal Software
+  int errorFlag = evaluateExpression (const_cast<char*>(expression.c_str()));
+
+  if (errorFlag)
+    throw Error (InvalidParam, "evaluate",
+		 "%s in expression\n\n  %s\n  %s",
+		 errorRecord.message, eval.c_str(), 
+		 pad (errorRecord.column-10, "^", false).c_str());
+    
+  double value = -1;
+  for (int i = 0; i < nVariables; i++)
+    if (variable[i].name == result)
+      value = variable[i].value;
+  
+  if (precision)
+    return tostring (value, precision);
+  else
+    return tostring (value);
+}
+
+string get_command (string& text)
+{
+  const char* commands[] = { "min", "max", "med", "sum", "avg", "rms", 0 };
+
+  for (const char** cmd = commands; *cmd != 0; cmd++)
+  {
+    DEBUG("cmd='" << *cmd << "'");
+
+    string test = *cmd;
+
+    string::size_type in = text.rfind (test);
+    if (in == string::npos)
+      continue;
+
+    // ensure that the command occurs at the end of the text
+    if (in + test.length() == text.length())
+    {
+      text = text.substr (0, in);
+      return test;
+    }
+  }
+
+  return string();
+}
+
+string evaluate2 (const string& command, string vals, unsigned precision)
+{
+  vector<double> values;
+  while (vals.length())
+  {
+    string val = stringtok (vals, ",");
+    values.push_back( fromstring<double>(val) );
+  }
+
+  if (!values.size())
+    return string();
+
+  double result = 0.0;
+
+  if (command == "min")
+    result = *std::min_element (values.begin(), values.end());
+
+  else if (command == "max")
+    result = *std::max_element (values.begin(), values.end());
+
+  else if (command == "med")
+  {
+    unsigned mid = values.size() / 2;
+    std::nth_element (values.begin(), values.begin()+mid, values.end());
+    result = values[mid];
+  }
+
+  else if (command == "sum")
+    result = sum (values);
+
+  else if (command == "avg")
+    result = mean (values);
+
+  else if (command == "rms")
+    result = sqrt( variance(values) );
+
+  if (precision)
+    return tostring (result, precision);
+  else
+    return tostring (result);
+}
+
+string evaluate (const string& text, char cstart, char cend) try
 {
   string remain = text;
-
+    
   string::size_type start;
   string::size_type end;
 
-  int(*pred)(int) = isdigit;
-
   while ( (start = remain.find(cstart)) != string::npos &&
-	  (end = remain.find(cend, start)) != string::npos ) {
+	  (end = remain.find(cend, start)) != string::npos )
+  {
 
     // find the end of the variable name
     string eval = remain.substr (start+1, end-start-1);
-      
+
+    DEBUG("eval='" << eval << "'");
+
     // string preceding the variable substitution
     string before = remain.substr (0, start);
+
+    DEBUG("before='" << before << "'");
+
+    // preceding text may be a command
+    string command = get_command (before);
+
+    DEBUG("command='" << command << "'");
 
     end ++;
 
     unsigned precision = 0;
 
-    if (remain[end] == '%') {
+    if (remain[end] == '%')
+    {
       end ++;
+
+      int(*pred)(int) = isdigit;
+
       string::size_type pend = find_first_not_if (remain, pred, end);
       string::size_type len = (pend != string::npos ) ? pend-end : pend;
+
       precision = fromstring<unsigned> (remain.substr (end, len));
+
       end = pend;
     }
 
     // string following the variable substitution
     string after = (end != string::npos) ? remain.substr (end) : "";
 
-    string result = "result";
-    string expression = result + " = (" + eval + ")";
-
-    // Call parser function from Parsifal Software
-    int errorFlag = evaluateExpression (const_cast<char*>(expression.c_str()));
-
-    if (errorFlag)
-      throw Error (InvalidParam, "Pulsar::Interpreter::test",
-		   "%s in expression\n\n  %s\n  %s",
-		   errorRecord.message, eval.c_str(), 
-		   pad (errorRecord.column-10, "^", false).c_str());
-    
-    double value = -1;
-    for (int i = 0; i < nVariables; i++)
-      if (variable[i].name == result)
-	value = variable[i].value;
+    DEBUG("after='" << after << "'");
 
     string subst;
 
-    if (precision)
-      subst = tostring (value, precision);
+    if (command.empty())
+      subst = evaluate1 (eval, precision);
     else
-      subst = tostring (value);
+      subst = evaluate2 (command, eval, precision);
 
     remain = before + subst + after;
   }
 
   return remain;
+}
+catch (std::exception& error)
+{
+  throw Error (InvalidParam, "evaluate", error.what());
 }
