7819e1b353fef9b2dbd858906e5962443c5eb29e
straten <straten>
2009-05-26 08:00:06 +0000

    use STL mem_fun to adapt member functions

--- Base/Classes/MoreProfiles.C
+++ Base/Classes/MoreProfiles.C
@@ -13,57 +13,19 @@ Pulsar::MoreProfiles::MoreProfiles (const char* name)
 {
 }
 
-//! Turns Class::Method() into AdaptableUnaryFunction(Class*)
-template<typename ClassPtr, typename Method>
-class GeneratorMethod : public std::unary_function<ClassPtr, void>
-{
-public:
-
-  GeneratorMethod (Method m)
-  { method = m; }
-
-  void operator () (ClassPtr ptr) const
-  { (ptr->*(this->method))(); }
-
-protected:
-
-  //! Pointer to Method of Class
-  Method method;
-};
-
-//! Turns Class::Method(Arg) into AdaptableBinaryFunction(Class*,Arg)
-template<typename ClassPtr, typename Method, typename Arg>
-class UnaryMethod : public std::binary_function<ClassPtr, Arg, void>
-{
-public:
-
-  UnaryMethod (Method m)
-  { method = m; }
-
-  void operator () (ClassPtr ptr, const Arg& arg) const
-  { (ptr->*(this->method))(arg); }
-
-protected:
-
-  //! Pointer to Method of Class
-  Method method;
-};
-
 //! For each element of container C, call method M
 template<typename C, typename M>
 void foreach (C& container, M method)
 {
-  for_each (container.begin(), container.end(),
-	    GeneratorMethod<typename C::value_type, M>(method));
+  for_each (container.begin(), container.end(), std::mem_fun(method));
 }
 
 //! For each element of container C, call method M with argument A
 template<typename C, typename M, typename A>
 void foreach (C& container, M method, const A& a)
 {
-  UnaryMethod<typename C::value_type, M, A> binary (method);
-
-  for_each (container.begin(), container.end(), std::bind2nd(binary,a));
+  for_each (container.begin(), container.end(),
+	    std::bind2nd( std::mem_fun(method), a ));
 }
 
 //! Resize the data area
