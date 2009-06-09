67af49280fa348e436597ce745468f1274472506
straten <straten>
2009-06-09 10:44:08 +0000

    1972493: remove_baseline method uses RemoveBaseline::Total algorithm by default

--- More/General/Archive_remove_baseline.C
+++ More/General/Archive_remove_baseline.C
@@ -1,39 +1,24 @@
 /***************************************************************************
  *
- *   Copyright (C) 2007 by Willem van Straten
+ *   Copyright (C) 2007-2009 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
 
-#include "Pulsar/Archive.h"
-#include "Pulsar/Integration.h"
-#include "Pulsar/Profile.h"
-#include "Pulsar/PhaseWeight.h"
+#include "Pulsar/RemoveBaseline.h"
 
-using namespace std;
+using namespace Pulsar;
 
-Pulsar::PhaseWeight* Pulsar::Archive::baseline () const
+Functor< void (Archive*) >
+Archive::remove_baseline_strategy ( new RemoveBaseline::Total,
+				    &RemoveBaseline::Total::transform );
+
+
+void Pulsar::Archive::remove_baseline () try
 {
-  Reference::To<const Archive> total = this->total();
-  return total->get_Profile(0,0,0)->baseline ();
+  remove_baseline_strategy (this); 
 }
-
-/*!
-  If phase is not specified, this method calls
-  Archive::find_min_phase to find the phase at which the mean in a
-  region of the total intensity (as returned by Archive::total)
-  reaches a minimum.  This phase is then used to remove the baseline from
-  each of the Integrations.
-  */
-
-void Pulsar::Archive::remove_baseline () try {
-
-  Reference::To<PhaseWeight> baseline = this->baseline();
-  
-  for (unsigned isub=0; isub < get_nsubint(); isub++)
-    get_Integration(isub) -> remove_baseline (baseline);
-  
-}
-catch (Error& error) {
+catch (Error& error)
+{
   throw error += "Pulsar::Archive::remove_baseline";
 }
