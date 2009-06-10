0f4587aeeedab916b721c6ab02cacd8af3666afd
straten <straten>
2009-06-10 10:53:45 +0000

    Phase made a template

--- More/MEAL/MEAL/Phase.h
+++ More/MEAL/MEAL/Phase.h
@@ -1,30 +1,34 @@
 //-*-C++-*-
 /***************************************************************************
  *
- *   Copyright (C) 2004 by Willem van Straten
+ *   Copyright (C) 2004-2009 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/More/MEAL/MEAL/Phase.h,v $
-   $Revision: 1.7 $
-   $Date: 2006/10/06 21:13:54 $
+   $Revision: 1.8 $
+   $Date: 2009/06/10 10:53:45 $
    $Author: straten $ */
 
 #ifndef __MEAL_Phase_H
 #define __MEAL_Phase_H
 
-#include "MEAL/Complex2.h"
+#include "MEAL/OneParameter.h"
+#include <complex>
 
-namespace MEAL {
-
-  //! A gain transformation
-  class Phase : public Complex2 {
+namespace MEAL
+{
+  //! A complex exponential parameterized by phase
+  template<typename Parent>
+  class Phase : public Parent 
+  {
 
   public:
 
     //! Default constructor
-    Phase ();
+    Phase () : parameter (this)
+    { parameter.set_name ("phase"); }
 
     // ///////////////////////////////////////////////////////////////////
     //
@@ -33,15 +37,43 @@ namespace MEAL {
     // ///////////////////////////////////////////////////////////////////
 
     //! Return the name of the class
-    std::string get_name () const;
+    std::string get_name () const
+    { return "Phase<" + std::string(Parent::Name) + ">"; }
 
   protected:
 
-    //! Calculate the Jones matrix and its gradient
-    void calculate (Jones<double>& result, std::vector<Jones<double> >*);
+    typedef typename Parent::Result Return;
+
+    //! Calculate the result and its gradient
+    void calculate (Return& result, std::vector<Return>* gradient);
    
+    OneParameter parameter;
   };
 
+}
+
+//! Calculate the Jones matrix and its gradient, as parameterized by gain
+template<typename Parent>
+void MEAL::Phase<Parent>::calculate (Return& result, std::vector<Return>* grad)
+{
+  double phase = this->get_param(0);
+
+  if (MEAL::Function::verbose)
+    std::cerr << "MEAL::Phase<Parent>::calculate phase=" << phase << std::endl;
+
+  double cos_phase = cos(phase);
+  double sin_phase = sin(phase);
+
+  result = std::complex<double>(cos_phase, sin_phase);
+
+  if (grad)
+  {
+    (*grad)[0] = std::complex<double>(-sin_phase, cos_phase);
+    
+    if (MEAL::Function::verbose)
+      std::cerr << "MEAL::Phase<Parent>::calculate gradient" << std::endl
+	   << "   " << (*grad)[0] << std::endl;
+  } 
 }
 
 #endif
