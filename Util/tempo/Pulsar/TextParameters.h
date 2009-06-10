4d0d70274d8df99107662467a2ee5fc86504e4bf
straten <straten>
2009-06-10 07:47:58 +0000

    get<T> (keyword) template method added

--- Util/tempo/Pulsar/TextParameters.h
+++ Util/tempo/Pulsar/TextParameters.h
@@ -7,8 +7,8 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/psrchive/psrchive/Util/tempo/Pulsar/TextParameters.h,v $
-   $Revision: 1.3 $
-   $Date: 2009/03/15 14:32:43 $
+   $Revision: 1.4 $
+   $Date: 2009/06/10 07:47:58 $
    $Author: straten $ */
 
 #ifndef __TextParameters_h
@@ -49,6 +49,11 @@ namespace Pulsar {
     template<typename T>
     bool get_value (T& value, const std::string& keyword, 
 		    bool exception=true) const;
+
+    //! Return a value from the text, throw exeception if not found
+    template<typename T>
+    T get (const std::string& keyword) const
+    { T value; get_value (value,keyword); return value; }
 
   protected:
 
