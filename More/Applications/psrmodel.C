f56f01fa3e5bd115fdf1db77f6afff2accb5d8c7
straten <straten>
2009-06-12 09:10:05 +0000

    allow global search grid size specification and plot result

--- More/Applications/psrmodel.C
+++ More/Applications/psrmodel.C
@@ -7,11 +7,12 @@
 
 #include "Pulsar/Application.h"
 #include "Pulsar/StandardOptions.h"
-//#include "Pulsar/PlotOptions.h"
+#include "Pulsar/PlotOptions.h"
 
 #include "Pulsar/Archive.h"
 #include "Pulsar/Integration.h"
 #include "Pulsar/PolnProfile.h"
+#include "Pulsar/StokesCylindrical.h"
 
 #include "Pulsar/ComplexRVMFit.h"
 #include "MEAL/ComplexRVM.h"
@@ -47,8 +48,12 @@ public:
 protected:
 
   Reference::To<ComplexRVMFit> rvm;
+  unsigned global_search;
+
   bool rvm_fit;
-  bool global_search;
+  bool plot_result;
+
+  Pulsar::PlotOptions plot;
 };
 
 int main (int argc, char** argv)
@@ -57,28 +62,33 @@ int main (int argc, char** argv)
   return program.main (argc, argv);
 }
 
-psrmodel::psrmodel () : Pulsar::Application ("psrmodel",
-					     "pulsar modeling program")
+psrmodel::psrmodel () :
+  Pulsar::Application ("psrmodel", "pulsar modeling program"),
+  plot (false)
 {
   has_manual = false;
-  version = "$Id: psrmodel.C,v 1.4 2009/06/11 09:55:58 straten Exp $";
+  version = "$Id: psrmodel.C,v 1.5 2009/06/12 09:10:05 straten Exp $";
 
   add( new Pulsar::StandardOptions );
+  add( &plot );
 
   rvm = new ComplexRVMFit;
+  global_search = 0;
+
   rvm_fit = false;
-  global_search = false;
+  plot_result = false;
 }
 
 std::string psrmodel::get_options ()
 {
-  return "AZBPga:z:b:p:t:";
+  return "AZBPds:a:z:b:p:t:x";
 }
 
 std::string psrmodel::get_usage ()
 {
   return
-    " -g               do a global minimum search \n"
+    " -d               plot the resulting model over the data \n"
+    " -s nstep         do a global minimum search on nstep^2 grid \n"
     " -t sigma         cutoff threshold when selecting bins [default 3] \n"
     " -a degrees       alpha: colatitude of magnetic axis \n"
     " -z degrees       zeta: colatitude of line of sight \n"
@@ -100,8 +110,13 @@ bool psrmodel::parse (char code, const std::string& arg)
   switch (code)
   {
 
-  case 'g':
-    global_search = true;
+  case 'd':
+    plot.set_open_device (true);
+    plot_result = true;
+    break;
+
+  case 's':
+    global_search = fromstring<unsigned>(arg);
     break;
 
   case 't':
@@ -136,6 +151,10 @@ bool psrmodel::parse (char code, const std::string& arg)
     RVM->reference_position_angle->set_infit (0, false);
     break;
 
+  case 'x':
+    rvm->set_chisq_map (true);
+    break;
+
   default:
     return false;
   }
@@ -160,14 +179,23 @@ void psrmodel::process (Pulsar::Archive* data)
   data->fscrunch();
   data->convert_state(Signal::Stokes);
   data->remove_baseline();
+  data->centre();
 
   Reference::To<PolnProfile> p = data->get_Integration(0)->new_PolnProfile(0);
   rvm->set_observation (p);
 
   if (global_search)
-    rvm->global_search ();
+  {
+    cerr << "psrmodel: performing global search over " << global_search
+	 << " square grid" << endl;
+
+    rvm->global_search (global_search);
+  }
   else
+  {
+    cerr << "psrmodel: solving with initial guess: " << endl;
     rvm->solve();
+  }
 
   cerr << "chisq=" << rvm->get_chisq() << "/nfree=" << rvm->get_nfree()
        << " = " << rvm->get_chisq()/ rvm->get_nfree() << endl;
@@ -177,13 +205,17 @@ void psrmodel::process (Pulsar::Archive* data)
   double d = 180/M_PI;
   double t = 0.5/M_PI;
 
-  // negative because sign of PA in RVM is opposite to IAU convention
-  Estimate<double> PA = - RVM->reference_position_angle->get_value();
+  Estimate<double> PA = RVM->reference_position_angle->get_value();
+
+#if 0
   PA.val = fmod (PA.val, M_PI);
   if (PA.val > M_PI/2)
-    PA.val -= M_PI/2;
+    PA.val -= M_PI;
   if (PA.val < -M_PI/2)
-    PA.val += M_PI/2;
+    PA.val += M_PI;
+
+  // RVM->line_of_sight->set_value( RVM->line_of_sight->get_value() - 2*MPI );
+#endif
 
   cerr <<
     "PA_0="  << d*PA << " deg\n"
@@ -192,4 +224,16 @@ void psrmodel::process (Pulsar::Archive* data)
     "phi_0=" << t*RVM->magnetic_meridian->get_value() << " turns"
 	     << endl;
 
+  if (plot_result)
+  {
+    StokesCylindrical plotter;
+
+    AnglePlot* pa = plotter.get_orientation();
+
+    pa->set_threshold( rvm->get_threshold() );
+    pa->model.set (RVM, &MEAL::RotatingVectorModel::compute);
+    pa->get_frame()->get_y_scale()->set_range_norm (0, 1.5);
+
+    plotter.plot( data );
+  }
 }
