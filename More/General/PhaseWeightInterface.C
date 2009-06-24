89cdca46eff1ef5245a3603829299b9eaf8e8b2a
straten <straten>
2009-06-24 05:01:47 +0000

    case consistency

--- More/General/PhaseWeightInterface.C
+++ More/General/PhaseWeightInterface.C
@@ -13,28 +13,28 @@ Pulsar::PhaseWeight::Interface::Interface (PhaseWeight* instance)
     set_instance (instance);
 
   add( &PhaseWeight::get_weight_sum,
-       "count", "count of selected phase bins" );
+       "count", "Count of selected phase bins" );
 
   add( &PhaseWeight::get_weighted_sum,
-       "sum", "sum of selected phase bins" );
+       "sum", "Sum of selected phase bins" );
 
   add( &PhaseWeight::get_min,
-       "min", "minimum of selected phase bins" );
+       "min", "Minimum of selected phase bins" );
 
   add( &PhaseWeight::get_max,
-       "max", "maximum of selected phase bins" );
+       "max", "Maximum of selected phase bins" );
 
   add( &PhaseWeight::get_avg,
-       "avg", "average of selected phase bins" );
+       "avg", "Average of selected phase bins" );
 
   add( &PhaseWeight::get_rms,
-       "rms", "standard deviation" );
+       "rms", "Standard deviation of selected phase bins" );
 
   add( &PhaseWeight::get_median,
-       "med", "median of selected phase bins" );
+       "med", "Median of selected phase bins" );
 
   add( &PhaseWeight::get_median_difference,
-       "mdm", "median difference from the median" );
+       "mdm", "Median difference from the median" );
 }
 
 
