76314a31c53d92261b63a9457ecdeab3585d2396
straten <straten>
2009-12-21 04:47:09 +0000

    test if any tempo2 keywords are in Parameters

--- Util/resources/Generator_factory.C
+++ Util/resources/Generator_factory.C
@@ -16,12 +16,49 @@
 #endif
 #include "Predict.h"
 
+#include "templates.h"
+#include "FilePtr.h"
+
 using namespace std;
+
+template<class T>
+vector<string> parsewords (T* data)
+{
+  FilePtr temp = tmpfile();
+  if (!temp)
+    throw Error (FailedSys, "words", "tmpfile");
+
+  vector<string> result;
+
+  vector<char> buffer (FILENAME_MAX);
+  char* line = &(buffer[0]);
+
+  char* whitespace = " \t\n";
+
+  while (fgets (line, FILENAME_MAX, temp) != NULL)
+  {
+    char* eol = strchr (line, '#');
+    if (eol)
+      *eol = '\0';
+
+    char* word = strtok (line, whitespace);
+    while (word)
+    {
+      result.push_back (word);
+      word = strtok (NULL, whitespace);
+    }
+  }
+
+  return result;
+}
 
 Pulsar::Generator* Pulsar::Generator::factory (const Pulsar::Parameters* param)
 {
 #ifdef HAVE_TEMPO2
-  return new Tempo2::Generator;
+  vector<string> words = parsewords (param);
+  for (unsigned i=0; i < words.size(); i++)
+    if (found (words[i], Tempo2::Generator::get_keywords()))
+      return new Tempo2::Generator;
 #endif
   return new Tempo::Predict;
 }
