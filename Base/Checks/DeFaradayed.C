b9c0216eb9f84806eb18164555da09277b9ed270
straten <straten>
2009-12-22 14:21:30 +0000

    error message corrected: dedispersed -> faraday_corrected

--- Base/Checks/DeFaradayed.C
+++ Base/Checks/DeFaradayed.C
@@ -22,31 +22,29 @@ void Pulsar::DeFaradayed::apply (const Archive* archive)
 
   unsigned nsubint = archive->get_nsubint();
 
-  for (unsigned isubint=0; isubint < nsubint; isubint++) {
-
+  for (unsigned isubint=0; isubint < nsubint; isubint++)
+  {
     const Integration* subint = archive->get_Integration (isubint);
-
     const DeFaraday* ext = subint->get<DeFaraday>();
 
-    if (!archive->get_faraday_corrected()) {
-
+    if (!archive->get_faraday_corrected())
+    {
       if (ext)
 	throw Error (InvalidState, "Pulsar::DeFaradayed::apply",
-		     "Archive::dedispsersed is not set and Integration[%d]\n\t"
+		     "Archive::faraday_corrected is not set and Integration[%d]\n\t"
 		     "has a DeFaraday Extension", isubint);
-
     }
-    else {
-
+    else
+    {
       if (!ext)
 	throw Error (InvalidState, "Pulsar::DeFaradayed::apply",
-		     "Archive::dedispsersed is set and Integration[%d]\n\t"
+		     "Archive::faraday_corrected is set and Integration[%d]\n\t"
 		     "has no DeFaraday Extension", isubint);
     
       if (diff( ext->get_reference_frequency(),
 		archive->get_centre_frequency() ))
 	throw Error (InvalidState, "Pulsar::DeFaradayed::apply",
-		     "Archive::dedispsersed is set and Integration[%d]\n\t"
+		     "Archive::faraday_corrected is set and Integration[%d]\n\t"
 		     "DeFaraday::reference_frequency = %lf does not equal\n\t"
 		     "Archive::centre_frequency = %lf  (diff = %lf)", isubint,
 		     ext->get_reference_frequency(),
@@ -55,13 +53,12 @@ void Pulsar::DeFaradayed::apply (const Archive* archive)
       if (diff( ext->get_rotation_measure(),
 		archive->get_rotation_measure() ))
 	throw Error (InvalidState, "Pulsar::DeFaradayed::apply",
-		     "Archive::dedispsersed is set and Integration[%d]\n\t"
+		     "Archive::faraday_corrected is set and Integration[%d]\n\t"
 		     "DeFaraday::rotation_measure = %lf does not equal\n\t"
 		     "Archive::rotation_measure = %lf", isubint,
 		     ext->get_rotation_measure(),
 		     archive->get_rotation_measure());
     }
-    
   }
-  
 }
+
