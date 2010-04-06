1a30699b1179e84d74118b427562125446f13140
straten <straten>
2010-04-06 00:14:52 +0000

    table restructured with one row per input channel

--- Base/Formats/PSRFITS/unload_CoherentDedispersion.C
+++ Base/Formats/PSRFITS/unload_CoherentDedispersion.C
@@ -12,14 +12,14 @@
 using namespace std;
 
 template<typename T, class C, class Method>
-void pack (std::vector<T>& result, const C* ext, unsigned n, Method get)
+void pack (std::vector<T>& result, const C& ext, unsigned n, Method get)
 {
   result.resize(n);
   bool all_equal = true;
 
   for (unsigned i=0; i<n; i++)
   {
-    result[i] = (ext->*get)(i);
+    result[i] = (ext.get_output(i).*get)();
     if (result[i] != result[0])
       all_equal = false;
   }
@@ -31,19 +31,22 @@ void pack (std::vector<T>& result, const C* ext, unsigned n, Method get)
 
 template<typename T, class Method>
 void output (fitsfile* fptr, const char* colname,
-	     const Pulsar::CoherentDedispersion* ext,
-	     unsigned nchan, Method get)
+	     const Pulsar::CoherentDedispersion::InputChannel& ext,
+	     unsigned ichan, Method get)
 {
   vector<T> data;
-  pack (data, ext, nchan, get);
+  pack (data, ext, ext.get_nchan_output(), get);
   vector<unsigned> dimensions (1, data.size());
 
-  psrfits_write_col (fptr, colname, 1, data, dimensions);
+  psrfits_write_col (fptr, colname, ichan+1, data, dimensions);
 }
 
 void Pulsar::FITSArchive::unload (fitsfile* fptr,
 				  const CoherentDedispersion* ext)
 {
+  if (verbose)
+    cerr << "Pulsar::FITSArchive::unload CoherentDedispersion" << endl;
+
   string domain = tostring (ext->get_domain());
   psrfits_update_key (fptr, "DOMAIN", domain);
   psrfits_update_key (fptr, "CHRPTYPE", ext->get_description());
@@ -51,26 +54,36 @@ void Pulsar::FITSArchive::unload (fitsfile* fptr,
   psrfits_update_key (fptr, "DM", ext->get_dispersion_measure());
   psrfits_update_key (fptr, "DOPPLER", ext->get_doppler_correction());
 
-  psrfits_update_key (fptr, "NCHAN_I", ext->get_nchan_input());
-  psrfits_update_key (fptr, "NCHAN_O", ext->get_nchan_output());
+  psrfits_update_key (fptr, "DATANBIT", ext->get_nbit_data());
+  psrfits_update_key (fptr, "CHRPNBIT", ext->get_nbit_chirp());
 
-  psrfits_update_key (fptr, "DATANLEV", ext->get_nlev_data());
-  psrfits_update_key (fptr, "CHRPNLEV", ext->get_nlev_chirp());
+  unsigned nchan = ext->get_nchan_input();
+  psrfits_update_key (fptr, "NCHAN", nchan);
 
-  output<double> (fptr, "IN_FREQ", ext, ext->get_nchan_input(),
-		  &CoherentDedispersion::get_centre_frequency_input);
-  output<double> (fptr, "IN_BW", ext, ext->get_nchan_input(),
-		  &CoherentDedispersion::get_bandwidth_input);
+  psrfits_set_rows (fptr, nchan);
 
-  output<double> (fptr, "OUT_FREQ", ext, ext->get_nchan_output(),
-		  &CoherentDedispersion::get_centre_frequency_output);
-  output<double> (fptr, "OUT_BW", ext, ext->get_nchan_output(),
-		  &CoherentDedispersion::get_bandwidth_output);
+  typedef Pulsar::CoherentDedispersion::InputChannel InputChannel;
+  typedef Pulsar::CoherentDedispersion::OutputChannel OutputChannel;
 
-  output<unsigned> (fptr, "NCHIRP", ext, ext->get_nchan_output(),
-		    &CoherentDedispersion::get_nsamp);
-  output<unsigned> (fptr, "NCYC_POS", ext, ext->get_nchan_output(),
-		    &CoherentDedispersion::get_nsamp_overlap_pos);
-  output<unsigned> (fptr, "NCYC_NEG", ext, ext->get_nchan_output(),
-		    &CoherentDedispersion::get_nsamp_overlap_neg);
+  for (unsigned ichan=0; ichan<nchan; ichan++)
+  {
+    const InputChannel& input = ext->get_input(ichan);
+
+    psrfits_write_col (fptr, "FREQ", ichan+1, input.get_centre_frequency());
+    psrfits_write_col (fptr, "BW", ichan+1, input.get_bandwidth());
+    psrfits_write_col (fptr, "OUT_NCHAN", ichan+1, input.get_nchan_output());
+
+    output<double> (fptr, "OUT_FREQ", input, ichan,
+		    &OutputChannel::get_centre_frequency);
+    output<double> (fptr, "OUT_BW", input, ichan,
+		    &OutputChannel::get_bandwidth);
+    
+    output<unsigned> (fptr, "NCHIRP", input, ichan,
+		      &OutputChannel::get_nsamp);
+    output<unsigned> (fptr, "NCYC_POS", input, ichan,
+		      &OutputChannel::get_nsamp_overlap_pos);
+    output<unsigned> (fptr, "NCYC_NEG", input, ichan,
+		      &OutputChannel::get_nsamp_overlap_neg);
+  }
 }
+
