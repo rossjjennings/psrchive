d9c7d1337c839a1c42e0a973aa6de4f85cda4876
jonathan_khoo <jonathan_khoo>
2010-07-05 05:43:02 +0000

    Halved the size of the channel-weights plot.

--- More/Plotting/BandpassChannelWeightPlot.C
+++ More/Plotting/BandpassChannelWeightPlot.C
@@ -14,17 +14,17 @@ using namespace std;
 
 BandpassChannelWeightPlot::BandpassChannelWeightPlot()
 {
-  manage( "psd", &psd);
-  manage( "weights", &weights );
+  manage("psd", &psd);
+  manage("weights", &weights);
   
-  psd.get_frame()->set_viewport( 0, 1, .3, 1 );
-  weights.get_frame()->set_viewport( 0, 1, 0, .3 );
+  psd.get_frame()->set_viewport(0, 1, 0.15, 1);
+  weights.get_frame()->set_viewport(0, 1, 0, 0.15);
 
   psd.get_frame()->get_x_axis()->set_label("");
   psd.get_frame()->get_x_axis()->set_opt("BCTS");
   
-  weights.get_frame()->get_label_above()->set_centre( "" );
-  weights.get_frame()->get_label_below()->set_left( "" );
+  weights.get_frame()->get_label_above()->set_centre("");
+  weights.get_frame()->get_label_below()->set_left("");
 }
 
 
