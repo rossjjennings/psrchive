2ced15a6b3b1a167a7503990f61fc5f491af1d2d
jonathan_khoo <jonathan_khoo>
2010-09-09 11:09:50 +0000

    reverted to 1.1 - not a good location for the method

--- More/General/Archive_weighted_frequency.C
+++ More/General/Archive_weighted_frequency.C
@@ -30,23 +30,3 @@ double Pulsar::Archive::weighted_frequency (unsigned ichan,
   return weighted (start, end);
 }
 
-/////////////////////////////////////////////////////////////////////////////
-//
-// Pulsar::Archive::get_weighted_frequency
-//
-/*!
-  \return the weighted centre frequency (in MHz, rounded to nearest kHz)
-          of the whole observation (every frequency channel and Integration).
-*/
-double Pulsar::Archive::get_weighted_frequency() const
-{
-  const unsigned nsub  = get_nsubint();
-  const unsigned nchan = get_nchan();
-  double weighted_frequency_sum = 0.0;
-
-  for (unsigned ichan = 0; ichan < nchan; ++ichan) {
-    weighted_frequency_sum += weighted_frequency(ichan,0,nsub);
-  }
-
-  return weighted_frequency_sum / static_cast<double>(nchan);
-}
