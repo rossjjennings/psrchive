35f2fd34b1c27bbcc9dce0c30823a5ee1b1602ed
Willem van Straten <willem@swin.edu.au>
2011-03-26 08:29:33 +1100

    Scalar functions that depend on an index

---
+++ More/MEAL/MEAL/ScalarVector.h
@@ -0,0 +1,49 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2011 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/psrchive/psrchive/More/MEAL/MEAL/ScalarVector.h,v $
+   $Revision: 1.3 $
+   $Date: 2006/10/06 21:13:54 $
+   $Author: straten $ */
+
+#ifndef __ScalarVector_H
+#define __ScalarVector_H
+
+#include "MEAL/Scalar.h"
+
+namespace MEAL {
+
+  //! Scalar functions that depend on an index
+  /*! This base class represents Scalar functions for which the evaluate
+    method depends on an unsigned integer index. */
+  class ScalarVector : public Scalar
+  {
+  public:
+
+    //! Default contructor
+    ScalarVector ();
+
+    //! Get the size of the position angle array
+    virtual unsigned size () const = 0;
+
+    //! Set the index of the position angle array
+    void set_index (unsigned);
+
+    //! Get the index of the position angle array
+    unsigned get_index () const;
+
+  protected:
+
+    //! The current index
+    unsigned index;
+
+  };
+
+}
+
+#endif
