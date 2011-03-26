08b5b3a3f0cc41180f5d74f9b1740c8b53cc43b8
Willem van Straten <willem@swin.edu.au>
2011-03-27 02:11:26 +1100

    also tests the Union class

--- More/MEAL/test_Vectorize.C
+++ More/MEAL/test_Vectorize.C
@@ -6,7 +6,35 @@
  ***************************************************************************/
 
 #include "MEAL/Vectorize.h"
+#include "MEAL/Union.h"
+
 #include "MEAL/Boost.h"
+#include "MEAL/Depolarizer.h"
+
+//! Maps the structure of Vector to other template methods
+template<unsigned N, typename T> struct DatumTraits< Vector<N,T> >
+{
+  typedef T element_type;
+
+  static inline unsigned ndim () { return N; }
+  static inline T& element (Vector<N,T>& t, unsigned i) 
+  { return t[i]; }
+  static inline const T& element (const Vector<N,T>& t, unsigned i)
+  { return t[i]; }
+};
+
+//! Maps the structure of Matrix to other template methods
+template<unsigned N, unsigned M, typename T> 
+struct DatumTraits< Matrix<N,M,T> >
+{
+  typedef T element_type;
+
+  static inline unsigned ndim () { return N*M; }
+  static inline T& element (Matrix<N,M,T>& t, unsigned i) 
+  { return t[i/M][i%M]; }
+  static inline const T element (const Matrix<N,M,T>& t, unsigned i)
+  { return t[i/M][i%M]; }
+};
 
 #include <iostream>
 using namespace std;
@@ -14,7 +42,6 @@ using namespace std;
 int main (int argc, char** argv) try
 {
   MEAL::Boost boost;
-
   MEAL::Vectorize<MEAL::Complex2> vboost (&boost);
 
   for (unsigned i=0; i<vboost.size(); i++)
@@ -22,6 +49,27 @@ int main (int argc, char** argv) try
     vboost.set_index(i);
     cout << "test_Vectorize: index=" << i 
 	 << " val=" << vboost.evaluate () << endl;
+  }
+
+  MEAL::Depolarizer depol;
+  MEAL::Vectorize<MEAL::Real4> vdepol (&depol);
+
+  for (unsigned i=0; i<vdepol.size(); i++)
+  {
+    vdepol.set_index(i);
+    cout << "test_Vectorize: index=" << i 
+	 << " val=" << vdepol.evaluate () << endl;
+  }
+
+  MEAL::Union space;
+  space.push_back( &vboost );
+  space.push_back( &vdepol );
+
+  for (unsigned i=0; i<space.size(); i++)
+  {
+    space.set_index(i);
+    cout << "test_Vectorize: index=" << i 
+	 << " val=" << space.evaluate () << endl;
   }
 
   return 0;
