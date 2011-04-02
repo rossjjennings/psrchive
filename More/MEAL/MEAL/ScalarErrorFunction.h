3fedb1432bb5d165d79bf52fd3aba4094ac723ed
Willem van Straten <willem@swin.edu.au>
2011-04-03 08:06:28 +1000

    implements the error function, erf(x)

---
+++ More/MEAL/MEAL/ScalarErrorFunction.h
@@ -0,0 +1,50 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2011 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+// psrchive/More/MEAL/MEAL/ScalarErrorFunction.h
+
+#ifndef __MEAL_ScalarErrorFunction_H
+#define __MEAL_ScalarErrorFunction_H
+
+#include "MEAL/UnaryScalar.h"
+
+namespace MEAL {
+
+  //! The error function \f$ \erf(x) \f$ of a Scalar function
+  class ScalarErrorFunction : public UnaryScalar
+  {
+
+  public:
+
+    // ///////////////////////////////////////////////////////////////////
+    //
+    // UnaryScalar implementation
+    //
+    // ///////////////////////////////////////////////////////////////////
+
+    //! The function
+    double function (double x) const;
+
+    //! And its first derivative
+    double dfdx (double x) const;
+
+    // ///////////////////////////////////////////////////////////////////
+    //
+    // Function implementation
+    //
+    // ///////////////////////////////////////////////////////////////////
+
+    //! Return the name of the class
+    std::string get_name () const;
+
+  };
+
+}
+
+
+#endif
