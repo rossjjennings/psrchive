7f167e1dadf5ca0a5fbddf8d772a5d64dfa2e8ee
Andrew Jameson <andrew.jameson@gmail.com>
2011-07-20 12:04:26 +1000

    interface to set_nsblk added

--- Base/Extensions/FITSSUBHdrExtensionTI.C
+++ Base/Extensions/FITSSUBHdrExtensionTI.C
@@ -25,7 +25,7 @@ FITSSUBHdrExtension::Interface::Interface ( FITSSUBHdrExtension *s_instance )
   add( &FITSSUBHdrExtension::get_tsamp, "tsamp", "[s] Sample interval for SEARCH-mode data" );
   add( &FITSSUBHdrExtension::get_nbits, "nbits", "Nr of bits/datum (SEARCH mode 'X' data, else 1)" );
   add( &FITSSUBHdrExtension::get_nch_strt, "nch_strt", "Start channel/sub-band number (0 to NCHAN-1)" );
-  add( &FITSSUBHdrExtension::get_nsblk, "nsblk", "Samples/row (SEARCH mode, else 1)" );
+  add( &FITSSUBHdrExtension::get_nsblk, &FITSSUBHdrExtension::set_nsblk, "nsblk", "Samples/row (SEARCH mode, else 1)" );
   add( &FITSSUBHdrExtension::get_nrows, "nrows", "Nr of rows in subint table (search mode)" );
   add( &FITSSUBHdrExtension::get_zero_off, "zero_off", "Zero offset for SEARCH-mode data" );
   add( &FITSSUBHdrExtension::get_signint, "signint", "1 for signed ints in SEARCH-mode data, else 0" );
