d141f40f9a7dfb5d64d29bf369252de8b51b789f
Willem van Straten <willem@swin.edu.au>
2011-08-18 16:44:13 +1000

    compiler warnings cleared

--- Base/Formats/PSRFITS/delete_hdu.C
+++ Base/Formats/PSRFITS/delete_hdu.C
@@ -13,20 +13,20 @@ using namespace std;
   \param ffptr reference to the PSRFITS file to be modified
   \param hdu_name name of the HDU to remove from ffptr
 */
-void Pulsar::FITSArchive::delete_hdu (fitsfile* ffptr, char* hdu_name) const
+void Pulsar::FITSArchive::delete_hdu (fitsfile* ffptr, const char* name) const
 {
   int status = 0;
 
-  fits_movnam_hdu (ffptr, BINARY_TBL, hdu_name, 0, &status);
+  fits_movnam_hdu (ffptr, BINARY_TBL, const_cast<char*>(name), 0, &status);
   if (status != 0)
     throw FITSError (status, "FITSArchive::delete_hdu", 
-		     "fits_movnam_hdu %s", hdu_name);
+		     "fits_movnam_hdu %s", name);
 
   fits_delete_hdu (ffptr, 0, &status);
   if (status != 0)
     throw FITSError (status, "FITSArchive::delete_hdu", 
-		     "fits_delete_hdu %s", hdu_name);
+		     "fits_delete_hdu %s", name);
 
   if (verbose == 3)
-    cerr << "FITSArchive::delete_hdu " << hdu_name << " HDU deleted" << endl;
+    cerr << "FITSArchive::delete_hdu " << name << " HDU deleted" << endl;
 }
