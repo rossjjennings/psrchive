d332368afc2db736a0894f337f65744a1d5748ef
Willem van Straten <willem@swin.edu.au>
2011-12-21 20:27:58 +1100

    remove baseline from PolnProfile

--- More/Polarimetry/PolnStatistics.C
+++ More/Polarimetry/PolnStatistics.C
@@ -11,6 +11,8 @@
 
 #include "Pulsar/Integration.h"
 
+using namespace std;
+
 Pulsar::PolnStatistics::PolnStatistics () {}
 Pulsar::PolnStatistics::~PolnStatistics () {}
 
@@ -32,7 +34,19 @@ void Pulsar::PolnStatistics::setup (Statistics* st)
     stats -> set_stats (st->get_stats());
   }
 
-  profile = Pulsar::get_Stokes (this->get_Integration(st), st->get_chan());
+  profile = Pulsar::get_Stokes (get_Integration(st), st->get_chan());
+
+  PhaseWeight* baseline = stats->get_stats()->get_baseline();
+
+  Reference::To<PolnProfile> p = profile->clone();
+  for (unsigned i=0; i<4; i++)
+  {
+    Profile* s = p->get_Profile(i);
+    baseline->set_Profile(s);
+    s->offset( -baseline->get_mean().get_value() );
+  }
+  profile = p;
 
   stats->set_profile( profile );
 }
+
