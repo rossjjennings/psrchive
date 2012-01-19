a7c1744735c637e997863a552c27a02aa4bb53af
Willem van Straten <willem@swin.edu.au>
2012-01-20 08:42:11 +1100

    inherits TextEditor<Archive>

--- Base/Classes/Pulsar/Editor.h
+++ Base/Classes/Pulsar/Editor.h
@@ -9,50 +9,23 @@
 #ifndef __Pulsar_Editor_h
 #define __Pulsar_Editor_h
 
-#include "Reference.h"
-#include <string>
-#include <vector>
+#include "TextEditor.h"
 
 namespace Pulsar {
 
   class Archive;
 
   //! Edit the metadata in a pulsar archive
-  class Editor : public Reference::Able
+  class Editor : public TextEditor<Archive>
   {
 
   public:
 
-    Editor ();
-    
-    //! Add a semi-colon or comma-separated list of commands to execute
-    void add_commands (const std::string&);
+    //! Add the named extension to the given archive
+    void add_extension (Archive*, const std::string& name);
 
-    //! Add a semi-colon or comma-separated list of extensions to install
-    void add_extensions (const std::string&);
-
-    //! Return true if the process method will modify the archive
-    bool will_modify () const;
-
-    //! Edit the Archive according to the current state
-    std::string process (Archive*);
-
-    //! Prefix parameter value queries with parameter name=
-    bool prefix_name;
-
-    //! Print the name of each file processed
-    bool output_filename;
-
-    //! Optionally set the delimiter used to separate elements of a container
-    std::string delimiter;
-
-  protected:
-
-    //! commands to be executed
-    std::vector<std::string> commands;
-
-    //! extensions to be added
-    std::vector<std::string> extensions;
+    //! Return the archive filename
+    std::string get_identifier (const Archive*);
 
   };
 
