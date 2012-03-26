725ac699fec21bad66d023591af42b140333c792
Willem van Straten <willem@swin.edu.au>
2012-03-26 12:39:03 +1100

    3009684: provide access to reduced chisq and control over MCMC option

--- More/Timing/FourierDomainFit.C
+++ More/Timing/FourierDomainFit.C
@@ -10,6 +10,12 @@
 
 using namespace std;
 
+Pulsar::FourierDomainFit::FourierDomainFit ()
+{
+  use_mcmc = true;
+  reduced_chisq = 0;
+}
+
 Estimate<double> Pulsar::FourierDomainFit::get_shift () const
 {
   // TODO: figure out a way to reliably cache a standard profile
@@ -21,13 +27,54 @@ Estimate<double> Pulsar::FourierDomainFit::get_shift () const
   Reference::To<Profile> obs_p = &prfcopy;
 
   ProfileShiftFit fit;
+
   fit.set_standard(std_p);
   fit.set_nharm(std_p->get_nbin()/4);  // Use half the harmonics
-  fit.set_error_method(ProfileShiftFit::MCMC_Variance); // How to make option?
+
+  if (use_mcmc)
+    fit.set_error_method(ProfileShiftFit::MCMC_Variance);
+  else
+    fit.set_error_method(ProfileShiftFit::Traditional_Chi2);
+
   fit.set_Profile(obs_p);
 
   fit.compute();
+
+  reduced_chisq = fit.get_reduced_chisq();
   return fit.get_shift();
 }
 
+//! Return the statistical goodness of fit
+double Pulsar::FourierDomainFit::get_reduced_chisq () const
+{
+  return reduced_chisq;
+}
 
+//
+// The following Interface defines the pat command-line options
+//
+
+using Pulsar::FourierDomainFit;
+
+// Text interface to the FourierDomainFit class
+class FourierDomainFit::Interface : public TextInterface::To<FourierDomainFit>
+{
+public:
+  Interface (FourierDomainFit* = 0);
+  std::string get_interface_name () const { return "FDM"; }
+};
+
+FourierDomainFit::Interface::Interface (FourierDomainFit* instance)
+{
+  if (instance)
+    set_instance (instance);
+
+  add( &FourierDomainFit::get_mcmc,
+       &FourierDomainFit::set_mcmc,
+       "mcmc", "Use Markov chain Monte Carlo to estimate uncertainty");
+}
+
+TextInterface::Parser* FourierDomainFit::get_interface ()
+{
+  return new Interface(this);
+}
