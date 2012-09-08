48e960745a939972952db19a58be32466bb0baf0
Willem van Straten <willem@swin.edu.au>
2012-09-08 15:31:41 +1000

    3565711: new Smooth::factory requires abstract clone and get_interface methods

--- More/General/Pulsar/Smooth.h
+++ More/General/Pulsar/Smooth.h
@@ -15,6 +15,7 @@
 #define __Pulsar_Smooth_h
 
 #include "Pulsar/Transformation.h"
+#include "TextInterface.h"
 
 namespace Pulsar {
 
@@ -30,6 +31,15 @@ namespace Pulsar {
 
     //! Destructor
     virtual ~Smooth ();
+
+    //! Return a copy constructed instance of self
+    virtual Smooth* clone () const = 0;
+
+    //! Return a text interface that can be used to configure this instance
+    virtual TextInterface::Parser* get_interface () = 0;
+
+    //! Text-based interface to Smooth class properties
+    class Interface;
 
     //! Set the width of the smoothing window in turns
     void set_turns (float);
@@ -49,6 +59,9 @@ namespace Pulsar {
     //! Get the width of the smoothing window in turns from the last transform
     float get_last_bins () const;
 
+    //! Construct a new Smooth instance from a string
+    static Smooth* factory (const std::string& name_and_options);
+
   protected:
 
     //! Get the width of the smoothing window in phase bins
@@ -67,6 +80,12 @@ namespace Pulsar {
 
   };
 
+  //! 
+  class Smooth::Interface : public TextInterface::To<Smooth>
+  {
+  public:
+    Interface (Smooth* instance);
+  };
 }
 
 #endif
