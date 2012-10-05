6eaff944533dfe913bdc9ebc5f30936c9470045d
Willem van Straten <vanstraten.willem@gmail.com>
2012-10-05 15:11:21 +1000

    optionally divide sessions into sub-directories designated by band

--- More/Applications/sesdiv.C
+++ More/Applications/sesdiv.C
@@ -37,18 +37,66 @@ void usage ()
        << endl;
 }
 
+class band
+{
+public:
+        band (const char* txt);
+	string name;
+	double min_frequency;
+	double max_frequency;
+};
+
+ostream& operator<< (ostream& os, const band& b)
+{
+  return os << b.name << ":" << b.min_frequency << "-" << b.max_frequency;
+}
+
+band::band (const char* txt)
+{
+  string text = txt;
+  size_t index = text.find(':');
+  if (index == string::npos)
+    throw ios_base::failure ("malformed band designation"
+                             " - missing colon after name in '" + text + "'");
+
+  name = text.substr (0, index);
+
+  text = text.substr (index+1);
+
+  char separator;
+  sscanf (text.c_str(), "%lf%c%lf", &min_frequency, &separator, &max_frequency);
+
+  if (index == string::npos)
+    throw ios_base::failure ("malformed band designation"
+                             " - missing hyphen between frequencies");
+
+  if (max_frequency < min_frequency)
+    throw ios_base::failure ("malformed band designation"
+                             " - max less than min frequency");
+}
+
 int main (int argc, char** argv) 
 {
+  vector<band> bands;
+
   float between_sessions = 10.0;
   bool include_minutes = false;
 
   bool verbose = false;
   bool vverbose = false;
 
-  const char* args = "hS:T:vV";
+  const char* args = "hb:S:T:vV";
   int c = 0;
   while ((c = getopt(argc, argv, args)) != -1)
     switch (c) {
+
+    case 'b':
+    {
+      band b (optarg);
+      cerr << "sesdiv: adding band designation " << b << endl;
+      bands.push_back(b);
+      break;
+    }
 
     case 'h':
       usage ();
@@ -102,17 +150,26 @@ int main (int argc, char** argv)
   list<Pulsar::ArchiveSort>::iterator entry;
   list<Pulsar::ArchiveSort>::iterator last = entries.end();
 
-  for (entry = entries.begin(); entry != entries.end(); entry++) try {
+  if (bands.size())
+  {
+    for (unsigned iband=0; iband < bands.size(); iband++)
+      if (makedir (bands[iband].name.c_str()) < 0)
+      {
+        cerr << "Could not create " << bands[iband].name << " sub-directory"
+             << endl;
+        return -1;
+      }
+  }
 
+  for (entry = entries.begin(); entry != entries.end(); entry++) try
+  {
     if (last != entries.end() && ! ( (*last) < (*entry) ))
       cerr << "Entries not sorted!" << endl;
 
-    string filename = entry->filename;
-
     MJD mjd = entry->epoch;
 
-    if (!session) {
-
+    if (!session)
+    {
       // returns a string formatted as in 'strftime()'
       if (include_minutes)
         mjd.datestr (buffer, 256, "%Y-%m-%d-%H%M");
@@ -123,10 +180,21 @@ int main (int argc, char** argv)
 
       sprintf (buffer + length, "_%.0f.session", entry->centre_frequency);
 
-      cerr << "Starting session " << session_count 
-	   << " in " << buffer << endl;
+      string filename = buffer;
 
-      session = fopen (buffer, "w");
+      if (bands.size())
+        for (unsigned iband=0; iband < bands.size(); iband++)
+          if (bands[iband].min_frequency < entry->centre_frequency &&
+              bands[iband].max_frequency > entry->centre_frequency)
+          {
+            filename = bands[iband].name + "/" + filename;
+            break;
+          }
+
+      cerr << "Starting session " << session_count 
+	   << " in " << filename << endl;
+
+      session = fopen (filename.c_str(), "w");
       if (!session) {
         perror ("sesdiv: Could not open file");
         return -1;
