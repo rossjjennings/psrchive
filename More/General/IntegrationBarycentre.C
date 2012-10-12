2daaa7e0fe79bf0852855b0bf7992c9cd877efc1
Paul Demorest <pdemores@nrao.edu>
2012-10-12 15:38:08 -0400

    IntegrationBarycentre:  Don't rely on integration parent for coords.

--- More/General/IntegrationBarycentre.C
+++ More/General/IntegrationBarycentre.C
@@ -6,10 +6,9 @@
  ***************************************************************************/
 
 #include "Pulsar/IntegrationBarycentre.h"
-#include "Pulsar/Archive.h"
 #include "Pulsar/Integration.h"
-#include "Pulsar/IntegrationExpert.h"
 #include "Pulsar/Site.h"
+#include "sky_coord.h"
 
 using namespace std;
 
@@ -35,20 +34,22 @@ MJD Pulsar::IntegrationBarycentre::get_barycentric_epoch() const
 }
 
 void
-Pulsar::IntegrationBarycentre::set_Integration (const Integration *_subint)
+Pulsar::IntegrationBarycentre::set_Integration (const Integration *_subint) try
 {
   if (subint == _subint)
     return;
 
   subint = _subint;
-  const Archive *arch = 
-    const_cast<Integration*>(subint.get())->expert()->get_parent();
 
   bary.set_epoch(subint->get_epoch());
-  bary.set_coordinates(arch->get_coordinates());
+  bary.set_coordinates(subint->get_coordinates());
 
   double x, y, z;
-  const Site *site = Site::location(arch->get_telescope());
+  const Site *site = Site::location(subint->get_telescope());
   site->get_xyz(x,y,z);
   bary.set_observatory_xyz(x,y,z);
+}
+catch (Error &error)
+{
+  throw error += "Pulsar::IntegrationBarycentre::set_Integration";
 }
