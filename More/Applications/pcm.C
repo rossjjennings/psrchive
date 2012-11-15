aa1151923ff1480b8977ad4bd6c46055b2b2972e
Willem van Straten <willem@swin.edu.au>
2012-11-15 16:08:08 +1100

    plot variation parameters in result_variations.ps

--- More/Applications/pcm.C
+++ More/Applications/pcm.C
@@ -1116,7 +1116,7 @@ int actual_main (int argc, char *argv[]) try
 
     if (get_time_variation())
     {
-      cpgbeg (0, "variations.ps/PS", 0, 0);
+      cpgbeg (0, "result_variations.ps/PS", 0, 0);
       cpgsvp (0.1,.9, 0.1,.9);
 
       unsigned panels = plotter.npanel;
