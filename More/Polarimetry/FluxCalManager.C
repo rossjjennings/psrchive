7c5d8c0da49b561b1e65a2cfe313671ea4c69dfb
Willem van Straten <willem@swin.edu.au>
2012-11-18 21:37:17 +1100

    3587771: manages all flux calibrator observations, source states, and backend transformations

---
+++ More/Polarimetry/FluxCalManager.C
@@ -0,0 +1,185 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2012 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "Pulsar/FluxCalManager.h"
+#include "Pulsar/BackendFeed.h"
+#include "Pulsar/SingleAxis.h"
+#include "Pauli.h"
+
+using namespace std;
+using Calibration::FluxCalManager;
+
+// #define _DEBUG 1
+
+Calibration::FluxCalManager::FluxCalManager (SignalPath* path)
+{
+  MEAL::Complex2* response = path->get_transformation();
+
+  BackendFeed* physical = dynamic_cast<BackendFeed*>( response );
+
+  //
+  // It must be possible to extract the frontend, so that the temporal
+  // variations applied to pulsar and reference source are not applied
+  // to the flux calibrator
+  //
+
+  if (!physical)
+    throw Error (InvalidState,
+	         "Calibration::FluxCalManager ctor",
+	         "Backend/Feed parameterization required to model fluxcal");
+
+  composite = path;
+
+  backend = new Calibration::SingleAxis;
+
+  frontend = new MEAL::ProductRule<MEAL::Complex2>;
+
+  frontend->add_model( physical->get_frontend() );
+
+  if (path->has_basis())
+    frontend->add_model( path->get_basis() );
+}
+
+void Calibration::FluxCalManager::add_observation (Signal::Source source_type)
+{
+  Reference::To<FluxCalObservation> obs = new FluxCalObservation;
+
+  add_backend (obs);
+  add_source (obs);
+
+  Reference::To<FluxCalPair> pair = new FluxCalPair;
+
+  // TODO: make pairs and add difference to model
+
+  if (source_type == Signal::FluxCalOn)
+    pair->on = obs;
+  else if (source_type == Signal::FluxCalOff)
+    pair->off = obs;
+
+  observations.push_back( pair );
+}
+
+
+void Calibration::FluxCalManager::add_backend (FluxCalObservation* obs)
+{
+  obs->backend = new BackendEstimate;
+
+  Reference::To< MEAL::ProductRule<MEAL::Complex2> > fcal_path;
+  fcal_path = new MEAL::ProductRule<MEAL::Complex2>;
+
+  if (backend)
+  {
+    obs->backend->backend = backend->clone();
+    fcal_path->add_model( obs->backend->backend );
+  }
+
+  fcal_path->add_model ( frontend );
+
+  composite->add_transformation ( fcal_path );
+
+  obs->backend->path_index 
+    = composite->get_equation()->get_transformation_index ();
+}
+
+void Calibration::FluxCalManager::add_source (FluxCalObservation* obs)
+{
+  Reference::To< SourceEstimate > source;
+
+  if (multiple_source_states || observations.size() == 0)
+  {
+    source = new SourceEstimate;
+
+    source->create_source( composite->get_equation() );
+
+    // set the initial guess
+    Stokes<double> flux_cal_state (1,0,0,0);
+  
+    source->source->set_stokes ( flux_cal_state );
+
+    // parameter index of Stokes V
+    const unsigned int StokesV = 3;
+
+    // TODO: on or off? ... does the user want to assume PolnCal[V] = 0?
+    source->source->set_infit ( StokesV, false );
+    source->source->set_Estimate ( StokesV, 0.0);
+
+    string name_prefix = "flux_" + tostring(observations.size()) + "_";
+    source->source->set_param_name_prefix( name_prefix );
+  }
+  else
+    // TODO: on or off?
+    source = observations[0]->on->source;
+}
+
+//! Integrate an estimate of the backend
+void FluxCalManager::integrate (const Calibration::SingleAxis* sa)
+{
+  if (!observations.size())
+    throw Error (InvalidState, 
+		 "Calibration::FluxCalManager::integrate",
+		 "no flux calibration backend added to signal path");
+
+  observations.back()->on->backend->estimate.integrate (sa);
+}
+
+void FluxCalManager::integrate (const Jones< Estimate<double> >& correct,
+				const SourceObservation& data)
+{
+  if (!observations.size())
+    throw Error (InvalidState, 
+		 "Calibration::FluxCalManager::integrate",
+		 "no flux calibration source added to model");
+
+  Stokes< Estimate<double> > result = transform( data.baseline, correct );
+  observations.back()->on->source->estimate.integrate (result);
+}
+
+void Calibration::FluxCalManager::update ()
+{
+  for (unsigned i=0; i < observations.size(); i++)
+    observations[i]->update ();
+}
+
+
+
+#if 0
+
+TODO
+
+void Calibration::FluxCalManager::submit_data 
+(
+ Calibration::CoherencyMeasurementSet& measurements,
+ const Stokes< Estimate<double> >& data
+ ) try
+{
+  // add the flux calibrator data to the model constraints
+
+  Calibration::CoherencyMeasurement state 
+    (flux_calibrator_estimate[ichan].input_index);
+
+  flux_calibrator_estimate[ichan].add_data_attempts ++;
+
+  state.set_stokes( data );
+
+  measurements.push_back (state);
+
+  unsigned index = model[ichan]->get_fluxcal()->get_path_index();
+  measurements.set_transformation_index (index);
+
+  DEBUG ("ReceptionCalibrator::submit_flux_calibrator_data ichan=" << ichan);
+
+  model[ichan]->get_equation()->add_data (measurements);
+}
+catch (Error& error)
+{
+  cerr << "Pulsar::ReceptionCalibrator::submit_flux_calibrator_data ichan="
+       << ichan << " error\n" << error << endl;
+
+  flux_calibrator_estimate[ichan].add_data_failures ++;
+}
+
+#endif
---
+++ More/Polarimetry/Pulsar/FluxCalManager.h
@@ -0,0 +1,112 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2012 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+// psrchive/More/Polarimetry/Pulsar/FluxCalManager.h
+
+#ifndef __Calibration_FluxCalManager_H
+#define __Calibration_FluxCalManager_H
+
+#include "Pulsar/SourceEstimate.h"
+#include "Pulsar/BackendEstimate.h"
+#include "Pulsar/SignalPath.h"
+
+namespace Calibration
+{
+  //! Manages flux calibrator source state, signal path, and initial estimates
+  class FluxCalObservation : public Reference::Able
+  {
+  public:
+
+    void update ();
+
+    //! Backend transformation for this observation
+    Reference::To<BackendEstimate> backend;
+
+    //! Model of source and its first guess/best estimate
+    Reference::To<SourceEstimate> source;
+  };
+
+  //! Manages a pair of on-source and off-source flux calibrator observations
+  class FluxCalPair : public Reference::Able
+  {
+  public:
+
+    void update ();
+
+    //! On-source observation
+    Reference::To<FluxCalObservation> on;
+
+    //! Off-source observation
+    Reference::To<FluxCalObservation> off;
+  };
+
+  //! Manages multiple flux calibrator observations
+
+  /*!
+    Whereas pulsar and reference source (noise diode) are assumed to
+    be observed around the same time, the flux calibrator observation
+    may be observed at a different time; therefore, for each flux
+    calibrator observation, a unique backend transformation instance
+    is created that is disengaged from any temporal variations.
+  */
+
+  class FluxCalManager : public Reference::Able
+  {
+  public:
+
+    //! Default constructor
+    FluxCalManager (SignalPath* composite);
+
+    //! Set the backend transformation to be cloned for each flux calibrator
+    void set_backend (const MEAL::Complex2*);
+
+    //! Add a new observation
+    void add_observation (Signal::Source type);
+
+    //! Integrate an estimate of the backend
+    void integrate (const SingleAxis*);
+
+    //! Integrate an estimate of the source
+    void integrate (const Jones< Estimate<double> >& correct,
+		    const SourceObservation& data);
+
+    void submit (CoherencyMeasurementSet&, const Stokes< Estimate<double> >&);
+
+    //! Update all backend models with current best estimate
+    void update ();
+
+    //! Return true if at least one complete flux calibrator observation
+    bool is_constrained () const;
+
+    bool multiple_source_states;
+
+  protected:
+
+    //! Add a new backend to the model
+    void add_backend (FluxCalObservation*);
+
+    //! Add a new source to the model
+    void add_source (FluxCalObservation*);
+
+    //! The backend transformation to be cloned for each flux calibrator
+    Reference::To< MEAL::Complex2 > backend;
+
+    //! The frontend component of the instrument
+    Reference::To< MEAL::ProductRule<MEAL::Complex2> > frontend;
+    
+    //! The SignalPath manager into which signal paths are added
+    Reference::To< SignalPath > composite;
+
+    //! The set of flux calibrator observations
+    std::vector< Reference::To<FluxCalPair> > observations;
+
+  };
+
+}
+
+#endif
