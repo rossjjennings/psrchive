e95366a7a69d74728f332d3c7d18f326250b98b7
Willem van Straten <willem@swin.edu.au>
2013-04-13 23:20:19 +1000

    simple functions to convert a C string to Fortran and vice versa

---
+++ Util/genutil/f77util.c
@@ -0,0 +1,37 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2013 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "f77util.h"
+#include <string.h>
+
+void f2cstr (const char* f_str, char* c_str, unsigned length)
+{
+  /* fprintf (stderr, "f2cstr in '%.*s'\n", length, f_str); */
+
+  unsigned i = length-1;
+  
+  strncpy (c_str, f_str, length);
+  
+  while (f_str[i] == ' ') {
+    c_str[i] = '\0';
+    if (i == 0)
+      break;
+    i--;
+  }
+
+  /* fprintf (stderr, "f2cstr out '%s'\n", c_str); */
+}
+
+void c2fstr (char* f_str, const char* c_str, unsigned length)
+{
+  unsigned i = strlen(c_str);
+  
+  strncpy (f_str, c_str, length);
+  for (; i < length; i++)
+    f_str[i] = ' ';
+}
+
