67b7f3a7191ef7225ec303df6500d108d18a390b
Willem van Straten <willem@swin.edu.au>
2013-07-09 04:35:21 +1000

    provide access to filtered and unfiltered means

--- Base/Extensions/SpectralKurtosisTI.C
+++ Base/Extensions/SpectralKurtosisTI.C
@@ -8,6 +8,7 @@
 #include "Pulsar/SpectralKurtosis.h"
 
 using Pulsar::SpectralKurtosis;
+using std::string;
 
 SpectralKurtosis::Interface::Interface ( SpectralKurtosis *s_instance )
 {
@@ -15,12 +16,22 @@ SpectralKurtosis::Interface::Interface ( SpectralKurtosis *s_instance )
     set_instance( s_instance );
 
   add( &SpectralKurtosis::get_M,
-         &SpectralKurtosis::set_M,
-         "m", "Integration factor (samples)" );
+       &SpectralKurtosis::set_M,
+       "m", "Integration factor (samples)" );
 
   add( &SpectralKurtosis::get_excision_threshold,
-         &SpectralKurtosis::set_excision_threshold,
-         "nsig", "Excision Threshold (stddevs)" );
+       &SpectralKurtosis::set_excision_threshold,
+       "nsig", "Excision threshold (stddevs)" );
+
+  VGenerator<float> fgenerator;
+  add_value(fgenerator( "all", string("Unfiltered mean spectral kurtosis"),
+			&SpectralKurtosis::get_unfiltered_mean,
+			&SpectralKurtosis::get_nchan ));
+
+  add_value(fgenerator( "filt", string("Filtered mean spectral kurtosis"),
+			&SpectralKurtosis::get_filtered_mean,
+			&SpectralKurtosis::get_nchan ));
+
 
 }
 
