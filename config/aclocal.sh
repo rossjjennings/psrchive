2382d9f8f0ff399dba7a94c3f5667ea2302d0289
Willem van Straten <willem@swin.edu.au>
2013-10-08 23:18:01 +1100

    add rules to copy Makefile.aclocal and psrchive.m4

--- config/aclocal.sh
+++ config/aclocal.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+files="$* Makefile.aclocal psrchive.m4"
+
 echo ""
 
 echo 'aclocal_update: \'
 
-for file in $*; do
+for file in $files; do
   echo '	$(top_srcdir)/config/'$file' \'
 done
 
 echo
 
-for file in $*; do
+for file in $files; do
   echo '$(top_srcdir)/config/'$file' : $(PSRCHIVE_ACLOCAL)/'$file
   echo '	cp $(PSRCHIVE_ACLOCAL)/'$file' $(top_srcdir)/config/'$file
   echo
