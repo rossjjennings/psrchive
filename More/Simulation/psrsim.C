75193e49c137e5f85d562e5eb3ccc6eae99a3fbe
Willem van Straten <willem@swin.edu.au>
2013-12-17 23:25:51 +1100

    options added to set pulse width and noise level

--- More/Simulation/psrsim.C
+++ More/Simulation/psrsim.C
@@ -70,6 +70,8 @@ psrsim::psrsim () :
   rotation_measure = 0;
 }
 
+// defined in RotatingVectorModelOptions.C
+double deg_to_rad (const std::string& arg);
 
 void psrsim::add_options (CommandLine::Menu& menu)
 {
@@ -81,8 +83,17 @@ void psrsim::add_options (CommandLine::Menu& menu)
   arg = menu.add (rotation_measure, 'r');
   arg->set_help ("rotation measure");
 
-  menu.add ("\n" "Pulse shape options:");
-  
+  menu.add ("\n" "Pulse shape and statistics:");
+
+  MEAL::ScaledVonMises* svm = sim->get_Intensity();
+
+  arg = menu.add (svm, &MEAL::ScaledVonMises::set_width,
+		  deg_to_rad, 'w', "degrees");
+  arg->set_help ("width of pulse in degrees");
+
+  arg = menu.add (sim.get(), &SyntheticPolnProfile::set_noise, 'n');
+  arg->set_help ("standard deviation of noise");
+		  
   RotatingVectorModelOptions rvm_options;
   rvm_options.set_model (sim->get_RVM());
   rvm_options.add_options (menu);
@@ -104,21 +115,33 @@ void psrsim::process (Pulsar::Archive* data)
     data->resize( nsubint, npol, nchan );
   }
 
-  data->set_state( Signal::Stokes);
+  data->set_state (Signal::Stokes);
+  data->set_poln_calibrated (true);
 
   for (unsigned isub=0; isub < nsubint; isub++)
+  {
+    Integration* subint = data->get_Integration(isub);
+
     for (unsigned ichan=0; ichan < nchan; ichan++)
-      sim->get_PolnProfile(data->get_Integration(isub)->new_PolnProfile(ichan));
+    {
+      sim->get_PolnProfile(subint->new_PolnProfile(ichan));
+      subint->set_weight(ichan, 1.0);
+    }
+  }
 
   if (rotation_measure != 0.0)
   {
     // perform Faraday rotation
     FaradayRotation xform;
-    xform.set_reference_wavelength( data->get_centre_frequency() );
-    xform.set_measure( rotation_measure );
-    xform.execute( data );
+    xform.set_reference_frequency( data->get_centre_frequency() );
+    xform.set_measure( -rotation_measure );
+    xform.just_do_it( data );
   }
-  
+
+  data->set_faraday_corrected (false);
+  data->set_rotation_measure (rotation_measure);
+  data->set_dispersion_measure (0.0);
+
   data->unload ("psrsim.ar");
 }
 
