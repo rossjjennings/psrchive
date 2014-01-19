4e8610923817de06621bd3a100cec3beff5cfe60
Willem van Straten <willem@swin.edu.au>
2014-01-19 15:11:58 +1100

    Logistic function added

---
+++ More/MEAL/MEAL/ScalarLogistic.h
@@ -0,0 +1,50 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2013 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+// psrchive/More/MEAL/MEAL/ScalarLogistic.h
+
+#ifndef __MEAL_ScalarLogistic_H
+#define __MEAL_ScalarLogistic_H
+
+#include "MEAL/UnaryScalar.h"
+
+namespace MEAL {
+
+  //! The logistic of a Scalar function
+  class ScalarLogistic : public UnaryScalar
+  {
+
+  public:
+
+    // ///////////////////////////////////////////////////////////////////
+    //
+    // UnaryScalar implementation
+    //
+    // ///////////////////////////////////////////////////////////////////
+
+    //! The function
+    double function (double x) const;
+
+    //! And its first derivative
+    double dfdx (double x) const;
+
+    // ///////////////////////////////////////////////////////////////////
+    //
+    // Function implementation
+    //
+    // ///////////////////////////////////////////////////////////////////
+
+    //! Return the name of the class
+    std::string get_name () const;
+
+  };
+
+}
+
+
+#endif
