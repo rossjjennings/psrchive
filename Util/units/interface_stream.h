cdc1ca29f5e683651d04ea2f40f83ee0eb131b1c
Willem van Straten <willem@swin.edu.au>
2014-01-27 19:06:57 +1100

    two new interface_insertion/extraction signatures added

--- Util/units/interface_stream.h
+++ Util/units/interface_stream.h
@@ -14,10 +14,11 @@
 //! insertion operator outputs object state as defined by its text interface
 /*!
  */
-template<typename T>
-std::ostream& operator<< (std::ostream& ostr, const Reference::To<T>& e) try
+template<class T>
+std::ostream& interface_insertion (std::ostream& ostr, const T* e) try
 {
-  Reference::To<TextInterface::Parser> interface = e->get_interface();
+  Reference::To<TextInterface::Parser> interface 
+    = const_cast<T*>(e)->get_interface();
 
   ostr << interface->get_interface_name ();
 
@@ -47,18 +48,25 @@ catch (Error& error)
 //
 
 template<class T>
-std::ostream& interface_insertion (std::ostream& ostr, const T* e)
+std::ostream& 
+interface_insertion (std::ostream& ostr, const Reference::To<T>& e)
 {
-  Reference::To<T> ref = const_cast<T*>(e);
-  return ostr << ref;
+  return interface_insertion (ostr, e.get());
+}
+
+template<typename T>
+std::ostream& operator<< (std::ostream& ostr, const Reference::To<T>& e)
+{
+  return interface_insertion (ostr, e);
 }
 
 
 //! extraction operator creates a new object using its text interface factory
 /*!
  */
+
 template<typename T>
-std::istream& operator >> (std::istream& istr, Reference::To<T>& e) try
+std::istream& interface_extraction (std::istream& istr, T* &e) try
 {
   std::string parse;
   istr >> parse;
@@ -72,18 +80,20 @@ catch (Error& error)
   return istr;
 }
 
-
 template<typename T>
-std::istream& interface_extraction (std::istream& istr, T* &e)
+std::istream& interface_extraction (std::istream& istr, Reference::To<T>& e)
 {
-  Reference::To<T> ref;
-
-  istr >> ref;
-
-  if (ref)
-    e = ref.release();
-
+  T* ptr;
+  interface_extraction (istr, ptr);
+  e = ptr;
   return istr;
 }
+
+template<typename T>
+std::istream& operator >> (std::istream& istr, Reference::To<T>& e)
+{
+  return interface_extraction (istr, e);
+}
+
 
 #endif
