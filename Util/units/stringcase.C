7d8342a8693491ff6d1a5e07c2c4c352389d3170
Willem van Straten <willem@swin.edu.au>
2014-01-28 21:17:54 +1100

    indentation

--- Util/units/stringcase.C
+++ Util/units/stringcase.C
@@ -26,10 +26,12 @@ string uppercase (string s)
 }
 
 bool c_casecmp (string::value_type l1, string::value_type r1)
-{ return toupper(l1) == toupper(r1); }
+{
+  return toupper(l1) == toupper(r1);
+}
 
 bool casecmp (const string& l, const string& r)
 {
-    return l.size() == r.size()
-        && equal(l.begin(), l.end(), r.begin(), c_casecmp);
+  return l.size() == r.size()
+    && equal(l.begin(), l.end(), r.begin(), c_casecmp);
 }
