9aff115deb3ef8472dda44db811a277d18179437
Willem van Straten <willem@swin.edu.au>
2014-05-22 11:48:33 +1000

    random_inti and random_double added

---
+++ Util/units/random.C
@@ -0,0 +1,33 @@
+
+#include "random.h"
+
+#include <stdlib.h>
+#include <sys/time.h>
+
+#if _DEBUG
+#include <iostream>
+using namespace std;
+#endif
+
+static const double random_max = uint32_t(0x01) << 32 -1;
+
+void random_init ()
+{
+  struct timeval t;
+  gettimeofday (&t, NULL);
+  srandom (t.tv_usec);
+}
+
+double random_double ()
+{
+  double val = double(random()) / random_max;
+#if _DEBUG
+  static double vmin = 1000;
+  vmin = std::min(vmin,val);
+  static double vmax = -1000;
+  vmax = std::max(vmax,val);
+  cerr << "check " << vmin << " " << vmax << endl;
+#endif
+  return val;
+}
+ 
