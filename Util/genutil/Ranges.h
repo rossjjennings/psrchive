8b3fc8d87665b1f1a1adbae7a92966ca1e775132
Willem van Straten <willem@swin.edu.au>
2016-02-05 10:15:58 +1100

    encode range notation used to determine when a value is part of a range

---
+++ Util/genutil/Ranges.h
@@ -0,0 +1,39 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2016 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+// psrchive/Util/units/Ranges.h
+
+#ifndef __Ranges_H
+#define __Ranges_H
+
+#include <vector>
+#include <iostream>
+
+class Range
+{
+  bool gt, lt;
+  double x0,x1;
+public:
+  Range () { gt = lt = false; x0 = x1 = 0; }
+  friend std::ostream& operator<< (std::ostream&, const Range&);
+  friend std::istream& operator>> (std::istream&, Range&);
+  bool within (double x) const;
+};
+
+class Ranges
+{
+  std::vector<Range> ranges;
+
+public:
+  friend std::ostream& operator<< (std::ostream&, const Ranges&);
+  friend std::istream& operator>> (std::istream&, Ranges&);
+  bool within (double x) const;
+  void parse (const std::string& text);
+};
+
+#endif // !__Ranges_H
