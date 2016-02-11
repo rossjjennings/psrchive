bc4abfee467bd54db327d961e886751ffdd8fd6d
Willem van Straten <willem@swin.edu.au>
2016-02-11 16:13:30 +1100

    PhaseScale::Units -> Phase::Unit (defined in Util/genutil/PhaseRange.h)

--- More/Plotting/PhaseScale.C
+++ More/Plotting/PhaseScale.C
@@ -13,7 +13,7 @@ using namespace std;
 
 Pulsar::PhaseScale::PhaseScale ()
 {
-  units = Turns;
+  units = Phase::Turns;
   origin_norm = 0;
 }
 
@@ -61,16 +61,16 @@ void Pulsar::PhaseScale::get_ordinates (const Archive* data,
 
 float Pulsar::PhaseScale::get_scale (const Archive* data) const
 {
-  if (units == Milliseconds && data != 0)
+  if (units == Phase::Milliseconds && data != 0)
     return data->get_Integration(0)->get_folding_period() * 1e3;
 
-  else if (units == Radians)
+  else if (units == Phase::Radians)
     return 2.0 * M_PI;
 
-  else if (units == Degrees)
+  else if (units == Phase::Degrees)
     return 360.0;
 
-  else if (units == Bins)
+  else if (units == Phase::Bins)
     return data->get_nbin();
 
   else
@@ -81,51 +81,12 @@ float Pulsar::PhaseScale::get_scale (const Archive* data) const
 std::string Pulsar::PhaseScale::get_label () const
 {
   switch (units) {
-  case Turns: return "Pulse Phase";
-  case Degrees: return "Phase (deg.)";
-  case Radians: return "Phase (rad.)";
-  case Milliseconds: return "Time (ms)";
-  case Bins: return "Bin Number";
+  case Phase::Turns: return "Pulse Phase";
+  case Phase::Degrees: return "Phase (deg.)";
+  case Phase::Radians: return "Phase (rad.)";
+  case Phase::Milliseconds: return "Time (ms)";
+  case Phase::Bins: return "Bin Number";
   default: return "Unknown";
   }
 }
 
-std::ostream& Pulsar::operator << (std::ostream& os, PhaseScale::Units units)
-{
-  switch (units) {
-  case PhaseScale::Turns:
-    return os << "turn";
-  case PhaseScale::Degrees:
-    return os << "deg";
-  case PhaseScale::Radians:
-    return os << "rad";
-  case PhaseScale::Milliseconds:
-    return os << "ms";
-  case PhaseScale::Bins:
-    return os << "bin";
-  default:
-    return os << "unknown";
-  }
-}
-
-std::istream& Pulsar::operator >> (std::istream& is, PhaseScale::Units& units)
-{
-  std::streampos pos = is.tellg();
-  std::string unit;
-  is >> unit;
-
-  if (unit == "turn")
-    units = PhaseScale::Turns;
-  else if (unit == "deg")
-    units = PhaseScale::Degrees;
-  else if (unit == "rad")
-    units = PhaseScale::Radians;
-  else if (unit == "ms")
-    units = PhaseScale::Milliseconds;
-  else if (unit == "bin")
-    units = PhaseScale::Bins;
-  else
-    is.setstate(std::istream::failbit);
-
-  return is;
-}
