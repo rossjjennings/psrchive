b8d5780b59ac13142526c75d0bcad78679bebe8a
Willem van Straten <willem@swin.edu.au>
2016-02-14 21:20:28 +1100

    Attribute Adapter does most of the skeleton work for Proxy classes

--- Util/units/TextInterfaceProxy.h
+++ Util/units/TextInterfaceProxy.h
@@ -2,7 +2,7 @@
 
 /***************************************************************************
  *
- *   Copyright (C) 2003-2013 by Willem van Straten
+ *   Copyright (C) 2003 - 2016 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
@@ -13,6 +13,8 @@
 #define __TextInterfaceProxy_h
 
 #include "TextInterfaceAttribute.h"
+#include "TextInterfaceAdapter.h"
+
 #include "separate.h"
 #include "stringtok.h"
 
@@ -21,72 +23,47 @@ namespace TextInterface
   //! Proxy enables attribute interface from parent to be used by child
   /*! In this template: C is a P */
   template<class C, class P>
-  class IsAProxy : public Attribute<C> {
+  class IsAProxy : public AttributeAdapter<C,P>
+  {
 
   public:
     
     //! Construct from a pointer to parent class attribute interface
-    IsAProxy (const Attribute<P>* pa) { attribute = pa->clone(); }
+    IsAProxy (const Attribute<P>* pa) 
+      { this->value = pa->clone(); }
 
     //! Copy constructor
     IsAProxy (const IsAProxy& copy)
-      { attribute = copy.attribute->clone(); }
+      { this->value = copy.value->clone(); }
 
     //! Retun a newly constructed copy
     Attribute<C>* clone () const
       { return new IsAProxy(*this); }
 
-    //! Get the name of the attribute
-    std::string get_name () const
-      { return attribute->get_name(); }
-
-    //! Get the description of the attribute
-    std::string get_description () const
-      { return attribute->get_description(); }
-
     //! Get the value of the attribute
     std::string get_value (const C* ptr) const 
-      { return attribute->get_value (ptr); }
+      { return this->value->get_value (ptr); }
 
     //! Set the value of the attribute
     void set_value (C* ptr, const std::string& value)
-      { attribute->set_value (ptr, value); }
-
-    //! Set the description of the attribute
-    void set_description (const std::string& description)
-      { attribute->set_description (description); }
-
-    //! Set the detailed description of the attribute
-    void set_detailed_description (const std::string& description)
-      { attribute->set_detailed_description (description); }
-
-    bool matches (const std::string& name) const
-      { return attribute->matches (name); }
-
-    void set_parent (Parser* p)
-    { Value::set_parent(p); attribute->set_parent(p); }
-
-  protected:
-
-    //! The parent attribute interface
-    Reference::To< Attribute<P> > attribute;
-
+      { this->value->set_value (ptr, value); }
   };
 
   //! Proxy enables attribute interface of member to be used by class
   /*! In this template: C has a M; Get is the method of C that returns M* */
   template<class C, class M, class Get>
-  class HasAProxy : public Attribute<C> {
+  class HasAProxy : public AttributeAdapter<C,M>
+  {
 
   public:
 
     //! Construct from a pointer to member attribute interface
     HasAProxy (const std::string& pre, const Attribute<M>* pa, Get g)
-      { prefix = pre; attribute = pa->clone(); get = g; }
+      { prefix = pre; this->value = pa->clone(); get = g; }
 
     //! Copy constructor
     HasAProxy (const HasAProxy& copy)
-      { attribute=copy.attribute->clone(); get=copy.get; prefix=copy.prefix; }
+      { this->value=copy.value->clone(); get=copy.get; prefix=copy.prefix; }
 
     //! Retun a newly constructed copy
     Attribute<C>* clone () const
@@ -96,49 +73,30 @@ namespace TextInterface
     std::string get_name () const
       { 
 	if (prefix.length())
-	  return prefix + ":" + attribute->get_name();
+	  return prefix + ":" + this->value->get_name();
 	else
-	  return attribute->get_name();
+	  return this->value->get_name();
       }
-
-    //! Get the description of the attribute
-    std::string get_description () const
-      { return attribute->get_description(); }
 
     //! Get the value of the attribute
     std::string get_value (const C* ptr) const 
-      { return attribute->get_value ((const_cast<C*>(ptr)->*get)()); }
+      { return this->value->get_value ((const_cast<C*>(ptr)->*get)()); }
 
     //! Set the value of the attribute
-    void set_value (C* ptr, const std::string& value)
-      { attribute->set_value ((ptr->*get)(), value); }
-
-    //! Set the description of the attribute
-    void set_description (const std::string& description)
-      { attribute->set_description (description); }
-
-    //! Set the detailed description of the attribute
-    void set_detailed_description (const std::string& description)
-      { attribute->set_detailed_description (description); }
+    void set_value (C* ptr, const std::string& val)
+      { this->value->set_value ((ptr->*get)(), val); }
 
     //! Return true if the name argument matches
     bool matches (const std::string& name) const
-    { return TextInterface::matches (name, prefix, attribute); }
-
-    void set_parent (Parser* p)
-    { Value::set_parent(p); attribute->set_parent(p); }
+    { return TextInterface::matches (name, prefix, this->value); }
 
   protected:
-
-    //! The parent attribute interface
-    Reference::To< Attribute<M> > attribute;
 
     //! Method of C that returns M*
     Get get;
 
     //! The name of the M attribute within C
     std::string prefix;
-
   };
 
   //! Proxy enables attribute interface of elements in a vector
@@ -146,17 +104,18 @@ namespace TextInterface
     that returns E*; and Size is the method of V that returns the number
     of elements in it */
   template<class V, class E, class Get, class Size>
-  class VectorOfProxy : public Attribute<V> {
+  class VectorOfProxy : public AttributeAdapter<V,E>
+  {
 
   public:
     
     //! Construct from a pointer to element attribute interface
     VectorOfProxy (const std::string& p, const Attribute<E>* a, Get g, Size s)
-      { prefix = p; attribute = a->clone(); get = g; size = s; }
+      { prefix = p; this->value = a->clone(); get = g; size = s; }
 
     //! Copy constructor
     VectorOfProxy (const VectorOfProxy& copy)
-      { attribute=copy.attribute->clone(); 
+      { this->value=copy.value->clone(); 
 	get=copy.get; size=copy.size; prefix=copy.prefix; }
 
     //! Retun a newly constructed copy
@@ -165,11 +124,7 @@ namespace TextInterface
 
     //! Get the name of the attribute
     std::string get_name () const
-      { return prefix + "*:" + attribute->get_name(); }
-
-    //! Get the description of the attribute
-    std::string get_description () const
-      { return attribute->get_description(); }
+      { return prefix + "*:" + this->value->get_name(); }
 
     //! Get the value of the attribute
     std::string get_value (const V* ptr) const;
@@ -177,24 +132,10 @@ namespace TextInterface
     //! Set the value of the attribute
     void set_value (V* ptr, const std::string& value);
 
-    //! Set the description of the attribute
-    void set_description (const std::string& description)
-      { attribute->set_description (description); }
-
-    //! Set the detailed description of the attribute
-    void set_detailed_description (const std::string& description)
-      { attribute->set_detailed_description (description); }
-
     //! Return true if the name argument matches
     bool matches (const std::string& name) const;
 
-    void set_parent (Parser* p)
-    { Value::set_parent(p); attribute->set_parent(p); }
-
   protected:
-
-    //! The element attribute interface
-    Reference::To< Attribute<E> > attribute;
 
     //! Method of V that returns E*
     Get get;
@@ -215,17 +156,18 @@ namespace TextInterface
   /*! In this template: M is a map of key K to element E; 
     Get is the method of V that returns E* given K */
   template<class M, class K, class E, class Get>
-  class MapOfProxy : public Attribute<M> {
+  class MapOfProxy : public AttributeAdapter<M,E>
+  {
 
   public:
     
     //! Construct from a pointer to parent class attribute interface
     MapOfProxy (const std::string& pre, const Attribute<E>* pa, Get g)
-      { prefix = pre; attribute = pa->clone(); get = g; }
+      { prefix = pre; this->value = pa->clone(); get = g; }
 
     //! Copy constructor
     MapOfProxy (const MapOfProxy& copy)
-      { prefix=copy.prefix; attribute=copy.attribute->clone(); get=copy.get;  }
+      { prefix=copy.prefix; this->value=copy.value->clone(); get=copy.get;  }
 
     //! Set the prefix to be added before attribute name
     
@@ -235,11 +177,7 @@ namespace TextInterface
 
     //! Get the name of the attribute
     std::string get_name () const
-      { return prefix + "?:" + attribute->get_name(); }
-
-    //! Get the description of the attribute
-    std::string get_description () const
-      { return attribute->get_description(); }
+      { return prefix + "?:" + this->value->get_name(); }
 
     //! Get the value of the attribute
     std::string get_value (const M* ptr) const;
@@ -247,24 +185,10 @@ namespace TextInterface
     //! Set the value of the attribute
     void set_value (M* ptr, const std::string& value);
 
-    //! Set the description of the attribute
-    void set_description (const std::string& description)
-      { attribute->set_description (description); }
-
-    //! Set the detailed description of the attribute
-    void set_detailed_description (const std::string& description)
-      { attribute->set_detailed_description (description); }
-
     //! Return true if the name argument matches
     bool matches (const std::string& name) const;
 
-    void set_parent (Parser* p)
-    { Value::set_parent(p); attribute->set_parent(p); }
-
   protected:
-
-    //! The parent attribute interface
-    Reference::To< Attribute<E> > attribute;
 
     //! Method of M that returns E*, given K
     Get get;
@@ -309,7 +233,7 @@ TextInterface::VectorOfProxy<V,E,G,S>::get_value (const V* ptr) const
     std::cerr << "VectorOfProxy[" << prefix << "]::get_value (" 
 	      << ptr << ") element=" << element << std::endl;
 #endif
-    result += attribute->get_value (element);
+    result += this->value->get_value (element);
   }
 
   return result;
@@ -328,7 +252,7 @@ void TextInterface::VectorOfProxy<V,E,G,S>::set_value (V* ptr,
     std::cerr << "VectorOfProxy[" << prefix << "]::set_value (" 
 	      << ptr << "," << val << ")" << std::endl;
 #endif
-    attribute->set_value (element, val);
+    this->value->set_value (element, val);
   }
 }
 
@@ -345,7 +269,7 @@ template<class C,class M,class Get,class Size>
   if (!match (prefix, name, &range, &remainder))
     return false;
 
-  return attribute->matches (remainder);
+  return this->value->matches (remainder);
 }
 
 
@@ -373,7 +297,7 @@ TextInterface::MapOfProxy<M,K,E,G>::get_value (const M* ptr) const
 
     E* element = (const_cast<M*>(ptr)->*get) (ind[i]);
     if (element)
-      result += attribute->get_value (element);
+      result += this->value->get_value (element);
   }
 
   return result;
@@ -387,7 +311,7 @@ void TextInterface::MapOfProxy<M,K,E,G>::set_value (M* ptr,
   get_indeces (ind, range);
 
   for (unsigned i=0; i<ind.size(); i++)
-    attribute->set_value ((ptr->*get)(ind[i]), val);
+    this->value->set_value ((ptr->*get)(ind[i]), val);
 }
 
 template<class M, class K, class E, class G>
@@ -431,7 +355,7 @@ template<class M, class K, class E, class G>
   if (!match (prefix, name, &range, &remainder))
     return false;
 
-  return attribute->matches (remainder);
+  return this->value->matches (remainder);
 }
 
 #endif
