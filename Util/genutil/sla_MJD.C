9abb122b6bd3a963ef74a2d25049cab14e2b8323
Willem <willem@swin.edu.au>
2017-09-27 21:04:18 +1300

    added GMST method

--- Util/genutil/sla_MJD.C
+++ Util/genutil/sla_MJD.C
@@ -12,19 +12,57 @@
 // #define _DEBUG
 
 #ifdef _DEBUG
+
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
+
 #define VERBOSE(x) cerr << x << endl;
+
+double c_gmst (double mjd)
+{
+  cerr << "c_gmst passed MJD=" << setprecision(15) << mjd << endl;
+  
+  double D2PI = 6.283185307179586476925286766559e0;
+  double S2R = 7.272205216643039903848711535369e-5;
+
+  // Julian centuries from fundamental epoch J2000 to this UT
+  double TU=(mjd-51544.5)/36525;
+
+  cerr << "Julian Century=" << TU << endl;
+  
+  // GMST at this UT
+  double sla_GMST = fmod(mjd,1.0)*D2PI+
+    (24110.54841e0+
+     (8640184.812866e0+
+      (0.093104e0 - 6.2e-6*TU)*TU)*TU)*S2R;
+
+  cerr << "GMST before fmod=" << sla_GMST << endl;
+  
+  return fmod(sla_GMST,D2PI);
+}
+
 #else
+
 #define VERBOSE(x)
+
 #endif
 
-double MJD::LST (double longitude) const
+double MJD::GMST () const
 {
   double passed_MJD = this->in_days();
 
-  VERBOSE("MJD::LST mjd=" << passed_MJD);
+  VERBOSE("MJD::GMST mjd=" << passed_MJD);
   double gmst = slaGmst(passed_MJD);
+  //double gmst = c_gmst(passed_MJD);
+  VERBOSE("MJD::GMST gmst=" << gmst);
+  return gmst;
+}
+  
+double MJD::LST (double longitude) const
+{
+  double gmst = this->GMST();
   VERBOSE("MJD::LST gmst=" << gmst);
   double lst = gmst/M_PI*180.0/15.0 + longitude/360.0*24.0;
   VERBOSE("MJD::LST lst=" << lst);
