e386a7002a9e4350ab4799bf9827eb8f6a126b73
Willem van Straten <vanstraten.willem@gmail.com>
2019-08-08 21:43:36 +1200

    some versions of GNU autotools include FILE in DEST

--- Management/install_with_githash.sh
+++ Management/install_with_githash.sh
@@ -9,6 +9,15 @@ then
 	exit 1
 fi
 FILE=`basename $1`
+
+# some versions of GNU autotools include FILE in DEST
+if test "$FILE" = "`basename $2`"; then
+  DEST=$2
+else
+  DEST=$2/$FILE
+fi
+
 ID=`git log -1 --format='(%cd %h)' --date=short 2>/dev/null || echo ''`
-sed "s/PSRCHIVEGITHASH/$ID/" $1 > $2/$FILE
-chmod 755 $2/$FILE
+sed "s/PSRCHIVEGITHASH/$ID/" $1 > $DEST
+chmod 755 $2/$DEST
+
