f9b2f8c62391f35d47de07b3a7395f875a8667b7
Willem van Straten <vanstraten.willem@gmail.com>
2021-02-07 14:29:55 +1300

    perform a test of results versus expecations

--- Util/units/test_ndIndex.C
+++ Util/units/test_ndIndex.C
@@ -22,17 +22,25 @@ int main ()
 
   cerr << "nrow=" << nrow << " ncol=" << ncol << " ndim=" << ndim << endl;
   cerr << "stride=" << idx.stride() << endl;
+  assert (idx.stride() == 60);
 
   cerr << "idx[0][0][0]=" << idx[0][0][0] << endl << endl;
+  assert (idx[0][0][0] == 0);
 
   cerr << "idx[1][0][0]=" << idx[1][0][0] << endl;
+  assert (idx[1][0][0] == 12);
   cerr << "idx[2][0][0]=" << idx[2][0][0] << endl << endl;
+  assert (idx[2][0][0] == 24);
 
   cerr << "idx[0][1][0]=" << idx[0][1][0] << endl;
+  assert (idx[0][1][0] == 3);
   cerr << "idx[0][2][0]=" << idx[0][2][0] << endl << endl;
+  assert (idx[0][2][0] == 6);
 
   cerr << "idx[0][0][1]=" << idx[0][0][1] << endl;
+  assert (idx[0][0][1] == 1);
   cerr << "idx[0][0][2]=" << idx[0][0][2] << endl << endl;
+  assert (idx[0][0][2] == 2);
 
   return 0;
 }
