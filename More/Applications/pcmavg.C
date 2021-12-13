a937906542238911e4d27a8d1f53875c4ae1dd5f
Willem van Straten <vanstraten.willem@gmail.com>
2021-12-13 21:02:33 +1300

    optionally compute the weighted median of model parameters

--- More/Applications/pcmavg.C
+++ More/Applications/pcmavg.C
@@ -5,9 +5,6 @@
  *
  ***************************************************************************/
 
-#include <vector>
-
-#include "Error.h"
 #include "Pulsar/Application.h"
 #include "Pulsar/Archive.h"
 #include "Pulsar/CalibratorStokes.h"
@@ -17,11 +14,12 @@
 #include "Pulsar/StandardOptions.h"
 #include "Pulsar/UnloadOptions.h"
 
-using std::cerr;
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
+#include "UnaryStatistic.h"
+#include "Error.h"
+
+#include <vector>
+
+using namespace std;
 
 class pcmavg : public Pulsar::Application
 {
@@ -57,9 +55,33 @@ class pcmavg : public Pulsar::Application
     bool has_same_attributes(Pulsar::Archive* archive) const;
 
     void average_pce(Pulsar::Archive* archive);
-
     void average_cse(Pulsar::Archive* archive);
 
+    bool compute_weighted_median;
+
+    //! Record PolnCalibrator values used to compute weighted median
+    void record_pce (Pulsar::Archive* archive);
+
+    //! Compute the weighted median of PolnCalibrator values
+    void weighted_median_pce ();
+
+    //! An array of estimates for each ichan and pce iparam
+    vector< vector< vector< Estimate<double> > > > pce_params;
+    //! An array of chi-squared values for each ichan
+    vector< vector<double> > chisq_vals;
+    //! An array of nfree values for each ichan
+    vector< vector<unsigned> > nfree_vals;
+    
+    //! Record CalibratorStokes values used to compute weighted median
+    void record_cse(Pulsar::Archive* archive);
+
+    //! Compute the weighted median of CalibratorStokes values
+    void weighted_median_cse ();
+
+    //! An array of estimates for each ichan and cse iparam
+    vector< vector< vector< Estimate<double> > > > cse_params;
+
+      
     void setup(Pulsar::Archive* archive);
 
     //!
@@ -101,7 +123,7 @@ class pcmavg : public Pulsar::Application
     unsigned get_average_nchan() const;
 
     //! The weighted average archive - a copy of the first file.
-    Pulsar::Archive* average;
+    Reference::To<Pulsar::Archive> average;
 
     //! Centre frequency of the first file.
     double average_centre_frequency;
@@ -236,6 +258,108 @@ void pcmavg::average_pce(Pulsar::Archive* archive)
   }
 }
 
+void pcmavg::record_pce(Pulsar::Archive* archive)
+{
+  Reference::To<Pulsar::PolnCalibratorExtension> ext =
+    archive->get<Pulsar::PolnCalibratorExtension>();
+
+  const unsigned nchan  = ext->get_nchan();
+  const unsigned nparam = ext->get_nparam();
+
+  if (nchan != pce_params.size())
+    throw Error (InvalidState, "pcmavg::record_pce",
+		 "archive nchan=%u != target nchan=%u",
+		 nchan, pce_params.size());
+
+  if (nchan == 0)
+    return;
+  
+  if (nparam != pce_params[0].size())
+    throw Error (InvalidState, "pcmavg::record_pce",
+		 "archive nparam=%u != target nparam=%u",
+		 nparam, pce_params[0].size());
+
+  for (unsigned ichan = 0; ichan < nchan; ++ichan)
+  {
+    const double chisq = ext->get_transformation(ichan)->get_chisq();
+    chisq_vals[ichan].push_back (chisq);
+
+    const unsigned nfree = ext->get_transformation(ichan)->get_nfree();
+    nfree_vals[ichan].push_back (nfree);
+
+    for (unsigned iparam = 0; iparam < nparam; ++iparam)
+    {
+      Estimate<double> val = ext->get_Estimate (iparam, ichan);
+      if (val.var == 0.0)
+	continue;
+
+      pce_params[ichan][iparam].push_back (val);
+    }
+  }
+}
+
+Estimate<double> weighted_median (std::vector< Estimate<double> > vals)
+{
+  if (vals.size () == 0)
+    return 0.0;
+  
+  double total_weight = 0.0;
+  for (auto element: vals)
+    total_weight += 1.0 / element.var;
+
+  std::sort (vals.begin(), vals.end());
+
+  double weight = 0.0;
+  unsigned ival = 0;
+  while (ival < vals.size() && 2*weight < total_weight)
+  {
+    weight += 1.0 / vals[ival].var;
+    ival ++;
+  }
+
+  Estimate<double> retval (vals[ival].val, 1.0/total_weight);
+  return retval;
+}
+				  
+void pcmavg::weighted_median_pce ()
+{
+  Reference::To<Pulsar::PolnCalibratorExtension> ext =
+    average->get<Pulsar::PolnCalibratorExtension>();
+
+  const unsigned nchan  = ext->get_nchan();
+  const unsigned nparam = ext->get_nparam();
+
+  if (nchan != pce_params.size())
+    throw Error (InvalidState, "pcmavg::weighted_median_pce",
+		 "archive nchan=%u != target nchan=%u",
+		 nchan, pce_params.size());
+
+  if (nchan == 0)
+    return;
+  
+  if (nparam != pce_params[0].size())
+    throw Error (InvalidState, "pcmavg::weighted_median_pce",
+		 "archive nparam=%u != target nparam=%u",
+		 nparam, pce_params[0].size());
+
+  for (unsigned ichan = 0; ichan < nchan; ++ichan)
+  {
+    double chisq = median (chisq_vals[ichan]);
+    ext->get_transformation(ichan)->set_chisq (chisq);
+
+    unsigned nfree = median (nfree_vals[ichan]);
+    ext->get_transformation(ichan)->set_nfree (nfree);
+
+    for (unsigned iparam = 0; iparam < nparam; ++iparam)
+    {
+      Estimate<float> val = weighted_median (pce_params[ichan][iparam]);
+      ext->set_Estimate (iparam, ichan, val);
+    }
+  }
+}
+
+
+
 void pcmavg::average_cse(Pulsar::Archive* archive)
 {
   Reference::To<Pulsar::CalibratorStokes> avg_cs =
@@ -266,12 +390,76 @@ void pcmavg::average_cse(Pulsar::Archive* archive)
   }
 }
 
-void pcmavg::setup(Pulsar::Archive* archive)
+void pcmavg::record_cse(Pulsar::Archive* archive)
 {
-  if (verbose) {
+  Reference::To<Pulsar::CalibratorStokes> ext =
+    archive->get<Pulsar::CalibratorStokes>();
+
+  const unsigned nchan  = ext->get_nchan();
+  const unsigned nparam = 3;
+
+  if (nchan != cse_params.size())
+    throw Error (InvalidState, "pcmavg::record_cse",
+		 "archive nchan=%u != target nchan=%u",
+		 nchan, cse_params.size());
+
+  if (nchan == 0)
+    return;
+  
+  if (nparam != cse_params[0].size())
+    throw Error (InvalidState, "pcmavg::record_cse",
+		 "archive nparam=%u != target nparam=%u",
+		 nparam, cse_params[0].size());
+
+  for (unsigned ichan = 0; ichan < nchan; ++ichan)
+  {
+    for (unsigned iparam = 0; iparam < nparam; ++iparam)
+    {
+      Estimate<double> val = ext->get_Estimate (iparam, ichan);
+      if (val.var == 0.0)
+	continue;
+
+      cse_params[ichan][iparam].push_back (val);
+    }
+  }
+}
+
+void pcmavg::weighted_median_cse ()
+{
+  Reference::To<Pulsar::CalibratorStokes> ext =
+    average->get<Pulsar::CalibratorStokes>();
+
+  const unsigned nchan  = ext->get_nchan();
+  const unsigned nparam = 3;
+
+  if (nchan != cse_params.size())
+    throw Error (InvalidState, "pcmavg::record_cse",
+		 "archive nchan=%u != target nchan=%u",
+		 nchan, cse_params.size());
+
+  if (nchan == 0)
+    return;
+  
+  if (nparam != cse_params[0].size())
+    throw Error (InvalidState, "pcmavg::record_cse",
+		 "archive nparam=%u != target nparam=%u",
+		 nparam, cse_params[0].size());
+
+  for (unsigned ichan = 0; ichan < nchan; ++ichan)
+  {
+    for (unsigned iparam = 0; iparam < nparam; ++iparam)
+    {
+      Estimate<float> val = weighted_median (cse_params[ichan][iparam]);
+      ext->set_Estimate (iparam, ichan, val);
+    }
+  }
+}
+
+void pcmavg::setup (Pulsar::Archive* archive)
+{
+  if (verbose)
     cerr << "Using " << archive->get_filename() <<
       " as a storage container." << endl;
-  }
 
   // Retain a copy of the first cal to act as a container of the weighted
   // average pcm file.
@@ -284,8 +472,25 @@ void pcmavg::setup(Pulsar::Archive* archive)
   set_average_bw(get_bw(average));
   set_average_nchan(get_nchan(average));
 
+  if (!compute_weighted_median)
+    return;
+  
   Reference::To<Pulsar::PolnCalibratorExtension> ext =
     archive->get<Pulsar::PolnCalibratorExtension>();
+
+  const unsigned nchan  = ext->get_nchan();
+  const unsigned nparam = ext->get_nparam();
+
+  pce_params.resize (nchan);
+  cse_params.resize (nchan);
+  chisq_vals.resize (nchan);
+  nfree_vals.resize (nchan);
+
+  for (unsigned ichan=0; ichan < nchan; ichan++)
+  {
+    pce_params[ichan].resize (nparam);
+    cse_params[ichan].resize (3);
+  }
 }
 
 //! Enter command into the HISTORY table. This required because
@@ -302,9 +507,8 @@ void pcmavg::set_history_command()
 
 void pcmavg::process(Pulsar::Archive* archive)
 {
-  if (verbose) {
+  if (verbose)
     cerr << "Processing: " << archive->get_filename() << endl;
-  }
 
   // Ensure that the loaded archive is a pcm file (by checking if the FEEDPAR
   // table exists).
@@ -317,16 +521,26 @@ void pcmavg::process(Pulsar::Archive* archive)
     return;
   }
 
-  if (average == NULL) {
+  if (!average)
+  {
     // If first file, extract parameters and store them for easy comparison.
-    setup(archive);
+    setup (archive);
     return;
   }
 
   // Compare parameters if not first file.
-  if (has_same_attributes(archive)) {
-    average_cse(archive);
-    average_pce(archive);
+  if (has_same_attributes(archive))
+  {
+    if (compute_weighted_median)
+    {
+      record_cse(archive);
+      record_pce(archive);
+    }
+    else
+    {
+      average_cse(archive);
+      average_pce(archive);
+    }
   } else {
     throw Error(InvalidState, "pcmavg::process",
         "file attributes (%s) differ from first pcm file",
@@ -334,15 +548,23 @@ void pcmavg::process(Pulsar::Archive* archive)
   }
 }
 
+
 void pcmavg::finalize()
 {
+  if (compute_weighted_median)
+  {
+    if (verbose)
+      cerr << "pcmavg::finalize computing weighted medians" << endl;
+    weighted_median_cse ();
+    weighted_median_pce ();
+  }
+  
   // If no user-specified unload name has been given, use filename of the first
   // pcm file and append as the extension.
-  if (unload_name.empty()) {
+  if (unload_name.empty())
     unload_name = unload->get_output_filename(average);
-  }
 
-  cerr << "pcmavg: Unloading archive: '" << unload_name << "'" << endl;
+  cerr << "pcmavg: unloading '" << unload_name << "'" << endl;
 
   set_history_command();
   average->unload(unload_name);
@@ -352,8 +574,11 @@ void pcmavg::add_options(CommandLine::Menu& menu)
 {
   CommandLine::Argument* arg;
 
-  arg = menu.add(unload_name, 'o', "fname");
-  arg->set_help("output result to 'fname'");
+  arg = menu.add (unload_name, 'o', "fname");
+  arg->set_help ("output result to 'fname'");
+
+  arg = menu.add (compute_weighted_median, "med");
+  arg->set_help ("compute the weighted median of each parameter");
 }
 
 static pcmavg program;
