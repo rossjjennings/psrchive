36a96a1d3997c2883532be9766d62daca97e9a52
Willem van Straten <vanstraten.willem@gmail.com>
2022-03-20 10:41:04 +1300

    framework in place to load/unload polarization calibration interpolator from/to PSRFITS files

---
+++ Base/Formats/PSRFITS/load_CalibrationInterpolatorExtension.C
@@ -0,0 +1,127 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2022 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "Pulsar/FITSArchive.h"
+#include "Pulsar/CalibrationInterpolatorExtension.h"
+
+#include "psrfitsio.h"
+#include "strutil.h"
+
+// #define _DEBUG 1
+#include "debug.h"
+
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+using namespace Pulsar;
+
+void load (fitsfile* fptr, int row,
+	   CalibrationInterpolatorExtension::Parameter* param)
+{
+  psrfits_read_col (fptr, "MODEL", &(param->code), row);
+  psrfits_read_col (fptr, "IPARAM", &(param->iparam), row);
+  psrfits_read_col (fptr, "LOGSMTH", &(param->log10_smoothing_factor), row);
+  psrfits_read_col (fptr, "CHISQ", &(param->total_chi_squared), row);
+  psrfits_read_col (fptr, "NDAT", &(param->ndat_input), row);
+  psrfits_read_col (fptr, "NFLAG_IN", &(param->ndat_flagged_before), row);
+  psrfits_read_col (fptr, "NFLAG_OUT", &(param->ndat_flagged_after), row);
+  psrfits_read_col (fptr, "INTERTXT", &(param->interpolator), row); 
+}
+
+void FITSArchive::load_CalibrationInterpolatorExtension (fitsfile* fptr) try
+{
+  if (verbose == 3)
+    cerr << "FITSArchive::load_CalibrationInterpolatorExtension entered"
+	 << endl;
+  
+  // Move to the PCMINTER HDU
+  bool optional = true;
+  if (!psrfits_move_hdu (fptr, "PCMINTER", optional))
+    return;
+
+  Reference::To<CalibrationInterpolatorExtension> ext;
+  ext = new CalibrationInterpolatorExtension;
+
+  // Get CAL_MTHD
+  string cal_mthd;
+  psrfits_read_key (fptr, "CAL_MTHD", &cal_mthd);
+
+  if (verbose > 2)
+    cerr << "FITSArchive::load_CalibrationInterpolatorExtension "
+            "CAL_MTHD='" << cal_mthd << "'" << endl;
+
+  ext->set_type( Calibrator::Type::factory (cal_mthd) );
+
+  if (verbose == 3)
+    cerr << "FITSArchive::load_CalibrationInterpolatorExtension type=" 
+	 << ext->get_type()->get_name() << endl;
+
+  // NPARAM
+  int nparam = 0;
+  psrfits_read_key (fptr, "NPARAM", &nparam);
+
+  // NCHAN_IN
+  int nchan_in = 0;
+  psrfits_read_key (fptr, "NCHAN_IN", &nchan_in);
+  ext->set_nchan_input (nchan_in);
+
+  // NSUB_IN
+  int nsub_in = 0;
+  psrfits_read_key (fptr, "NSUB_IN", &nsub_in);
+  ext->set_nsub_input (nsub_in);
+
+  // REFFREQ
+  double reffreq = 0;
+  psrfits_read_key (fptr, "REFFREQ", &reffreq);
+  ext->set_reference_frequency (reffreq);
+  
+  // MINFREQ
+  double minfreq = 0;
+  psrfits_read_key (fptr, "MINFREQ", &minfreq);
+  ext->set_minimum_frequency (minfreq);
+  
+  // MAXFREQ
+  double maxfreq = 0;
+  psrfits_read_key (fptr, "MAXFREQ", &maxfreq);
+  ext->set_maximum_frequency (maxfreq);
+  
+  // REFEPOCH
+  double refepoch = 0;
+  psrfits_read_key (fptr, "REFEPOCH", &refepoch);
+  ext->set_reference_epoch (refepoch);
+  
+  // MINEPOCH
+  double minepoch = 0;
+  psrfits_read_key (fptr, "MINEPOCH", &minepoch);
+  ext->set_minimum_epoch (minepoch);
+  
+  // MAXEPOCH
+  double maxepoch = 0;
+  psrfits_read_key (fptr, "MAXEPOCH", &maxepoch);
+  ext->set_maximum_epoch (maxepoch);
+  
+  for (int iparam = 0; iparam < nparam; iparam++)
+  {
+    cerr << "load interpolation row=" << iparam+1 << endl;
+    
+    auto param = new CalibrationInterpolatorExtension::Parameter;
+    ::load (fptr, iparam+1, param);
+    ext->add_parameter (param);
+  }
+  
+  add_extension (ext);
+  
+  if (verbose == 3)
+    cerr << "FITSArchive::load_CalibrationInterpolatorExtension exiting" << endl;
+
+}
+catch (Error& error)
+{
+  throw error += "FITSArchive::load CalibrationInterpolatorExtension";
+}
+
---
+++ Base/Formats/PSRFITS/unload_CalibrationInterpolatorExtension.C
@@ -0,0 +1,106 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2022 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "Pulsar/FITSArchive.h"
+#include "Pulsar/CalibrationInterpolatorExtension.h"
+
+#include "psrfitsio.h"
+#include "strutil.h"
+
+// #define _DEBUG 1
+#include "debug.h"
+
+#include <assert.h>
+
+using namespace std;
+using namespace Pulsar;
+
+void unload (fitsfile* fptr, int row,
+	     const CalibrationInterpolatorExtension::Parameter* param,
+	     unsigned nchar)
+{
+  psrfits_write_col (fptr, "MODEL", row, param->code);
+  psrfits_write_col (fptr, "IPARAM", row, param->iparam);
+  psrfits_write_col (fptr, "LOGSMTH", row, param->log10_smoothing_factor);
+  psrfits_write_col (fptr, "CHISQ", row, param->total_chi_squared);
+  psrfits_write_col (fptr, "NDAT", row, param->ndat_input);
+  psrfits_write_col (fptr, "NFLAG_IN", row, param->ndat_flagged_before);
+  psrfits_write_col (fptr, "NFLAG_OUT", row, param->ndat_flagged_after);
+
+  string txt = param->interpolator;
+  txt.resize (nchar, 0);
+  psrfits_write_col (fptr, "INTERTXT", row, txt);  
+}
+
+void FITSArchive::unload (fitsfile* fptr, 
+			  const CalibrationInterpolatorExtension* ext) try
+{
+  if (verbose == 3)
+    cerr << "FITSArchive::unload CalibrationInterpolatorExtension" << endl;
+  
+  // Initialize the PCMINTER Binary Table
+  
+  psrfits_init_hdu (fptr, "PCMINTER");  
+
+  // CAL_MTHD
+  string cal_mthd = ext->get_type()->get_name();
+  psrfits_update_key (fptr, "CAL_MTHD", cal_mthd);
+
+  // NPARAM
+  int nparam = ext->get_nparam ();
+  psrfits_update_key (fptr, "NPARAM", nparam);
+
+  // NSUB_IN
+  int nsub = ext->get_nsub_input();
+  psrfits_update_key (fptr, "NSUB_IN", nsub);
+
+  // REFFREQ
+  double ref_freq = ext->get_reference_frequency();
+  psrfits_update_key (fptr, "REFFREQ", ref_freq);
+
+  // MINFREQ
+  double min_freq = ext->get_minimum_frequency();
+  psrfits_update_key (fptr, "MINFREQ", min_freq);
+
+  // MAXFREQ
+  double max_freq = ext->get_maximum_frequency();
+  psrfits_update_key (fptr, "MAXFREQ", max_freq);
+
+  // REFEPOCH
+  double ref_epoch = ext->get_reference_epoch().in_days();
+  psrfits_update_key (fptr, "REFEPOCH", ref_epoch);
+
+  // MINEPOCH
+  double min_epoch = ext->get_minimum_epoch().in_days();
+  psrfits_update_key (fptr, "MINEPOCH", min_epoch);
+
+  // MAXEPOCH
+  double max_epoch = ext->get_maximum_epoch().in_days();
+  psrfits_update_key (fptr, "MAXEPOCH", max_epoch);
+
+  // NCHAR
+  unsigned max_strlen = 0;
+  for (int iparam=0; iparam < nparam; iparam++)
+  {
+    unsigned strlen = ext->get_parameter(iparam)->interpolator.size();
+    max_strlen = std::max (max_strlen, strlen);
+  }
+
+  assert (max_strlen > 0);
+
+  for (unsigned iparam=0; iparam < nparam; iparam++)
+    ::unload (fptr, iparam+1, ext->get_parameter(iparam), max_strlen);
+  
+  if (verbose == 3)
+    cerr << "FITSArchive::unload CalibrationInterpolatorExtension exiting" << endl; 
+
+}
+catch (Error& error)
+{
+  throw error += "FITSArchive::unload CalibrationInterpolatorExtension";
+}
+
