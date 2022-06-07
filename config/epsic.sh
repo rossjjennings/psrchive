6128f21f44ad14c541832364b3a485b09d3747cf
Willem van Straten <vanstraten.willem@gmail.com>
2022-06-07 19:31:30 +1200

    pwd -> owd in case pwd is special

--- config/epsic.sh
+++ config/epsic.sh
@@ -6,19 +6,19 @@ if test "x$1" != "x"; then
   dir=$1
 fi
 
-pwd=$PWD
+owd=$PWD
 
 if test ! -d $dir/Util/epsic/src; then
   echo Updating EPSIC submodule
   cd $dir
   git submodule update --init --recursive
-  cd $pwd
+  cd $owd
 fi
 
 if test ! -f $dir/Util/epsic/src/configure; then
   echo Running EPSIC bootstrap script
   cd $dir/Util/epsic/src
   ./bootstrap
-  cd $pwd
+  cd $owd
 fi
 
